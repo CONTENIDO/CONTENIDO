3ee14fb578aa44ad537c9769f28044bd19735781
Timo Trautmann <timo.trautmann@4fb.de>
2008-04-14 18:22:16 +0000

    dropped old contenido client and corresponding setup data

--- setup/data/standard/standard_modules_translations2.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO !PREFIX!_mod_translations VALUES('259', '46', '1', 'Open+handler+article+in+new+window', 'Handler-Artikel+in+neuem+Fenster+%26ouml%3Bffnen');
-INSERT INTO !PREFIX!_mod_translations VALUES('260', '47', '1', '-+Please+select+-', '-+Bitte+ausw%26auml%3Bhlen+-');
-INSERT INTO !PREFIX!_mod_translations VALUES('261', '47', '1', 'Options%3A', 'Optionen%3A');
-INSERT INTO !PREFIX!_mod_translations VALUES('262', '47', '1', 'User+selected', 'Nutzerauswahl');
-INSERT INTO !PREFIX!_mod_translations VALUES('263', '47', '1', 'Text+only', 'Nur+Text');
-INSERT INTO !PREFIX!_mod_translations VALUES('264', '47', '1', 'HTML+and+text', 'HTML+und+Text');
-INSERT INTO !PREFIX!_mod_translations VALUES('265', '47', '1', 'Default+message+type%3A', 'Standard-Nachrichtenformat%3A');
