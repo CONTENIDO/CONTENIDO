66f65266f50b3be27b89a38b66eb72059abcb6a0
Timo Trautmann <timo.trautmann@4fb.de>
2008-04-14 18:25:11 +0000

    check in new contenido demo client

---
+++ cms/error.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+  <title>Fehler</title>
+  <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+  <meta http-equiv="expires" content="0">
+  <meta http-equiv="pragma" content="no-cache">
+  <meta http-equiv="Refresh" content="0; URL=http://www.contenido.org">
+  <meta name="robots" content="follow,noindex">
+</head>
+<body bgcolor="#ffffff">
+&nbsp;
+</body>
+</html>
