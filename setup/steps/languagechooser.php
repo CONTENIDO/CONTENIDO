0734937965f1a0d5fb664af00ac2b2d58e27374a
Timo Trautmann <timo.trautmann@4fb.de>
2008-04-15 13:01:35 +0000

    Fixxed title bug on setup welcome screen

--- setup/steps/languagechooser.php
+++ setup/steps/languagechooser.php
@@ -6,7 +6,7 @@ class cSetupLanguageChooser extends cSetupMask
 	function cSetupLanguageChooser ()
 	{
 		cSetupMask::cSetupMask("templates/languagechooser.tpl");
-		$this->setHeader("Contenido Setup Contenido " . C_SETUP_VERSION);
+		$this->setHeader('Version '.C_SETUP_VERSION);
 		$this->_oStepTemplate->set("s", "DE_HINT", "Diese Anwendung hilft Ihnen bei der Installation von Contenido.");
 		$this->_oStepTemplate->set("s", "EN_HINT", "This application will guide you trough the setup process.");
 		$this->_oStepTemplate->set("s", "DE_HINT_LANG", "W&auml;hlen Sie bitte die gew&uuml;nschte Sprache f&uuml;r das Setup aus.");
