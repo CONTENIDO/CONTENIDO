89d4c462fd3b57a2c6be8ec69f9ab4bad662db53
Timo Trautmann <timo.trautmann@4fb.de>
2008-04-17 13:28:19 +0000

    experimental: added tinymce3 activating by setting tiny path to tinymce3 instead of tinymce2 in config.php of contenido

---
+++ contenido/external/wysiwyg/tinymce3/jscripts/tiny_mce/plugins/searchreplace/langs/tw_dlg.js
@@ -0,0 +1,16 @@
+tinyMCE.addI18n('tw.searchreplace_dlg',{
+searchnext_desc:"\u518D\u6B21\u5C0B\u627E",
+notfound:"\u5C0B\u627E\u5DF2\u5B8C\u6210 ! \u627E\u4E0D\u5230\u4EFB\u4F55\u76EE\u6A19\u3002",
+search_title:"\u5C0B\u627E",
+replace_title:"\u5C0B\u627E/\u53D6\u4EE3",
+allreplaced:"\u5DF2\u53D6\u4EE3\u6240\u6709\u5339\u914D\u7684\u5B57\u7B26\u4E32.",
+findwhat:"\u5C0B\u627E\u76EE\u6A19",
+replacewith:"\u53D6\u4EE3\u70BA",
+direction:"\u65B9\u5411",
+up:"\u5411\u4E0A",
+down:"\u5411\u4E0B",
+mcase:"\u5927\u5C0F\u5BEB\u9808\u76F8\u7B26",
+findnext:"\u627E\u4E0B\u4E00\u500B",
+replace:"\u53D6\u4EE3",
+replaceall:"\u53D6\u4EE3\u5168\u90E8"
+});
