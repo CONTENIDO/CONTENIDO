a356b456ca6174d21049b5266a3f4944ded48881
Timo Trautmann <timo.trautmann@4fb.de>
2008-04-17 15:58:49 +0000

    setting standard values for valid_from valid_to and last_pw_request

--- setup/data/client/client1.sql
+++ setup/data/client/client1.sql
@@ -3,4 +3,4 @@ INSERT INTO !PREFIX!_clients_lang VALUES (1, 1, 1);
 INSERT INTO !PREFIX!_clients_lang VALUES (2, 1, 2);
 INSERT INTO !PREFIX!_lang VALUES (1, 'deutsch', 1, '48a365b4ce1e322a55ae9017f3daf0c0', '2008-04-14 15:46:39', '2008-04-14 15:46:50', 'iso-8859-1', 'ltr');
 INSERT INTO !PREFIX!_lang VALUES (2, 'english', 1, '48a365b4ce1e322a55ae9017f3daf0c0', '2008-04-14 15:46:55', '2008-04-14 15:47:04', 'iso-8859-1', 'ltr');
-INSERT INTO !PREFIX!_phplib_auth_user_md5 VALUES ('21232f297a57a5a743894a0e4a801fc3', 'admin', '21232f297a57a5a743894a0e4a801fc3', 'admin[1]', 'Administrator Contenido Demo', 'admin_kunde@IhreSite.de', '', '', '', '', '', 1, NULL, NULL, '0000-00-00 00:00:00');
+INSERT INTO !PREFIX!_phplib_auth_user_md5 VALUES ('21232f297a57a5a743894a0e4a801fc3', 'admin', '21232f297a57a5a743894a0e4a801fc3', 'admin[1]', 'Administrator Contenido Demo', 'admin_kunde@IhreSite.de', '', '', '', '', '', 1, '0000-00-00', '0000-00-00', '0000-00-00 00:00:00');
--- setup/data/sysadmin/sysadmin1.sql
+++ setup/data/sysadmin/sysadmin1.sql
@@ -1,1 +1,1 @@
-INSERT INTO !PREFIX!_phplib_auth_user_md5 VALUES('48a365b4ce1e322a55ae9017f3daf0c0', 'sysadmin', '48a365b4ce1e322a55ae9017f3daf0c0', 'sysadmin', 'Systemadministrator', 'sysadmin@IhreSite.de', '', '', '', '', '', '1', '', '', '');
+INSERT INTO !PREFIX!_phplib_auth_user_md5 VALUES('48a365b4ce1e322a55ae9017f3daf0c0', 'sysadmin', '48a365b4ce1e322a55ae9017f3daf0c0', 'sysadmin', 'Systemadministrator', 'sysadmin@IhreSite.de', '', '', '', '', '', '1', '0000-00-00', '0000-00-00', '0000-00-00 00:00:00');
