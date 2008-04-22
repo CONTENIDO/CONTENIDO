9b868ed3adecfb3d552ddcc3c422621bec396ea0
Timo Trautmann <timo.trautmann@4fb.de>
2008-04-22 07:28:18 +0000

    Fix already checked in by HerrB (Content Allocation language bug) but there is a problem: After fix table con_pica_lang has no primary key. This is very bad. Fix checked in which creates a primary key for con_pica_lang which contains columns idpica_alloc and idlang this should be a unique key.

--- setup/data/base/base_misc1.sql
+++ setup/data/base/base_misc1.sql
@@ -1,2 +1,3 @@
 UPDATE !PREFIX!_art_lang SET published=lastmodified, publishedby=modifiedby WHERE online=1 and published="0000-00-00 00:00:00";
 ALTER TABLE !PREFIX!_pica_alloc_con DROP PRIMARY KEY, ADD PRIMARY KEY ( `idpica_alloc` , `idartlang` );
+ALTER TABLE !PREFIX!_pica_lang DROP PRIMARY KEY, ADD PRIMARY KEY ( `idpica_alloc` , `idlang` );
