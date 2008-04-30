9dcf66d7dd75cdfc879042f5ac1f48a45bb73278
Timo Trautmann <timo.trautmann@4fb.de>
2008-04-30 10:37:50 +0000

    fix in sql statement which causes some error. Dropping and adding index seperated into two statements

--- setup/data/base/base_misc1.sql
+++ setup/data/base/base_misc1.sql
@@ -1,3 +1,5 @@
 UPDATE !PREFIX!_art_lang SET published=lastmodified, publishedby=modifiedby WHERE online=1 and published="0000-00-00 00:00:00";
-ALTER TABLE !PREFIX!_pica_alloc_con DROP PRIMARY KEY, ADD PRIMARY KEY ( `idpica_alloc` , `idartlang` );
-ALTER TABLE !PREFIX!_pica_lang DROP PRIMARY KEY, ADD PRIMARY KEY ( `idpica_alloc` , `idlang` );
+ALTER TABLE !PREFIX!_pica_alloc_con DROP PRIMARY KEY;
+ALTER TABLE !PREFIX!_pica_alloc_con ADD PRIMARY KEY ( `idpica_alloc` , `idartlang` );
+ALTER TABLE !PREFIX!_pica_lang DROP PRIMARY KEY;
+ALTER TABLE !PREFIX!_pica_lang ADD PRIMARY KEY ( `idpica_alloc` , `idlang` );
