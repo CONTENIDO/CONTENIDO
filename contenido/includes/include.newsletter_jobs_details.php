2edb029c7d6185f34f786e6389eab7af7bdae64d
Björn Behrens <b.behrens@btech.de>
2008-05-21 23:03:51 +0000

    Job details area enhanced to be usable with more than a few recipients (tested with > 2000). Needs still some UI work...

--- contenido/includes/include.newsletter_jobs_details.php
+++ contenido/includes/include.newsletter_jobs_details.php
@@ -84,45 +84,123 @@ if ($action == "news_job_run" && $perm->have_perm_area_action($area, $action) &&
 } else if ($action == "news_job_details" || $action == "news_job_detail_delete") {
 	// Show job details (recipients)
 	
+	$oLogs = new cNewsletterLogCollection;
+	
 	// Remove recipient from a job
 	if ($action == "news_job_detail_delete" && is_numeric($_REQUEST["idnewslog"]) && $perm->have_perm_area_action($area, "news_job_detail_delete"))
 	{
-		$oLogs = new cNewsletterLogCollection;
 		$oLogs->delete($_REQUEST["idnewslog"]);
 	}
 	
 	// Initialize
+	$iNextPage	= (int)$_GET['nextpage'];
+	if ($iNextPage <= 0) {
+		$iNextPage = 1;
+	}
+	
 	if ($_REQUEST["sortmode"] !== "DESC") {
 		$_REQUEST["sortmode"] = "ASC";
 	}
 	
 	$sDateFormat = getEffectiveSetting("backend", "timeformat", "d.m.Y H:i");
 	
-	$oList = new cScrollList (true, "news_job_details");
-	$oList->setCustom("idnewsjob", $_REQUEST["idnewsjob"]);
+	// Set default values
+	$oUser = new cApiUser($auth->auth["uid"]);
+	if (!isset($_REQUEST["elemperpage"]) || !is_numeric($_REQUEST["elemperpage"]) || $_REQUEST["elemperpage"] < 0) {
+		$_REQUEST["elemperpage"] = $oUser->getProperty("itemsperpage", $area."_job_details");
+	}
+	if (!is_numeric($_REQUEST["elemperpage"])) {
+		$_REQUEST["elemperpage"] = 50;
+	}
+	if ($_REQUEST["elemperpage"] > 0) {
+		// - All - will not be saved
+		$oUser->setProperty("itemsperpage", $area."_job_details", $_REQUEST["elemperpage"]);
+	}
 	
+	$oFrmOptions = new UI_Table_Form("frmOptions");
+	$oFrmOptions->setVar("contenido", $sess->id);
+	$oFrmOptions->setVar("area",      $area);
+	$oFrmOptions->setVar("action",    $action);
+	$oFrmOptions->setVar("frame",     $frame);
+	$oFrmOptions->setVar("sortmode",  $_REQUEST["sortmode"]);
+	$oFrmOptions->setVar("sortby",    $_REQUEST["sortby"]);
+	$oFrmOptions->setVar("idnewsjob", $_REQUEST["idnewsjob"]);
+	//$oFrmOptions->setVar("startpage", $startpage);
+	//$oFrmOptions->setVar("appendparameters", $appendparameters);
+	$oFrmOptions->addHeader(i18n("List options"));
+
+	$oSelElements = new cHTMLSelectElement("elemperpage");
+	$oSelElements->setEvent("onchange", "document.forms.frmOptions.submit();");
+	
+	$aData = Array("0"   => i18n("-All-"),
+				   "50"  => "50",
+				   "100" => "100",
+				   "250" => "250",
+				   "500" => "500");
+					
+	foreach ($aData as $sKey => $sValue)
+	{
+		$oOption = new cHTMLOptionElement($sValue, $sKey);
+		$oSelElements->addOptionElement($sKey, $oOption);
+	}
+	
+	$oSelElements->setDefault($_REQUEST["elemperpage"]);
+	
+	//$oSelElements->setStyle('border:1px;border-style:solid;border-color:black;');
+	$oFrmOptions->add(i18n("Items per page:"), $oSelElements->render());
+	
+	// Ouput data
+	$oList = new cScrollList (true, "news_job_details");
+	$oList->setCustom("idnewsjob",   $_REQUEST["idnewsjob"]);
+	$oList->setCustom("nextpage",    $iNextPage);
+	$oList->setCustom("elemperpage", $_REQUEST["elemperpage"]);	
+	
+	$aCols = array("rcpname", "rcpemail", "", "status", "sent");
 	$oList->setHeader(i18n("Recipient"), i18n("E-Mail"), i18n("Type"), i18n("Status"), i18n("Sent"), i18n("Actions"));
 	$oList->setSortable(0, true);
 	$oList->setSortable(1, true);
-	$oList->setSortable(2, true);
+	$oList->setSortable(2, false);
 	$oList->setSortable(3, true);
 	$oList->setSortable(4, true);
-		
-	if (!is_object($oLogs)) {
-		$oLogs = new cNewsletterLogCollection;
-	} else {
-		$oLogs->resetQuery();
-	}
+	
+	// Get data
+	$oLogs->resetQuery();
 	$oLogs->setWhere("idnewsjob", $_REQUEST["idnewsjob"]);
-			
-	$aCols = array("rcpname", "rcpemail", "status", "sent");
-	if (!array_key_exists($_REQUEST["sortby"], $aCols)) { 
-		$oLogs->setOrder("rcpname " . $_REQUEST["sortmode"]);
-	} else {
-		$oLogs->setOrder($aCols[$_REQUEST["sortby"]] . " " . $_REQUEST["sortmode"]);
+	
+	$sBrowseLinks = "1";
+	if ($_REQUEST["elemperpage"] > 0)
+	{
+		// First, get total data count
+		$oLogs->query();
+		$iRecipients = $oLogs->count(); // Getting item count without limit (for page function) - better idea anybody (performance)?
+		
+		if ($iRecipients > 0 && $iRecipients > $_REQUEST["elemperpage"])
+		{
+			$sBrowseLinks = "";
+			for ($i = 1; $i <= ceil($iRecipients / $_REQUEST["elemperpage"]); $i++)
+			{  
+				//$iNext = (($i - 1) * $_REQUEST["elemperpage"]) + 1;
+				if ($sBrowseLinks !== "") { 
+					$sBrowseLinks .= "&nbsp;";
+				}
+				if ($iNextPage == $i)
+				{ 
+					$sBrowseLinks .= $i."\n"; // I'm on the current page, no link 
+				} else {	
+					$sBrowseLinks .= '<a href="'.$sess->url("main.php?area=$area&action=$action&frame=$frame&idnewsjob=" . 
+															$_REQUEST["idnewsjob"] . "&nextpage=$i&sortmode=" . 
+															$_REQUEST["sortmode"] . "&sortby=" . $_REQUEST["sortby"]) . '">'.$i.'</a>'."\n"; 
+				}
+			}
+		}
+		
+		$oLogs->setLimit($_REQUEST["elemperpage"] * ($iNextPage - 1), $_REQUEST["elemperpage"]);
 	}
 	
-	// TODO: Limitierung der Datensätze und LIMIT einfügen
+	if (!array_key_exists($_REQUEST["sortby"], $aCols)) {
+		$_REQUEST["sortby"] = 0; // Sort by rcpname by default
+	}
+	$oLogs->setOrder($aCols[$_REQUEST["sortby"]] . " " . $_REQUEST["sortmode"]);
 	$oLogs->query();
 	
 	$oImgDelete = new cHTMLImage("images/delete.gif");
@@ -181,8 +259,20 @@ if ($action == "news_job_run" && $perm->have_perm_area_action($area, $action) &&
 	
 	// A little bit senseless, as the data is already sorted, but
 	// we need the sortmode in the header link  
-	$oList->sort($_REQUEST["sortby"], $_REQUEST["sortmode"]); 
-	$oPage->setContent($oList->render());
+	$oList->sort($_REQUEST["sortby"], $_REQUEST["sortmode"]);
+		
+	// HerrB: Hardcore UI for browsing elements ... sorry
+	$sBrowseHTML = '<table width="100%" cellspacing="0" cellpadding="2" border="0">
+    <tr>
+        <td><img src="images/spacer.gif" width="1" height="10"></td>
+    </tr>
+	<tr class="text_medium" style="background-color: #ffffff;">
+        <td style="border: 1px; border-color: #747488; border-style: solid; background-color: #F4F4F7"> ' .
+        sprintf(i18n("Go to page: %s"), $sBrowseLinks) . '</td>
+    </tr>
+</table>';
+
+	$oPage->setContent($oFrmOptions->render() . "<br />" . $oList->render() . $sBrowseHTML);
 } else {
 	// Just show the job data
 	$oJob  = new cNewsletterJob($_REQUEST["idnewsjob"]);
@@ -277,7 +367,10 @@ if ($action == "news_job_run" && $perm->have_perm_area_action($area, $action) &&
 	$oForm->add(i18n("Options"), $sOptionsInfo);
 		
 	$oForm->add(i18n("Author"),		$oJob->get("authorname")); 
-	$oForm->add(i18n("Created"),	$oJob->get("created"));	
+	$oForm->add(i18n("Created"),	$oJob->get("created"));
+	
+	// Just remove the "save changes" message (as it is not possible to remove the image completely in ui_table_form)
+	$oForm->setActionButton("submit", $cfg['path']['contenido_fullhtml']."images/but_ok.gif", "", "s");
 	
 	$oPage->setContent($oForm->render(true));
 }
