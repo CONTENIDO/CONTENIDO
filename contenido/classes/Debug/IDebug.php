b2d8363a0c753a4898569c6311622fcb87624723
Rudi Bieller <discard.rudi.bieller@4fb.de>
2008-05-23 15:22:22 +0000

    added new methods add($mVariable, $sVariableDescription = ''), reset(), showAll()

--- contenido/classes/Debug/IDebug.php
+++ contenido/classes/Debug/IDebug.php
@@ -4,19 +4,23 @@
 *
 * Description: Debug interface. Can be extended to a visible, invisible, logged, ... Debugger
 *
-* @version 1.0.0
+* @version 1.1.0
 * @author Rudi Bieller
 * @copyright four for business AG <www.4fb.de>
 *
 * {@internal
 * created 2007-01-01
+* modified 2008-05-21 Added methods add(), reset(), showAll()
 * }}
 *
-* $Id$
+* $Id$:
 */
 
 interface IDebug {
 	static public function getInstance();
 	public function show($mVariable, $sVariableDescription = '', $bExit = false);
+	public function add($mVariable, $sVariableDescription = '');
+	public function reset();
+	public function showAll();
 }
 ?>
