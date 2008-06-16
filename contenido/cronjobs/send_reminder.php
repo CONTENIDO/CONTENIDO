b8e67419eefca49dd9ee42f6cbb810c827fd511b
Holger Librenz <discard.holger.librenz@4fb.de>
2008-06-16 18:37:54 +0000

    "* hotfix (CON-152)"

--- contenido/cronjobs/send_reminder.php
+++ contenido/cronjobs/send_reminder.php
@@ -1,19 +1,25 @@
 <?php
-
-/*****************************************
-* File      :   $RCSfile: send_reminder.php,v $
-* Project   :   Contenido
-* Descr     :   Cron Job to send reminder items
-*
-* Author    :   Timo A. Hummel
-*               
-* Created   :   12.02.2004
-* Modified  :   $Date: 2007/10/12 13:53:00 $
-*
-* © four for business AG, www.4fb.de
-*
-* $Id: send_reminder.php,v 1.10 2006/04/28 09:20:55 timo.hummel Exp $
-******************************************/
+/**
+ * File      :   $RCSfile: send_reminder.php,v $
+ * Project   :   Contenido
+ * Descr     :   Cron Job to send reminder items
+ *
+ * Author    :   Timo A. Hummel
+ *
+ * Created   :   12.02.2004
+ * Modified  :   $Date: 2007/10/12 13:53:00 $
+ * @version $Revision$
+ * @copyright four for business AG, www.4fb.de
+ *
+ * @internal  {
+ *  modified 2008-06-16, H. Librenz - Hotfix: Added check for malicious script call
+ *
+ *  $Id$
+ * }
+ **/
+if (isset($_REQUEST['cfg']) || isset($_REQUEST['contenido_path'])) {
+    die ('Illegal call!');
+}
 
 if (isset($cfg['path']['contenido'])) {
 	include_once ($cfg['path']['contenido'].$cfg["path"]["includes"] . 'startup.php');
@@ -35,38 +41,38 @@ $oldclient = $client;
 if(!isRunningFromWeb() || function_exists("runJob") || $area == "cronjobs")
 {
 	$db = new DB_Contenido;
-		
+
 	$sql = "SELECT idclient FROM ".$cfg["tab"]["clients"];
 	$db->query($sql);
-	
+
 	$clients = array();
-	
+
 	while ($db->next_record())
 	{
 		$clients[] = $db->f("idclient");
 	}
-	
+
 	foreach ($clients as $client)
 	{
 		$mydate = time();
-	
+
     	$props = new PropertyCollection;
     	$props->select("itemtype = 'idcommunication' AND type = 'todo' AND name = 'reminderdate' AND value < $mydate AND value != 0 AND idclient=$client");
 		$pastreminders = array();
-		
+
     	while ($prop = $props->next())
     	{
     		$pastreminders[] = $prop->get("itemid");
     	}
-    	
+
     	$todoitem = new TODOItem;
-		
- 	
+
+
     	foreach ($pastreminders as $reminder)
     	{
-    		
+
     		$todoitem->loadByPrimaryKey($reminder);
-    		
+
     		if ($todoitem->get("idclient") == $client)
     		{
         		/* Check if email noti is active */
@@ -74,32 +80,32 @@ if(!isRunningFromWeb() || function_exists("runJob") || $area == "cronjobs")
         		{
         			$user = new User;
         			$user->loadUserByUserID($todoitem->get("recipient"));
-        		
+
         			$recipient = $user->getField("email");
         			$realname = $user->getField("realname");
         			$subject = $todoitem->get("subject")."\n";
-        			
+
         			$client = $todoitem->get("idclient");
         			$clientname = getClientName($client);
-        			
+
         			$todoitem->setProperty("todo", "emailnoti-sent", "1");
         			$todoitem->setProperty("todo", "emailnoti", "0");
-       			
+
         			$message = i18n("Hello %s,\n\nyou've got a new reminder for the client '%s' at\n%s:\n\n%s");
-        			
+
         			$path = $cfg["path"]["contenido_fullhtml"];
-        					
+
         			$message = sprintf($message,$realname, $clientname, $path, $todoitem->get("message"));
         			mail($recipient, $subject, $message);
-        			
+
         		}
-        		
-        		$todoitem->setProperty("todo", "reminderdate", "0");        		
-    		}	
+
+        		$todoitem->setProperty("todo", "reminderdate", "0");
+    		}
     	}
-		
+
 	}
-	
+
 }
 
 $client = $oldclient;
