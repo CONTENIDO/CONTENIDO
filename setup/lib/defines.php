3f8abe39f158ef9320cbb86841e99d1e36179e85
Holger Librenz <discard.holger.librenz@4fb.de>
2008-06-16 18:51:53 +0000

    "* version string updated"

--- setup/lib/defines.php
+++ setup/lib/defines.php
@@ -5,7 +5,7 @@ if (array_key_exists("setuptype", $_SESSION))
 	switch ($_SESSION["setuptype"])
 	{
 		case "setup":
-			define("C_SETUP_STEPS", 8);	
+			define("C_SETUP_STEPS", 8);
 			break;
 		case "upgrade":
 			define("C_SETUP_STEPS", 7);
@@ -20,5 +20,5 @@ define("C_SETUP_STEPFILE", "images/steps/s%d.png");
 define("C_SETUP_STEPFILE_ACTIVE", "images/steps/s%da.png");
 define("C_SETUP_STEPWIDTH", 28);
 define("C_SETUP_STEPHEIGHT", 28);
-define("C_SETUP_VERSION", "4.8.4");
+define("C_SETUP_VERSION", "4.8.5");
 ?>
