45fb1b8520fdf7927cf1386557b3d829fed75eca
Thorsten Granz <discard.thorsten.granz@4fb.de>
2008-06-25 13:57:11 +0000

    added security fix

--- contenido/classes/contenido/class.module.php
+++ contenido/classes/contenido/class.module.php
@@ -1,18 +1,35 @@
 <?php
-/*****************************************
-* File      :   $RCSfile: class.module.php,v $
-* Project   :   Contenido
-* Descr     :   Module access classes
-*
-* Author    :   Timo A. Hummel
-*               
-* Created   :   26.02.2003
-* Modified  :   $Date: 2007/07/19 20:31:05 $
-*
-* © four for business AG, www.4fb.de
-*
-* $Id: class.module.php,v 1.15 2007/07/19 20:31:05 bjoern.behrens Exp $
-******************************************/
+/**
+ * Project: 
+ * Contenido Content Management System
+ * 
+ * Description: 
+ * Area management class
+ * 
+ * Requirements: 
+ * @con_php_req 5.0
+ * 
+ *
+ * @package    Contenido Backend classes
+ * @version    1.15
+ * @author     Timo Hummel
+ * @copyright  four for business AG <www.4fb.de>
+ * @license    http://www.contenido.org/license/LIZENZ.txt
+ * @link       http://www.4fb.de
+ * @link       http://www.contenido.org
+ * 
+ * {@internal 
+ *   created 2003-02-26
+ *
+ *   $Id$:
+ * }}
+ * 
+ */
+
+if(!defined('CON_FRAMEWORK')) {
+	die('Illegal call');
+}
+
 
 cInclude("classes", "class.genericdb.php");
 
