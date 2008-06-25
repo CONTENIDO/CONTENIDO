3f848520454d8a55d203802888da49c80dc01455
Thorsten Granz <discard.thorsten.granz@4fb.de>
2008-06-25 13:09:23 +0000

    added security fix

--- contenido/classes/contenido/class.category.php
+++ contenido/classes/contenido/class.category.php
@@ -1,14 +1,35 @@
 <?php
-/*****************************************
-* File      :   $RCSfile: class.category.php,v $
-* Project   :   Contenido
-* Descr     :   Category access class
-* Modified  :   $Date: 2005/08/30 09:24:19 $
-*
-* © four for business AG, www.4fb.de
-*
-* $Id: class.category.php,v 1.4 2005/08/30 09:24:19 timo.hummel Exp $
-******************************************/
+/**
+ * Project: 
+ * Contenido Content Management System
+ * 
+ * Description: 
+ * Area management class
+ * 
+ * Requirements: 
+ * @con_php_req 5.0
+ * 
+ *
+ * @package    Contenido Backend classes
+ * @version    1.4
+ * @author     Timo Hummel
+ * @copyright  four for business AG <www.4fb.de>
+ * @license    http://www.contenido.org/license/LIZENZ.txt
+ * @link       http://www.4fb.de
+ * @link       http://www.contenido.org
+ * 
+ * {@internal 
+ *   created 2005-08-30
+ *
+ *   $Id$:
+ * }}
+ * 
+ */
+
+if(!defined('CON_FRAMEWORK')) {
+	die('Illegal call');
+}
+
 cInclude("classes", "class.genericdb.php");
 
 class cApiCategoryCollection extends ItemCollection
