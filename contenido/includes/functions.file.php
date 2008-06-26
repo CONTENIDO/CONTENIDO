6e0cf54f8b32e84b17a0da5f48107d815236ea6f
Frederic Schneider <frederic.schneider@4fb.de>
2008-06-26 10:04:57 +0000

    added security fix

--- contenido/includes/functions.file.php
+++ contenido/includes/functions.file.php
@@ -1,15 +1,37 @@
 <?php
-/******************************************
-* File      :   functions.file.php
-* Project   :   Contenido
-* Descr     :	Functions to edit files. Included in Area style, js, htmltpl in Frame right_bottom.
-*
-* Author    :   Willi Man
-* Created   :   13.07.2004
-* Modified  :   14.07.2004
-*
-* © four for business AG
-******************************************/
+/**
+ * Project: 
+ * Contenido Content Management System
+ * 
+ * Description: 
+ * Functions to edit files. Included in Area style,
+ * js, htmltpl in Frame right_bottom.
+ * 
+ * Requirements: 
+ * @con_php_req 5.0
+ * 
+ *
+ * @package    Contenido Backend includes
+ * @version    1.0.1
+ * @author     Willi Man
+ * @copyright  four for business AG <www.4fb.de>
+ * @license    http://www.contenido.org/license/LIZENZ.txt
+ * @link       http://www.4fb.de
+ * @link       http://www.contenido.org
+ * @since      file available since contenido release <= 4.6
+ * 
+ * {@internal 
+ *   created 2004-07-13
+ *   modified 2008-06-26, Frederic Schneider, add security fix
+ *
+ *   $Id$:
+ * }}
+ * 
+ */
+
+if(!defined('CON_FRAMEWORK')) {
+	die('Illegal call');
+}
 
 function fileEdit($filename, $sCode, $path) 
 {
