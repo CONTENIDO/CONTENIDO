e21eae554b369a43f44c277146b86e1caa16494f
Frederic Schneider <frederic.schneider@4fb.de>
2008-06-26 09:56:15 +0000

    added security fix

--- contenido/includes/functions.domxml-php4-to-php5.php
+++ contenido/includes/functions.domxml-php4-to-php5.php
@@ -1,18 +1,37 @@
 <?php
-/*****************************************
-* File      :   $RCSfile$
-* Project   :   Contenido
-* Descr     :   Compatibility fix, as the domxml extension is not available in
-* 				PHP 5.x anymore
-*
-* Author    :   Alexandre Alapetite
-*               
-* Created   :   05.06.2007
-* Version	:   1.17 2007-06-05
-* Modified  :   $Date$
-*
-* $Id$
-******************************************/
+/**
+ * Project: 
+ * Contenido Content Management System
+ * 
+ * Description: 
+ * Compatibility fix, as the domxml extension is not available in
+ * PHP 5.x anymore
+ * 
+ * Requirements: 
+ * @con_php_req 5.0
+ * 
+ *
+ * @package    Contenido Backend includes
+ * @version    1.1.7
+ * @author     Alexandre Alapetite
+ * @copyright  four for business AG <www.4fb.de>
+ * @license    http://www.contenido.org/license/LIZENZ.txt
+ * @link       http://www.4fb.de
+ * @link       http://www.contenido.org
+ * @since      file available since contenido release <= 4.6
+ * 
+ * {@internal 
+ *   created 2007-06-05
+ *   modified 2008-06-26, Frederic Schneider, add security fix
+ *
+ *   $Id$:
+ * }}
+ * 
+ */
+
+if(!defined('CON_FRAMEWORK')) {
+	die('Illegal call');
+}
 
 /**
  * Requires PHP5, uses built-in DOM extension,
