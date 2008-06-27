c488d88caffff468bc4123f28ed7b3f7eac05c36
Timo Trautmann <timo.trautmann@4fb.de>
2008-06-27 08:32:52 +0000

    moved settings to system settings

--- contenido/includes/config.misc.php
+++ contenido/includes/config.misc.php
@@ -75,12 +75,6 @@ $cfg["urlpathresolve"] = false;
 /* E-Mail-Address where bug reports will be sent to */
 $cfg['bugreport']['targetemail'] = 'bugreport@contenido.de';
 
-/* Show version and update info on mycontenido page */
-$cfg["backend"]["updateinfo"] = true;
-
-/* Show newsfeed on mycontenido page */
-$cfg["backend"]["newsfeed"] = true;
-
 /* The available charsets */
 $cfg['AvailableCharsets'] = array(
     'iso-8859-1',
