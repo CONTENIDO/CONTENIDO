62a076d54d6f10a82b38504d5f39a5dd66b47c85
Frederic Schneider <frederic.schneider@4fb.de>
2008-06-27 14:26:40 +0000

    added security fix

--- contenido/includes/include.subnav.php
+++ contenido/includes/include.subnav.php
@@ -1,17 +1,37 @@
 <?php
+/**
+ * Project: 
+ * Contenido Content Management System
+ * 
+ * Description: 
+ * Builds the third navigation layer
+ * 
+ * Requirements: 
+ * @con_php_req 5.0
+ * 
+ *
+ * @package    Contenido Backend includes
+ * @version    1.0.1
+ * @author     Jan Lengowski
+ * @copyright  four for business AG <www.4fb.de>
+ * @license    http://www.contenido.org/license/LIZENZ.txt
+ * @link       http://www.4fb.de
+ * @link       http://www.contenido.org
+ * @since      file available since contenido release <= 4.6
+ * 
+ * {@internal 
+ *   created 2003-01-25
+ *   modified 2008-06-27, Frederic Schneider, add security fix
+ *
+ *   $Id$:
+ * }}
+ * 
+ */
 
-/******************************************
-* File      :   include.subnav.php
-* Project   :   Contenido
-* Descr     :   Builds the third navigation
-*               layer
-*
-* Author    :   Jan Lengowski
-* Created   :   25.01.2003
-* Modified  :   25.01.2003
-*
-* © four for business AG
-******************************************/
+if(!defined('CON_FRAMEWORK')) {
+	die('Illegal call');
+}
+
 if ( $_REQUEST['cfg'] ) { exit; }
 
 if ( isset($_GET['idcat']) ) {
@@ -57,9 +77,6 @@ if ( isset($_GET['idcat']) ) {
     $tpl->generate($cfg["path"]["templates"] . $cfg["templates"]["subnav"]);
 
 } else {
-
     include ($cfg["path"]["contenido"].$cfg["path"]["templates"] . $cfg["templates"]["right_top_blank"]);
-
 }
-
 ?>
--- contenido/includes/include.symbolhelp.php
+++ contenido/includes/include.symbolhelp.php
@@ -1,21 +1,37 @@
 <?php
+/**
+ * Project: 
+ * Contenido Content Management System
+ * 
+ * Description: 
+ * Display the symbol help
+ * 
+ * Requirements: 
+ * @con_php_req 5.0
+ * 
+ *
+ * @package    Contenido Backend includes
+ * @version    1.0.1
+ * @author     Timo A. Hummel
+ * @copyright  four for business AG <www.4fb.de>
+ * @license    http://www.contenido.org/license/LIZENZ.txt
+ * @link       http://www.4fb.de
+ * @link       http://www.contenido.org
+ * @since      file available since contenido release <= 4.6
+ * 
+ * {@internal 
+ *   created 2003-05-14
+ *   modified 2008-06-27, Frederic Schneider, add security fix
+ *
+ *   $Id$:
+ * }}
+ * 
+ */
 
-/******************************************
-* File      :   includes.symbolhelp.php
-* Project   :   Contenido
-* Descr     :   Displays the symbol help
-*
-* Author    :   Timo A. Hummel
-* Created   :   14.05.2003
-* Modified  :   14.05.2003
-*
-* © four for business AG
-*****************************************/
+if(!defined('CON_FRAMEWORK')) {
+	die('Illegal call');
+}
 
-
-        # Generate template
-        $tpl->generate($cfg['path']['templates'] . $cfg['templates']['symbolhelp']);
-    
-
-
+# Generate template
+$tpl->generate($cfg['path']['templates'] . $cfg['templates']['symbolhelp']);
 ?>
