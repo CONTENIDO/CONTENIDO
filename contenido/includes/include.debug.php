46e81999070f460323a991d994200800942fc17d
Frederic Schneider <frederic.schneider@4fb.de>
2008-06-27 10:27:38 +0000

    added security fix

--- contenido/includes/include.debug.php
+++ contenido/includes/include.debug.php
@@ -1,18 +1,36 @@
 <?php
-/*****************************************
-* File      :   $RCSfile: include.debug.php,v $
-* Project   :   Contenido
-* Descr     :   Contenido Debug Interface
-*
-* Author    :   
-*               
-* Created   :   04.06.2003
-* Modified  :   $Date: 2006/04/28 09:20:54 $
-*
-* © four for business AG, www.4fb.de
-*
-* $Id: include.debug.php,v 1.3 2006/04/28 09:20:54 timo.hummel Exp $
-******************************************/
+/**
+ * Project: 
+ * Contenido Content Management System
+ * 
+ * Description: 
+ * Contenido Debug Interface
+ * 
+ * Requirements: 
+ * @con_php_req 5.0
+ * 
+ *
+ * @package    Contenido Backend includes
+ * @version    1.3.1
+ * @author     unknown
+ * @copyright  four for business AG <www.4fb.de>
+ * @license    http://www.contenido.org/license/LIZENZ.txt
+ * @link       http://www.4fb.de
+ * @link       http://www.contenido.org
+ * @since      file available since contenido release <= 4.6
+ * 
+ * {@internal 
+ *   created 2003-06-04
+ *   modified 2008-06-27, Frederic Schneider, add security fix
+ *
+ *   $Id$:
+ * }}
+ * 
+ */
+
+if(!defined('CON_FRAMEWORK')) {
+	die('Illegal call');
+}
 
 $areaList = new Area();
 $areas = $areaList = $areaList->getAvailableAreas();
