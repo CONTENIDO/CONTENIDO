89cc9fe78e48780978fe29a3ece681604a624f0e
Frederic Schneider <frederic.schneider@4fb.de>
2008-06-27 10:29:14 +0000

    added security fix

--- contenido/includes/include.frontend.group.subnav.php
+++ contenido/includes/include.frontend.group.subnav.php
@@ -1,14 +1,37 @@
 <?php
-/*****************************************
-* File      :   $RCSfile: include.frontend.group.subnav.php,v $
-* Project   :   Contenido
-* Descr     :   Custom subnavigation for the frontend groups
-* Modified  :   $Date: 2006/01/20 17:35:56 $
-*
-* © four for business AG, www.4fb.de
-*
-* $Id: include.frontend.group.subnav.php,v 1.5 2006/01/20 17:35:56 timo.hummel Exp $
-******************************************/
+/**
+ * Project: 
+ * Contenido Content Management System
+ * 
+ * Description: 
+ * Costum subnavigation for the frontend groups
+ * 
+ * Requirements: 
+ * @con_php_req 5.0
+ * 
+ *
+ * @package    Contenido Backend includes
+ * @version    1.5.0
+ * @author     Olaf Niemann
+ * @copyright  four for business AG <www.4fb.de>
+ * @license    http://www.contenido.org/license/LIZENZ.txt
+ * @link       http://www.4fb.de
+ * @link       http://www.contenido.org
+ * @since      file available since contenido release <= 4.6
+ * 
+ * {@internal 
+ *   created unknown
+ *   modified 2008-06-27, Frederic Schneider, add security fix
+ *
+ *   $Id$:
+ * }}
+ * 
+ */
+
+if(!defined('CON_FRAMEWORK')) {
+	die('Illegal call');
+}
+
 if ( $_REQUEST['cfg'] ) { exit; }
 
 if ( isset($_GET['idfrontendgroup']) )
