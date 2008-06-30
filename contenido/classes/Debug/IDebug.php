34964d864fa59749108dfdb351623ed7e2f2ad4b
Thorsten Granz <discard.thorsten.granz@4fb.de>
2008-06-30 08:42:01 +0000

    security fixes

--- contenido/classes/Debug/IDebug.php
+++ contenido/classes/Debug/IDebug.php
@@ -1,20 +1,36 @@
 <?php
 /**
-* $RCSfile$
-*
-* Description: Debug interface. Can be extended to a visible, invisible, logged, ... Debugger
-*
-* @version 1.1.0
-* @author Rudi Bieller
-* @copyright four for business AG <www.4fb.de>
-*
-* {@internal
-* created 2007-01-01
-* modified 2008-05-21 Added methods add(), reset(), showAll()
-* }}
-*
-* $Id$:
-*/
+ * Project: 
+ * Contenido Content Management System
+ * 
+ * Description: 
+ * Debug interface. Can be extended to a visible, invisible, logged, ... Debugger
+ *  
+ * Requirements: 
+ * @con_php_req 5.0
+ * 
+ *
+ * @package    Contenido Backend classes
+ * @version    1.1.0
+ * @author     Rudi Bieller
+ * @copyright  four for business AG <www.4fb.de>
+ * @license    http://www.contenido.org/license/LIZENZ.txt
+ * @link       http://www.4fb.de
+ * @link       http://www.contenido.org
+ * 
+ * {@internal 
+ * created 2007-01-01
+ * modified 2008-05-21 Added methods add(), reset(), showAll()
+ *   
+ * $Id$:
+ * }}
+ * 
+ */
+
+if(!defined('CON_FRAMEWORK')) {
+	die('Illegal call');
+}
+
 
 interface IDebug {
 	static public function getInstance();
