9db49c8f7c2115e884c2327f627f3cbb59f8f210
Thorsten Granz <discard.thorsten.granz@4fb.de>
2008-06-30 10:44:51 +0000

    security fixes

--- contenido/classes/UrlBuilder/Contenido_UrlBuilderConfig.class.php
+++ contenido/classes/UrlBuilder/Contenido_UrlBuilderConfig.class.php
@@ -1,20 +1,36 @@
 <?php
 /**
-* $RCSfile$
-*
-* Description: Configure UrlBuilder URL style. Per default, configures for style index-a-1.html.
-* If you need another style, extend this class to your needs and pass it to desired UrlBuilder.
-*
-* @version 1.0.0
-* @author Rudi Bieller
-* @copyright four for business AG <www.4fb.de>
-*
-* {@internal
-* created 2008-02-28
-* }}
-*
-* $Id$
-*/
+ * Project: 
+ * Contenido Content Management System
+ * 
+ * Description: 
+ * Configure UrlBuilder URL style. Per default, configures for style index-a-1.html.
+ * If you need another style, extend this class to your needs and pass it to desired UrlBuilder.
+ * 
+ * Requirements: 
+ * @con_php_req 5.0
+ * 
+ *
+ * @package    Contenido Backend classes
+ * @version    1.0.0
+ * @author     Rudi Bieller
+ * @copyright  four for business AG <www.4fb.de>
+ * @license    http://www.contenido.org/license/LIZENZ.txt
+ * @link       http://www.4fb.de
+ * @link       http://www.contenido.org
+ * 
+ * {@internal 
+ *   created 2008-02-28
+ *   
+ *   $Id: 
+ * }}
+ * 
+ */
+
+if(!defined('CON_FRAMEWORK')) {
+	die('Illegal call');
+}
+
 
 class Contenido_UrlBuilderConfig {
     public static function getConfig() {
