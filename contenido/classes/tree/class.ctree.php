615d90a1f9f4f4c0bdab75bc25cd1fe1f96b5cc5
Thorsten Granz <discard.thorsten.granz@4fb.de>
2008-06-30 10:26:23 +0000

    security fixes

--- contenido/classes/tree/class.ctree.php
+++ contenido/classes/tree/class.ctree.php
@@ -1,14 +1,34 @@
 <?php
-/*****************************************
-* File      :   $RCSfile: class.ctree.php,v $
-* Project   :   Contenido
-* Descr     :   logical cTree
-* Modified  :   $Date: 2004/08/04 07:56:19 $
-*
-* © four for business AG, www.4fb.de
-*
-* $Id: class.ctree.php,v 1.2 2004/08/04 07:56:19 timo.hummel Exp $
-******************************************/
+/**
+ * Project: 
+ * Contenido Content Management System
+ * 
+ * Description: 
+ * logical cTree
+ * 
+ * Requirements: 
+ * @con_php_req 5.0
+ * 
+ *
+ * @package    Contenido Backend classes
+ * @version    1.12
+ * @author     Timo Hummel
+ * @copyright  four for business AG <www.4fb.de>
+ * @license    http://www.contenido.org/license/LIZENZ.txt
+ * @link       http://www.4fb.de
+ * @link       http://www.contenido.org
+ * 
+ * {@internal 
+ *   created 2004-08-04
+ *   
+ *   $Id: class.ctree.php,v 1.2 2004/08/04 07:56:19 timo.hummel Exp $
+ * }}
+ * 
+ */
+
+if(!defined('CON_FRAMEWORK')) {
+	die('Illegal call');
+}
 
 cInclude("classes", "tree/class.ctreeitem.php");
 
@@ -25,7 +45,6 @@ class cTree extends cTreeItem
 	{
 		/* The root item currently has to be a "0".
 		 * This is a bug, feel free to fix it. */
-		  
 		cTreeItem::cTreeItem(0,$name);	
 	}
 	
