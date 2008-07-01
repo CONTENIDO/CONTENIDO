0995e715a27224cbbcd0afe7e28f47a5b5c4d8b5
Thorsten Granz <discard.thorsten.granz@4fb.de>
2008-07-01 08:06:10 +0000

    security fix

--- contenido/classes/widgets/class.views.advancedmenu.php
+++ contenido/classes/widgets/class.views.advancedmenu.php
@@ -1,14 +1,35 @@
 <?php
-/*****************************************
-* File      :   $RCSfile: class.views.advancedmenu.php,v $
-* Project   :   Contenido
-* Descr     :   Contenido Advanced Menu View
-* Modified  :   $Date: 2004/08/04 07:15:30 $
-*
-* © four for business AG, www.4fb.de
-*
-* $Id: class.views.advancedmenu.php,v 1.1 2004/08/04 07:15:30 timo.hummel Exp $
-******************************************/
+/**
+ * Project: 
+ * Contenido Content Management System
+ * 
+ * Description: 
+ * Contenido Advanced Menu View
+ * 
+ * Requirements: 
+ * @con_php_req 5.0
+ * 
+ *
+ * @package    Contenido Backend classes
+ * @version    1.1
+ * @author     Timo Hummel
+ * @copyright  four for business AG <www.4fb.de>
+ * @license    http://www.contenido.org/license/LIZENZ.txt
+ * @link       http://www.4fb.de
+ * @link       http://www.contenido.org
+ * 
+ * {@internal 
+ *   created 2004-08-04
+ *   
+ *   $Id: class.views.advancedmenu.php,v 1.1 2004/08/04 07:15:30 timo.hummel Exp $
+ * }}
+ * 
+ */
+
+if(!defined('CON_FRAMEWORK')) {
+	die('Illegal call');
+}
+
 
 cInclude("classes", "class.htmlelements.php");
 
