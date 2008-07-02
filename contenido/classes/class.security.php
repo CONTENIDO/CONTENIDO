f311c6775e242fca601b52c4388c659b6ffd2901
Frederic Schneider <frederic.schneider@4fb.de>
2008-07-02 11:46:47 +0000

    add boolean functions and checkRequests()

--- contenido/classes/class.security.php
+++ contenido/classes/class.security.php
@@ -11,7 +11,7 @@
  * 
  *
  * @package    Contenido Backend classes
- * @version    1.0.0
+ * @version    1.0.1
  * @author     Frederic Schneider
  * @copyright  four for business AG <www.4fb.de>
  * @license    http://www.contenido.org/license/LIZENZ.txt
@@ -21,6 +21,7 @@
  * 
  * {@internal 
  *   created 2008-06-25
+ *   modified 2008-07-02, Frederic Schneider, add boolean functions and checkRequests() 
  *
  *   $Id$:
  * }}
@@ -37,17 +38,17 @@ if(!defined('CON_FRAMEWORK')) {
 class Contenido_Security {
 
     /**
-     * Check: Is the variable an integer?
+     * Check: Has the variable an boolean value?
      * @access public
      * @param string $sVar
-     * @return true or false
+     * @return boolean
      */
-    public static function isInteger($iVar) {
+    public static function isBoolean($sVar) {
 
-        $iTempVar = $iVar;
-        $iVar = (int) $iVar;
+        $sTempVar = $sVar;
+        $sVar = (bool) $sVar;
 
-        if($iTempVar === $iVar) {
+        if($sTempVar === $sVar) {
             return true;
         } else {
             return false;
@@ -69,6 +70,16 @@ class Contenido_Security {
             return false;
         }
 
+    }
+
+    /**
+     * Convert an string to an boolean
+     * @access public
+     * @param string $sInteger
+     * @return $sInteger converted boolean
+     */
+    public static function toBoolean($sInteger) {    
+        return (bool) $sInteger;    
     }
 
     /**
@@ -98,6 +109,21 @@ class Contenido_Security {
         }
 
         return $sString;
+
+    }
+
+    /**
+     * Checks some request-vars (XSS)
+     * @access public
+     * @return die() or true
+     */
+    public static function checkRequests() {
+
+        if(isset($_REQUEST['cfg']) || isset($_REQUEST['cfgClient']) || isset($_REQUEST['contenido_path'])) {
+            die("Invalid call!");
+        } else {
+            return true;
+        }
 
     }
 
