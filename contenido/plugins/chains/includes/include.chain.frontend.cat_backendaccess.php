327d0c71fd0e63c5e79d4ec046386d99959da093
Thorsten Granz <discard.thorsten.granz@4fb.de>
2008-07-03 10:20:36 +0000

    security fix

--- contenido/plugins/chains/includes/include.chain.frontend.cat_backendaccess.php
+++ contenido/plugins/chains/includes/include.chain.frontend.cat_backendaccess.php
@@ -1,4 +1,37 @@
 <?php
+/**
+ * Project: 
+ * Contenido Content Management System
+ * 
+ * Description: 
+ * 
+ * Requirements: 
+ * @con_php_req 5.0
+ * 
+ *
+ * @package    Contenido Backend classes
+ * @version    1.0
+ * @author     Unknown
+ * @copyright  four for business AG <www.4fb.de>
+ * @license    http://www.contenido.org/license/LIZENZ.txt
+ * @link       http://www.4fb.de
+ * @link       http://www.contenido.org
+ * 
+ * {@internal 
+ *   created 
+ *
+ *   $Id: 
+ * }}
+ * 
+ */
+ 
+if(!defined('CON_FRAMEWORK')) {
+	die('Illegal call');
+}
+
+cInclude('classes', 'class.security.php');
+
+
 function cecFrontendCategoryAccess_Backend($idlang, $idcat, $user)
 {
 	global $cfg;
@@ -9,7 +42,7 @@ function cecFrontendCategoryAccess_Backend($idlang, $idcat, $user)
 					 WHERE B.name = 'front_allow' AND C.name = 'str' AND A.user_id = '".$user."' AND A.idcat = '$idcat'
 							AND A.idarea = C.idarea AND B.idaction = A.idaction AND A.idlang = $idlang";
 	$db2 = new DB_Contenido;
-	$db2->query($sql);
+	$db2->query(Contenido_Security::escapeDB($sql, $db2));
 
 	if (!$db2->next_record())
 	{
