0ae512c702b6f317d61d090d7658c3b65c3ab45f
Frederic Schneider <frederic.schneider@4fb.de>
2008-07-03 09:14:02 +0000

    fixed wrong escape

--- contenido/includes/rights_mod.inc.php
+++ contenido/includes/rights_mod.inc.php
@@ -22,6 +22,7 @@
  * {@internal 
  *   created unknown
  *   modified 2008-06-27, Frederic Schneider, add security fix
+ *   modified 2008-07-03, Frederic Schneider, fixed wrong escape
  *
  *   $Id$:
  * }}
@@ -34,7 +35,7 @@ if(!defined('CON_FRAMEWORK')) {
 
 //set the areas which are in use fore selecting these
 $possible_area = "'".implode("','", $area_tree[$perm->showareas("mod")])."'";
-$sql = "SELECT A.idarea, A.idaction, A.idcat, B.name, C.name FROM ".$cfg["tab"]["rights"]." AS A, ".$cfg["tab"]["area"]." AS B, ".$cfg["tab"]["actions"]." AS C WHERE user_id='".Contenido_Security::escapeDB($userid)."'
+$sql = "SELECT A.idarea, A.idaction, A.idcat, B.name, C.name FROM ".$cfg["tab"]["rights"]." AS A, ".$cfg["tab"]["area"]." AS B, ".$cfg["tab"]["actions"]." AS C WHERE user_id='".Contenido_Security::escapeDB($userid, $db)."'
         AND idclient='".Contenido_Security::escapeDB($rights_client, $db)."' AND A.type = 0 AND idlang='".Contenido_Security::escapeDB($rights_lang, $db)."' AND B.idarea IN ($possible_area) AND idcat!='0' AND A.idaction = C.idaction AND A.idarea = C.idarea AND A.idarea = B.idarea";
 $db->query($sql);
 
@@ -43,10 +44,9 @@ while ($db->next_record()) { //set a new rights list fore this user
    $rights_list_old[$db->f(3)."|".$db->f(4)."|".$db->f("idcat")] = "x";
 }
 
-if (($perm->have_perm_area_action($area, $action)) && ($action == "user_edit"))
-{
+if (($perm->have_perm_area_action($area, $action)) && ($action == "user_edit")) {
     saverights();
-}else {
+} else {
     if (!$perm->have_perm_area_action($area, $action))
     {
     $notification->displayNotification("error", i18n("Permission denied"));
