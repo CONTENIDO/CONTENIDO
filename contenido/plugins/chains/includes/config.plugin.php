bb64d9e4e909216f66db443e60ef7d581e3f89a0
Thorsten Granz <discard.thorsten.granz@4fb.de>
2008-07-03 10:01:22 +0000

    security fix

--- contenido/plugins/chains/includes/config.plugin.php
+++ contenido/plugins/chains/includes/config.plugin.php
@@ -1,4 +1,33 @@
 <?php
+/**
+ * Project: 
+ * Contenido Content Management System
+ * 
+ * Description: 
+ * 
+ * Requirements: 
+ * @con_php_req 5.0
+ * 
+ *
+ * @package    Contenido Backend classes
+ * @version    1.0
+ * @author     Unknown
+ * @copyright  four for business AG <www.4fb.de>
+ * @license    http://www.contenido.org/license/LIZENZ.txt
+ * @link       http://www.4fb.de
+ * @link       http://www.contenido.org
+ * 
+ * {@internal 
+ *   created 
+ *
+ *   $Id: 
+ * }}
+ * 
+ */
+ 
+if(!defined('CON_FRAMEWORK')) {
+	die('Illegal call');
+}
 global $_cecRegistry, $cfg;
 
 #$cfg['plugins']['frontendlogic'][] = "category";
