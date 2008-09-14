aedaaa712c4b30bd3328509f6b5ce93e52b5d516
Murat Purc <murat@purc.de>
2008-09-14 20:07:24 +0000

    CON-206: fixed backend access logic

--- contenido/plugins/chains/includes/include.chain.frontend.cat_backendaccess.php
+++ contenido/plugins/chains/includes/include.chain.frontend.cat_backendaccess.php
@@ -20,6 +20,7 @@
  * {@internal 
  *   created 
  *   modified 2008-07-04, bilal arslan, added security fix
+ *   modified 2008-09-14, Murat Purc, CON-206: fixed backend access logic
  *   $Id$: 
  * }}
  * 
@@ -34,16 +35,21 @@ cInclude('classes', 'class.security.php');
 
 function cecFrontendCategoryAccess_Backend($idlang, $idcat, $user)
 {
-	global $cfg;
+	global $cfg, $perm;
+
+	if ($perm->have_perm()) {
+        // sysadmin or client admin can allways access to protected areas
+        return true;
+    }
+    
+    $db2 = new DB_Contenido;
 	$sql = "SELECT idright 
 					FROM ".$cfg["tab"]["rights"]." AS A,
 						 ".$cfg["tab"]["actions"]." AS B,
 						 ".$cfg["tab"]["area"]." AS C
-					 WHERE B.name = 'front_allow' AND C.name = 'str' AND A.user_id = '". Contenido_Security::toInteger($user)."' AND A.idcat = '".Contenido_Security::toInteger($idcat)."'
+					 WHERE B.name = 'front_allow' AND C.name = 'str' AND A.user_id = '". Contenido_Security::escapeDB($user, $db2) ."' AND A.idcat = '".Contenido_Security::toInteger($idcat)."'
 							AND A.idarea = C.idarea AND B.idaction = A.idaction AND A.idlang = '".Contenido_Security::toInteger($idlang)."'";
-	$db2 = new DB_Contenido;
 	$db2->query($sql);
-
 	if (!$db2->next_record())
 	{
 		return false;
@@ -52,5 +58,4 @@ function cecFrontendCategoryAccess_Backend($idlang, $idcat, $user)
 	{
 		return true;
 	}
-}
-?>
+}?>
