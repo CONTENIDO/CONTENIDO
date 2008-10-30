07b19cc940f0369d2ec16a3be4ff7bf417387a44
Andreas Lindner <discard.andreas.lindner@4fb.de>
2008-10-30 10:15:01 +0000

    Also consider group rights of backend user when checking frontend permission

--- contenido/plugins/chains/includes/include.chain.frontend.cat_backendaccess.php
+++ contenido/plugins/chains/includes/include.chain.frontend.cat_backendaccess.php
@@ -21,6 +21,7 @@
  *   created 
  *   modified 2008-07-04, bilal arslan, added security fix
  *   modified 2008-09-14, Murat Purc, CON-206: fixed backend access logic
+ *   modified 2008-10-30, Andreas Lindner, also consider group memnership of backend users    
  *   $Id$: 
  * }}
  * 
@@ -32,30 +33,41 @@ if(!defined('CON_FRAMEWORK')) {
 
 cInclude('classes', 'class.security.php');
 
-
 function cecFrontendCategoryAccess_Backend($idlang, $idcat, $user)
 {
 	global $cfg, $perm;
 
 	if ($perm->have_perm()) {
-        // sysadmin or client admin can allways access to protected areas
+        // sysadmin or client admin can always access to protected areas
         return true;
     }
     
-    $db2 = new DB_Contenido;
-	$sql = "SELECT idright 
+	$db2 = new DB_Contenido;
+	
+	$arrSearchFor = array("'".Contenido_Security::escapeDB($user, $db2)."'");
+
+	$sql = "SELECT * FROM ".$cfg['tab']['groupmembers']." WHERE user_id = '".Contenido_Security::escapeDB($user, $db2)."'";
+
+	$db2->query($sql);
+	
+	while ($db2->next_record()) {
+		$arrSearchFor[] = "'".Contenido_Security::escapeDB($db2->f('group_id'), $db2)."'";
+	}
+
+	$sSearchFor = implode(",", $arrSearchFor);
+	
+	$sql = "SELECT idright
 					FROM ".$cfg["tab"]["rights"]." AS A,
 						 ".$cfg["tab"]["actions"]." AS B,
 						 ".$cfg["tab"]["area"]." AS C
-					 WHERE B.name = 'front_allow' AND C.name = 'str' AND A.user_id = '". Contenido_Security::escapeDB($user, $db2) ."' AND A.idcat = '".Contenido_Security::toInteger($idcat)."'
+					 WHERE B.name = 'front_allow' AND C.name = 'str' AND A.user_id IN (". $sSearchFor .") AND A.idcat = '".Contenido_Security::toInteger($idcat)."'
 							AND A.idarea = C.idarea AND B.idaction = A.idaction AND A.idlang = '".Contenido_Security::toInteger($idlang)."'";
+
 	$db2->query($sql);
-	if (!$db2->next_record())
-	{
+
+	if (!$db2->next_record()) {
 		return false;
-	}
-	else
-	{
+	} else {
 		return true;
 	}
 }?>
