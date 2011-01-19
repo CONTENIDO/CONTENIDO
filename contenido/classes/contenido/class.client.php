53a496a90a3d5b6515f32127b3e49094be655f60
Murat Purc <murat@purc.de>
2011-01-19 23:20:05 +0000

    Cleanup, optimization, formatting, documentation of cApiClient* classes

--- contenido/classes/contenido/class.client.php
+++ contenido/classes/contenido/class.client.php
@@ -11,7 +11,7 @@
  * 
  *
  * @package    Contenido Backend classes
- * @version    1.12
+ * @version    1.13
  * @author     Bjoern Behrens
  * @copyright  four for business AG <www.4fb.de>
  * @license    http://www.contenido.org/license/LIZENZ.txt
@@ -27,274 +27,276 @@
  */
 
 if(!defined('CON_FRAMEWORK')) {
-	die('Illegal call');
+    die('Illegal call');
 }
 
 
 class cApiClientCollection extends ItemCollection
 {
-	/**
-	 * Constructor
-	 */
-	function cApiClientCollection()
-	{
-		global $cfg;
-		
-		parent::ItemCollection($cfg['tab']['clients'], 'idclient');
-		$this->_setItemClass("cApiClient");
-		$this->_setJoinPartner("cApiClientLanguageCollection");
-	}
-	
-	/**
-     * getAvailableClients()
-     * Returns all clients available in the system
-     * @return array   Array with id and name entries
+    /**
+     * Constructor
      */
-	function getAvailableClients()
-	{
+    function cApiClientCollection()
+    {
+        global $cfg;
+        parent::ItemCollection($cfg['tab']['clients'], 'idclient');
+        $this->_setItemClass("cApiClient");
+        $this->_setJoinPartner("cApiClientLanguageCollection");
+    }
+
+    /**
+     * Returns all clients available in the system
+     *
+     * @return  array   Array with id and name entries
+     */
+    function getAvailableClients()
+    {
         $aClients = array();
 
-		$this->select();
+        $this->select();
 
-        while ($oItem = $this->next())
-        {
-			$aNewEntry['name'] = $oItem->get('name');
-			$aClients[$oItem->get('idclient')] = $aNewEntry;
+        while ($oItem = $this->next()) {
+            $aNewEntry['name'] = $oItem->get('name');
+            $aClients[$oItem->get('idclient')] = $aNewEntry;
         }
 
         return ($aClients);
-	}
-	
+    }
+
     /**
-     * getAvailableClients()
      * Returns all clients available in the system
-     * @return array   Array with id and name entries
+     *
+     * @return  array   Array with id and name entries
      */
-	function getAccessibleClients()
+    function getAccessibleClients()
     {
-    	global $perm;
-    	
-    	$aClients = array();
-    	
-    	$this->select();
-    	
-    	while ($oItem = $this->next())
-    	{
-    		if ($perm->have_perm_client("client[".$oItem->get('idclient')."]") ||
-    			$perm->have_perm_client("admin[".$oItem->get('idclient')."]") ||
-    			$perm->have_perm_client())
-    		{
-    			$aNewEntry['name'] = $oItem->get('name');
-    			$aClients[$oItem->get('idclient')] = $aNewEntry;
-    		}
-    	}
-    
-    	return ($aClients);
+        global $perm;
+        $aClients = array();
+        $this->select();
+        while ($oItem = $this->next()) {
+            if ($perm->have_perm_client("client[".$oItem->get('idclient')."]") ||
+                $perm->have_perm_client("admin[".$oItem->get('idclient')."]") ||
+                $perm->have_perm_client()) {
+                $aNewEntry['name'] = $oItem->get('name');
+                $aClients[$oItem->get('idclient')] = $aNewEntry;
+            }
+        }
+        return ($aClients);
     }
-    
+
     /**
-     * getClientname()
      * Returns the clientname of the given clientid
-     * @return string  Clientname if found, or emptry string if not.
+     *
+     * @param   int   $iIdClient
+     * @return  string  Clientname if found, or empty string if not.
      */
-    function getClientname($idclient)
+    function getClientname($iIdClient)
     {
-    	$this->select("idclient='$idclient'");
-    	if ($oItem = $this->next())
-    	{
-    		return $oItem->get('name');
-    	} else
-    	{
-    		return i18n("No client");
-    	}
+        $this->select("idclient='" . (int) $iIdClient . "'");
+        if ($oItem = $this->next()) {
+            return $oItem->get('name');
+        } else {
+            return i18n("No client");
+        }
     }
-    
+
     /**
-     * hasLanguageAssigned()
      * Returns if the given client has a language
-     * @return bool  true if the client has a language
+     *
+     * @param   int   $iIdClient
+     * @return  bool  true if the client has a language
      */
-    function hasLanguageAssigned($idclient)
+    function hasLanguageAssigned($iIdClient)
     {
-    	global $cfg;
-    
-    	$db = new DB_Contenido;
-    
-    	$sql = "SELECT
-    				idlang
-    			FROM
-    			". $cfg["tab"]["clients_lang"]."
-    			WHERE
-    				idclient = \"".$idclient."\"";
-    
-    	$db->query($sql);
-    	if ($db->next_record())
-    	{
-    		return (true);
-    	} else {
-    		return (false);
-    	}
+        global $cfg;
+        $db = new DB_Contenido();
+        $sql = 'SELECT idlang FROM ' . $cfg['tab']['clients_lang'] . ' WHERE idclient = "' . (int) $iIdClient . '"';
+        $db->query($sql);
+        if ($db->next_record()) {
+            return true;
+        } else {
+            return false;
+        }
     }
-    
 }
 
 /**
- * Class cApiClientCollection
+ * Class cApiClient
  * @author Marco Jahn <Marco.Jahn@4fb.de>
- * @version 1.0
+ * @version 1.01
  * @copyright four for business 2004
  */
 class cApiClient extends Item
 {
-	var $idclient;
-	
-	/**
-	 * Constructor
-	 *
-	 * @param integer client to load
-	 */
-	function cApiClient($idclient = false)
-	{
-		global $cfg;
-		
-		parent::Item($cfg['tab']['clients'], 'idclient');
-		
-		if ($idclient !== false)
-		{
-			$this->loadByPrimaryKey($idclient);	
-		}
-	}
-	
-	/**
-	 * Static accessor to the singleton instance.
-	 * 
-	 * @return Object Reference to the singleton instance.
-	 */
-	public static function getInstance($iClient = false) {
-		
-		static $oCurrentInstance;
-		
-		if (!$iClient) {
-			/*
-			 * Use global $client
-			 */
-			$iClient = $GLOBALS['client'];
-		}
-		
-		if (!isset($oCurrentInstance[$iClient])) {
-			$oCurrentInstance[$iClient] = new cApiClient($iClient);
-		}
-		
-		return $oCurrentInstance[$iClient];
-	}
-	
-	function loadByPrimaryKey ($value)
-	{
-		if (parent::loadByPrimaryKey($value) == true) 
-        {
-			$this->idclient = $value;
-            return true;
-		}
-        return false;
-	}
-	
-	/**
-	 * Set client property
-	 *
-	 * @param type	 	mixed Type of the data to store (arbitary data)
- 	 * @param name		mixed Entry name
-	 * @param value		mixed Value
-	 */
-	function setProperty($mType, $mName, $mValue, $mIdproperty = 0)
-	{
-		// Runtime on-demand allocation of the properties object
-		if (!is_object($this->properties))
-		{
-			$this->properties = new PropertyCollection;
-			$this->properties->changeClient($this->idclient);
-		}
+    var $idclient;
+
+    /**
+     * Property collection instance
+     * @var PropertyCollection
+     */
+    var $_oPropertyCollection;
+
+    /**
+     * Constructor
+     *
+     * @param  int  client to load
+     */
+    function cApiClient($iIdClient = false)
+    {
+        global $cfg;
+        parent::Item($cfg['tab']['clients'], 'idclient');
+        if ($iIdClient !== false) {
+            $this->loadByPrimaryKey($iIdClient);
+        }
+    }
+    
+    /**
+     * Static accessor to the singleton instance.
+     *
+     * @param   int  $iClient
+     * @return  cApiClient  Reference to the singleton instance.
+     */
+    public static function getInstance($iClient = false) {
+        static $oCurrentInstance;
+
+        if (!$iClient) {
+            // Use global $client
+            $iClient = $GLOBALS['client'];
+        }
         
-		$this->properties->setValue('clientsetting', $this->idclient, $mType, $mName, $mValue, $mIdproperty);
-	}
-	
-	function getProperty($mType, $mName)
-	{
-		// Runtime on-demand allocation of the properties object
-		if (!is_object($this->properties))
-		{
-			$this->properties = new PropertyCollection;
-			$this->properties->changeClient($this->idclient);
-		}
-		
-		return $this->properties->getValue('clientsetting', $this->idclient, $mType, $mName);
-	}
-	
-	function deleteProperty($idprop)
-	{
-		// Runtime on-demand allocation of the properties object
-		if (!is_object($this->properties))
-		{
-			$this->properties = new PropertyCollection;
-			$this->properties->changeClient($this->idclient);
-		}
-		
-		$this->properties->delete($idprop);
-	}
-	
-	function getProperties()
-	{
-		// Runtime on-demand allocation of the properties object
-		if (!is_object($this->properties))
-		{
-			$this->properties = new PropertyCollection;
-			$this->properties->changeClient($this->idclient);
-		}
-		
-		$this->properties->select("itemid='".$this->idclient."' AND itemtype='clientsetting'", "", "type, name, value ASC");
-		
-		if ($this->properties->count() > 0)
-		{
-			$aArray = array();
-			
-			while ($oItem = $this->properties->next())
-			{
-				$aArray[$oItem->get('idproperty')]['type'] = $oItem->get('type');
-				$aArray[$oItem->get('idproperty')]['name'] = $oItem->get('name');
-				$aArray[$oItem->get('idproperty')]['value'] = $oItem->get('value');
-			}
-			
-			return $aArray;
-		} else
-		{
-			return false;	
-		}
-	}
-	
-	function getPropertiesByType($mType)
-	{
-		// Runtime on-demand allocation of the properties object
-		if (!is_object($this->properties))
-		{
-			$this->properties = new PropertyCollection;
-			$this->properties->changeClient($this->idclient);
-		}
-		
-		return $this->properties->getValuesByType('clientsetting', $this->idclient, $mType);
-	}
-   
-	function hasLanguages ()
-	{
-		$cApiClientLanguageCollection = new cApiClientLanguageCollection;
-		$cApiClientLanguageCollection->setWhere("idclient", $this->get("idclient"));
-		$cApiClientLanguageCollection->query();
-		
-		if ($cApiClientLanguageCollection->next())
-		{
-			return true;	
-		} else {
-			return false;	
-		}
-	}
+        if (!isset($oCurrentInstance[$iClient])) {
+            $oCurrentInstance[$iClient] = new cApiClient($iClient);
+        }
+        
+        return $oCurrentInstance[$iClient];
+    }
+
+    /**
+     * Load dataset by primary key
+     *
+     * @param   int  $iIdKey
+     * @return  bool
+     */
+    function loadByPrimaryKey($iIdKey)
+    {
+        if (parent::loadByPrimaryKey($iIdKey) == true) {
+            $this->idclient = $iIdKey;
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Set client property
+     *
+     * @param  mixed  $mType   Type of the data to store (arbitary data)
+     * @param  mixed  $mName   Entry name
+     * @param  mixed  $mValue  Value
+     * @param  mixed  $mIdproperty
+     */
+    function setProperty($mType, $mName, $mValue, $mIdproperty = 0)
+    {
+        $oPropertyColl = $this->_getPropertyCollection();
+        $oPropertyColl->setValue('clientsetting', $this->idclient, $mType, $mName, $mValue, $mIdproperty);
+    }
+
+    /**
+     * Get client property
+     *
+     * @param   mixed  $mType   Type of the data to get
+     * @param   mixed  $mName   Entry name
+     * @return  mixed  Value
+     */
+    function getProperty($mType, $mName)
+    {
+        $oPropertyColl = $this->_getPropertyCollection();
+        return $oPropertyColl->getValue('clientsetting', $this->idclient, $mType, $mName);
+    }
+
+    /**
+     * Delete client property
+     *
+     * @param   int  $iIdProp  Id of property
+     * @return  void
+     */
+    function deleteProperty($iIdProp)
+    {
+        $oPropertyColl = $this->_getPropertyCollection();
+        $oPropertyColl->delete($iIdProp);
+    }
+
+    /**
+     * Get client properties by type
+     *
+     * @param   mixed  $mType   Type of the data to get
+     * @return  array  Assoziative array
+     */
+    function getPropertiesByType($mType)
+    {
+        $oPropertyColl = $this->_getPropertyCollection();
+        return $oPropertyColl->getValuesByType('clientsetting', $this->idclient, $mType);
+    }
+
+    /**
+     * Get all client properties
+     *
+     * @param   mixed  $mType   Type of the data to get
+     * @return  array|false  Assoziative array
+     * @todo    return value should be the same as getPropertiesByType(), e. g. an empty array instead false
+     */
+    function getProperties()
+    {
+        $oPropertyColl = $this->_getPropertyCollection();
+        $oPropertyColl->select("itemid='".$this->idclient."' AND itemtype='clientsetting'", "", "type, name, value ASC");
+
+        if ($oPropertyColl->count() > 0) {
+            $aArray = array();
+
+            while ($oItem = $oPropertyColl->next()) {
+                $aArray[$oItem->get('idproperty')]['type']  = $oItem->get('type');
+                $aArray[$oItem->get('idproperty')]['name']  = $oItem->get('name');
+                $aArray[$oItem->get('idproperty')]['value'] = $oItem->get('value');
+            }
+
+            return $aArray;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Check if client has at least one language
+     *
+     * @return  bool
+     */
+    function hasLanguages()
+    {
+        $cApiClientLanguageCollection = new cApiClientLanguageCollection();
+        $cApiClientLanguageCollection->setWhere("idclient", $this->get("idclient"));
+        $cApiClientLanguageCollection->query();
+
+        if ($cApiClientLanguageCollection->next()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Lazy instantiation and return of properties object
+     *
+     * @return PropertyCollection
+     */
+    function _getPropertyCollection()
+    {
+        // Runtime on-demand allocation of the properties object
+        if (!is_object($this->_oPropertyCollection)) {
+            $this->_oPropertyCollection = new PropertyCollection();
+            $this->_oPropertyCollection->changeClient($this->idclient);
+        }
+        return $this->_oPropertyCollection;
+    }
+
 }
 
 ?>
--- contenido/classes/contenido/class.clientslang.php
+++ contenido/classes/contenido/class.clientslang.php
@@ -11,7 +11,7 @@
  * 
  *
  * @package    Contenido Backend classes
- * @version    1.4
+ * @version    1.41
  * @author     Timo Hummel
  * @copyright  four for business AG <www.4fb.de>
  * @license    http://www.contenido.org/license/LIZENZ.txt
@@ -27,159 +27,179 @@
  */
 
 if(!defined('CON_FRAMEWORK')) {
-	die('Illegal call');
+    die('Illegal call');
 }
 
 
 class cApiClientLanguageCollection extends ItemCollection
 {
-/**
-	 * Constructor
-	 */
-	function cApiClientLanguageCollection()
-	{
-		global $cfg;
-		parent::ItemCollection($cfg["tab"]["clients_lang"], "idclientslang");
-		$this->_setItemClass("cApiClientLanguage");
-	}	
+    /**
+     * Constructor
+     */
+    function cApiClientLanguageCollection()
+    {
+        global $cfg;
+        parent::ItemCollection($cfg["tab"]["clients_lang"], "idclientslang");
+        $this->_setItemClass("cApiClientLanguage");
+    }
 }
 
 class cApiClientLanguage extends Item
 {
-	var $idclient;
-	
-	/**
-	 * Constructor
-	 *
-	 * @param integer idclientslang	If specified, load item
-	 * @param integer idclient		If idclient and idlang specified, load item; ignored, if idclientslang specified
-	 * @param integer idlang		If idclient and idlang specified, load item; ignored, if idclientslang specified
-	 */
-	function cApiClientLanguage ($idclientslang = false, $idclient = false, $idlang = false)
-	{
-		global $cfg;
-		parent::Item($cfg["tab"]["clients_lang"], "idclientslang");
-		
-		if ($idclientslang !== false)
-		{
-			$this->loadByPrimaryKey($idclientslang);	
-		} else if ($idclient !== false && $idlang !== false)
-		{
-			/* One way, but the other should be faster
-			$oCollection = new cApiClientLanguageCollection;
-			$oCollection->setWhere("idclient", $idclient);
-			$oCollection->setWhere("idlang", $idlang);
-			$oCollection->query();
-			
-			if ($oItem = $oCollection->next())
-			{ 
-				$this->loadByPrimaryKey($oItem->get($oItem->primaryKey));
-			} */ 
-			
-			$sSQL = "SELECT ".$this->primaryKey." FROM ".$this->table.
-					#" WHERE idclient = '".$idclient."' AND idlang = '".$idlang."'";
-					" WHERE idclient = '" . Contenido_Security::escapeDB($idclient, $this->db) . "' AND idlang = '" . Contenido_Security::escapeDB($idlang, $this->db) . "'";
+    var $idclient;
 
-			/* Query the database */
-			$this->db->query($sSQL);
-			
-			if ($this->db->next_record()) {
-				$this->loadByPrimaryKey($this->db->f($this->primaryKey));
-			}
-		}
-	}
-	
-	function loadByPrimaryKey ($iID)
-	{
-		if (parent::loadByPrimaryKey($iID) == true)
-		{
-			$this->idclient = $this->get("idclient");
+    /**
+     * Property collection instance
+     * @var PropertyCollection
+     */
+    var $_oPropertyCollection;
+
+    /**
+     * Constructor
+     *
+     * @param  int  $iIdClientsLang  If specified, load item
+     * @param  int  $iIdClient       If idclient and idlang specified, load item; ignored, if idclientslang specified
+     * @param  int  $iIdLang         If idclient and idlang specified, load item; ignored, if idclientslang specified
+     */
+    function cApiClientLanguage ($iIdClientsLang = false, $iIdClient = false, $iIdLang = false)
+    {
+        global $cfg;
+        parent::Item($cfg["tab"]["clients_lang"], "idclientslang");
+
+        if ($iIdClientsLang !== false) {
+            $this->loadByPrimaryKey($iIdClientsLang);
+        } else if ($iIdClient !== false && $iIdLang !== false) {
+            /* One way, but the other should be faster
+            $oCollection = new cApiClientLanguageCollection;
+            $oCollection->setWhere("idclient", $iIdClient);
+            $oCollection->setWhere("idlang", $iIdLang);
+            $oCollection->query();
+
+            if ($oItem = $oCollection->next()) {
+                $this->loadByPrimaryKey($oItem->get($oItem->primaryKey));
+            } */
+
+            $sSQL = "SELECT ".$this->primaryKey." FROM ".$this->table.
+                    " WHERE idclient = '" . Contenido_Security::escapeDB($iIdClient, $this->db) . "' AND idlang = '" . Contenido_Security::escapeDB($iIdLang, $this->db) . "'";
+
+            /* Query the database */
+            $this->db->query($sSQL);
+
+            if ($this->db->next_record()) {
+                $this->loadByPrimaryKey($this->db->f($this->primaryKey));
+            }
+        }
+    }
+
+    /**
+     * Load dataset by primary key
+     *
+     * @param   int  $iIdClientsLang
+     * @return  bool
+     */
+    function loadByPrimaryKey($iIdClientsLang)
+    {
+        if (parent::loadByPrimaryKey($iIdClientsLang) == true) {
+            $this->idclient = $this->get("idclient");
             return true;
-		}
+        }
         return false;
-	}
-	
-	/**
-	 * Set client property
-	 *
-	 * @param type	 	mixed Type of the data to store (arbitary data)
- 	 * @param name		mixed Entry name
-	 * @param value		mixed Value
-	 */
-	function setProperty($mType, $mName, $mValue)
-	{
-		/* Runtime on-demand allocation of the properties object */
-		if (!is_object($this->properties))
-		{
-			$this->properties = new PropertyCollection;
-			$this->properties->changeClient($this->idclient);
-		}
-		$this->properties->setValue($this->primaryKey, $this->get($this->primaryKey), $mType, $mName, $mValue);
-	}
-	
-	function getProperty($mType, $mName)
-	{
-		/* Runtime on-demand allocation of the properties object */
-		if (!is_object($this->properties))
-		{
-			$this->properties = new PropertyCollection;
-			$this->properties->changeClient($this->idclient);
-		}
-		
-		return $this->properties->getValue($this->primaryKey, $this->get($this->primaryKey), $mType, $mName);
-	}
-	
-	function deleteProperty($idprop)
-	{
-		/* Runtime on-demand allocation of the properties object */
-		if (!is_object($this->properties))
-		{
-			$this->properties = new PropertyCollection;
-			$this->properties->changeClient($this->idclient);
-		}
-		
-		$this->properties->delete($idprop);
-	}
-    
+    }
+
+    /**
+     * Set client property
+     *
+     * @param  mixed  $mType   Type of the data to store (arbitary data)
+     * @param  mixed  $mName   Entry name
+     * @param  mixed  $mValue  Value
+     */
+    function setProperty($mType, $mName, $mValue)
+    {
+        $oPropertyColl = $this->_getPropertyCollection();
+        $oPropertyColl->setValue($this->primaryKey, $this->get($this->primaryKey), $mType, $mName, $mValue);
+    }
+
+    /**
+     * Get client property
+     *
+     * @param   mixed  $mType   Type of the data to get
+     * @param   mixed  $mName   Entry name
+     * @return  mixed  Value
+     */
+    function getProperty($mType, $mName)
+    {
+        $oPropertyColl = $this->_getPropertyCollection();
+        return $oPropertyColl->getValue($this->primaryKey, $this->get($this->primaryKey), $mType, $mName);
+    }
+
+    /**
+     * Delete client property
+     *
+     * @param   int  $idprop   Id of property
+     * @return  void
+     */
+    function deleteProperty($idprop)
+    {
+        $oPropertyColl = $this->_getPropertyCollection();
+        $oPropertyColl->delete($idprop);
+    }
+
+    /**
+     * Get client properties by type
+     *
+     * @param   mixed  $mType   Type of the data to get
+     * @return  array  Assoziative array
+     */
     function getPropertiesByType($mType)
-	{
-		// Runtime on-demand allocation of the properties object
-		if (!is_object($this->properties))
-		{
-			$this->properties = new PropertyCollection;
-			$this->properties->changeClient($this->idclient);
-		}
-		
-		return $this->properties->getValuesByType('idclientslang', $this->idclient, $mType);
-	}
-	
-	function getProperties()
-	{
-		/* Runtime on-demand allocation of the properties object */
-		if (!is_object($this->properties))
-		{
-			$this->properties = new PropertyCollection;
-			$this->properties->changeClient($this->idclient);
-		}
-		
-		$this->properties->select("itemtype='".Contenido_Security::escapeDB($this->primaryKey, $this->db)."' AND itemid='".Contenido_Security::escapeDB($this->get($this->primaryKey), $this->db)."'", "", "type, value ASC");
-		
-		if ($this->properties->count() > 0)
-		{
-			$aArray = array();
-			
-			while ($oItem = $this->properties->next())
-			{
-				$aArray[$oItem->get('idproperty')]['type']	= $oItem->get('type');
-				$aArray[$oItem->get('idproperty')]['name']	= $oItem->get('name');
-				$aArray[$oItem->get('idproperty')]['value']	= $oItem->get('value');
-			}
-			
-			return $aArray;
-		} else
-		{
-			return false;	
-		}
-	}
+    {
+        $oPropertyColl = $this->_getPropertyCollection();
+        return $oPropertyColl->getValuesByType($this->primaryKey, $this->idclient, $mType);
+    }
+
+    /**
+     * Get all client properties
+     *
+     * @param   mixed  $mType   Type of the data to get
+     * @return  array|false  Assoziative array
+     * @todo    return value should be the same as getPropertiesByType(), e. g. an empty array instead false
+     */
+    function getProperties()
+    {
+        $itemtype = Contenido_Security::escapeDB($this->primaryKey, $this->db);
+        $itemid   = Contenido_Security::escapeDB($this->get($this->primaryKey), $this->db);
+        $oPropertyColl = $this->_getPropertyCollection();
+        $oPropertyColl->select("itemtype='".$itemtype."' AND itemid='".$itemid."'", "", "type, value ASC");
+
+        if ($oPropertyColl->count() > 0) {
+            $aArray = array();
+
+            while ($oItem = $oPropertyColl->next()) {
+                $aArray[$oItem->get('idproperty')]['type']  = $oItem->get('type');
+                $aArray[$oItem->get('idproperty')]['name']  = $oItem->get('name');
+                $aArray[$oItem->get('idproperty')]['value'] = $oItem->get('value');
+            }
+
+            return $aArray;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Lazy instantiation and return of properties object
+     *
+     * @return PropertyCollection
+     */
+    function _getPropertyCollection()
+    {
+        // Runtime on-demand allocation of the properties object
+        if (!is_object($this->_oPropertyCollection)) {
+            $this->_oPropertyCollection = new PropertyCollection();
+            $this->_oPropertyCollection->changeClient($this->idclient);
+        }
+        return $this->_oPropertyCollection;
+    }
+
 }
+
 ?>
