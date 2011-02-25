119087b4aa13f2c11bbc4aa28aca3308575818c8
Murat Purc <murat@purc.de>
2011-02-25 00:07:42 +0000

    Some cleanup and formatting in functions.api.general.php

--- contenido/includes/api/functions.api.general.php
+++ contenido/includes/api/functions.api.general.php
@@ -22,7 +22,7 @@
  * {@internal 
  *   created  2003-09-01
  *   modified 2008-06-25, Frederic Schneider, add security fix
- *   modified 2009-10-27, Murat Purc, initialization of variable $error to prevent PHP strict messages
+ *   modified 2009-10-27, Murat Purc, initialization of variable $bError to prevent PHP strict messages
  *   modified 2010-05-20, Murat Purc, standardized Contenido startup and security check invocations, see [#CON-307]
  *
  *   $Id$:
@@ -31,17 +31,9 @@
  */
 
 if (!defined('CON_FRAMEWORK')) {
-	die('Illegal call');
+    die('Illegal call');
 }
 
-// @TODO: PATH_SEPARATOR/DIRECTORY_SEPARATOR exists since PHP 4.3.4, Contenido requires PHP 5...
-if (!defined('PATH_SEPARATOR')) {
-    define('PATH_SEPARATOR', (substr(PHP_OS, 0, 3) == 'WIN') ? ';' : ':');
-}
-
-if (!defined('DIRECTORY_SEPARATOR')) {
-    define('DIRECTORY_SEPARATOR', (substr(PHP_OS, 0, 3) == 'WIN') ? '\\' : '/');
-}
 
 /* Info:
  * This file contains Contenido General API functions.
@@ -54,11 +46,10 @@ if (!defined('DIRECTORY_SEPARATOR')) {
  */
 
 /**
- * contenido_include: Includes a file
- * and takes care of all path transformations.
+ * Includes a file and takes care of all path transformations.
  *
  * Example:
- * contenido_include("classes", "class.backend.php");
+ * contenido_include('classes', 'class.backend.php');
  *
  * Currently defined areas:
  *
@@ -72,179 +63,160 @@ if (!defined('DIRECTORY_SEPARATOR')) {
  * scripts     Path to the contenido scripts
  *
  * NOTE: Since Contenido (since v 4.8.15) provides autoloading of required
- *       class files, there is no need to load class files by using
+ *       class files, there is no need to load Contenido class files of by using
  *       contenido_include() or cInclude().
  *
- * @param $where string The area which should be included
- * @param $what string The filename of the include
- * @param $force boolean If true, force the file to be included
- * @param $returnpath $string or boolean false if file is not found
- *
- * @return none
- *
+ * @param   string  $sWhere       The area which should be included
+ * @param   string  $sWhat        The filename of the include
+ * @param   bool    $bForce       If true, force the file to be included
+ * @param   string  $bReturnPath  Flag to return the path instead of including the file
+ * @return  void
  */
-function contenido_include ($where, $what, $force = false, $returnpath = false)
+function contenido_include($sWhere, $sWhat, $bForce = false, $bReturnPath = false)
 {
-   global $client, $cfg, $cfgClient;
+    global $client, $cfg, $cfgClient;
 
-   /* Sanity check for $what */
-   $what = trim($what);
-   $where = strtolower($where);
-   $error = false;
+    // Sanity check for $sWhat
+    $sWhat  = trim($sWhat);
+    $sWhere = strtolower($sWhere);
+    $bError = false;
 
-   switch ($where)
-
-   {
-      case "frontend":
-            $include = $cfgClient[$client]["path"]["frontend"] . $what;
+    switch ($sWhere) {
+        case 'frontend':
+            $sInclude = $cfgClient[$client]['path']['frontend'] . $sWhat;
             break;
-      case "wysiwyg":
-            $include = $cfg['path']['wysiwyg'] . $what;
+        case 'wysiwyg':
+            $sInclude = $cfg['path']['wysiwyg'] . $sWhat;
             break;
-      case "all_wysiwyg":
-      		$include = $cfg['path']['all_wysiwyg'] . $what;
-      		break;
-      case "conlib":
-      case "phplib":
-            $include = $cfg['path']['phplib'] . $what;
+        case 'all_wysiwyg':
+            $sInclude = $cfg['path']['all_wysiwyg'] . $sWhat;
             break;
-      case "pear":
-            $include = $what;
-            $include_path = ini_get('include_path');
+        case 'conlib':
+        case 'phplib':
+            $sInclude = $cfg['path']['phplib'] . $sWhat;
+            break;
+        case 'pear':
+            $sInclude = $sWhat;
+            $sIncludePath = ini_get('include_path');
 
-            if (!preg_match("|".$cfg['path']['pear']."|i", $include_path)) {
-               // contenido pear path is not set in include_path
-
-               // we try to add it via ini_set
-               if (!@ini_set( 'include_path' , $include_path.PATH_SEPARATOR.$cfg['path']['pear'])) {
-                  // not able to change include_path
-                  trigger_error("Can't add {$cfg['path']['pear']} to include_path", E_USER_NOTICE);
-                  $include = $cfg['path']['pear'] . $what; unset($where);
-               } else {
-
-                  $paths = explode(PATH_SEPARATOR, ini_get('include_path'));
-                  $last = count($paths)-1;
-                  if ($last >= 2) {
-                     $tmp = $paths[1];
-                     $paths[1] = $paths[$last];
-                     $paths[$last] = $tmp;
-                     @ini_set( 'include_path' ,implode(PATH_SEPARATOR, $paths));
-
-                  }
-                  unset ($paths, $last, $tmp);
-
-               }
+            if (!preg_match('|' . $cfg['path']['pear'] . '|i', $sIncludePath)) {
+                // contenido pear path is not set in include_path
+                // we try to add it via ini_set
+                if (!@ini_set('include_path', $sIncludePath . PATH_SEPARATOR . $cfg['path']['pear'])) {
+                    // not able to change include_path
+                    trigger_error("Can't add {$cfg['path']['pear']} to include_path", E_USER_NOTICE);
+                    $sInclude = $cfg['path']['pear'] . $sWhat;
+                    unset($sWhere);
+                } else {
+                    $aPaths = explode(PATH_SEPARATOR, ini_get('include_path'));
+                    $iLast  = count($aPaths) - 1;
+                    if ($iLast >= 2) {
+                        $tmp = $aPaths[1];
+                        $aPaths[1] = $aPaths[$iLast];
+                        $aPaths[$iLast] = $tmp;
+                        @ini_set('include_path', implode(PATH_SEPARATOR, $aPaths));
+                    }
+                    unset($aPaths, $iLast, $tmp);
+                }
             }
             break;
-      default:
-            // FIXME: A workaround to provide inclusion of classes wich are not
+        default:
+            // FIXME: A workaround to provide inclusion of classes which are not
             //        handled by the autoloader
-            if ($where === 'classes') {
-                if (Contenido_Autoload::isAutoloadable($cfg['path'][$where] . $what)) {
-                    // it's a class file and it will be loaded automatically by 
+            if ($sWhere === 'classes') {
+                if (Contenido_Autoload::isAutoloadable($cfg['path'][$sWhere] . $sWhat)) {
+                    // it's a class file and it will be loaded automatically by
                     // the autoloader - get out here
                     return;
                 }
             }
-            $include = $cfg['path']['contenido'] . $cfg['path'][$where] . $what;
+            $sInclude = $cfg['path']['contenido'] . $cfg['path'][$sWhere] . $sWhat;
             break;
-   }
+    }
 
-   if ( $where == "pear" ) {
+    $sFoundPath = '';
 
-      // now we check if the file is available in the include path
-      $foundinpath = false;
-      $paths = explode(PATH_SEPARATOR, ini_get('include_path'));
-      foreach ($paths as $path) {
-         if (@file_exists($path . DIRECTORY_SEPARATOR . $include) && !$foundinpath) {
-            $foundinpath = $path;
-         }
-      }
+    if ($sWhere === 'pear') {
+        // now we check if the file is available in the include path
+        $aPaths = explode(PATH_SEPARATOR, ini_get('include_path'));
+        foreach ($aPaths as $sPath) {
+            if (@file_exists($sPath . DIRECTORY_SEPARATOR . $sInclude)) {
+                $sFoundPath = $sPath;
+                break;
+            }
+        }
 
-      if (!$foundinpath)
-      {
-         $error = true;
-      }
+        if (!$sFoundPath) {
+            $bError = true;
+        }
 
-      unset ($paths, $path);
+        unset($aPaths, $sPath);
+    } else {
+        if (!file_exists($sInclude) || preg_match('#^\.\./#', $sWhat)) {
+            $bError = true;
+        }
+    }
 
-   } else {
+    // should the path be returned?
+    if ($bReturnPath) {
+        if ($sFoundPath !== '') {
+            $sInclude = $sFoundPath . DIRECTORY_SEPARATOR . $sInclude;
+        }
 
-      if (!file_exists($include) || preg_match("#^\.\./#",$what))
-      {
-         $error = true;
-      }
+        if (!$bError) {
+            return $sInclude;
+        } else {
+            return false;
+        }
+    }
 
-   }
+    if ($bError) {
+        trigger_error("Error: Can't include $sInclude", E_USER_ERROR);
+        return;
+    }
 
-   if ($returnpath) {
-
-      if ($foundinpath) {
-         $include = $foundinpath . DIRECTORY_SEPARATOR . $include;
-      }
-
-      if (!$error) {
-         return $include;
-      } else {
-         return false;
-      }
-   }
-
-   if ($error) {
-      trigger_error("Error: Can't include $include", E_USER_ERROR);
-      return;
-   }
-
-   if ($force == true)
-   {
-      include($include);
-   } else {
-      include_once($include);
-   }
-
+    // now include the file
+    if ($bForce == true) {
+        include($sInclude);
+    } else {
+        include_once($sInclude);
+    }
 }
 
 
 /**
- * cInclude: Shortcut to contenido_include.
+ * Shortcut to contenido_include.
  *
  * @see contenido_include
  *
- * @param $where string The area which should be included
- * @param $what string The filename of the include
- * @param $force boolean If true, force the file to be included
- *
- * @return none
- *
+ * @param   string  $sWhere  The area which should be included
+ * @param   string  $sWhat   The filename of the include
+ * @param   bool    $bForce  If true, force the file to be included
+ * @return  void
  */
-function cInclude ($where, $what, $force = false)
+function cInclude($sWhere, $sWhat, $bForce = false)
 {
-	contenido_include($where, $what, $force);
+    contenido_include($sWhere, $sWhat, $bForce);
 }
 
 /**
- * plugin_include: Includes a file
- * from a plugin and takes care of all
- * path transformations.
+ * Includes a file from a plugin and takes care of all path transformations.
  *
  * Example:
- * plugin_include("formedit", "classes/class.formedit.php");
+ * plugin_include('formedit', 'classes/class.formedit.php');
  *
- * @param $which string The name of the plugin
- * @param $what string The filename of the include
- *
- * @return none
- *
+ * @param   string  $sWhere  The name of the plugin
+ * @param   string  $sWhat   The filename of the include
+ * @return  void
  */
-function plugin_include ($where, $what)
+function plugin_include($sWhere, $sWhat)
 {
-	global $cfg;
+    global $cfg;
 
-	$include = $cfg['path']['contenido'] . $cfg['path']['plugins'] . $where. "/" . $what;
+    $sInclude = $cfg['path']['contenido'] . $cfg['path']['plugins'] . $sWhere. '/' . $sWhat;
 
-	include_once($include);
+    include_once($sInclude);
 }
-
 
 
 ?>
