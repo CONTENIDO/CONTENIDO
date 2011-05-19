ee32dd78f05cd585b8949d25082c471d24fa9948
Murat Purc <murat@purc.de>
2011-05-19 08:23:05 +0000

    Bugfix: Check for defined constant C_SETUP_STEPS to prevent thrown warnings

--- setup/lib/functions.setup.php
+++ setup/lib/functions.setup.php
@@ -1,16 +1,17 @@
 <?php
 /**
- * Project: 
+ * Project:
  * Contenido Content Management System
- * 
- * Description: 
- * 
- * Requirements: 
- * @con_php_req 5
- * 
  *
- * @package    Contenido Backend <Area>
- * @version    0.3
+ * Description:
+ * Contains setup related functions
+ *
+ * Requirements:
+ * @con_php_req 5
+ *
+ *
+ * @package    Contenido setup
+ * @version    0.3.1
  * @author     unknown
  * @copyright  four for business AG <www.4fb.de>
  * @license    http://www.contenido.org/license/LIZENZ.txt
@@ -18,38 +19,42 @@
  * @link       http://www.contenido.org
  * @since      file available since contenido release <Contenido Version>
  * @deprecated file deprecated in contenido release <Contenido Version>
- * 
- * {@internal 
+ *
+ * {@internal
  *   created  unknown
  *   modified 2008-07-07, bilal arslan, added security fix
  *   modified 2011-02-08, Dominik Ziegler, removed old PHP compatibility stuff as contenido now requires at least PHP 5
+ *   modified 2011-05-19, Murat Purc, check for defined constant C_SETUP_STEPS to prevent thrown warnings
  *
  *   $Id$:
  * }}
- * 
+ *
  */
- if(!defined('CON_FRAMEWORK')) {
-                die('Illegal call');
+
+if (!defined('CON_FRAMEWORK')) {
+    die('Illegal call');
 }
 
 /**
- * cGenerateSetupStepsDisplay
- * 
  * Generates the step display.
- * 
- * @param iCurrentStep integer The current step to display active.
+ *
+ * @param   int  iCurrentStep  The current step to display active.
+ * @return  string
  * @modified 2008-02-26 Rudi Bieller
  */
-function cGenerateSetupStepsDisplay ($iCurrentStep)
+function cGenerateSetupStepsDisplay($iCurrentStep)
 {
-	$sStepsPath = '';
-	for ($i=1; $i < C_SETUP_STEPS + 1; $i++) {
-		$sCssActive = '';
-		if ($iCurrentStep == $i) {
-			$sCssActive = 'background-color:#fff;color:#0060B1;';
-		}
-		$sStepsPath .= '<span style="'.$sCssActive.'">&nbsp;'.strval($i).'&nbsp;</span>&nbsp;&nbsp;&nbsp;';
-	}
-	return $sStepsPath;
+    if (!defined('C_SETUP_STEPS')) {
+        return '';
+    }
+    $sStepsPath = '';
+    for ($i=1; $i < C_SETUP_STEPS + 1; $i++) {
+        $sCssActive = '';
+        if ($iCurrentStep == $i) {
+            $sCssActive = 'background-color:#fff;color:#0060B1;';
+        }
+        $sStepsPath .= '<span style="'.$sCssActive.'">&nbsp;'.strval($i).'&nbsp;</span>&nbsp;&nbsp;&nbsp;';
+    }
+    return $sStepsPath;
 }
 ?>
