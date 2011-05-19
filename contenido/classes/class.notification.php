1c8106e043909c409728108da4114440ddf2931e
Murat Purc <murat@purc.de>
2011-05-19 19:58:55 +0000

    Adapted class Contenido_Notification to PHP 5, formatted and documented code

--- contenido/classes/class.notification.php
+++ contenido/classes/class.notification.php
@@ -1,171 +1,245 @@
 <?php
 /**
- * Project: 
+ * Project:
  * Contenido Content Management System
- * 
- * Description: 
- * Class for displaying notifications
- * 
- * Requirements: 
+ *
+ * Description:
+ * Class for displaying notifications.
+ *
+ * Usage:
+ * <code>
+ * // render a error directly
+ * $oNotification = new Contenido_Notification();
+ * $oNotification->displayNotification(
+ *     Contenido_Notification::LEVEL_ERROR, 'Foobar does not exists'
+ * );
+ *
+ * // assign a notification to a variable
+ * $oNotification = new Contenido_Notification();
+ * $sNotification = $oNotification->displayNotification(
+ *     Contenido_Notification::LEVEL_NOTIFICATION, 'Hey dude, you did it!'
+ * );
+ * </code>
+ *
+ * Requirements:
  * @con_php_req 5.0
- * 
+ *
  *
  * @package    Contenido Backend classes
- * @version    1.0.0
+ * @version    1.1.0
  * @author     Timo A. Hummel
  * @copyright  four for business AG <www.4fb.de>
  * @license    http://www.contenido.org/license/LIZENZ.txt
  * @link       http://www.4fb.de
  * @link       http://www.contenido.org
  * @since      file available since contenido release <= 4.6
- * 
- * {@internal 
- *   created unknown
+ *
+ * {@internal
+ *   created  unknown
  *   modified 2008-04-04, Timo Trautmann, added new colors and functions for direct output
  *   modified 2008-06-30, Dominik Ziegler, add security fix
+ *   modified 2011-05-19, Murat Purc, adapted to PHP 5, formatted and documented code
  *
  *   $Id$:
  * }}
- * 
+ *
  */
 
-if(!defined('CON_FRAMEWORK')) {
-	die('Illegal call');
+if (!defined('CON_FRAMEWORK')) {
+    die('Illegal call');
 }
 
 
-class Contenido_Notification {
+class Contenido_Notification
+{
+    /**
+     * Error message level
+     * @var string
+     */
+    const LEVEL_ERROR = 'error';
+
+    /**
+     * Warning message level
+     * @var string
+     */
+    const LEVEL_WARNING = 'warning';
+
+    /**
+     * Info message level
+     * @var string
+     */
+    const LEVEL_INFO = 'info';
+
+    /**
+     * Notification message level
+     * @var string
+     */
+    const LEVEL_NOTIFICATION = 'notification';
+
+    /**
+     * Colors configuration
+     * @var array
+     */
+    protected $_aColors;
+
+    /**
+     * HTML path to images
+     * @var string
+     */
+    protected $_sPathImages;
+
 
     /**
      * Constructor
      */
-    function Contenido_Notification() {
-
-    } # end function
-
-	/**
-	 * New message style without tables - please use this
-	 */
-	function messageBox ($level, $message, $style)
-	{
-        global $cfg;
-        
-        switch ($level)
-        {
-        case "error":
-            $head = i18n('Error');
-            $head_class = 'alertbox_error';
-            $frameColor = $cfg["color"]["notify_error"];
-            $imgPath = $cfg["path"]["contenido_fullhtml"].$cfg["path"]["images"]."icon_fatalerror.gif";
-          break;
-                
-        case "warning":
-            $head = i18n('Warning');
-            $head_class = 'alertbox_warning';
-            $bgColor = $cfg["color"]["notify_warning"];
-            $imgPath = $cfg["path"]["contenido_fullhtml"].$cfg["path"]["images"]."icon_warning.gif";
-          break;
-                
-        case "info":
-            $head = i18n('Info');
-            $head_class = 'alertbox_info';
-            $message = '<span style="color:#435d06">'.$message.'</span>';
-            $bgColor = $cfg["color"]["notify_info"];
-            $imgPath = $cfg["path"]["contenido_fullhtml"].$cfg["path"]["images"]."but_ok.gif";
-          break;
-
-        default:
-            $head = i18n('Notification');
-            $head_class = 'alertbox_notification';
-            $message = '<span style="color:#435d06">'.$message.'</span>';
-            $bgColor = $cfg["color"]["notify"];
-            $imgPath = $cfg["path"]["contenido_fullhtml"].$cfg["path"]["images"]."but_ok.gif";
-          break;
-        }
-		
-				// Box on login page
-				if($style == 1) {
-					$messageBox = 
-						'<div class="alertbox '.$head_class.'_color" id="contenido_notification" style="border-top:0px;">' .
-							'<h1 class="alertbox_head ' . $head_class . '">' . $head . '</h1>' .
-							'<div class="alertbox_message">' . $message . '</div>' .
-						'</div>';
-						
-				}
-				// Simple box
-				else {
-					$messageBox = 
-						'<div class="alertbox_line '.$head_class.'_color" id="contenido_notification">' .
-							'<h1 class=" alertbox_head ' . $head_class . ' '.$head_class.'_color">' . $head . '</h1>' .
-							'<div class="alertbox_message '.$head_class.'_color">' . $message . '</div>' .
-						'</div>';
-			    }
-		return $messageBox;
-	}
-	
-    function returnNotification($level, $message)
+    public function __construct()
     {
         global $cfg;
-        
-        switch ($level)
-        {
-            case "error":
-                $bgColor = $cfg["color"]["notify_error"];
-                $imgPath = $cfg["path"]["contenido_fullhtml"].$cfg["path"]["images"]."icon_fatalerror.gif";
-                break;
-                
-            case "warning":
-                $bgColor = $cfg["color"]["notify_warning"];
-                $imgPath = $cfg["path"]["contenido_fullhtml"].$cfg["path"]["images"]."icon_warning.gif";
-                break;
-                
-            case "info":
-                $message = '<span style="color:#435d06">'.$message.'</span>';
-                $bgColor = $cfg["color"]["notify_info"];
-                $imgPath = $cfg["path"]["contenido_fullhtml"].$cfg["path"]["images"]."but_ok.gif";
-                break;
+        $this->_aColors = $cfg['color'];
+        $this->_sPathImages = $cfg['path']['contenido_fullhtml'] . $cfg['path']['images'];
+    }
 
+
+    /** @deprecated  [2011-05-19] Old constructor function for downwards compatibility */
+    public function Contenido_Notification()
+    {
+        cWarning(__FILE__, __LINE__, 'Deprecated method call, use __construct()');
+        $this->__construct();
+    }
+
+
+    /**
+     * Generates message box and returns it back.
+     *
+     * @param   string  $sLevel  Message level, one of Contenido_Notification::LEVEL_* constants
+     * @param   string  $sMessage  The message to display
+     * @param   int     $iStyle   Flag tp use styles for display or not (feasible 1 or 0)
+     * @return  string
+     */
+    public function messageBox($sLevel, $sMessage, $iStyle = 1)
+    {
+        switch ($sLevel) {
+            case self::LEVEL_ERROR:
+                $sHead = i18n('Error');
+                $sHeadClass = 'alertbox_error';
+                $frameColor = $this->_aColors['notify_error'];
+                $sImgPath = $this->_sPathImages . 'icon_fatalerror.gif';
+                break;
+            case self::LEVEL_WARNING:
+                $sHead = i18n('Warning');
+                $sHeadClass = 'alertbox_warning';
+                $sBgColor = $this->_aColors['notify_warning'];
+                $sImgPath = $this->_sPathImages . 'icon_warning.gif';
+                break;
+            case self::LEVEL_INFO:
+                $sHead = i18n('Info');
+                $sHeadClass = 'alertbox_info';
+                $sMessage = '<span style="color:#435d06">' . $sMessage . '</span>';
+                $sBgColor = $this->_aColors['notify_info'];
+                $sImgPath = $this->_sPathImages . 'but_ok.gif';
+                break;
             default:
-                $message = '<span style="color:#435d06">'.$message.'</span>';
-                $bgColor = $cfg["color"]["notify"];
-                $imgPath = $cfg["path"]["contenido_fullhtml"].$cfg["path"]["images"]."but_ok.gif";
+                $sHead = i18n('Notification');
+                $sHeadClass = 'alertbox_notification';
+                $sMessage = '<span style="color:#435d06">' . $sMessage . '</span>';
+                $sBgColor = $this->_aColors['notify'];
+                $sImgPath = $this->_sPathImages . 'but_ok.gif';
                 break;
         }
 
-        $table = new Table($bgColor, "solid", 0, 2, "#FFFFFF", "#FFFFFF", "#FFFFFF", true, false);
-        
-        $noti = '<div id="contenido_notification" style="position: relative; left: 0; top: 0; z-index: 10;">';
-        $noti .= $table->start_table();
-        $noti .= $table->header_row();
-        $noti .= $table->borderless_cell('<img src="'.$imgPath.'" />');
-        $noti .= $table->borderless_cell('<font color="'.$bgColor.'" style="font-family: Verdana, Arial, Helvetica, Sans-Serif; font-size: 11px;">' .$message. '</font>', "left", "middle");
-        $noti .= $table->end_row();
-        $noti .= $table->end_table();
-        $noti .= '</div>';
-
-        return $noti;
+        if ($iStyle == 1) {
+            // Box on login page
+            $sMessageBox =
+                '<div class="alertbox ' . $sHeadClass . '_color" id="contenido_notification" style="border-top:0px;">' .
+                    '<h1 class="alertbox_head ' . $sHeadClass . '">' . $sHead . '</h1>' .
+                    '<div class="alertbox_message">' . $sMessage . '</div>' .
+                '</div>';
+        } else {
+            // Simple box
+            $sMessageBox =
+                '<div class="alertbox_line ' . $sHeadClass . '_color" id="contenido_notification">' .
+                    '<h1 class=" alertbox_head ' . $sHeadClass . ' ' . $sHeadClass . '_color">' . $sHead . '</h1>' .
+                    '<div class="alertbox_message ' . $sHeadClass . '_color">' . $sMessage . '</div>' .
+                '</div>';
+        }
+        return $sMessageBox;
     }
-    
-    /**
-     * Function displays small message box directly
-     * @param string $level - warning, error or info
-     * @param string $message - displayed messagestring
-     * @return void
-     */
-    function displayNotification($level, $message) {
-        echo $this->returnNotification($level,$message)."<br>"; 
-    } # end function
-    
-    /**
-     * Function displays large message box directly
-     * @param string $level - warning, error or info
-     * @param string $message - displayed messagestring
-     * @param boolean $style - use styles for display or not
-     * @return void
-     */
-    function displayMessageBox($level, $message, $style = 1) {
-        echo $this->messageBox($level, $message, $style)."<br>";  
-    } # end function
 
-} # end class
+
+    /**
+     * Generates message box and returns it back, uses markup with table.
+     *
+     * @param   string  $sLevel  Message level, one of Contenido_Notification::LEVEL_* constants
+     * @param   string  $sMessage  The message to display
+     * @return  string
+     */
+    public function returnNotification($sLevel, $sMessage)
+    {
+        switch ($sLevel) {
+            case self::LEVEL_ERROR:
+                $sBgColor = $this->_aColors['notify_error'];
+                $sImgPath = $this->_sPathImages . 'icon_fatalerror.gif';
+                break;
+            case self::LEVEL_WARNING:
+                $sBgColor = $this->_aColors['notify_warning'];
+                $sImgPath = $this->_sPathImages . 'icon_warning.gif';
+                break;
+            case self::LEVEL_INFO:
+                $sMessage = '<span style="color:#435d06">' . $sMessage . '</span>';
+                $sBgColor = $this->_aColors['notify_info'];
+                $sImgPath = $this->_sPathImages . 'but_ok.gif';
+                break;
+            default:
+                $sMessage = '<span style="color:#435d06">'.$sMessage.'</span>';
+                $sBgColor = $this->_aColors['notify'];
+                $sImgPath = $this->_sPathImages . 'but_ok.gif';
+                break;
+        }
+
+        $oTable = new Table($sBgColor, 'solid', 0, 2, '#FFFFFF', '#FFFFFF', '#FFFFFF', true, false);
+
+        $sNoti = '<div id="contenido_notification" style="position:relative;left:0;top:0;z-index:10;">';
+        $sNoti .= $oTable->start_table();
+        $sNoti .= $oTable->header_row();
+        $sNoti .= $oTable->borderless_cell('<img src="' . $sImgPath . '" alt="">');
+        $sNoti .= $oTable->borderless_cell(
+            '<font color="' . $sBgColor . '" style="font-family:Verdana,Arial,Helvetica,'
+          . 'Sans-Serif;font-size:11px;">' . $sMessage . '</font>', 'left', 'middle'
+        );
+        $sNoti .= $oTable->end_row();
+        $sNoti .= $oTable->end_table();
+        $sNoti .= '</div>';
+
+        return $sNoti;
+    }
+
+
+    /**
+     * Displays small message box directly.
+     *
+     * @param   string  $sLevel  Message level, one of Contenido_Notification::LEVEL_* constants
+     * @param   string  $sMessage  The message to display
+     * @return  void
+     */
+    public function displayNotification($sLevel, $sMessage)
+    {
+        echo $this->returnNotification($sLevel, $sMessage) . '<br>';
+    }
+
+
+    /**
+     * Displays large message box directly.
+     *
+     * @param   string  $sLevel  Message level, one of Contenido_Notification::LEVEL_* constants
+     * @param   string  $sMessage  The message to display
+     * @param   int     $iStyle   Flag tp use styles for display or not (feasible 1 or 0)
+     * @return  void
+     */
+    public function displayMessageBox($sLevel, $sMessage, $iStyle = 1)
+    {
+        echo $this->messageBox($sLevel, $sMessage, $iStyle) . '<br>';
+    }
+
+}
+
 ?>
