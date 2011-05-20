993ceb16810d3d985a6fa9bb597acfdcff2ec273
Murat Purc <murat@purc.de>
2011-05-20 08:23:59 +0000

    Bug fix: Wrong condition in Contenido_Url::parse(), see [#CON-399]

--- contenido/classes/Url/Contenido_Url.class.php
+++ contenido/classes/Url/Contenido_Url.class.php
@@ -27,6 +27,7 @@
  *   modified 2009-01-13, Murat Purc, added new function isIdentifiableFrontContentUrl() for better 
  *                                    identification of internal urls
  *   modified 2009-10-27, Murat Purc, fixed/modified CEC_Hook, see [#CON-256]
+ *   modified 2011-05-20, Murat Purc, fixed wrong condition in function parse(), see [#CON-399]
  *
  *   $Id$:
  * }}
@@ -184,7 +185,7 @@ final class Contenido_Url {
             $aUrl['query'] = str_replace('&amp;', '&', $aUrl['query']);
             parse_str($aUrl['query'], $aUrl['params']);
         }
-        if (!isset($aUrl['params']) && !is_array($aUrl['params'])) {
+        if (!isset($aUrl['params']) || !is_array($aUrl['params'])) {
             $aUrl['params'] = array();
         }
         return $aUrl;
