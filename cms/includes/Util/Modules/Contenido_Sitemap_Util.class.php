fada5aa08ef57804585301d75f53d4adada1f931
Murat Purc <murat@purc.de>
2011-09-07 22:35:07 +0000

    Adapted module outputs to provide generating of URL's usable for AMR-Plugin

--- cms/includes/Util/Modules/Contenido_Sitemap_Util.class.php
+++ cms/includes/Util/Modules/Contenido_Sitemap_Util.class.php
@@ -10,7 +10,7 @@
  * @con_php_req 5.0
  *
  *
- * @version    1.0.0
+ * @version    1.0.1
  * @author     Rudi Bieller
  * @copyright  four for business AG <www.4fb.de>
  *
@@ -36,38 +36,40 @@ class Contenido_Sitemap_Util {
      * @return void
      */
     public static function loopCats(Contenido_Category $oCategory, Contenido_FrontendNavigation $oFrontendNavigation, Template $oTpl, $sUrlStyle, array $aCfg, $iLang, array $aDepthInfo = array()) {
-    	$aDepthInfo[0] = isset($aDepthInfo[0]) ? $aDepthInfo[0] + 1 : 1;
-    	$aDepthInfo[1] = isset($aDepthInfo[1]) ? $aDepthInfo[1] : 1;
+        $aDepthInfo[0] = isset($aDepthInfo[0]) ? $aDepthInfo[0] + 1 : 1;
+        $aDepthInfo[1] = isset($aDepthInfo[1]) ? $aDepthInfo[1] : 1;
         // display current item
-    	$iItemLevel = $oFrontendNavigation->getLevel($oCategory->getIdCat());
-    	// this is just for sample client - modify to your needs!
-    	if ($aCfg['url_builder']['name'] == 'front_content') {
-    	    $aParams = array('lang' => $iLang, 'idcat' => $oCategory->getIdCat());
-    	} else {
-        	$aParams = array('a' => $oCategory->getIdCat(), 
-        					'idcat' => $oCategory->getIdCat(), // needed to build category path
-        					'lang' => $iLang, // needed to build category path
-        					'level' => 1); // needed to build category path
-    	}
-    	// fill template with values
-    	$oTpl->set('d', 'name', $oCategory->getCategoryLanguage()->getName());
-    	$oTpl->set('d', 'css_level', $iItemLevel);
-    	try {
-    	   $oTpl->set('d', 'url', Contenido_Url::getInstance()->build($aParams));
-    	} catch (InvalidArgumentException $e) {
-    	    $oTpl->set('d', 'url', '#');
-    	}
-    	$oTpl->next();
-    	// continue until max level depth
-    	if ($aDepthInfo[1] > $aDepthInfo[0]) {
-        	// check if current item has sub-items
-    		$oSubCategories = $oFrontendNavigation->getSubCategories($oCategory->getIdCat());
-        	if ($oSubCategories->count() > 0) {
-        		foreach ($oSubCategories as $oSubCategory) {
-        			self::loopCats($oSubCategory, $oFrontendNavigation, $oTpl, $sUrlStyle, $aCfg, $iLang, $aDepthInfo);
-        		}
-        	}
-    	}
+        $iItemLevel = $oFrontendNavigation->getLevel($oCategory->getIdCat());
+        // this is just for sample client - modify to your needs!
+        if ($aCfg['url_builder']['name'] == 'front_content' || $aCfg['url_builder']['name'] == 'MR') {
+            $aParams = array('lang' => $iLang, 'idcat' => $oCategory->getIdCat());
+        } else {
+            $aParams = array(
+                'a' => $oCategory->getIdCat(),
+                'idcat' => $oCategory->getIdCat(), // needed to build category path
+                'lang' => $iLang, // needed to build category path
+                'level' => 1, // needed to build category path
+            );
+        }
+        // fill template with values
+        $oTpl->set('d', 'name', $oCategory->getCategoryLanguage()->getName());
+        $oTpl->set('d', 'css_level', $iItemLevel);
+        try {
+           $oTpl->set('d', 'url', Contenido_Url::getInstance()->build($aParams));
+        } catch (InvalidArgumentException $e) {
+            $oTpl->set('d', 'url', '#');
+        }
+        $oTpl->next();
+        // continue until max level depth
+        if ($aDepthInfo[1] > $aDepthInfo[0]) {
+            // check if current item has sub-items
+            $oSubCategories = $oFrontendNavigation->getSubCategories($oCategory->getIdCat());
+            if ($oSubCategories->count() > 0) {
+                foreach ($oSubCategories as $oSubCategory) {
+                    self::loopCats($oSubCategory, $oFrontendNavigation, $oTpl, $sUrlStyle, $aCfg, $iLang, $aDepthInfo);
+                }
+            }
+        }
     }
 }
 ?>
