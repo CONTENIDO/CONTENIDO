7bf0156682582be382e873a793e36a3136a023c2
Murat Purc <murat@purc.de>
2011-11-10 00:09:53 +0000

    Added caching feature to cApiSystemPropertyCollection, which results in approx. 20 fewer database queries in example clients frontend.

--- contenido/classes/contenido/class.system.property.php
+++ contenido/classes/contenido/class.system.property.php
@@ -12,12 +12,22 @@
  * - name          (string)
  * - value         (string)
  *
+ * If caching is enabled, see $cfg['properties']['system_prop']['enable_cache'],
+ * all entries will be loaded at first time. Caching is used only at frontend,
+ * backend will still work without caching.
+ * If enabled, each call of cApiSystemPropertyCollection functions to retrieve properties
+ * will return the cached entries without stressing the database.  
+ *
+ * The cApiSystemPropertyCollection class keeps also track of changed and deleted
+ * properties and synchronizes them with cached values, as long as you use the 
+ * interface of cApiSystemPropertyCollection to manage the properties.
+ *
  * Requirements:
  * @con_php_req 5.0
  *
  *
  * @package    CONTENIDO Backend classes
- * @version    0.1
+ * @version    0.2
  * @author     Murat Purc <murat@purc.de>
  * @copyright  four for business AG <www.4fb.de>
  * @license    http://www.contenido.org/license/LIZENZ.txt
@@ -26,6 +36,7 @@
  *
  * {@internal
  *   created  2011-11-03
+ *   created  2011-11-10, Murat Purc, added caching feature
  *
  *   $Id$:
  * }}
@@ -39,16 +50,43 @@ if (!defined('CON_FRAMEWORK')) {
 
 class cApiSystemPropertyCollection extends ItemCollection
 {
+    protected static $_entries;
+    protected static $_enableCache;
+
     /**
      * Constructor
      */
     public function __construct()
     {
-        global $cfg;
+        global $cfg, $contenido;
         parent::__construct($cfg['tab']['system_prop'], 'idsystemprop');
         $this->_setItemClass('cApiSystemProperty');
+
+        if (!isset(self::$_enableCache)) {
+            if (isset($contenido)) {
+                self::$_enableCache = false;
+            } elseif (isset($cfg['properties']) && isset($cfg['properties']['system_prop']) 
+                && isset($cfg['properties']['system_prop']['enable_cache']))
+            {
+                self::$_enableCache = (bool) $cfg['properties']['system_prop']['enable_cache'];
+            } else {
+                self::$_enableCache = false;
+            }
+        }
+
+        if (self::$_enableCache && !isset(self::$_entries)) {
+            $this->_loadFromCache();
+        }
     }
 
+    /**
+     * Resets the states of static properties.
+     */
+    public static function reset()
+    {
+        unset(self::$_enableCache, self::$_entries);
+    }
+    
     /**
      * Updatess a existing system property entry or creates it.
      * @param  string  $type
@@ -64,6 +102,10 @@ class cApiSystemPropertyCollection extends ItemCollection
             $item->store();
         } else {
             $item = $this->create($type, $name, $value, $idcatlang);
+        }
+
+        if (self::$_enableCache) {
+            return $this->_addToCache($item);
         }
 
         return $item;
@@ -85,6 +127,10 @@ class cApiSystemPropertyCollection extends ItemCollection
         $item->set('value', $this->escape($value));
         $item->store();
 
+        if (self::$_enableCache) {
+            return $this->_addToCache($item);
+        }
+
         return $item;
     }
 
@@ -95,6 +141,11 @@ class cApiSystemPropertyCollection extends ItemCollection
      */
     public function fetchAll($orderBy = '')
     {
+        if (self::$_enableCache) {
+            // no order for cached results
+            return $this->_fetchAllFromCache();
+        }
+
         $this->select('', '', $this->escape($orderBy));
         $props = array();
         while ($property = $this->next()) {
@@ -111,6 +162,10 @@ class cApiSystemPropertyCollection extends ItemCollection
      */
     public function fetchByTypeName($type, $name)
     {
+        if (self::$_enableCache) {
+            return $this->_fetchByTypeNameFromCache($type, $name);
+        }
+
         $this->select("type='" . $this->escape($type) . "' AND name='" . $this->escape($name) . "'");
         if ($property = $this->next()) {
             return $property;
@@ -125,6 +180,10 @@ class cApiSystemPropertyCollection extends ItemCollection
      */
     public function fetchByType($type)
     {
+        if (self::$_enableCache) {
+            return $this->_fetchByTypeFromCache($type);
+        }
+
         $this->select("type='" . $this->escape($type) . "'");
         $props = array();
         while ($property = $this->next()) {
@@ -164,10 +223,100 @@ class cApiSystemPropertyCollection extends ItemCollection
     {
         $result = false;
         while ($system = $this->next()) {
-            $result = $this->delete($system->get('idsystemprop'));
+            $id = $system->get('idsystemprop');
+            if (self::$_enableCache) {
+                $this->_deleteFromCache();
+            }
+            $result = $this->delete($id);
         }
         return $result;
     }
+
+    /**
+     * Loads/Caches all system properties.
+     */
+    protected function _loadFromCache()
+    {
+        self::$_entries = array();
+        $this->select();
+        while ($property = $this->next()) {
+            $data = $property->toArray();
+            self::$_entries[$data['idsystemprop']] = $data;
+        }
+    }
+
+    /**
+     * Adds a entry to the cache.
+     * @param  cApiSystemProperty  $entry
+     */
+    protected function _addToCache($entry)
+    {
+        $data = $entry->toArray();
+        self::$_entries[$data['idsystemprop']] = $data;
+    }
+
+    /**
+     * Fetches all entries from cache.
+     * @return  cApiSystemProperty[]
+     */
+    protected function _fetchAllFromCache()
+    {
+        $props = array();
+        $obj = new cApiSystemProperty();
+        foreach (self::$_entries as $entry) {
+            $obj->loadByRecordSet($entry);
+            $props[] = clone $obj;
+        }
+        return $props;
+    }
+
+    /**
+     * Fetches entry by type and name from cache.
+     * @param   string  $type
+     * @param   string  $name
+     * @return  cApiSystemProperty|null
+     */
+    protected function _fetchByTypeNameFromCache($type, $name)
+    {
+        $obj = new cApiSystemProperty();
+        foreach (self::$_entries as $entry) {
+            if ($entry['type'] == $type && $entry['name'] == $name) {
+                $obj->loadByRecordSet($entry);
+                return $obj;
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Fetches entries by type from cache.
+     * @param   string  $type
+     * @return  cApiSystemProperty[]
+     */
+    protected function _fetchByTypeFromCache($type)
+    {
+        $props = array();
+        $obj = new cApiSystemProperty();
+        foreach (self::$_entries as $entry) {
+            if ($entry['type'] == $type) {
+                $obj->loadByRecordSet($entry);
+                $props[] = clone $obj;
+            }
+        }
+        return $props;
+    }
+
+    /**
+     * Removes a entry from cache.
+     * @param   int  $id
+     */
+    protected function _deleteFromCache($id)
+    {
+        if (isset(self::$_entries[$id])) {
+            unset(self::$_entries[$id]);
+        }
+    }
+
 }
 
 
