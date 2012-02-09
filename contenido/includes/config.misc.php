ca4f45139f8c7eed3ed23552a411e0204594d8ac
Dominik Ziegler <dominik.ziegler@4fb.de>
2012-02-09 16:17:59 +0000

    changed default configuration to log errors (according to error reporting) and removed not needed error log ini setting

--- contenido/includes/config.misc.php
+++ contenido/includes/config.misc.php
@@ -124,10 +124,7 @@ $cfg['AvailableCharsets'] = array(
 //@ini_set('display_errors', true);
 
 /* Log errors to a file */
-//@ini_set('log_errors', true);
-
-/* The file in which we write the error log */
-//@ini_set('error_log', $cfg['path']['contenido'] . 'logs/errorlog.txt');
+@ini_set('log_errors', true);
 
 /* Report all errors except warnings */
 error_reporting(E_ALL ^E_NOTICE);
