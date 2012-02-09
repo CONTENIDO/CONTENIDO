a15d408e2d16d0e6c5e6385472aca2813785d4c6
Dominik Ziegler <dominik.ziegler@4fb.de>
2012-02-09 20:43:32 +0000

    removed whitespaces and fixed deprecation class name

--- contenido/classes/class.cat.php
+++ contenido/classes/class.cat.php
@@ -1,80 +1,47 @@
 <?php
 /**
-
  * Project: 
-
  * CONTENIDO Content Management System
-
  * 
-
  * Description: 
-
  * Class for category information and management
-
  * 
-
  * Requirements: 
-
  * @con_php_req 5.0
-
  * 
-
  *
-
  * @package    CONTENIDO Backend Classes
-
  * @version    1.0.0
-
  * @author     Timo A. Hummel
-
  * @copyright  four for business AG <www.4fb.de>
-
  * @license    http://www.contenido.org/license/LIZENZ.txt
-
  * @link       http://www.4fb.de
-
  * @link       http://www.contenido.org
-
  * @since      file available since CONTENIDO release <= 4.6
-
- * @deprecated [2011-11-15] Class Cat is nowhere used in CONTENIDO core. If Cat is still used in your modules/plugins, please switch to cApiCategory.
+ * @deprecated [2011-11-15] Class Cat is nowhere used in CONTENIDO core. If Cat is still used in your modules/plugins, please switch to cApiCategoryLanguage.
  * 
-
  * {@internal 
-
  *   created unknown
-
  *   modified 2008-06-30, Dominik Ziegler, add security fix
-
  *
-
  *   $Id$:
-
  * }}
-
  * 
-
  */
 
-
-
 if(!defined('CON_FRAMEWORK')) {
-
 	die('Illegal call');
-
 }
 
-
 class Cat {
-
     function Cat() {
-        cDeprecated("Use class cApiCategory instead.");
+       cDeprecated("Use class cApiCategoryLanguage instead.");
     }
 
     function getCatName($category, $idlang) {
         global $cfg;
 
-        cDeprecated("Use class cApiCategory instead.");
+        cDeprecated("Use class cApiCategoryLanguage instead.");
 
         $db = new DB_Contenido;
 
@@ -94,7 +61,6 @@ class Cat {
         $db->next_record();
 
         return ($db->f("name"));
-
     }
 }
 ?>
