0e368787d97c9319f526bf165c025237f316ae1b
Dominik Ziegler <dominik.ziegler@4fb.de>
2012-02-26 17:58:48 +0000

    reworked system variables overview with new design and new structured informations, deprecated writeSystemValues

--- contenido/includes/functions.system.php
+++ contenido/includes/functions.system.php
@@ -40,38 +40,25 @@ if(!defined('CON_FRAMEWORK')) {
 
 
 /**
- * emptyLogFile - clears errorlog.txt
- *
- * clears CONTENIDO standard errorlog.txt
- * 
+ * Clears CONTENIDO standard errorlog.txt
  * @return string returns message if clearing was successfull or not		
- * @author Marco Jahn
  */
-function emptyLogFile()
-{
+function emptyLogFile() {
 	global $cfg, $notification;
-	if ($_GET['log'] == 1)
-	{ // clear errorlog.txt
-		if (file_exists($cfg['path']['contenido']."logs/errorlog.txt") && is_writeable($cfg['path']['contenido']."logs/errorlog.txt"))
-		{
-			$errorLogHandle = fopen($cfg['path']['contenido']."logs/errorlog.txt", "wb+");
-			fclose($errorLogHandle);
-			$tmp_notification = $notification->returnNotification("info", i18n("Error log successfully cleared!"));
-		} else if (file_exists($cfg['path']['contenido']."logs/errorlog.txt") && !is_writeable($cfg['path']['contenido']."logs/errorlog.txt")) {
-		    $tmp_notification = $notification->returnNotification("error", i18n("Can't clear error log : Access is denied!"));
-		}
+	
+	$tmp_notification = false;
+	
+	// clear errorlog.txt
+	$filename = $cfg['path']['contenido'] . "logs/errorlog.txt";
+	
+	if (file_exists($filename) && is_writeable($filename)) {
+		$errorLogHandle = fopen($filename, "wb+");
+		fclose($errorLogHandle);
+		$tmp_notification = $notification->returnNotification("info", i18n("Error log successfully cleared!"));
+	} else if (file_exists($filename) && !is_writeable($filename)) {
+		$tmp_notification = $notification->returnNotification("error", i18n("Can't clear error log : Access is denied!"));
 	}
-	elseif ($_GET['log'] == 2)
-	{
-		if (file_exists($cfg['path']['contenido']."logs/install.log.txt") && is_writeable($cfg['path']['contenido']."logs/install.log.txt"))
-		{ // clear install.log.txt
-			$errorLogHandle = fopen($cfg['path']['contenido']."logs/install.log.txt", "wb+");
-			fclose($errorLogHandle);
-			$tmp_notification = $notification->returnNotification("info", i18n("install error log successfully cleared"));
-		} else if (file_exists($cfg['path']['contenido']."logs/install.log.txt") && !is_writeable($cfg['path']['contenido']."logs/install.log.txt")) {
-		    $tmp_notification = $notification->returnNotification("error", i18n("Can't clear install error log : Access is denied!"));
-		}
-	}
+	
 	return $tmp_notification;
 }
 
@@ -250,274 +237,13 @@ function compareUrlStrings($arrConUrl, $arrBrowserUrl, $isIP = false)
  * writeSystemValuesOutput - get several server and CONTENIDO settings
  *
  * parse system and CONTENIDO output into a string
+ *
+ * @deprecated 2012-02-26 Moved directly to include.system_sysvalues.php
  * 
- * @return string returns a string containing several server and CONTENIDO settings		
- * @author Marco Jahn
+ * @return void
  */
 function writeSystemValuesOutput($usage)
 {
-
-	global $db, $_SERVER, $cfg, $i18n, $tpl, $cfgClient;
-
-	/* variables to proof against each other*/
-
-	$contenidoFullHtml = $cfg['path']['contenido_fullhtml'];
-	$browserPath  = $_SERVER['SERVER_PORT'] == '443' ? 'https' : 'http'; 
-    $browserPath .= "://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
-	$browserPath = substr($browserPath, 0, strrpos($browserPath, "/") + 1);
-
-	$status = checkPathInformation($contenidoFullHtml, $browserPath);
-
-	if ($status == 1)
-	{ // green
-		$contenidoFullHtml = "<span style=\"color:green;\">".$contenidoFullHtml."</span><br>";
-		$browserPath = "<span style=\"color:green;\">".$browserPath."</span>";
-
-	}
-	elseif ($status == 2)
-	{ // red
-		$contenidoFullHtml = "<span style=\"color:red;\">".$contenidoFullHtml."</span><br>";
-		$browserPath = "<span style=\"color:red;\">".$browserPath."</span>";
-
-	}
-	elseif ($status == 3)
-	{ //orange
-		$contenidoFullHtml = "<span style=\"color:orange;\">".$contenidoFullHtml."</span><br>";
-		$browserPath = "<span style=\"color:orange;\">".$browserPath."</span>";
-
-	}
-
-	/* generate sysvalue output */
-	$i = 0; // array start value
-	// current Contenido version
-	$sysvalues[$i]['variable'] = i18n('CONTENIDO version');
-	$sysvalues[$i ++]['value'] = $cfg['version'];
-	// paths from config.php
-	$sysvalues[$i]['variable'] = i18n('CONTENIDO path');
-	$sysvalues[$i ++]['value'] = $cfg['path']['contenido'];
-	$sysvalues[$i]['variable'] = i18n('CONTENIDO HTML path');
-	$sysvalues[$i ++]['value'] = $cfg['path']['contenido_html'];
-	$sysvalues[$i]['variable'] = i18n('CONTENIDO full HTML path');
-	$sysvalues[$i ++]['value'] = $contenidoFullHtml;
-	$sysvalues[$i]['variable'] = i18n('CONTENIDO frontend path');
-	$sysvalues[$i ++]['value'] = $cfg['path']['frontend'];
-	$sysvalues[$i]['variable'] = i18n('CONTENIDO PHPLIB path');
-	$sysvalues[$i ++]['value'] = $cfg['path']['phplib'];
-	$sysvalues[$i]['variable'] = i18n('CONTENIDO wysiwyg path');
-	$sysvalues[$i ++]['value'] = $cfg['path']['wysiwyg'];
-	$sysvalues[$i]['variable'] = i18n('CONTENIDO wysiwyg HTML path');
-	$sysvalues[$i ++]['value'] = $cfg['path']['wysiwyg_html'];
-	// host name
-	$sysvalues[$i]['variable'] = i18n('Host name');
-	$sysvalues[$i ++]['value'] = $_SERVER['HTTP_HOST'];
-	// CONTENIDO browser path 
-	$sysvalues[$i]['variable'] = i18n('Browser path');
-	/* cut of file information */
-	$sysvalues[$i ++]['value'] = $browserPath;
-	// get number of clients
-	$sql = "SELECT count(name) clientcount FROM ".$cfg["tab"]["clients"];
-	$db->query($sql);
-	$db->next_record();
-	$clientcount = $db->f("clientcount");
-
-	// get all clients and their language
-	$sql = "SELECT count(a.name) clientcount,
-	    		a.name clientname,
-	    		a.idclient
-	    		FROM
-	    		".$cfg["tab"]["clients"]." a
-	    		GROUP BY a.name";
-	$db->query($sql);
-
-	// create 'value' output
-	$db2 = new DB_Contenido;
-	$clientInformation = "<table width=\"100%\" cellspacing=\"0\" cellpadding=\"2\" border=\"0\">
-	        <tr class=\"textw_medium\" style=\"background-color: #E2E2E2\">
-					<td width=\"20%\" class=\"textg_medium\" style=\"border:1px; border-color:#B3B3B3; border-style:solid;; border-bottom: none;\" nowrap=\"nowrap\">".i18n("client settings")."</td>
-	            <td class=\"textg_medium\" style=\"border:1px; border-left:0px; border-color: #B3B3B3; border-style: solid;border-bottom: none;\" nowrap=\"nowrap\">".i18n("values")."</td>
-	        </tr>";
-
-	$clientPermCount = 0;
-	while ($db->next_record())
-	{
-		if (system_have_perm($db->f("idclient")))
-		{
-			$clientlang = "";
-
-			// get client name
-			$clientName = urldecode($db->f("clientname"));
-			$clientInformation .= "<tr class=\"text_medium\" style=\"background-color: {BGCOLOR};\" >
-			                	<td colspan=\"2\" class=\"text_medium\" style=\"border:1px; border-top:0px; border-color: #B3B3B3; border-style: solid\" nowrap=\"nowrap\" align=\"left\" valign=\"top\"><i>$clientName</i></td>
-			                </tr>";
-			$clientlang = "";
-			// select languages belong to a client
-			$sql = "SELECT c.name clientlang
-			        	FROM ".$cfg["tab"]["clients"]." a
-			        	LEFT JOIN ".$cfg["tab"]["clients_lang"]." b ON a.idclient = b.idclient
-			        	LEFT JOIN ".$cfg["tab"]["lang"]." c ON b.idlang = c.idlang
-			        	WHERE a.idclient=".Contenido_Security::toInteger($db->f("idclient"))." AND c.name IS NOT NULL";
-			$db2->query($sql);
-			while ($db2->next_record())
-			{
-				$clientlang .= $db2->f("clientlang").", ";
-			}
-			// cut off last ","
-			$clientlang = substr($clientlang, 0, strlen($clientlang) - 2);
-
-			$clientInformation .= "<tr class=\"text_medium\" style=\"background-color: {BGCOLOR};\" >
-			                	<td class=\"text_medium\" style=\"border:1px; border-top:0px; border-color: #B3B3B3; border-style: solid; \" nowrap=\"nowrap\" align=\"left\" valign=\"top\">".i18n("language(s)")."</td>
-			                	<td class=\"text_medium\" width=\"60%\" style=\"border:1px; border-left:0px; border-top:0px; border-color: #B3B3B3; border-style: solid; \" nowrap=\"nowrap\">$clientlang&nbsp;</td>
-			           		</tr>";
-							
-			$htmlpath = $cfgClient[$db->f('idclient')]['path']['htmlpath'];
-			$frontendpath = $cfgClient[$db->f('idclient')]['path']['frontend'];
-		
-		
-			$clientInformation .= "<tr class=\"text_medium\" style=\"background-color: {BGCOLOR};\" >
-								<td class=\"text_medium\" style=\"border:1px; border-top:0px; border-color: #B3B3B3; border-style: solid\" nowrap=\"nowrap\" align=\"left\" valign=\"top\">".i18n("htmlpath")."</td>
-								<td class=\"text_medium\" width=\"60%\" style=\"border:1px; border-left:0px; border-top:0px; border-color: #B3B3B3; border-style: solid;\" nowrap=\"nowrap\">".$htmlpath."&nbsp;</td>
-							</tr>";
-			$clientInformation .= "<tr class=\"text_medium\" style=\"background-color: {BGCOLOR};\" >
-								<td class=\"text_medium\" style=\"border:1px; border-top:0px; border-color: #B3B3B3; border-style: solid\" nowrap=\"nowrap\" align=\"left\" valign=\"top\">".i18n("frontendpath")."</td>
-								<td class=\"text_medium\" width=\"60%\" style=\"border:1px; border-left:0px; border-top:0px; border-color: #B3B3B3; border-style: solid;\" nowrap=\"nowrap\">".$frontendpath."&nbsp;</td>
-							</tr>";
-
-			$clientPermCount ++;
-		}
-
-	}
-
-	if ($clientPermCount == 0)
-	{
-		$clientInformation .= "<tr class=\"text_medium\" style=\"background-color: {BGCOLOR};\" >
-		                	<td colspan=\"2\" class=\"text_medium\" style=\"border:1px; border-top:0px; border-color: #B3B3B3; border-style: solid; \" nowrap=\"nowrap\" align=\"left\" valign=\"top\">".i18n("No permissions!")."</td>
-		           		</tr>";
-	}
-
-	$clientInformation .= '</table>';
-
-	$clientdata = i18n('Number of installed clients: ').$clientcount."<br>".$clientInformation;
-
-	// client quantity and their assigned language and are they online 
-	$sysvalues[$i]['variable'] = i18n('Client informations');
-	$sysvalues[$i ++]['value'] = "$clientdata";
-	// get number of users installed
-	$sql = "SELECT count(user_id) usercount FROM ".$cfg["tab"]["phplib_auth_user_md5"];
-	$db->query($sql);
-	$db->next_record();
-	// number of users
-	$sysvalues[$i]['variable'] = i18n('Number of users');
-	$sysvalues[$i ++]['value'] = $db->f("usercount");
-	//get number of articles
-	$sql = "SELECT count(idart) articlecount FROM ".$cfg["tab"]["art"];
-	$db->query($sql);
-	$db->next_record();
-	// number of articles
-	$sysvalues[$i]['variable'] = i18n('Number of articles');
-	$sysvalues[$i ++]['value'] = $db->f("articlecount");
-	// server operating system
-	$sysvalues[$i]['variable'] = i18n('Server operating system');
-	$sysvalues[$i ++]['value'] = $_SERVER['SERVER_SOFTWARE'];
-	// SQL version
-	$sql_server_info = $db->server_info();
-	$sysvalues[$i]['variable'] = i18n('PHP database extension');
-	$sysvalues[$i ++]['value'] = $cfg["database_extension"];
-	$sysvalues[$i]['variable'] = i18n('Database server version');
-	$sysvalues[$i ++]['value'] = $sql_server_info['description'];
-	// php version
-	$sysvalues[$i]['variable'] = i18n('Installed PHP version');
-	$sysvalues[$i ++]['value'] = phpversion();
-	// php config values
-	// config values
-	// php safe_mode
-	 (ini_get('safe_mode') == 1) ? $safe_mode = "<span stlye=\"color:red;\">".i18n('activated')."</span>" : $safe_mode = "<span style=\"color:green;\">".i18n('deactivated')."</span>";
-	$sysvalues[$i]['variable'] = "safe_mode";
-	$sysvalues[$i ++]['value'] = $safe_mode;
-	// magig quotes GPC
-	 (ini_get('magic_quotes_gpc') == 1) ? $magic_quotes_gpc = i18n('activated') : $magic_quotes_gpc = i18n('deactivated');
-	$sysvalues[$i]['variable'] = "magic_quotes_gpc";
-	$sysvalues[$i ++]['value'] = $magic_quotes_gpc;
-	// magic quotes runtime
-	 (ini_get('magic_quotes_runtime') == 1) ? $magic_quotes_runtime = i18n('activated') : $magic_quotes_runtime = i18n('deactivated');
-	$sysvalues[$i]['variable'] = "magic_quotes_runtime";
-	$sysvalues[$i ++]['value'] = $magic_quotes_runtime;
-	// GPC order
-	$sysvalues[$i]['variable'] = "gpc_order";
-	$sysvalues[$i ++]['value'] = ini_get('gpc_order');
-	// memory limit
-	$sysvalues[$i]['variable'] = "memory_limit";
-	$sysvalues[$i ++]['value'] = ini_get('memory_limit');
-	// max execution time
-	$sysvalues[$i]['variable'] = "max_execution_time";
-	$sysvalues[$i ++]['value'] = ini_get('max_execution_time');
-	// disabled functions
-	 (strlen(ini_get('disable_functions')) > 0) ? $disable_functions = "<span style=\"color:red;\">".ini_get('disable_functions')."</span>" : $disable_functions = "<span style=\"color:green\">".i18n('nothing disabled')."</span>";
-	$sysvalues[$i]['variable'] = i18n('Disabled functions');
-	$sysvalues[$i ++]['value'] = $disable_functions;
-	// gettext loaded
-	 (extension_loaded('gettext') == true) ? $gettext = "<span style=\"color:green;\">".i18n('loaded')."</span>" : $gettext = "<span stlye=\"color:red;\">".i18n('not loaded')."</span>";
-	$sysvalues[$i]['variable'] = i18n('Gettext extension');
-	$sysvalues[$i ++]['value'] = $gettext;
-	// sql.safe_mode
-	 (ini_get('sql.safe_mode') == 1) ? $sql_safe_mode = "<span style=\"color:red;\">".i18n('activated')."</span>" : $sql_safe_mode = "<span style=\"color:green;\">".i18n('deactivated')."</span>";
-	$sysvalues[$i]['variable'] = "sql.safe_mode";
-	$sysvalues[$i ++]['value'] = $sql_safe_mode;
-	// gdlib with installed features
-	$gdLib = array();
-	$gdLib = getPhpModuleInfo($moduleName = 'gd');
-	$gdLibFeatures = "<table width=\"100%\" cellspacing=\"0\" cellpadding=\"2\" border=\"0\">
-	        <tr class=\"textg_medium\" style=\"background-color: #E2E2E2\">
-	            <td width=\"20%\" class=\"textg_medium\" style=\"border:1px; border-color:#B3B3B3; border-style:solid;border-bottom:none\" nowrap=\"nowrap\">".i18n("Settings")."</td>
-	            <td class=\"textg_medium\" style=\"border:1px; border-left:0px; border-color: #B3B3B3; border-style: solid; border-bottom:none\" nowrap=\"nowrap\">".i18n("Values")."</td>
-	        </tr>";
-
-	foreach ($sysvalues as $key => $value) {
-		if (trim ($value['value']) == '') {
-			$sysvalues[$key]['value'] = '&nbsp;'; 
-		}
-	}
-
-	foreach ($gdLib as $setting => $value)
-	{
-		$gdLibFeatures .= "<tr class=\"text_medium\" style=\"background-color: {BGCOLOR};\" >
-		            <td class=\"text_medium\" style=\"border:1px; border-top:0px; border-color: #B3B3B3; border-style: solid;\" nowrap=\"nowrap\" align=\"left\" valign=\"top\">".$setting."</td>
-		            <td class=\"text_medium\" width=\"60%\" style=\"border:1px; border-left:0px; border-top:0px; border-color: #B3B3B3; border-style: solid;\" nowrap=\"nowrap\">".$value[0]."</td>
-		        </tr>";
-	}
-	$gdLibFeatures .= '</table>';
-	$sysvalues[$i]['variable'] = i18n('GD library');
-	$sysvalues[$i ++]['value'] = $gdLibFeatures;
-
-	// include path settings
-	$sysvalues[$i]['variable'] = "include_path";
-	$sysvalues[$i ++]['value'] = ini_get('include_path');
-
-$iRowId = 1;
-$sRowBgColor2 = $sRowBgColor1 = "#fff";
-//loop array for every parameter
-	foreach ($sysvalues AS $sysvalue)
-	{
-		$tpl->set('d', 'VARIABLE', $sysvalue['variable']);
-		$tpl->set('d', 'LOCALVALUE', $sysvalue['value']);
-  $tpl->set('d', 'ROWID', 'sysrow_'.$iRowId);
-  if($iRowId % 2) {
-      $tpl->set('d', 'BGCOLOR', $sRowBgColor1);
-  } else {
-      $tpl->set('d', 'BGCOLOR', $sRowBgColor2);
-  }
-		$tpl->next();
-  $iRowId++;
-	}
-
-	/* irgendwas sinnvolles :) */
-	if ($usage == 'mail')
-	{
-		return $tpl->generate($cfg['path']['templates'].$cfg['templates']['systam_variables_mailattach'], true);
-	}
-	elseif ($usage == 'output')
-	{
-		// do nothing
-	}
-
+	cDeprecated("This function is not longer supported.");
 }
 ?>
--- contenido/includes/include.system_sysvalues.php
+++ contenido/includes/include.system_sysvalues.php
@@ -38,55 +38,230 @@ $tpl->reset();
 
 /*
  * print out tmp_notifications if any action has been done
-*/
-if (isset($tmp_notification))
-{
+ */
+if (isset($tmp_notification)) {
 	$tpl->set('s', 'TEMPNOTIFICATION', $tmp_notification);
-}
-else
-{
+} else {
 	$tpl->set('s', 'TEMPNOTIFICATION', '');	
 }
 
-/* get system variables for output */
-writeSystemValuesOutput($usage='output');
-
 // error log
-if (file_exists($cfg['path']['contenido']."logs/errorlog.txt"))
-{
+if (file_exists($cfg['path']['contenido']."logs/errorlog.txt")) {
     $errorLogHandle = fopen ($cfg['path']['contenido']."logs/errorlog.txt", "rb");
     $txtAreaHeight = "200";
     
     /* If the file is larger than 16KB, seek to the file's length - 16KB) */
-    fseek($errorLogHandle, -16384,SEEK_END);
+    fseek($errorLogHandle, -16384, SEEK_END);
     
-    while (!feof($errorLogHandle))
-    {
+    while (!feof($errorLogHandle)) {
         $errorLogBuffer .= fgets($errorLogHandle, 16384);
     }
-    fclose ($errorLogHandle);
-    if (strlen ($errorLogBuffer) == 0)
-    {
+    fclose($errorLogHandle);
+    if (strlen($errorLogBuffer) == 0) {
     	$errorLogBuffer = i18n("No error log entries found");
     	$txtAreaHeight = "20";	
-    }
-    
-}
-else
-{
+    } 
+} else {
 	$errorLogBuffer = i18n("No error log file found");
 	$txtAreaHeight = "20";	
 }
+
 $tpl->set('s', 'TXTERRORLOGSIZE', $txtAreaHeight);
 $tpl->set('s', 'ERRORLOG', $errorLogBuffer);
+$tpl->set('s', 'LOGEMPTYURL', htmlentities($sess->url("main.php?area=$area&frame=$frame&action=emptyLog")));
 
-/*
- * parameter which log shoult be cleared
- * log = 1	clear /contenido/logs/errorlog.txt
-*/
-$tpl->set('s', 'LOGEMPTYURL', htmlentities($sess->url("main.php?area=$area&frame=$frame&action=emptyLog&log=1")));
+// server configuration
+$aChecks = array(
+	1 => $cfg['path']['frontend'],
+	2 => $cfg['path']['contenido'],
+	3 => $cfg['path']['all_wysiwyg'],
+	4 => $cfg['path']['contenido_fullhtml'],
+	5 => $cfg['path']['all_wysiwyg_html']
+);
+
+$aServerConfiguration = array(
+	array(i18n('System environment'), CONTENIDO_ENVIRONMENT),
+	array(i18n('Host name'), $_SERVER['HTTP_HOST'], 0),
+	array(i18n('CONTENIDO server path'), $cfg['path']['frontend'], 0),
+	array(i18n('CONTENIDO PEAR path'), $cfg['path']['pear'], 1),
+	array(i18n('CONTENIDO PHPLIB path'), $cfg['path']['phplib'], 1),
+	array(i18n('CONTENIDO backend path'), $cfg['path']['contenido'], 1),
+	array(i18n('CONTENIDO WYSIWYG repository path'), $cfg['path']['all_wysiwyg'], 2),
+	array(i18n('CONTENIDO WYSIWYG editor path'), $cfg['path']['wysiwyg'], 3),
+	array(i18n('CONTENIDO backend URL'), $cfg['path']['contenido_fullhtml'], 0),
+	array(i18n('CONTENIDO WYSIWYG repository URL'), $cfg['path']['all_wysiwyg_html'], 4),
+	array(i18n('CONTENIDO WYSIWYG editor URL'), $cfg['path']['wysiwyg_html'], 5),
+);
+
+$oTpl2 = new Template();
+$oTpl2->set('s', 'HEADLINE', i18n('System configuration'));
+foreach ($aServerConfiguration as $aConfData) {
+	$sValue = $aConfData[1];
+
+	if (isset($aChecks[$aConfData[2]])) {
+		$sValue = str_replace(
+			$aChecks[$aConfData[2]],
+			'<span style="color:grey">' . $aChecks[$aConfData[2]] . '</span>',
+			$sValue
+		);
+	}
+	
+	$oTpl2->set('d', 'NAME', $aConfData[0]);
+	$oTpl2->set('d', 'VALUE', $sValue);
+	$oTpl2->next();
+}
+
+$oTpl2->set('s', 'ADDITIONAL', '');
+
+$sServerConfiguration = $oTpl2->generate($cfg['path']['templates'] . $cfg['templates']['systam_variables_block'], 1);
+$tpl->set('s', 'SERVER_CONFIGURATION', $sServerConfiguration);
+
+// system statistics
+$aSystemStatistics = array(
+	array(i18n('Number of clients'), 'cApiClientCollection'),
+	array(i18n('Number of languages'), 'cApiLanguageCollection'),
+	array(i18n('Number of layouts'), 'cApiLayoutCollection'),
+	array(i18n('Number of modules'), 'cApiModuleCollection'),
+	array(i18n('Number of templates'), 'cApiTemplateCollection'),
+	array(i18n('Number of articles'), 'cApiArticleCollection'),
+	array(i18n('Number of categories'), 'cApiCategoryCollection'),
+	array(i18n('Number of users'), 'cApiUserCollection'),
+	array(i18n('Number of groups'), 'cApiGroupCollection'),
+);
+
+$oTpl2 = new Template();
+$oTpl2->set('s', 'HEADLINE', i18n('System statistics (all clients)'));
+foreach ($aSystemStatistics as $aStatData) {
+	$sCollName = $aStatData[1];
+	$oColl = new $sCollName();
+	$oColl->select();
+
+	$oTpl2->set('d', 'NAME', $aStatData[0]);
+	$oTpl2->set('d', 'VALUE', $oColl->count());
+	$oTpl2->next();
+}
+
+$oTpl2->set('s', 'ADDITIONAL', '');
+
+$sSystemStatistics = $oTpl2->generate($cfg['path']['templates'] . $cfg['templates']['systam_variables_block'], 1);
+$tpl->set('s', 'SYSTEM_STATISTICS', $sSystemStatistics);
+
+// installed versions
+$sql_server_info = $db->server_info();
+
+$aInstalledVersions = array(
+	array(i18n('CONTENIDO version'), $cfg['version']),
+	array(i18n('Server operating system'), $_SERVER['SERVER_SOFTWARE']),
+	array(i18n('Installed PHP version'), phpversion()),
+	array(i18n('Database server version'), $sql_server_info['description']),
+	array(i18n('PHP database extension'), $cfg['database_extension'])
+);
+
+$oTpl2 = new Template();
+$oTpl2->set('s', 'HEADLINE', i18n('Installed versions'));
+foreach ($aInstalledVersions as $aVersionInfo) {
+	$oTpl2->set('d', 'NAME', $aVersionInfo[0]);
+	$oTpl2->set('d', 'VALUE', $aVersionInfo[1]);
+	$oTpl2->next();
+}
+
+$oTpl2->set('s', 'ADDITIONAL', '');
+
+$sInstalledVersions = $oTpl2->generate($cfg['path']['templates'] . $cfg['templates']['systam_variables_block'], 1);
+$tpl->set('s', 'INSTALLED_VERSIONS', $sInstalledVersions);
+
+// php configuration
+$aPhpConfiguration = array(
+	'date.timezone', 'include_path', 'memory_limit', 'upload_max_filesize', 'post_max_size',
+	'max_execution_time', 'max_file_uploads', 'max_input_time',  'sql.safe_mode', 'disable_classes', 'disable_functions'
+);
+
+$oTpl2 = new Template();
+$oTpl2->set('s', 'HEADLINE', i18n('PHP configuration'));
+foreach ($aPhpConfiguration as $sConfigName) {
+	$sValue = ini_get($sConfigName);
+	
+	if ($sConfigName == 'disable_classes' || $sConfigName == 'disable_functions') {
+		if ($sValue == '') {
+			$sValue = '<span style="color:green">' . i18n('nothing disabled') . "</span>";
+		} else {
+			$sValue = '<span style="color:red">' . $sValue . "</span>";
+		}
+	}
+	
+	if ($sConfigName == 'sql.safe_mode') {
+		if ($sValue == 1) {
+			$sValue = '<span style="color:red">' . i18n('activated') . "</span>";
+		} else {
+			$sValue = '<span style="color:green">' . i18n('deactivated') . "</span>";
+		}
+	}
+
+	$oTpl2->set('d', 'NAME', $sConfigName);
+	$oTpl2->set('d', 'VALUE', $sValue);
+	$oTpl2->next();
+}
+
+$extensions = get_loaded_extensions();
+sort($extensions);
+$oTpl2->set('s', 'ADDITIONAL', '<tr><td style="border:1px; border-top:0px; border-color: #B3B3B3; border-style: solid;" colspan="2"><b>' . i18n('Loaded extensions') . ':</b><br />' . implode(', ', $extensions) . '</td></tr>');
+
+$sPhpConfig = $oTpl2->generate($cfg['path']['templates'] . $cfg['templates']['systam_variables_block'], 1);
+$tpl->set('s', 'PHP_CONFIGURATION', $sPhpConfig);
+
+// clients
+$oClientColl = new cApiClientCollection();
+$oClientColl->select();
+$sClients = '';
+
+while ($oItem = $oClientColl->next()) {
+	$iIdClient = $oItem->get('idclient');
+	
+	if (system_have_perm($iIdClient)) {
+		$htmlpath = $cfgClient[$iIdClient]['path']['htmlpath'];
+		$frontendpath = $cfgClient[$iIdClient]['path']['frontend'];
+		
+		$oTpl2 = new Template();
+		$oTpl2->set('s', 'HEADLINE', i18n('Client') . ' ' . $oItem->get('name'));
+		$oTpl2->set('s', 'ADDITIONAL', '');
+		
+		$oTpl2->set('d', 'NAME', i18n("htmlpath"));
+		$oTpl2->set('d', 'VALUE', $htmlpath);
+		$oTpl2->next();
+		
+		$oTpl2->set('d', 'NAME', i18n("frontendpath"));
+		$oTpl2->set('d', 'VALUE', $frontendpath);
+		$oTpl2->next();
+		
+		$oClientLanguageColl = new cApiClientLanguageCollection();
+        	$oClientLanguageColl->setWhere('idclient', $iIdClient);
+        	$oClientLanguageColl->query();
+
+		$aLanguages = array();
+		
+		if ($oClientLanguageColl->count() > 0) {
+			while ($oClientLang = $oClientLanguageColl->next()) {
+				$iIdLang = $oClientLang->get('idlang');
+				
+				$oLang = new cApiLanguage($iIdLang);
+				$aLanguages[$iIdLang] = $oLang->get('name') . ' (' . $iIdLang . ')';
+			}
+			
+			$sLanguages = implode(', ', $aLanguages);
+		} else {
+			$sLanguages = i18n('No languages were found for this client');
+		}
+		
+		$oTpl2->set('d', 'NAME', i18n("language(s)"));
+		$oTpl2->set('d', 'VALUE', $sLanguages);
+		$oTpl2->next();
+
+		$sClients .= $oTpl2->generate($cfg['path']['templates'] . $cfg['templates']['systam_variables_block'], 1) . '<br />';
+	}
+}
+
+$tpl->set('s', 'CLIENTS', $sClients);
 
 // parse out template
 $tpl->generate($cfg['path']['templates'] . $cfg['templates']['systam_variables']);
-
 ?>
