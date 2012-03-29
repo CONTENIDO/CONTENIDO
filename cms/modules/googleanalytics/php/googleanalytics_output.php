3a51c9f0d82e578ad53cb3ee026839905c8ebd88
Dominik Ziegler <dominik.ziegler@4fb.de>
2012-03-29 19:40:55 +0000

    removed classes from tracking modules, removed usage of smarty due to call problems on renamed modules, simplified url integration in piwik module

--- cms/modules/googleanalytics/php/googleanalytics_output.php
+++ cms/modules/googleanalytics/php/googleanalytics_output.php
@@ -1,33 +1,9 @@
 <?php
-cInclude('backend', 'classes/module/AbstractModule.php', true);
+$account = getEffectiveSetting('googleanalytics', 'account', '');
 
-
-if (!class_exists('GoogleAnalytics')) {
-    class GoogleAnalytics extends AbstractModule {
-		
-		private $_authcode = '';
-    	public function __construct() {
-    		
-    		$this->_authcode = getEffectiveSetting('googleanalytics', 'googleanalytics_authentication', '');
-    	}
-    	
-    	
-    	
-    	public function renderOutput() {
-		  
-    		self::$_smarty->assign('auth', $this->_authcode);
-           self::$_smarty->display('googleanalytics/template/googleanalytics.html');
-        }
-        
-    	
-    }
+if ($account != '') {
+	$tpl = new Template();
+	$tpl->set('s', 'account', $account);
+	$tpl->generate('googleanalytics.html');
 }
-
-
-$googleAnalytics = new GoogleAnalytics();
-$googleAnalytics->renderOutput();
-
-
 ?>
-
-
--- cms/modules/piwik/php/piwik_output.php
+++ cms/modules/piwik/php/piwik_output.php
@@ -1,45 +1,12 @@
 <?php 
-cInclude('backend', 'classes/module/AbstractModule.php', true);
+$url	= getEffectiveSetting('piwik', 'url', '');
+$site	= getEffectiveSetting('piwik', 'site', '');
 
+if ($url != '' && $site != '') {
+	$tpl = new Template();
 
-if (!class_exists('Piwik')) {
-	class Piwik extends AbstractModule {
-
-		private $_httpsUrl = '';
-		private $_httpUrl = '';
-		private $_idsite = '';
-		public function __construct() {
-
-			$this->_httpsUrl = getEffectiveSetting('piwik', 'https_url', '');
-			$this->_httpUrl = getEffectiveSetting('piwik', 'http_url', '');
-			$this->_idsite = getEffectiveSetting('piwik','idsite', '');
-		}
-		 
-		 
-		public function renderOutput() {
-
-			self::$_smarty->assign('httpsUrl', $this->_httpsUrl);
-			self::$_smarty->assign('httpUrl', $this->_httpUrl);
-			self::$_smarty->assign('idsite', $this->_idsite);
-			$noScriptUrl = $this->_httpUrl;
-			if(isset($_SERVER["https"])) {
-				$noScriptUrl = $this->_httpsUrl;
-				
-			}
-			self::$_smarty->assign('noScriptUrl', $noScriptUrl);
-			self::$_smarty->display('piwik/template/piwik.html');
-		}
-
-		 
-	}
+	$tpl->set('s', 'url', $url);
+	$tpl->set('s', 'site', $site);
+	$tpl->generate('piwik.html');
 }
-
-
-$piwik = new Piwik();
-$piwik->renderOutput();
-
-
 ?>
-
-
-
