aaa29d5a4aa844bdd683582135690d062f673aae
Dominik Ziegler <dominik.ziegler@4fb.de>
2012-03-31 16:49:27 +0000

    do not display anything if module is not configured, xing

--- cms/modules/xing/php/xing_output.php
+++ cms/modules/xing/php/xing_output.php
@@ -1,27 +1,23 @@
 <?php
-
-$tpl = new Template();
-
-//url to profile
+// profile url
 $url = "CMS_VALUE[0]";
-//small pictures or big
+
+// big button or small button?
 $look = "CMS_VALUE[1]";
 
-//alt for bild
+// name of user
 $name = "CMS_VALUE[2]";
 
+if ($url != '' && $look != '') {
+	$tpl = new Template();
 
+	$tpl->set("s", "NAME", $name);
+	$tpl->set("s", "URL" , $url);
 
-$tpl->set("s", "NAME", $name);
-$tpl->set("s", "URL" , $url);
-
-if($look == "small")
-    $tpl->generate("xing_small.html");
-elseif($look == "big")
-    $tpl->generate("xing_big.html");
-else {
-	$tpl->generate("xing_no_config.html");
+	if ($look == "small") {
+		$tpl->generate("xing_small.html");
+	} elseif($look == "big") {
+		$tpl->generate("xing_big.html");
+	}
 }
-
-
 ?>
