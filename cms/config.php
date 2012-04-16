e897c6950770b92889cd1d4a334f60c2039db9df
Murat Purc <murat@purc.de>
2012-04-16 21:35:07 +0000

    Formatted config.php in clients frontend directory and in template in external/frontend/

--- cms/config.php
+++ cms/config.php
@@ -1,14 +1,13 @@
 <?php
 /**
- * Project: 
+ * Project:
  * CONTENIDO Content Management System
- * 
- * Description: 
+ *
+ * Description:
  * Configuration File
- * 
- * Requirements: 
+ *
+ * Requirements:
  * @con_php_req 5
- * 
  *
  * @package    CONTENIDO Frontend Config
  * @version    0.1
@@ -17,37 +16,34 @@
  * @license    http://www.contenido.org/license/LIZENZ.txt
  * @link       http://www.4fb.de
  * @link       http://www.contenido.org
- * 
- * 
- * 
- * {@internal 
+ *
+ * {@internal
  *   created  unknown
  *   modified 2008-07-03, bilal arslan, added security fix
  *
  *   $Id$:
  * }}
- * 
+ *
  */
 
-if(!defined('CON_FRAMEWORK')) {
-  die('Illegal call');
+if (!defined('CON_FRAMEWORK')) {
+    die('Illegal call');
 }
- 
- 
-// Relative path to CONTENIDO directory, for all inclusions, in most cases: "../contenido/"
-$contenido_path = "../contenido/";
+
+// Relative path to CONTENIDO directory, for all inclusions, in most cases: '../contenido/'
+$contenido_path = '../contenido/';
 
 // If language isn't specified, set this client and language (ID)
-$load_lang		= "1";
-$load_client	= "1";
+$load_lang   = '1';
+$load_client = '1';
 
-/* Various debugging options */
-$frontend_debug["container_display"]		= false;
-$frontend_debug["module_display"]			= false;
-$frontend_debug["module_timing"]			= false;
-$frontend_debug["module_timing_summary"]	= false;
+// Various debugging options
+$frontend_debug['container_display']     = false;
+$frontend_debug['module_display']        = false;
+$frontend_debug['module_timing']         = false;
+$frontend_debug['module_timing_summary'] = false;
 
-/* Set to 1 to brute-force module regeneration */
+// Set to 1 to brute-force module regeneration
 $force = 0;
 
 ?>
--- contenido/external/frontend/config.php
+++ contenido/external/frontend/config.php
@@ -1,52 +1,49 @@
 <?php
 /**
- * Project: 
+ * Project:
  * CONTENIDO Content Management System
- * 
- * Description: 
- * <Description>
- * 
- * Requirements: 
- * @con_php_req 5
- * @con_template <Templatefiles>
- * @con_notice <Notice>
- * 
  *
- * @package    CONTENIDO Frontend
- * @version    <version>
- * @author     <author>
+ * Description:
+ * Configuration File
+ *
+ * Requirements:
+ * @con_php_req 5
+ *
+ * @package    CONTENIDO Frontend Config
+ * @version    0.1
+ * @author     unknown
  * @copyright  four for business AG <www.4fb.de>
  * @license    http://www.contenido.org/license/LIZENZ.txt
  * @link       http://www.4fb.de
  * @link       http://www.contenido.org
- * 
- * 
- * 
- * {@internal 
- *   created  <date>
+ *
+ * {@internal
+ *   created  unknown
  *   modified 2008-07-04, bilal arslan, added security fix
  *
  *   $Id$:
  * }}
- * 
+ *
  */
- if(!defined('CON_FRAMEWORK')) {
-   die('Illegal call');
+
+if (!defined('CON_FRAMEWORK')) {
+    die('Illegal call');
 }
- 
-// Relative path to CONTENIDO directory, for all inclusions, in most cases: "../contenido/"
-$contenido_path = "!PATH!";
+
+// Relative path to CONTENIDO directory, for all inclusions, in most cases: '../contenido/'
+$contenido_path = '!PATH!';
 
 // If language isn't specified, set this client and language (ID)
-$load_lang		= "!LANG!";
-$load_client	= "!CLIENT!";
+$load_lang   = '!LANG!';
+$load_client = '!CLIENT!';
 
-/* Various debugging options */
-$frontend_debug["container_display"]		= false;
-$frontend_debug["module_display"]			= false;
-$frontend_debug["module_timing"]			= false;
-$frontend_debug["module_timing_summary"]	= false;
+// Various debugging options
+$frontend_debug['container_display']     = false;
+$frontend_debug['module_display']        = false;
+$frontend_debug['module_timing']         = false;
+$frontend_debug['module_timing_summary'] = false;
 
-/* Set to 1 to brute-force module regeneration */
+// Set to 1 to brute-force module regeneration
 $force = 0;
+
 ?>
