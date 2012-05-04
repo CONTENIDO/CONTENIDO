ffbbd7f1ffcb1838e99199a9e3dd8805d9cfbd3c
Fulai Zhang <fulai.zhang@4fb.de>
2012-05-04 15:28:12 +0000

    con-514 Meta Tags in neu Tab

--- setup/data/base/base_files1.sql
+++ setup/data/base/base_files1.sql
@@ -212,3 +212,4 @@ INSERT INTO !PREFIX!_files VALUES ('398', '32', 'include.files_subnav.php', 'mai
 INSERT INTO !PREFIX!_files VALUES ('403', '403', 'include.html_tpl_history.php', 'main');
 INSERT INTO !PREFIX!_files VALUES ('415', '415', 'include.system_configuration.php', 'main');
 INSERT INTO !PREFIX!_files VALUES ('813', '811', 'include.system_purge.php', 'main');
+INSERT INTO !PREFIX!_files VALUES ('850', '100', 'include.con_meta_form.php', 'main');
