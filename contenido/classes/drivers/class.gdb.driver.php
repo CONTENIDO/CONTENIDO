3c3c76d41024cf3e50efd3bcb0d02b3c18d06d4e
Murat Purc <murat@purc.de>
2012-05-25 21:46:10 +0000

    Formatting, Formatting, Formatting...

--- contenido/classes/drivers/class.gdb.driver.php
+++ contenido/classes/drivers/class.gdb.driver.php
@@ -1,14 +1,14 @@
 <?php
 /**
- * Project: 
+ * Project:
  * CONTENIDO Content Management System
- * 
- * Description: 
- * Root Driver for GenericDB 
- * 
- * Requirements: 
+ *
+ * Description:
+ * Root Driver for GenericDB
+ *
+ * Requirements:
  * @con_php_req 5.0
- * 
+ *
  *
  * @package    CONTENIDO Backend Classes
  * @version    1.3
@@ -17,47 +17,48 @@
  * @license    http://www.contenido.org/license/LIZENZ.txt
  * @link       http://www.4fb.de
  * @link       http://www.contenido.org
- * 
- * {@internal 
+ *
+ * {@internal
  *   created 2005-08-29
  *   modified 2008-05-23 Added Debug_DevNull and Debug_VisibleAdv
- *   
+ *
  *   $Id: class.gdb.driver.php,v 1.3 2005/08/29 15:41:07 timo.hummel Exp $
  * }}
- * 
+ *
  */
 
-if(!defined('CON_FRAMEWORK')) {
-	die('Illegal call');
+if (!defined('CON_FRAMEWORK')) {
+    die('Illegal call');
 }
 
 class gdbDriver
 {
-	var $_sEncoding;
-	
-	var $_oItemClassInstance;
-	
-	function gdbDriver ()
-	{}
-	
-	function setEncoding ($sEncoding)
-	{
-		$this->_sEncoding = $sEncoding;
-	}
-	
-	function setItemClassInstance ($oInstance)
-	{
-		$this->_oItemClassInstance = $oInstance;
-	}
-	
-	function buildJoinQuery ($destinationTable, $destinationClass, $destinationPrimaryKey, $sourceClass, $primaryKey)
-	{
-		
-	}
-	
-	function buildOperator ($sField, $sOperator, $sRestriction)
-	{
-	}
+    var $_sEncoding;
+
+    var $_oItemClassInstance;
+
+    function gdbDriver()
+    {
+    }
+
+    function setEncoding($sEncoding)
+    {
+        $this->_sEncoding = $sEncoding;
+    }
+
+    function setItemClassInstance($oInstance)
+    {
+        $this->_oItemClassInstance = $oInstance;
+    }
+
+    function buildJoinQuery($destinationTable, $destinationClass, $destinationPrimaryKey, $sourceClass, $primaryKey)
+    {
+
+    }
+
+    function buildOperator($sField, $sOperator, $sRestriction)
+    {
+    }
 }
 
 ?>
--- contenido/classes/drivers/mysql/class.gdb.mysql.php
+++ contenido/classes/drivers/mysql/class.gdb.mysql.php
@@ -1,14 +1,14 @@
 <?php
 /**
- * Project: 
+ * Project:
  * CONTENIDO Content Management System
- * 
- * Description: 
- * MySQL Driver for GenericDB 
- * 
- * Requirements: 
+ *
+ * Description:
+ * MySQL Driver for GenericDB
+ *
+ * Requirements:
  * @con_php_req 5.0
- * 
+ *
  *
  * @package    CONTENIDO Backend Classes
  * @version    1.12
@@ -17,180 +17,166 @@
  * @license    http://www.contenido.org/license/LIZENZ.txt
  * @link       http://www.4fb.de
  * @link       http://www.contenido.org
- * 
- * {@internal 
+ *
+ * {@internal
  *   created 2006-05-10
  *   modified 2008-05-23 Added Debug_DevNull and Debug_VisibleAdv
- *   
+ *
  *   $Id: class.gdb.mysql.php,v 1.12 2006/10/05 23:44:43 bjoern.behrens Exp $
  * }}
- * 
+ *
  */
 
-if(!defined('CON_FRAMEWORK')) {
-	die('Illegal call');
+if (!defined('CON_FRAMEWORK')) {
+    die('Illegal call');
 }
 
 
 class gdbMySQL extends gdbDriver
 {
-	function buildJoinQuery ($destinationTable, $destinationClass, $destinationPrimaryKey, $sourceClass, $primaryKey)
-	{
-		// Build a regular LEFT JOIN
-		$field  = "$destinationClass.$destinationPrimaryKey";
-		$tables = "";
-		$join   = "LEFT JOIN $destinationTable AS $destinationClass ON " . 
-					Contenido_Security::toString($sourceClass . "." . $primaryKey) . " = " .  
-					Contenido_Security::toString($destinationClass . "." . $primaryKey);
-		$where  = "";
-		
-		return array("field" => $field, "table" => $tables, "join" => $join, "where" => $where);
-	}
-	
-	function buildOperator ($sField, $sOperator, $sRestriction)
-	{
-		$sOperator = strtolower($sOperator);
-		
-		$sWhereStatement = "";
-		
-		switch ($sOperator)
-		{
-			case "matchbool":
-				$sqlStatement = "MATCH (%s) AGAINST ('%s' IN BOOLEAN MODE)";
-				$sWhereStatement = sprintf($sqlStatement, $sField, $this->_oItemClassInstance->_inFilter($sRestriction));
-				break;
-			case "match":
-				$sqlStatement = "MATCH (%s) AGAINST ('%s')";
-				$sWhereStatement = sprintf($sqlStatement, $sField, $this->_oItemClassInstance->_inFilter($sRestriction));
-				break;
-			case "like":
-				$sqlStatement = "%s LIKE '%%%s%%'";
-				$sWhereStatement = sprintf($sqlStatement, Contenido_Security::toString($sField), $this->_oItemClassInstance->_inFilter($sRestriction));
-				break;
-			case "likeleft":
-				$sqlStatement = "%s LIKE '%s%%'";
-				$sWhereStatement = sprintf($sqlStatement, Contenido_Security::toString($sField), $this->_oItemClassInstance->_inFilter($sRestriction));
-				break;
-			case "likeright":
-				$sqlStatement = "%s LIKE '%%%s'";
-				$sWhereStatement = sprintf($sqlStatement, Contenido_Security::toString($sField), $this->_oItemClassInstance->_inFilter($sRestriction));
-				break;
-			case "notlike":
-				$sqlStatement = "%s NOT LIKE '%%%s%%'";
-				$sWhereStatement = sprintf($sqlStatement, Contenido_Security::toString($sField), $this->_oItemClassInstance->_inFilter($sRestriction));
-				break;
-			case "notlikeleft":
-				$sqlStatement = "%s NOT LIKE '%s%%'";
-				$sWhereStatement = sprintf($sqlStatement, Contenido_Security::toString($sField), $this->_oItemClassInstance->_inFilter($sRestriction));
-				break;
-			case "notlikeright":
-				$sqlStatement = "%s NOT LIKE '%%%s'";
-				$sWhereStatement = sprintf($sqlStatement, Contenido_Security::toString($sField), $this->_oItemClassInstance->_inFilter($sRestriction));
-				break;				
-			case "diacritics":
-				if (!is_object($GLOBALS["_cCharTable"]))
-				{
-					$GLOBALS["_cCharTable"] = new cCharacterConverter;
-				}
+    function buildJoinQuery($destinationTable, $destinationClass, $destinationPrimaryKey, $sourceClass, $primaryKey)
+    {
+        // Build a regular LEFT JOIN
+        $field  = "$destinationClass.$destinationPrimaryKey";
+        $tables = "";
+        $join   = "LEFT JOIN $destinationTable AS $destinationClass ON " .
+                    Contenido_Security::toString($sourceClass . "." . $primaryKey) . " = " .
+                    Contenido_Security::toString($destinationClass . "." . $primaryKey);
+        $where  = "";
 
-				$aliasSearch = array ();
+        return array("field" => $field, "table" => $tables, "join" => $join, "where" => $where);
+    }
 
-				$metaCharacters = array ("*", "[", "]", "^", '$', "\\", "*", "'", '"', '+');
+    function buildOperator($sField, $sOperator, $sRestriction)
+    {
+        $sOperator = strtolower($sOperator);
 
-				for ($i = 0; $i < strlen($sRestriction); $i ++)
-				{
-					$char = substr($sRestriction, $i, 1);
+        $sWhereStatement = "";
 
-					$aliases = array ();
+        switch ($sOperator) {
+            case "matchbool":
+                $sqlStatement = "MATCH (%s) AGAINST ('%s' IN BOOLEAN MODE)";
+                $sWhereStatement = sprintf($sqlStatement, $sField, $this->_oItemClassInstance->_inFilter($sRestriction));
+                break;
+            case "match":
+                $sqlStatement = "MATCH (%s) AGAINST ('%s')";
+                $sWhereStatement = sprintf($sqlStatement, $sField, $this->_oItemClassInstance->_inFilter($sRestriction));
+                break;
+            case "like":
+                $sqlStatement = "%s LIKE '%%%s%%'";
+                $sWhereStatement = sprintf($sqlStatement, Contenido_Security::toString($sField), $this->_oItemClassInstance->_inFilter($sRestriction));
+                break;
+            case "likeleft":
+                $sqlStatement = "%s LIKE '%s%%'";
+                $sWhereStatement = sprintf($sqlStatement, Contenido_Security::toString($sField), $this->_oItemClassInstance->_inFilter($sRestriction));
+                break;
+            case "likeright":
+                $sqlStatement = "%s LIKE '%%%s'";
+                $sWhereStatement = sprintf($sqlStatement, Contenido_Security::toString($sField), $this->_oItemClassInstance->_inFilter($sRestriction));
+                break;
+            case "notlike":
+                $sqlStatement = "%s NOT LIKE '%%%s%%'";
+                $sWhereStatement = sprintf($sqlStatement, Contenido_Security::toString($sField), $this->_oItemClassInstance->_inFilter($sRestriction));
+                break;
+            case "notlikeleft":
+                $sqlStatement = "%s NOT LIKE '%s%%'";
+                $sWhereStatement = sprintf($sqlStatement, Contenido_Security::toString($sField), $this->_oItemClassInstance->_inFilter($sRestriction));
+                break;
+            case "notlikeright":
+                $sqlStatement = "%s NOT LIKE '%%%s'";
+                $sWhereStatement = sprintf($sqlStatement, Contenido_Security::toString($sField), $this->_oItemClassInstance->_inFilter($sRestriction));
+                break;
+            case "diacritics":
+                if (!is_object($GLOBALS["_cCharTable"])) {
+                    $GLOBALS["_cCharTable"] = new cCharacterConverter;
+                }
 
-					$aliases = array_merge($aliases, $GLOBALS["_cCharTable"]->fetchDiacriticCharactersForNormalizedChar($this->_sEncoding, $char));
-					$normalizedChars = $GLOBALS["_cCharTable"]->fetchNormalizedCharsForDiacriticCharacter($this->_sEncoding, $char);
+                $aliasSearch = array();
 
-					foreach ($normalizedChars as $normalizedChar)
-					{
-						$aliases = array_merge($aliases, $GLOBALS["_cCharTable"]->fetchDiacriticCharactersForNormalizedChar($this->_sEncoding, $normalizedChar));
-					}
+                $metaCharacters = array("*", "[", "]", "^", '$', "\\", "*", "'", '"', '+');
 
-					$aliases = array_merge($aliases, $normalizedChars);
+                for ($i = 0; $i < strlen($sRestriction); $i ++) {
+                    $char = substr($sRestriction, $i, 1);
 
-					if (count($aliases) > 0)
-					{
-						$aliases[] = $char;
-						$allAliases = array ();
+                    $aliases = array();
 
-						foreach ($aliases as $alias)
-						{
-							$alias1 = $this->_oItemClassInstance->_inFilter($alias);
-							$allAliases[] = $alias1;
-							$allAliases[] = $alias;
-						}
+                    $aliases = array_merge($aliases, $GLOBALS["_cCharTable"]->fetchDiacriticCharactersForNormalizedChar($this->_sEncoding, $char));
+                    $normalizedChars = $GLOBALS["_cCharTable"]->fetchNormalizedCharsForDiacriticCharacter($this->_sEncoding, $char);
 
-						$allAliases = array_unique($allAliases);
-						$aliasSearch[] = "(".implode("|", $allAliases).")";
+                    foreach ($normalizedChars as $normalizedChar) {
+                        $aliases = array_merge($aliases, $GLOBALS["_cCharTable"]->fetchDiacriticCharactersForNormalizedChar($this->_sEncoding, $normalizedChar));
+                    }
 
-					} else
-					{
-						$addChars = array();
-						
-						
-                        
-						if (in_array($char, $metaCharacters))
-						{
-							$addChars[] = "\\\\".$char;
-						} else
-						{
+                    $aliases = array_merge($aliases, $normalizedChars);
+
+                    if (count($aliases) > 0) {
+                        $aliases[] = $char;
+                        $allAliases = array();
+
+                        foreach ($aliases as $alias) {
+                            $alias1 = $this->_oItemClassInstance->_inFilter($alias);
+                            $allAliases[] = $alias1;
+                            $allAliases[] = $alias;
+                        }
+
+                        $allAliases = array_unique($allAliases);
+                        $aliasSearch[] = "(".implode("|", $allAliases).")";
+
+                    } else {
+                        $addChars = array();
+
+                        if (in_array($char, $metaCharacters)) {
+                            $addChars[] = "\\\\".$char;
+                        } else {
                             $addChars[] = $char;
-                            
-							$vChar = $this->_oItemClassInstance->_inFilter($char);
-                            
-                            if ($char != $vChar)
-                            {
-                                if (in_array($vChar, $metaCharacters))
-                                {
+
+                            $vChar = $this->_oItemClassInstance->_inFilter($char);
+
+                            if ($char != $vChar) {
+                                if (in_array($vChar, $metaCharacters)) {
                                     $addChars[] = "\\\\".$vChar;
                                 } else {
-                                    $addChars[] = $vChar;	
+                                    $addChars[] = $vChar;
                                 }
                             }
-						}
-                        
+                        }
+
                         $aliasSearch[] = "(".implode("|", $addChars).")";
-					}
-				}
+                    }
+                }
 
-				$restriction = "'".implode("", $aliasSearch)."'";
-				$sWhereStatement = implode(" ", array ($sField, "REGEXP", $restriction));
+                $restriction = "'".implode("", $aliasSearch)."'";
+                $sWhereStatement = implode(" ", array($sField, "REGEXP", $restriction));
 
-				break;
-			case "fulltext":
-				
-				break;
-			case "in":
-				if (is_array($sRestriction))
-				{
-					$items = array();
-					
-					foreach ($sRestriction as $key => $sRestrictionItem)
-					{
-						$items[] = "'".$this->_oItemClassInstance->_inFilter($sRestrictionItem)."'";
-					}
-					
-					$sRestriction = implode(", ", $items);
-				} else {
-					$sRestriction = "'" . $sRestriction ."'";	
-				}
-				
-				$sWhereStatement = implode(" ", array($sField, "IN (", $sRestriction, ")"));
-				break; 
-			default :
-				$sRestriction = "'".$this->_oItemClassInstance->_inFilter($sRestriction)."'";
-				
-				$sWhereStatement = implode(" ", array ($sField, $sOperator, $sRestriction));
-				
-		}
-		
-		return $sWhereStatement;
-		
-	}	
+                break;
+            case "fulltext":
+
+                break;
+            case "in":
+                if (is_array($sRestriction)) {
+                    $items = array();
+
+                    foreach ($sRestriction as $key => $sRestrictionItem) {
+                        $items[] = "'".$this->_oItemClassInstance->_inFilter($sRestrictionItem)."'";
+                    }
+
+                    $sRestriction = implode(", ", $items);
+                } else {
+                    $sRestriction = "'" . $sRestriction ."'";
+                }
+
+                $sWhereStatement = implode(" ", array($sField, "IN (", $sRestriction, ")"));
+                break;
+            default :
+                $sRestriction = "'".$this->_oItemClassInstance->_inFilter($sRestriction)."'";
+
+                $sWhereStatement = implode(" ", array($sField, $sOperator, $sRestriction));
+
+        }
+
+        return $sWhereStatement;
+
+    }
 }
+
 ?>
