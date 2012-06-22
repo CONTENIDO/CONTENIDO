f0a3c8451760e55c67da4ec928cbac157a544e1c
Murat Purc <murat@purc.de>
2012-06-22 21:39:53 +0000

    Disabled forcing code generation.

--- cms/config.php
+++ cms/config.php
@@ -41,6 +41,6 @@ $frontend_debug['module_timing']         = false;
 $frontend_debug['module_timing_summary'] = false;
 
 // Set to 1 to brute-force module regeneration
-$force = 1;
+$force = 0;
 
 ?>
