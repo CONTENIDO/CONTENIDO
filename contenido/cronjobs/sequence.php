f1f3fbe4a41493bc12cff3ea88ba7451f1ac2de7
Murat Purc <murat@purc.de>
2012-06-26 20:31:23 +0000

    Formatting and some fixes.

--- contenido/cronjobs/sequence.php
+++ contenido/cronjobs/sequence.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 if (!defined('CON_FRAMEWORK')) {
     define('CON_FRAMEWORK', true);
@@ -18,7 +18,7 @@ $db->free();
 // first all values higher then 0
 $filterTables = array($cfg['sql']['sqlprefix'].'_pica_alloc_con', $cfg['sql']['sqlprefix'].'_pica_lang', $cfg['sql']['sqlprefix'].'_sequence');
 $sql2 = 'SELECT *
-		FROM '.$cfg['db']['connection']['database'].'.con_sequence';
+        FROM '.$cfg['db']['connection']['database'].'.con_sequence';
 
 $sql = 'SHOW TABLES FROM  '.$cfg['db']['connection']['database'].'';
 $db->query($sql);
@@ -28,7 +28,7 @@ if($db->Error !=0) {
 
 $i = 0;
 while ($row = mysql_fetch_row($db->Query_ID)) {
-    
+
     if(in_array($row[0], $filterTables) === false) {
   # echo "<br/> Tabelle: {$row[0]}\n";
        getNextId($row);
@@ -46,12 +46,12 @@ function getNextId($row) {
     $tableName = $row[0];
     //$nextId = $row[1];
     debug($row);
-    
+
     $db = cRegistry::getDb();
     $sql = 'SHOW KEYS FROM '.$tableName.' WHERE Key_name="PRIMARY"';
     $db->query($sql);
      while ($row = mysql_fetch_row($db->Query_ID)) {
-        
+
         $primaryKey = $row[4];
         $dbAlter = cRegistry::getDb();
         $sqlAlter = 'ALTER TABLE `'.$tableName.'` CHANGE `'.$primaryKey.'` `'.$primaryKey.'` INT( 10 ) NOT NULL AUTO_INCREMENT';
@@ -61,7 +61,7 @@ function getNextId($row) {
             echo "<pre>" . $sqlAlter . "\nMysql Error:" . $db->Error . "(" . $db->Errno . ")</pre>";
         }
     }
-   
+
 }
 
 function debug($string) {
