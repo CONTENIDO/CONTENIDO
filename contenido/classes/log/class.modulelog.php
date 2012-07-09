c0ba484383fa01b2f06e56859174653078ac20d9
Marcus Gnaß <marcus.gnass@4fb.de>
2012-07-09 11:48:37 +0000

    fixed formatting

--- contenido/classes/log/class.modulelog.php
+++ contenido/classes/log/class.modulelog.php
@@ -6,14 +6,14 @@
  * Description:
  * This file contains the module log class.
  *
- * @package    CONTENIDO Backend Classes
- * @version    1.0.0
- * @author     Dominik Ziegler
- * @copyright  four for business AG <www.4fb.de>
- * @license    http://www.contenido.org/license/LIZENZ.txt
- * @link       http://www.4fb.de
- * @link       http://www.contenido.org
- * @since      file available since CONTENIDO release 4.9.0
+ * @package CONTENIDO Backend Classes
+ * @version 1.0.0
+ * @author Dominik Ziegler
+ * @copyright four for business AG <www.4fb.de>
+ * @license http://www.contenido.org/license/LIZENZ.txt
+ * @link http://www.4fb.de
+ * @link http://www.contenido.org
+ * @since file available since CONTENIDO release 4.9.0
  */
 
 if (!defined('CON_FRAMEWORK')) {
@@ -21,12 +21,14 @@ if (!defined('CON_FRAMEWORK')) {
 }
 
 /**
- * This class contains the main functionalities for the module logging in CONTENIDO.
- * The funcationality is almost the same like normal logging with the exception, that
- * log entries contains an additional information about the used module.
+ * This class contains the main functionalities for the module logging in
+ * CONTENIDO.
+ * The funcationality is almost the same like normal logging with the exception,
+ * that log entries contains an additional information about the used module.
  *
  * Example:
- * $writer = cLogWriter::factory("File", array('destination' => 'contenido.log'));
+ * $writer = cLogWriter::factory("File", array('destination' =>
+ * 'contenido.log'));
  *
  * $log = new cModuleLog($writer);
  * $log->setModule(1);
@@ -34,51 +36,57 @@ if (!defined('CON_FRAMEWORK')) {
  */
 class cModuleLog extends cLog {
 
-	/**
-	 * @var cApiModule	Object instance of module model
-	 */
-	private $_module;
-
-	/**
-	 * Constructor of the module log.
+    /**
      *
-     * @param  mixed  writer   Writer object (any subclass of cLogWriter), or false if cLog should handle the writer creation
-	 *
-	 */
-	public function __construct($writer = false) {
-		parent::__construct($writer);
+     * @var cApiModule instance of module model
+     */
+    private $_module;
 
-		$this->setShortcutHandler('module', array($this, 'shModule'));
-		$this->getWriter()->setOption("log_format", "[%date] [%level] [%module] %message", true);
-	}
+    /**
+     * Constructor of the module log.
+     *
+     * @param mixed writer Writer object (any subclass of cLogWriter), or false
+     *        if cLog should handle the writer creation
+     *
+     */
+    public function __construct($writer = false) {
+        parent::__construct($writer);
+
+        $this->setShortcutHandler('module', array(
+            $this,
+            'shModule'
+        ));
+        $this->getWriter()->setOption("log_format", "[%date] [%level] [%module] %message", true);
+    }
 
     /**
      * Sets the module to use.
      *
-     * setModule automatically buffers basic module information to the log to assist the
-     * developer in debugging his modules.
+     * setModule automatically buffers basic module information to the log to
+     * assist the developer in debugging his modules.
      *
-     * @param  int  idmod  The module ID to use
-	 *
-	 * @return	void
+     * @param int idmod The module ID to use
+     *
+     * @return void
      */
     public function setModule($idmod) {
         $this->_module = new cApiModule($idmod);
-		if ($this->_module->isLoaded() == false) {
-			cWarning(__FILE__, __LINE__, "Could not load module information.");
-			return false;
-		}
+        if ($this->_module->isLoaded() == false) {
+            cWarning(__FILE__, __LINE__, "Could not load module information.");
+            return false;
+        }
     }
 
     /**
      * Shortcut Handler Module.
-	 * Returns the ID and the name of the module.
-     * @return	string	 ID and name of the module
+     * Returns the ID and the name of the module.
+     *
+     * @return string ID and name of the module
      */
     public function shModule() {
-		if ($this->_module->isLoaded() == false) {
-			return '';
-		}
+        if ($this->_module->isLoaded() == false) {
+            return '';
+        }
 
         return $this->_module->get("idmod") . ": " . $this->_module->get("name");
     }
