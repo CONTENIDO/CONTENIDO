3c814c4e6f275694c2278a42b95f67e9fcb1b576
Marcus Gnaß <marcus.gnass@4fb.de>
2012-07-12 10:28:26 +0000

    fixed formatting and added comments for new mailing classes

--- contenido/classes/class.mail.php
+++ contenido/classes/class.mail.php
@@ -1,67 +1,79 @@
 <?php
 
+/**
+ *
+ * @author
+ */
 class cMail extends PHPMailer {
 
-
+    /**
+     *
+     * @var unknown_type
+     */
     private $_isPreSendError = true;
 
     /**
-   * Constructor
-   * @param boolean $exceptions Should we throw external exceptions?
-   */
-  public function __construct($exceptions = false) {
-    parent::__construct($exceptions);
-  }
-
+     * Constructor
+     *
+     * @param boolean $exceptions Should we throw external exceptions?
+     */
+    public function __construct($exceptions = false) {
+        parent::__construct($exceptions);
+    }
 
     /**
      * Log the information about sending the email.
+     *
      * @param boolean $success
-     * @param Exception $exception
+     * @param string $exception
      */
     private function _logData($success, $exception = '') {
 
         $cc = $bcc = '';
 
-        //success, header, body, $mailer, exception text
+        // success, header, body, $mailer, exception text
         $mailLog = new cApiMailLogCollection();
         $address = array();
-        $address['from']    = $this->From;
+        $address['from'] = $this->From;
 
-        //extract email address
+        // extract email address
         foreach ($this->cc as $item) {
             $cc[] = $item[0];
         }
-        //extract email address
+        // extract email address
         foreach ($this->bcc as $item) {
             $bcc[] = $item[0];
         }
 
-
-
-        $address['to']         = $this->to[0][0];
-        $address['cc']         = $cc;
-        $address['bcc']        = $bcc;
-        //get email address
-        $address['reply_to']= array_keys($this->ReplyTo);
-        $address['reply_to']= $this->ReplyTo[$address['reply_to'][0]][0];
+        $address['to'] = $this->to[0][0];
+        $address['cc'] = $cc;
+        $address['bcc'] = $bcc;
+        // get email address
+        $address['reply_to'] = array_keys($this->ReplyTo);
+        $address['reply_to'] = $this->ReplyTo[$address['reply_to'][0]][0];
 
         $mailLog->saveLog($success, $this->MIMEHeader, $this->MIMEBody, $this->Mailer, $address, $this->Subject, $exception);
 
     }
 
+    /**
+     *
+     * @param string $header
+     * @param string $body
+     * @param string $mailer
+     * @throws phpmailerException
+     * @return string|boolean
+     */
     public function resendEmail($header, $body, $mailer) {
-
 
         $this->MIMEHeader = $header;
         $this->MIMEBody = $body;
         $this->Mailer = $mailer;
 
-
         try {
             $ret = $this->PostSend();
             $this->_logData($ret);
-            return ret ;
+            return ret;
         } catch (phpmailerException $e) {
             $this->SentMIMEMessage = '';
             $this->SetError($e->getMessage());
@@ -72,21 +84,22 @@ class cMail extends PHPMailer {
             return false;
         }
 
-
     }
 
-
+    /**
+     * (non-PHPdoc)
+     * @see PHPMailer::Send()
+     */
     public function Send() {
-
         try {
-            if(!$this->PreSend())  {
+            if (!$this->PreSend()) {
                 $this->_logData(false);
                 return false;
             }
             $this->_isPreSendError = false;
             $ret = $this->PostSend();
             $this->_logData($ret, $this->ErrorInfo);
-            return $ret ;
+            return $ret;
         } catch (phpmailerException $e) {
             $this->SentMIMEMessage = '';
             $this->SetError($e->getMessage());
@@ -98,6 +111,6 @@ class cMail extends PHPMailer {
         }
     }
 
-
 }
+
 ?>
--- contenido/classes/class.maillog.php
+++ contenido/classes/class.maillog.php
@@ -1,120 +1,151 @@
 <?php
 
-    class cApiMailLog extends Item  {
+/**
+ *
+ * @author marcus.gnass
+ *
+ */
+class cApiMailLog extends Item {
 
-        public function __construct($mId = false)
-        {
-            global $cfg;
-            parent::__construct($cfg['tab']['mail_log'], 'idmail');
-
-        }
+    /**
+     *
+     * @param unknown_type $mId
+     */
+    public function __construct($mId = false) {
+        global $cfg;
+        parent::__construct($cfg['tab']['mail_log'], 'idmail');
     }
 
-    class cApiMailLogCollection extends ItemCollection{
+}
 
-        private $_idclient = '';
+/**
+ *
+ * @author
+ */
+class cApiMailLogCollection extends ItemCollection {
 
-        private $_idlang = '';
+    /**
+     *
+     * @var unknown_type
+     */
+    private $_idclient = '';
 
-        protected $mailLogDirectory = '';
-        protected $mailLogEnding = 'mail';
+    /**
+     *
+     * @var unknown_type
+     */
+    private $_idlang = '';
 
-        public function __construct($mId = false) {
-            global $cfg, $client, $lang;
+    /**
+     *
+     * @var unknown_type
+     */
+    protected $mailLogDirectory = '';
 
-            $this->mailLogDirectory = $this->_cronlogDirectory = $cfg['path']['contenido_maillog'] . '/';
-            $this->_idclient = $client;
-            $this->_idlang = $lang;
+    /**
+     *
+     * @var unknown_type
+     */
+    protected $mailLogEnding = 'mail';
 
-            parent::__construct($cfg['tab']['mail_log'], 'idmail');
+    /**
+     *
+     * @param unknown_type $mId
+     */
+    public function __construct($mId = false) {
+        global $cfg, $client, $lang;
 
-            if ($mId !== false) {
-                $this->loadByPrimaryKey($mId);
-            }
-            $this->_setItemClass('cApiMailLog');
+        $this->mailLogDirectory = $this->_cronlogDirectory = $cfg['path']['contenido_maillog'] . '/';
+        $this->_idclient = $client;
+        $this->_idlang = $lang;
+
+        parent::__construct($cfg['tab']['mail_log'], 'idmail');
+
+        if ($mId !== false) {
+            $this->loadByPrimaryKey($mId);
+        }
+        $this->_setItemClass('cApiMailLog');
+    }
+
+    /**
+     * Delete body and head files of email.
+     *
+     * @param cApiMailLog $oItem
+     */
+    public function deleteFiles(cApiMailLog $oItem) {
+
+        $fileName = md5($oItem->getField('idmail') . $oItem->getField('created'));
+        unlink($this->mailLogDirectory . 'body_' . $fileName . '.' . $this->mailLogEnding);
+        unlink($this->mailLogDirectory . 'head_' . $fileName . '.' . $this->mailLogEnding);
+    }
+
+    /**
+     * Get the body content of email
+     *
+     * @param cApiMailLog $oItem
+     * @return string content of the file of body
+     */
+    public function getBody(cApiMailLog $oItem) {
+        $fileName = md5($oItem->getField('idmail') . $oItem->getField('created'));
+        return cFileHandler::read($this->mailLogDirectory . 'body_' . $fileName . '.' . $this->mailLogEnding);
+    }
+
+    /**
+     * Get the head content of email
+     *
+     * @param cApiMailLog $oItem
+     * @return string content of the file of head
+     */
+    public function getHeader(cApiMailLog $oItem) {
+
+        $fileName = md5($oItem->getField('idmail') . $oItem->getField('created'));
+        return cFileHandler::read($this->mailLogDirectory . 'header_' . $fileName . '.' . $this->mailLogEnding);
+    }
+
+    /**
+     * Save Email log
+     *
+     * @param unknown_type $success
+     * @param unknown_type $header
+     * @param unknown_type $body
+     * @param unknown_type $mailer
+     * @param unknown_type $address
+     * @param unknown_type $subject
+     * @param unknown_type $exception
+     */
+    public function saveLog($success, $header, $body, $mailer, $address, $subject, $exception = '') {
+
+        $item = parent::createNewItem();
+        $success = ($success == true);
+        $item->set('success', $success);
+        $item->set('mailer', $mailer);
+        $item->set('idclient', $this->_idclient);
+        $item->set('idlang', $this->_idlang);
+        $date = date('Y-m-d H:i:s', time());
+        $item->set('from', $address['from']);
+        $item->set('to', $address['to']);
+        $item->set('cc', implode('+', $address['cc']));
+        $item->set('bcc', implode('+', $address['bcc']));
+        $item->set('reply_to', $address['reply_to']);
+        $item->set('created', $date, false);
+        $item->set('subject', $subject);
+        $item->set('exception', $exception);
+        $fileName = md5($item->getField('idmail') . $date);
+
+        if (cFileHandler::write($this->mailLogDirectory . 'body_' . $fileName . '.' . $this->mailLogEnding, $body) === false) {
+            return false;
         }
 
-        /**
-         * Delete body and head files of email.
-         * @param cApiMailLog $oItem
-         */
-        public function deleteFiles(cApiMailLog $oItem) {
-
-            $fileName = md5($oItem->getField('idmail'). $oItem->getField('created'));
-            unlink($this->mailLogDirectory . 'body_'. $fileName .'.'. $this->mailLogEnding);
-            unlink($this->mailLogDirectory . 'head_'. $fileName .'.'. $this->mailLogEnding);
+        if (cFileHandler::write($this->mailLogDirectory . 'header_' . $fileName . '.' . $this->mailLogEnding, $header) === false) {
+            return false;
         }
 
-        /**
-         * Get the body content of email
-         * @param cApiMailLog $oItem
-         * @return string content of the file of body
-         */
-        public function getBody(cApiMailLog $oItem){
-            $fileName = md5($oItem->getField('idmail'). $oItem->getField('created'));
-            return cFileHandler::read($this->mailLogDirectory . 'body_'. $fileName .'.'. $this->mailLogEnding);
-        }
+        $item->store();
 
-
-        /**
-         * Get the head content of email
-         * @param cApiMailLog $oItem
-         * @return string content of the file of head
-         */
-        public function getHeader(cApiMailLog $oItem) {
-
-            $fileName = md5($oItem->getField('idmail'). $oItem->getField('created'));
-            return cFileHandler::read($this->mailLogDirectory . 'header_'. $fileName .'.'. $this->mailLogEnding);
-        }
-
-        /**
-         * Save Email log
-         *
-         * @param unknown_type $success
-         * @param unknown_type $header
-         * @param unknown_type $body
-         * @param unknown_type $mailer
-         * @param unknown_type $address
-         * @param unknown_type $subject
-         * @param unknown_type $exception
-         */
-        public function saveLog($success, $header, $body, $mailer, $address, $subject,  $exception = '') {
-
-            $item = parent::createNewItem();
-            $success = ($success == true);
-            $item->set('success', $success);
-            $item->set('mailer', $mailer);
-            $item->set('idclient', $this->_idclient);
-            $item->set('idlang', $this->_idlang);
-            $date =  date('Y-m-d H:i:s', time());
-            $item->set('from', $address['from']);
-            $item->set('to', $address['to']);
-            $item->set('cc', implode('+', $address['cc']));
-            $item->set('bcc', implode('+', $address['bcc']));
-            $item->set('reply_to', $address['reply_to']);
-            $item->set('created', $date, false);
-            $item->set('subject', $subject);
-            $item->set('exception', $exception);
-            $fileName = md5($item->getField('idmail'). $date);
-
-            if (cFileHandler::write($this->mailLogDirectory . 'body_'. $fileName .'.'. $this->mailLogEnding, $body) === false) {
-                return false;
-            }
-
-            if (cFileHandler::write($this->mailLogDirectory . 'header_'.$fileName .'.'. $this->mailLogEnding, $header) === false) {
-                return false;
-            }
-
-
-
-
-            $item->store();
-
-            return true;
-
-
-        }
-
+        return true;
 
     }
+
+}
+
 ?>
