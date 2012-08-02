2eb75d5fc05cb603c0a779d3ada432b7f8b5aef0
Murat Purc <murat@purc.de>
2012-08-02 21:34:25 +0000

    Formatting

--- contenido/includes/include.con_editcontent.php
+++ contenido/includes/include.con_editcontent.php
@@ -23,15 +23,8 @@
  *
  * {@internal
  *   created  2003
- *   modified 2008-06-16, Holger Librenz, Hotfix: check for illegal calls added
- *   modified 2008-06-27, Frederic Schneider, add security fix
- *   modified 2009-10-29, Murat Purc, replaced deprecated functions (PHP 5.3 ready) and some formatting
- *   modified 2010-05-20, Murat Purc, removed request check during processing ticket [#CON-307]
- *   modified 2011-01-11, Rusmir Jusufovic, load output of moduls from file
- *   modified 2011-06-24, Rusmir Jusufovic, load layout code from file
  *   $Id$:
  * }}
- *
  */
 
 if (!defined('CON_FRAMEWORK')) {
@@ -42,7 +35,6 @@ if (!isset($idcat)) {
     cRegistry::shutdown();
     return;
 }
-
 
 $edit = 'true';
 $db2 = cRegistry::getDb();
@@ -70,7 +62,7 @@ if ($action == 20 || $action == 10) {
         conMakeArticleIndex($idartlang, $idart);
 
         // restore orginal values
-        $data  = $_REQUEST['data'];
+        $data = $_REQUEST['data'];
         $value = $_REQUEST['value'];
     }
 
@@ -78,81 +70,82 @@ if ($action == 20 || $action == 10) {
 }
 
 $areaCode = '';
-if(isset($area) && $area == 'con_content_list'){
-    $areaCode = '&area='.$area;
+if (isset($area) && $area == 'con_content_list') {
+    $areaCode = '&area=' . $area;
 }
 if ($action == 10) {
     header('Location: ' . $cfg['path']['contenido_fullhtml'] . $cfg['path']['includes']
-        . "include.backendedit.php?type=$type&typenr=$typenr&client=$client&lang=$lang&idcat=$idcat&idart=$idart&idartlang=$idartlang&contenido=$contenido&lang=$lang$areaCode");
+            . "include.backendedit.php?type=$type&typenr=$typenr&client=$client&lang=$lang&idcat=$idcat&idart=$idart&idartlang=$idartlang&contenido=$contenido&lang=$lang$areaCode");
+    return;
+}
+
+//@fulai.zhang: Mark submenuitem 'Editor' in the CONTENIDO Backend (Area: Contenido --> Articles --> Editor)
+$markSubItem = markSubMenuItem(4, true);
+
+//Include tiny class
+include($cfg['path']['contenido'] . 'external/wysiwyg/tinymce3/editorclass.php');
+$oEditor = new cTinyMCEEditor('', '');
+$oEditor->setToolbar('inline_edit');
+
+//Get configuration for popup und inline tiny
+$sConfigInlineEdit = $oEditor->getConfigInlineEdit();
+$sConfigFullscreen = $oEditor->getConfigFullscreen();
+
+
+//Replace vars in Script
+$oScriptTpl = new cTemplate();
+
+$oScriptTpl->set('s', 'CONTENIDO_FULLHTML', $cfg['path']['contenido_fullhtml']);
+
+//Set urls to file browsers
+$oScriptTpl->set('s', 'IMAGE', $cfg['path']['contenido_fullhtml'] . 'frameset.php?area=upl&contenido=' . $sess->id . '&appendparameters=imagebrowser');
+$oScriptTpl->set('s', 'FILE', $cfg['path']['contenido_fullhtml'] . 'frameset.php?area=upl&contenido=' . $sess->id . '&appendparameters=filebrowser');
+$oScriptTpl->set('s', 'FLASH', $cfg['path']['contenido_fullhtml'] . 'frameset.php?area=upl&contenido=' . $sess->id . '&appendparameters=imagebrowser');
+$oScriptTpl->set('s', 'MEDIA', $cfg['path']['contenido_fullhtml'] . 'frameset.php?area=upl&contenido=' . $sess->id . '&appendparameters=imagebrowser');
+$oScriptTpl->set('s', 'FRONTEND', $cfgClient[$client]['path']['htmlpath']);
+
+//Add tiny options and fill function leave_check()
+$oScriptTpl->set('s', 'TINY_OPTIONS', $sConfigInlineEdit);
+$oScriptTpl->set('s', 'TINY_FULLSCREEN', $sConfigFullscreen);
+$oScriptTpl->set('s', 'IDARTLANG', $idartlang);
+$oScriptTpl->set('s', 'CON_PATH', $cfg['path']['contenido_fullhtml']);
+$oScriptTpl->set('s', 'CLOSE', i18n('Close editor'));
+$oScriptTpl->set('s', 'SAVE', i18n('Close editor and save changes'));
+$oScriptTpl->set('s', 'QUESTION', i18n('Do you want to save changes?'));
+
+if (getEffectiveSetting('system', 'insight_editing_activated', 'true') == 'false') {
+    $oScriptTpl->set('s', 'USE_TINY', '');
 } else {
-    //@fulai.zhang: Mark submenuitem 'Editor' in the CONTENIDO Backend (Area: Contenido --> Articles --> Editor)
-    $markSubItem = markSubMenuItem(4, true);
+    $oScriptTpl->set('s', 'USE_TINY', 'swapTiny(this);');
+}
 
-    //Include tiny class
-    include($cfg['path']['contenido'] . 'external/wysiwyg/tinymce3/editorclass.php');
-    $oEditor = new cTinyMCEEditor('', '');
-    $oEditor->setToolbar('inline_edit');
+$scripts = $oScriptTpl->generate($cfg['path']['contenido'] . $cfg['path']['templates'] . $cfg['templates']['con_editcontent'], 1);
 
-    //Get configuration for popup und inline tiny
-    $sConfigInlineEdit = $oEditor->getConfigInlineEdit();
-    $sConfigFullscreen = $oEditor->getConfigFullscreen();
-
-
-    //Replace vars in Script
-    $oScriptTpl = new cTemplate();
-
-    $oScriptTpl->set('s', 'CONTENIDO_FULLHTML', $cfg['path']['contenido_fullhtml']);
-
-    //Set urls to file browsers
-    $oScriptTpl->set('s', 'IMAGE', $cfg['path']['contenido_fullhtml'] . 'frameset.php?area=upl&contenido=' . $sess->id . '&appendparameters=imagebrowser');
-    $oScriptTpl->set('s', 'FILE', $cfg['path']['contenido_fullhtml'] . 'frameset.php?area=upl&contenido=' . $sess->id . '&appendparameters=filebrowser');
-    $oScriptTpl->set('s', 'FLASH', $cfg['path']['contenido_fullhtml'] . 'frameset.php?area=upl&contenido=' . $sess->id . '&appendparameters=imagebrowser');
-    $oScriptTpl->set('s', 'MEDIA', $cfg['path']['contenido_fullhtml'] . 'frameset.php?area=upl&contenido=' . $sess->id . '&appendparameters=imagebrowser');
-    $oScriptTpl->set('s', 'FRONTEND', $cfgClient[$client]['path']['htmlpath']);
-
-    //Add tiny options and fill function leave_check()
-    $oScriptTpl->set('s', 'TINY_OPTIONS', $sConfigInlineEdit);
-    $oScriptTpl->set('s', 'TINY_FULLSCREEN', $sConfigFullscreen);
-    $oScriptTpl->set('s', 'IDARTLANG', $idartlang);
-    $oScriptTpl->set('s', 'CON_PATH', $cfg['path']['contenido_fullhtml']);
-    $oScriptTpl->set('s', 'CLOSE', i18n('Close editor'));
-    $oScriptTpl->set('s', 'SAVE', i18n('Close editor and save changes'));
-    $oScriptTpl->set('s', 'QUESTION', i18n('Do you want to save changes?'));
-
-    if (getEffectiveSetting('system', 'insight_editing_activated', 'true') == 'false') {
-        $oScriptTpl->set('s', 'USE_TINY', '');
-    } else {
-        $oScriptTpl->set('s', 'USE_TINY', 'swapTiny(this);');
-    }
-
-    $scripts = $oScriptTpl->generate($cfg['path']['contenido'] . $cfg['path']['templates'] . $cfg['templates']['con_editcontent'], 1);
-
-    $contentform  = '
+$contentform = '
 <form name="editcontent" method="post" action="' . $sess->url($cfg['path']['contenido_fullhtml'] . "external/backendedit/front_content.php?area=con_editcontent&idart=$idart&idcat=$idcat&lang=$lang&action=20&client=$client") . '">
-    <input type="hidden" name="changeview" value="edit">
-    <input type="hidden" name="data" value="">
+<input type="hidden" name="changeview" value="edit">
+<input type="hidden" name="data" value="">
 </form>
 ';
 
-    // generate code
-    $code = conGenerateCode($idcat, $idart, $lang, $client, false, false);
-    if($code == "0601") {
-        markSubMenuItem("1");
-        $code = "<script type='text/javascript'>location.href = '".$cfg['path']['contenido_fullhtml']."main.php?frame=4&area=con_editart&action=con_edit&idart=".$idart."&idcat=".$idcat."&contenido=".$contenido."'; console.log(location.href);</script>";
-    } else {
-        // inject some additional markup
-        $code = cString::iReplaceOnce("</head>", "$markSubItem $scripts\n<meta http-equiv=\"Content-Type\" content=\"text/html; charset=$encoding[$lang]\"></head>", $code);
-        $code = cString::iReplaceOnceReverse("</body>", "$contentform</body>", $code);
-        $code = cString::iReplaceOnce("<head>", "<head>\n" . '<base href="' . $cfgClient[$client]["path"]["htmlpath"] . '">', $code);
-    }
-
-    if ($cfg["debug"]["codeoutput"]) {
-        cDebug(htmlspecialchars($code));
-    }
-
-    chdir($cfgClient[$client]["path"]["frontend"]);
-    eval("?>\n".$code."\n<?php\n");
+// generate code
+$code = conGenerateCode($idcat, $idart, $lang, $client, false, false);
+if ($code == "0601") {
+    markSubMenuItem("1");
+    $code = "<script type='text/javascript'>location.href = '" . $cfg['path']['contenido_fullhtml'] . "main.php?frame=4&area=con_editart&action=con_edit&idart=" . $idart . "&idcat=" . $idcat . "&contenido=" . $contenido . "'; console.log(location.href);</script>";
+} else {
+    // inject some additional markup
+    $code = cString::iReplaceOnce("</head>", "$markSubItem $scripts\n<meta http-equiv=\"Content-Type\" content=\"text/html; charset=$encoding[$lang]\"></head>", $code);
+    $code = cString::iReplaceOnceReverse("</body>", "$contentform</body>", $code);
+    $code = cString::iReplaceOnce("<head>", "<head>\n" . '<base href="' . $cfgClient[$client]["path"]["htmlpath"] . '">', $code);
 }
+
+if ($cfg["debug"]["codeoutput"]) {
+    cDebug(htmlspecialchars($code));
+}
+
+chdir($cfgClient[$client]["path"]["frontend"]);
+eval("?>\n" . $code . "\n<?php\n");
 
 cRegistry::shutdown();
 
