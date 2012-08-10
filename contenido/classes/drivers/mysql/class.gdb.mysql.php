cb7ab19de9e3ac0be8f6a8c4b5ccc25dee34a66e
Murat Purc <murat@purc.de>
2012-08-10 00:15:48 +0000

    Restored "drivers" folder in contenido/classes. Otherwhise it results in fatal PHP error, if user tries an update by overwriting existing installation.

---
+++ contenido/classes/drivers/mysql/class.gdb.mysql.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Project: 
+ * Contenido Content Management System
+ * 
+ * Description: 
+ * MySQL Driver for GenericDB 
+ * 
+ * Requirements: 
+ * @con_php_req 5.0
+ * 
+ *
+ * @package    Contenido Backend classes
+ * @version    1.12
+ * @author     Bjoern Behrens
+ * @copyright  four for business AG <www.4fb.de>
+ * @license    http://www.contenido.org/license/LIZENZ.txt
+ * @link       http://www.4fb.de
+ * @link       http://www.contenido.org
+ * @deprecated [2012-08-10] Use class in contenido/classes/genericdb/class.gdb.mysql.php
+ *                          - Use cGenericDbDriverMysql
+ * 
+ * {@internal 
+ *   created 2006-05-10
+ *   $Id$
+ * }}
+ */
+
+if (!defined('CON_FRAMEWORK')) {
+	die('Illegal call');
+}
+
+?>
