9ac1710aa169648204019de9e04772690c3741b8
Simon Sprankel <discard.simon.sprankel@4fb.de>
2012-08-15 13:16:02 +0000

    CON-606 Unexpected appearance and behaviour of CodeMirror editor Updated CodeMirror to version 2.32

--- contenido/external/codemirror/class.codemirror.php
+++ contenido/external/codemirror/class.codemirror.php
@@ -5,20 +5,17 @@
  *
  * Description: This file defines the CodeMirror editor integration class.
  *
- * @package    CONTENIDO Backend
- * @version    1.0.0
- * @author     Dominik Ziegler
- * @copyright  four for business AG <www.4fb.de>
- * @license    http://www.contenido.org/license/LIZENZ.txt
- * @link       http://www.4fb.de
- * @since      file available since CONTENIDO 4.9.0
+ * @package CONTENIDO Backend
+ * @version 1.0.0
+ * @author Dominik Ziegler
+ * @copyright four for business AG <www.4fb.de>
+ * @license http://www.contenido.org/license/LIZENZ.txt
+ * @link http://www.4fb.de
+ * @since file available since CONTENIDO 4.9.0
  *
- * $Id:
  */
 
-
 /**
- *
  * Description: Class for handling and displaying CodeMirror
  *
  * @version 1.0.0
@@ -26,254 +23,261 @@
  * @copyright four for business AG <www.4fb.de>
  */
 class CodeMirror {
-    /**
-      * Properties which were used to init CodeMirror
-      *
-      * @var array
-      * @access private
-      */
-    private $_aProperties = array();
 
     /**
-      * HTML-ID of textarea which is replaced by CodeMirror
-      *
-      * @var string
-      * @access private
-      */
-    private $_sTextareaId  = '';
+     * Properties which were used to init CodeMirror
+     *
+     * @var array
+     * @access private
+     */
+    private $_properties = array();
 
     /**
-      * defines if textarea is used or not (by system/client/user property)
-      *
-      * @var boolean
-      * @access private
-      */
-    private $_bActivated = true;
+     * HTML-ID of textarea which is replaced by CodeMirror
+     *
+     * @var string
+     * @access private
+     */
+    private $_textareaId = '';
 
     /**
-      * defines if js-script for CodeMirror is included on rendering process
-      *
-      * @var boolean
-      * @access private
-      */
-    private $_bAddScript = true;
+     * defines if textarea is used or not (by system/client/user property)
+     *
+     * @var boolean
+     * @access private
+     */
+    private $_activated = true;
 
     /**
-      * The CONTENIDO configuration array
-      *
-      * @var array
-      * @access private
-      */
-    private $_aCfg = array();
-
-	/**
-      * Language of CodeMirror
-      *
-      * @var string
-      * @access private
-      */
-    private $_sLanguage  = '';
-
-	/**
-      * Syntax of CodeMirror
-      *
-      * @var string
-      * @access private
-      */
-    private $_sSyntax  = '';
-
-    /*################################################################*/
+     * defines if js-script for CodeMirror is included on rendering process
+     *
+     * @var boolean
+     * @access private
+     */
+    private $_addScript = true;
 
     /**
-      * Constructor of CodeMirror initializes class variables
-      *
-      * @param  string $sId - The id of textarea which is replaced by editor
-      * @param  string $sSyntax - Name of syntax highlighting which is used (html, css, js, php, ...)
-      * @param  string $sLang - lang which is used into editor. Notice NOT CONTENIDO language id
-      *                         ex: de, en ... To get it from CONTENIDO language use:
-      *                         substr(strtolower($belang), 0, 2) in backend
-      * @param  boolean $bAddScript - defines if CodeMirror script is included or not
-      *                               interesting when there is more than only one editor on page
-      * @param  array $aCfg - The CONTENIDO configuration array
-      * @param  boolean $bEditable - Optional defines if content is editable or not
-      *
-      * @access public
-      */
-    public function __construct($sId, $sSyntax, $sLang, $bAddScript, $aCfg, $bEditable = true) {
-        //init class variables
-        $this->_aProperties = array();
-        $this->_aCfg = (array) $aCfg;
-        $this->_bAddScript = (boolean) $bAddScript;
-        $this->_sTextareaId = (string) $sId;
-        $this->_bActivated = true;
-		$this->_sLanguage = (string) $sLang;
-		$this->_sSyntax = (string) $sSyntax;
+     * The CONTENIDO configuration array
+     *
+     * @var array
+     * @access private
+     */
+    private $_cfg = array();
 
-        //make content not editable if not allowed
-        if ($bEditable == false) {
+    /**
+     * Language of CodeMirror
+     *
+     * @var string
+     * @access private
+     */
+    private $_language = '';
+
+    /**
+     * Syntax of CodeMirror
+     *
+     * @var string
+     * @access private
+     */
+    private $_syntax = '';
+
+    /**
+     * Constructor of CodeMirror initializes class variables
+     *
+     * @param string $id - The id of textarea which is replaced by editor
+     * @param string $syntax - Name of syntax highlighting which is used (html,
+     *        css, js, php, ...)
+     * @param string $lang - lang which is used into editor. Notice NOT
+     *        CONTENIDO language id
+     *        ex: de, en ... To get it from CONTENIDO language use:
+     *        substr(strtolower($belang), 0, 2) in backend
+     * @param boolean $addScript - defines if CodeMirror script is included or
+     *        not
+     *        interesting when there is more than only one editor on page
+     * @param array $cfg - The CONTENIDO configuration array
+     * @param boolean $editable - Optional defines if content is editable or not
+     *
+     * @access public
+     */
+    public function __construct($id, $syntax, $lang, $addScript, $cfg, $editable = true) {
+        // init class variables
+        $this->_properties = array();
+        $this->_cfg = (array) $cfg;
+        $this->_addScript = (boolean) $addScript;
+        $this->_textareaId = (string) $id;
+        $this->_activated = true;
+        $this->_language = (string) $lang;
+        $this->_syntax = (string) $syntax;
+
+        // make content not editable if not allowed
+        if ($editable == false) {
             $this->setProperty('readOnly', 'true', true);
         }
 
-		$this->setProperty('lineNumbers', 'true', true);
-		$this->setProperty('matchBrackets', 'true', true);
-		$this->setProperty('indentUnit', 4, true);
-		$this->setProperty('indentWithTabs', 'true', true);
-		$this->setProperty('enterMode', 'keep', false);
-		$this->setProperty('tabMode', 'shift', false);
+        $this->setProperty('lineNumbers', 'true', true);
+        $this->setProperty('lineWrapping', 'true', true);
+        $this->setProperty('matchBrackets', 'true', true);
+        $this->setProperty('indentUnit', 4, true);
+        $this->setProperty('indentWithTabs', 'true', true);
+        $this->setProperty('enterMode', 'keep', false);
+        $this->setProperty('tabMode', 'shift', false);
 
-        //internal function which appends more properties to $this->setProperty wich where defined
-        //by user or sysadmin in systemproperties / client settings / user settings ...
+        // internal function which appends more properties to $this->setProperty
+        // wich where defined
+        // by user or sysadmin in systemproperties / client settings / user
+        // settings ...
         $this->_getSystemProperties();
     }
 
     /**
-      * Function gets properties from CONTENIDO for CodeMirror and stores it into
-      * $this->setProperty so user is able to overwride standard settings or append
-      * other settings. Function also checks if CodeMirror is activated or deactivated
-      * by user
-      *
-      * @access private
-      */
+     * Function gets properties from CONTENIDO for CodeMirror and stores it into
+     * $this->setProperty so user is able to overwride standard settings or
+     * append
+     * other settings.
+     * Function also checks if CodeMirror is activated or deactivated
+     * by user
+     *
+     * @access private
+     */
     private function _getSystemProperties() {
-        //check if editor is disabled or enabled by user/admin
-        if (getEffectiveSetting("codemirror", "activated", "true") == "false") {
-            $this->_bActivated = false;
+        // check if editor is disabled or enabled by user/admin
+        if (getEffectiveSetting('codemirror', 'activated', 'true') == 'false') {
+            $this->_activated = false;
         }
 
-        $aUserSettings = getEffectiveSettingsByType("codemirror");
-        foreach ($aUserSettings as $sKey => $sValue) {
-            if ($sKey != 'activated') {
-                if ($sValue == "true" || $sValue == "false" || is_numeric($sValue)) {
-                    $this->setProperty($sKey, $sValue, true);
+        $userSettings = getEffectiveSettingsByType('codemirror');
+        foreach ($userSettings as $key => $value) {
+            if ($key != 'activated') {
+                if ($value == 'true' || $value == 'false' || is_numeric($value)) {
+                    $this->setProperty($key, $value, true);
                 } else {
-                    $this->setProperty($sKey, $sValue, false);
+                    $this->setProperty($key, $value, false);
                 }
             }
         }
     }
 
     /**
-      * Function for setting a property for CodeMirror to $this->setProperty
-      * existing properties were overwritten
-      *
-      * @param  string $sName - Name of CodeMirror property
-      * @param  string $sValue - Value of CodeMirror property
-      * @param  boolean $bIsNumeric - Defines if value is numeric or not
-      *                               in case of a numeric value, there is no need to use
-      *                               quotes
-      *
-      * @access public
-      */
-    public function setProperty($sName, $sValue, $bIsNumeric = false) {
-        //datatype check
-        $sName = (string) $sName;
-        $sValue = (string) $sValue;
-        $bIsNumeric = (boolean) $bIsNumeric;
+     * Function for setting a property for CodeMirror to $this->setProperty
+     * existing properties were overwritten
+     *
+     * @param string $name - Name of CodeMirror property
+     * @param string $value - Value of CodeMirror property
+     * @param boolean $isNumeric - Defines if value is numeric or not
+     *        in case of a numeric value, there is no need to use
+     *        quotes
+     *
+     * @access public
+     */
+    public function setProperty($name, $value, $isNumeric = false) {
+        // datatype check
+        $name = (string) $name;
+        $value = (string) $value;
+        $isNumeric = (boolean) $isNumeric;
 
-        //generate a new array for new property
-        $aRecord = array();
-        $aRecord['name'] = $sName;
-        $aRecord['value'] = $sValue;
-        $aRecord['is_numeric'] = $bIsNumeric;
+        // generate a new array for new property
+        $record = array();
+        $record['name'] = $name;
+        $record['value'] = $value;
+        $record['is_numeric'] = $isNumeric;
 
-        //append it to class variable $this->aProperties
-        //when key already exists, overwride it
-        $this->_aProperties[$sName] = $aRecord;
+        // append it to class variable $this->aProperties
+        // when key already exists, overwride it
+        $this->_properties[$name] = $record;
     }
 
-	private function _getSyntaxScripts() {
-		$sPath = $this->_aCfg['path']['contenido_fullhtml'] . 'external/codemirror';
+    private function _getSyntaxScripts() {
+        $path = $this->_cfg['path']['contenido_fullhtml'] . 'external/codemirror';
 
-		$sJs = '';
-		$sJsTemplate = '<script type="text/javascript" src="%s/mode/%s/%s.js"></script>';
+        $js = '';
+        $jsTemplate = '<script type="text/javascript" src="%s/mode/%s/%s.js"></script>';
 
-		$aModes = array();
+        $modes = array();
 
-		$sSyntax = $this->_sSyntax;
-		if ($sSyntax == 'js' || $sSyntax == 'html' || $sSyntax == 'php') {
-			$aModes[] = 'javascript';
-		}
+        $syntax = $this->_syntax;
+        if ($syntax == 'js' || $syntax == 'html' || $syntax == 'php') {
+            $modes[] = 'javascript';
+        }
 
-		if ($sSyntax == 'css' || $sSyntax == 'html' || $sSyntax == 'php') {
-			$aModes[] = 'css';
-		}
+        if ($syntax == 'css' || $syntax == 'html' || $syntax == 'php') {
+            $modes[] = 'css';
+        }
 
-		if ($sSyntax == 'html' || $sSyntax == 'php') {
-			$aModes[] = 'xml';
-		}
+        if ($syntax == 'html' || $syntax == 'php') {
+            $modes[] = 'xml';
+        }
 
-		if ($sSyntax == 'php') {
-			$aModes[] = 'php';
-			$aModes[] = 'clike';
-		}
+        if ($syntax == 'php') {
+            $modes[] = 'php';
+            $modes[] = 'clike';
+        }
 
-		if ($sSyntax == 'html') {
-			$aModes[] = 'htmlmixed';
-		}
+        if ($syntax == 'html') {
+            $modes[] = 'htmlmixed';
+        }
 
-		foreach ($aModes as $sMode) {
-			$sJs .= sprintf($sJsTemplate, $sPath, $sMode, $sMode) . PHP_EOL;
-		}
+        foreach ($modes as $mode) {
+            $js .= sprintf($jsTemplate, $path, $mode, $mode) . PHP_EOL;
+        }
 
-		return $sJs;
-	}
+        return $js;
+    }
 
-	private function _getSyntaxName() {
-		if ($this->_sSyntax == 'php') {
-			return 'application/x-httpd-php';
-		}
+    private function _getSyntaxName() {
+        if ($this->_syntax == 'php') {
+            return 'application/x-httpd-php';
+        }
 
-		if ($this->_sSyntax == 'html') {
-			return 'text/html';
-		}
+        if ($this->_syntax == 'html') {
+            return 'text/html';
+        }
 
-		if ($this->_sSyntax == 'css') {
-			return 'text/css';
-		}
+        if ($this->_syntax == 'css') {
+            return 'text/css';
+        }
 
-		if ($this->_sSyntax == 'js') {
-			return 'text/javascript';
-		}
-	}
+        if ($this->_syntax == 'js') {
+            return 'text/javascript';
+        }
+    }
 
     /**
-      * Function renders js_script for inclusion into an header of a html file
-      *
-      * @return string - js_script for CodeMirror
-      * @access public
-      */
+     * Function renders js_script for inclusion into an header of a html file
+     *
+     * @return string - js_script for CodeMirror
+     * @access public
+     */
     public function renderScript() {
-        //if editor is disabled, there is no need to render this script
-        if ($this->_bActivated == false) {
+        // if editor is disabled, there is no need to render this script
+        if ($this->_activated == false) {
             return '';
         }
 
-        //if external js file for editor should be included, do this here
-        $sJs = '';
-        if ($this->_bAddScript) {
-			$sConPath = $this->_aCfg['path']['contenido_fullhtml'];
-            $sPath = $sConPath . 'external/codemirror/';
+        // if external js file for editor should be included, do this here
+        $js = '';
+        if ($this->_addScript) {
+            $conPath = $this->_cfg['path']['contenido_fullhtml'];
+            $path = $conPath . 'external/codemirror/';
 
-			$sLanguage = $this->_sLanguage;
-			if (!file_exists($this->_aCfg['path']['contenido'] . "external/codemirror/lib/lang/" . $sLanguage . ".js")) {
-				$sLanguage = 'en';
-			}
+            $language = $this->_language;
+            if (!file_exists($this->_cfg['path']['contenido'] . 'external/codemirror/lib/lang/' . $language . '.js')) {
+                $language = 'en';
+            }
 
-			$sJs .= '<script type="text/javascript" src="' . $sPath . 'lib/lang/' . $sLanguage . '.js"></script>'. PHP_EOL;
-            $sJs .= '<script type="text/javascript" src="' . $sPath . 'lib/codemirror.js"></script>'. PHP_EOL;
-			$sJs .= '<script type="text/javascript" src="' . $sPath . 'lib/util/foldcode.js"></script>'. PHP_EOL;
-			$sJs .= '<script type="text/javascript" src="' . $sPath . 'lib/util/dialog.js"></script>'. PHP_EOL;
-			$sJs .= '<script type="text/javascript" src="' . $sPath . 'lib/util/searchcursor.js"></script>'. PHP_EOL;
-			$sJs .= '<script type="text/javascript" src="' . $sPath . 'lib/util/search.js"></script>'. PHP_EOL;
-			$sJs .= '<script type="text/javascript" src="' . $sPath . 'lib/contenido_integration.js"></script>'. PHP_EOL;
-			$sJs .= '<script type="text/javascript" src="' . $sConPath . 'scripts/jquery/jquery.js"></script>'. PHP_EOL;
-			$sJs .= $this->_getSyntaxScripts();
-            $sJs .= '<link rel="stylesheet" href="' . $sPath . 'lib/codemirror.css" />'. PHP_EOL;
-			$sJs .= '<link rel="stylesheet" href="' . $sPath . 'lib/util/dialog.css" />'. PHP_EOL;
+            $js .= '<script type="text/javascript" src="' . $path . 'lib/lang/' . $language . '.js"></script>' . PHP_EOL;
+            $js .= '<script type="text/javascript" src="' . $path . 'lib/codemirror.js"></script>' . PHP_EOL;
+            $js .= '<script type="text/javascript" src="' . $path . 'lib/util/foldcode.js"></script>' . PHP_EOL;
+            $js .= '<script type="text/javascript" src="' . $path . 'lib/util/dialog.js"></script>' . PHP_EOL;
+            $js .= '<script type="text/javascript" src="' . $path . 'lib/util/searchcursor.js"></script>' . PHP_EOL;
+            $js .= '<script type="text/javascript" src="' . $path . 'lib/util/search.js"></script>' . PHP_EOL;
+            $js .= '<script type="text/javascript" src="' . $path . 'lib/contenido_integration.js"></script>' . PHP_EOL;
+            $js .= '<script type="text/javascript" src="' . $conPath . 'scripts/jquery/jquery.js"></script>' . PHP_EOL;
+            $js .= $this->_getSyntaxScripts();
+            $js .= '<link rel="stylesheet" href="' . $path . 'lib/codemirror.css" />' . PHP_EOL;
+            $js .= '<link rel="stylesheet" href="' . $path . 'lib/util/dialog.css" />' . PHP_EOL;
         }
 
-        //define template for CodeMirror script
-        $sJs .= "<script type=\"text/javascript\">
+        // define template for CodeMirror script
+        $js .= "<script type=\"text/javascript\">
 					function toggleCodeMirrorFullscreen_{ID}() {
 						toggleCodeMirrorFullscreenEditor('{ID}');
                     }
@@ -286,24 +290,26 @@ class CodeMirror {
 					window.setTimeout('initCodeMirror(\"{ID}\", properties_{ID})', 100);
                 </script>";
 
-		$this->setProperty('mode', $this->_getSyntaxName(), false);
-		$this->setProperty('theme', 'default ' . $this->_sTextareaId, false);
+        $this->setProperty('mode', $this->_getSyntaxName(), false);
+        $this->setProperty('theme', 'default ' . $this->_textareaId, false);
 
-        //get all stored properties and convert it in order to insert it into CodeMirror js template
-        $sProperties = '';
-        foreach ($this->_aProperties as $aProperty) {
-            if ($aProperty['is_numeric'] == true) {
-                $sProperties .= ', '.$aProperty['name'].':'.$aProperty['value']."\n";
+        // get all stored properties and convert it in order to insert it into
+        // CodeMirror js template
+        $properties = '';
+        foreach ($this->_properties as $property) {
+            if ($property['is_numeric'] == true) {
+                $properties .= ', ' . $property['name'] . ':' . $property['value'] . "\n";
             } else {
-                $sProperties .= ', '.$aProperty['name'].': "'.$aProperty['value']."\"\n";
+                $properties .= ', ' . $property['name'] . ': "' . $property['value'] . "\"\n";
             }
         }
 
-        //fill js template
-        $sTextareaId = $this->_sTextareaId;
-		$sJsResult = str_replace('{ID}', $sTextareaId, $sJs);
-		$sJsResult = str_replace('{PROPERTIES}', $sProperties, $sJsResult);
+        // fill js template
+        $textareaId = $this->_textareaId;
+        $jsResult = str_replace('{ID}', $textareaId, $js);
+        $jsResult = str_replace('{PROPERTIES}', $properties, $jsResult);
 
-        return $sJsResult;
+        return $jsResult;
     }
+
 }
--- contenido/external/codemirror/lib/util/foldcode.js
+++ contenido/external/codemirror/lib/util/foldcode.js
@@ -1,8 +1,124 @@
-CodeMirror.braceRangeFinder = function(cm, line) {
+// the tagRangeFinder function is
+//   Copyright (C) 2011 by Daniel Glazman <daniel@glazman.org>
+// released under the MIT license (../../LICENSE) like the rest of CodeMirror
+CodeMirror.tagRangeFinder = function(cm, line, hideEnd) {
+  var nameStartChar = "A-Z_a-z\\u00C0-\\u00D6\\u00D8-\\u00F6\\u00F8-\\u02FF\\u0370-\\u037D\\u037F-\\u1FFF\\u200C-\\u200D\\u2070-\\u218F\\u2C00-\\u2FEF\\u3001-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFFD";
+  var nameChar = nameStartChar + "\-\:\.0-9\\u00B7\\u0300-\\u036F\\u203F-\\u2040";
+  var xmlNAMERegExp = new RegExp("^[" + nameStartChar + "][" + nameChar + "]*");
+
   var lineText = cm.getLine(line);
-  var startChar = lineText.lastIndexOf("{");
-  if (startChar < 0 || lineText.lastIndexOf("}") > startChar) return;
-  var tokenType = cm.getTokenAt({line: line, ch: startChar}).className;
+  var found = false;
+  var tag = null;
+  var pos = 0;
+  while (!found) {
+    pos = lineText.indexOf("<", pos);
+    if (-1 == pos) // no tag on line
+      return;
+    if (pos + 1 < lineText.length && lineText[pos + 1] == "/") { // closing tag
+      pos++;
+      continue;
+    }
+    // ok we weem to have a start tag
+    if (!lineText.substr(pos + 1).match(xmlNAMERegExp)) { // not a tag name...
+      pos++;
+      continue;
+    }
+    var gtPos = lineText.indexOf(">", pos + 1);
+    if (-1 == gtPos) { // end of start tag not in line
+      var l = line + 1;
+      var foundGt = false;
+      var lastLine = cm.lineCount();
+      while (l < lastLine && !foundGt) {
+        var lt = cm.getLine(l);
+        var gt = lt.indexOf(">");
+        if (-1 != gt) { // found a >
+          foundGt = true;
+          var slash = lt.lastIndexOf("/", gt);
+          if (-1 != slash && slash < gt) {
+            var str = lineText.substr(slash, gt - slash + 1);
+            if (!str.match( /\/\s*\>/ )) { // yep, that's the end of empty tag
+              if (hideEnd === true) l++;
+              return l;
+            }
+          }
+        }
+        l++;
+      }
+      found = true;
+    }
+    else {
+      var slashPos = lineText.lastIndexOf("/", gtPos);
+      if (-1 == slashPos) { // cannot be empty tag
+        found = true;
+        // don't continue
+      }
+      else { // empty tag?
+        // check if really empty tag
+        var str = lineText.substr(slashPos, gtPos - slashPos + 1);
+        if (!str.match( /\/\s*\>/ )) { // finally not empty
+          found = true;
+          // don't continue
+        }
+      }
+    }
+    if (found) {
+      var subLine = lineText.substr(pos + 1);
+      tag = subLine.match(xmlNAMERegExp);
+      if (tag) {
+        // we have an element name, wooohooo !
+        tag = tag[0];
+        // do we have the close tag on same line ???
+        if (-1 != lineText.indexOf("</" + tag + ">", pos)) // yep
+        {
+          found = false;
+        }
+        // we don't, so we have a candidate...
+      }
+      else
+        found = false;
+    }
+    if (!found)
+      pos++;
+  }
+
+  if (found) {
+    var startTag = "(\\<\\/" + tag + "\\>)|(\\<" + tag + "\\>)|(\\<" + tag + "\\s)|(\\<" + tag + "$)";
+    var startTagRegExp = new RegExp(startTag, "g");
+    var endTag = "</" + tag + ">";
+    var depth = 1;
+    var l = line + 1;
+    var lastLine = cm.lineCount();
+    while (l < lastLine) {
+      lineText = cm.getLine(l);
+      var match = lineText.match(startTagRegExp);
+      if (match) {
+        for (var i = 0; i < match.length; i++) {
+          if (match[i] == endTag)
+            depth--;
+          else
+            depth++;
+          if (!depth) {
+            if (hideEnd === true) l++;
+            return l;
+          }
+        }
+      }
+      l++;
+    }
+    return;
+  }
+};
+
+CodeMirror.braceRangeFinder = function(cm, line, hideEnd) {
+  var lineText = cm.getLine(line), at = lineText.length, startChar, tokenType;
+  for (;;) {
+    var found = lineText.lastIndexOf("{", at);
+    if (found < 0) break;
+    tokenType = cm.getTokenAt({line: line, ch: found}).className;
+    if (!/^(comment|string)/.test(tokenType)) { startChar = found; break; }
+    at = found - 1;
+  }
+  if (startChar == null || lineText.lastIndexOf("}") > startChar) return;
   var count = 1, lastLine = cm.lineCount(), end;
   outer: for (var i = line + 1; i < lastLine; ++i) {
     var text = cm.getLine(i), pos = 0;
@@ -20,11 +136,25 @@ CodeMirror.braceRangeFinder = function(cm, line) {
     }
   }
   if (end == null || end == line + 1) return;
+  if (hideEnd === true) end++;
   return end;
 };
 
+CodeMirror.indentRangeFinder = function(cm, line) {
+  var tabSize = cm.getOption("tabSize");
+  var myIndent = cm.getLineHandle(line).indentation(tabSize), last;
+  for (var i = line + 1, end = cm.lineCount(); i < end; ++i) {
+    var handle = cm.getLineHandle(i);
+    if (!/^\s*$/.test(handle.text)) {
+      if (handle.indentation(tabSize) <= myIndent) break;
+      last = i;
+    }
+  }
+  if (!last) return null;
+  return last + 1;
+};
 
-CodeMirror.newFoldFunction = function(rangeFinder, markText) {
+CodeMirror.newFoldFunction = function(rangeFinder, markText, hideEnd) {
   var folded = [];
   if (markText == null) markText = '<div style="position: absolute; left: 2px; color:#600">&#x25bc;</div>%N%';
 
@@ -49,7 +179,7 @@ CodeMirror.newFoldFunction = function(rangeFinder, markText) {
         folded.splice(known.pos, 1);
         expand(cm, known.region);
       } else {
-        var end = rangeFinder(cm, line);
+        var end = rangeFinder(cm, line, hideEnd);
         if (end == null) return;
         var hidden = [];
         for (var i = line + 1; i < end; ++i) {
