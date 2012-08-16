23aac3bad749cd043b95705d900ce003b8d9c3ac
Murat Purc <murat@purc.de>
2012-08-16 23:42:50 +0000

    Fixed wrong English texts, added possibility to xml files to be detected by translation text parser.

--- contenido/classes/gui/class.navigation.php
+++ contenido/classes/gui/class.navigation.php
@@ -256,7 +256,7 @@ class cGuiNavigation {
         $link->setClass('main');
         $link->setTargetFrame('content');
         $link->setLink($sess->url("frameset.php?area=mycontenido&frame=4"));
-        $link->setContent('<img src="' . $cfg['path']['contenido_fullhtml'] . $cfg['path']['images'] . 'my_contenido.gif" border="0" alt="My CONTENIDO" id="imgMyContenido" title="My CONTENIDO">');
+        $link->setContent('<img src="' . $cfg['path']['contenido_fullhtml'] . $cfg['path']['images'] . 'my_contenido.gif" border="0" alt="My CONTENIDO" id="imgMyContenido" title="' . i18n("My CONTENIDO") . '">');
         $main->set('s', 'MYCONTENIDO', $link->render());
 
         // info link
