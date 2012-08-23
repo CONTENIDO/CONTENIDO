5aecddac52b762ad1227f1f3ebcb08fa6364410f
Konstantinos Katikakis <discard.konstantinos.katikakis@4fb.de>
2012-08-23 08:50:39 +0000

    CON-687 The constants CON_PATH_BACKEND,CON_PATH_FRONTEND,CON_URL_BACKEND CON_URL_FRONTEND are removed again

--- contenido/includes/startup.php
+++ contenido/includes/startup.php
@@ -113,19 +113,6 @@ if (cFileHandler::exists($cfg['path']['contenido_config'] . 'config.local.php'))
     require_once($cfg['path']['contenido_config'] . 'config.local.php');
 }
 
-/*
- * Constants with paths after the last possible place changing via configuration file.
- * The frontend path point to the current client
- */
-define('CON_PATH_BACKEND' , $cfg['path']['contenido']);
-define('CON_PATH_FRONTEND' , $cfgClient[$client]['path']['frontend']);
-
-/*
- * Constants with URL after the last possible place changing via configuration file.
- * The frontend path point to the current client
- */
-define('CON_URL_BACKEND' , $cfg['path']['contenido_fullhtml']);
-define('CON_URL_FRONTEND' , $cfgClient[$client]['path']['htmlpath']);
 
 // Takeover configured PHP settings
 if ($cfg['php_settings'] && is_array($cfg['php_settings'])) {
