23c9cbddd67b076e36aee3fcf121dcab01d9e8a5
Simon Sprankel <discard.simon.sprankel@4fb.de>
2012-09-07 08:41:12 +0000

    CON-728 Rework getEncodingByLanguage to use cApiLanguage

--- contenido/includes/functions.encoding.php
+++ contenido/includes/functions.encoding.php
@@ -10,19 +10,14 @@
  * @con_php_req 5.0
  *
  *
- * @package    CONTENIDO Backend Includes
- * @version    1.3.1
- * @author     Holger Librenz
- * @copyright  four for business AG <www.4fb.de>
- * @license    http://www.contenido.org/license/LIZENZ.txt
- * @link       http://www.4fb.de
- * @link       http://www.contenido.org
- * @since      file available since CONTENIDO release <= 4.6
- *
- * {@internal
- *   created unknown
- *   $Id$:
- * }}
+ * @package CONTENIDO Backend Includes
+ * @version 1.3.1
+ * @author Holger Librenz
+ * @copyright four for business AG <www.4fb.de>
+ * @license http://www.contenido.org/license/LIZENZ.txt
+ * @link http://www.4fb.de
+ * @link http://www.contenido.org
+ * @since file available since CONTENIDO release <= 4.6
  */
 
 if (!defined('CON_FRAMEWORK')) {
@@ -30,62 +25,76 @@ if (!defined('CON_FRAMEWORK')) {
 }
 
 /**
- * Returns encoding for language with ID $iLang (global $lang in CONTENIDO style).
- * The parameter $oDb has to be an instance of DB_Contenido (global $db in con) and
+ * Returns encoding for language with ID $iLang (global $lang in CONTENIDO
+ * style).
+ * The parameter $db has to be an instance of DB_Contenido (global $db in con)
+ * and
  * $cfg is the equivalent to global $cfg array in CONTENIDO.
- * If no encoding is found or any parameter is not valid, the function will return
+ * If no encoding is found or any parameter is not valid, the function will
+ * return
  * false, otherwise the encoding as string like it is stored in database.
- * modified 18.03.2008 - Removed special mySQl behaviour (using db object instead) Timo Trautmann
+ * modified 18.03.2008 - Removed special mySQl behaviour (using db object
+ * instead) Timo Trautmann
  *
- * @param DB_Contenido $oDb
- * @param int $iLang
+ * @param DB_Contenido $db
+ * @param int $lang
  * @param array $cfg
  * @return string
  */
-function getEncodingByLanguage (&$oDb, $iLang, $cfg) {
-    $sResult = false;
-
-    if (!is_object($oDb)) {
-        $oDb = cRegistry::getDb();
+function getEncodingByLanguage(&$db, $lang, $cfg) {
+    // check parameters and use cRegistry values if they are invalid
+    if (!is_object($db)) {
+        $db = cRegistry::getDb();
+    }
+    if (!is_numeric($lang)) {
+        $lang = cRegistry::getLanguageId();
+    }
+    if (!is_array($cfg)) {
+        $cfg = cRegistry::getConfig();
     }
 
-    $iLang = (int) $iLang;
-    if ($iLang > 0 && is_array($cfg) && is_array($cfg['tab'])) {
-        // prepare query
-        $sQuery = "
-        SELECT
-            encoding
-        FROM
-            " .  $cfg["tab"]["lang"] . "
-        WHERE
-            idlang = " . cSecurity::toInteger($iLang);
-
-        if ($oDb->query($sQuery)) {
-            if ($oDb->next_record()) {
-                $sResult = trim($oDb->f('encoding'));
-            }
+    $lang = cSecurity::toInteger($lang);
+    if ($lang > 0 && is_array($cfg) && is_array($cfg['tab'])) {
+        // load the language object with the given ID and return the encoding
+        $apiLanguage = new cApiLanguage($lang);
+        if ($apiLanguage->isLoaded()) {
+            return trim($apiLanguage->get('encoding'));
         }
     }
 
-    return $sResult;
+    return false;
 }
 
 /**
  * Special version of htmlentites for iso-8859-2
  * Returns transformed string
  *
- * @param string $sInput
+ * @param string $input
  * @return string
  */
-
-function htmlentities_iso88592 ($sInput = '') {
-
-    $arrEntities_pl = array('&ecirc;', '&oacute;', '&plusmn;', '&para;', '&sup3;', '&iquest;', '&frac14;', '&aelig;', '&ntilde;', '&Ecirc;', '&Oacute;', '&iexcl;', '&brvbar;', '&pound;', '&not;', '&macr;', '&AElig;', '&Ntilde;');
+function htmlentities_iso88592($input = '') {
+    $arrEntities_pl = array(
+        '&ecirc;',
+        '&oacute;',
+        '&plusmn;',
+        '&para;',
+        '&sup3;',
+        '&iquest;',
+        '&frac14;',
+        '&aelig;',
+        '&ntilde;',
+        '&Ecirc;',
+        '&Oacute;',
+        '&iexcl;',
+        '&brvbar;',
+        '&pound;',
+        '&not;',
+        '&macr;',
+        '&AElig;',
+        '&Ntilde;'
+    );
     $arrEntities = get_html_translation_table(HTML_ENTITIES);
     $arrEntities = array_diff($arrEntities, $arrEntities_pl);
 
-    return strtr($sInput, $arrEntities);
-
+    return strtr($input, $arrEntities);
 }
-
-?>
