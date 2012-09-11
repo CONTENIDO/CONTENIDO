7f5067f5dd54041a192d49da07fd804c7a9659c2
Murat Purc <murat@purc.de>
2012-09-11 21:48:52 +0000

    Improvement: Checking write permissions in cms-folder during migration setup [#CON-251]

--- setup/steps/forms/systemtest.php
+++ setup/steps/forms/systemtest.php
@@ -712,7 +712,6 @@ class cSetupSystemtest extends cSetupMask
 								
 		if ($_SESSION["setuptype"] == "setup" || ($_SESSION["setuptype"] == "migration" && is_dir("../cms/")))
 		{
-echo '<pre>setup migration logFilePrediction</pre>';
 			$this->logFilePrediction(   "cms/cache/",
 										C_SEVERITY_WARNING);
 			$this->logFilePrediction(   "cms/css/",
