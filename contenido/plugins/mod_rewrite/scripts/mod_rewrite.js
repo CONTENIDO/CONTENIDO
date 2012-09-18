114a3c001291ce4019eb57db07f0053911e78d23
Murat Purc <murat@purc.de>
2012-09-18 22:05:21 +0000

    Bugfix: Fixed wrong JavaScript logic in AMR plug-in

--- contenido/plugins/mod_rewrite/scripts/mod_rewrite.js
+++ contenido/plugins/mod_rewrite/scripts/mod_rewrite.js
@@ -42,20 +42,29 @@ var mrPlugin = {
 
     initializeSettingsPage: function() {
         $(document).ready(function() {
-            $("#mr_use_language").click(function() {
-                $("#mr_use_language_name").attr("disabled", ($(this).attr("checked") ? "" : "disabled"));
-            });
-
-            $("#mr_use_client").click(function() {
-                $("#mr_use_client_name").attr("disabled", ($(this).attr("checked") ? "" : "disabled"));
-            });
-
-            $("#mr_add_startart_name_to_url").click(function() {
-                $("#mr_default_startart_name").attr("disabled", ($(this).attr("checked") ? "" : "disabled"));
-                if ($(this).attr("checked")) {
-                    $("#mr_default_startart_name").removeClass("disabled");
+            $("#mr_use_language").change(function() {
+                if ($(this).attr("checked") == "checked") {
+                    $("#mr_use_language_name").removeAttr("disabled");
                 } else {
-                    $("#mr_default_startart_name").addClass("disabled");
+                    $("#mr_use_language_name").attr("disabled", "disabled");
+                }
+            });
+
+            $("#mr_use_client").change(function() {
+                if ($(this).attr("checked") == "checked") {
+                    $("#mr_use_client_name").removeAttr("disabled");
+                } else {
+                    $("#mr_use_client_name").attr("disabled", "disabled");
+                }
+            });
+
+            $("#mr_add_startart_name_to_url").change(function() {
+                if ($(this).attr("checked") == "checked") {
+                    $("#mr_default_startart_name").removeAttr("disabled")
+                                                  .removeClass("disabled");
+                } else {
+                    $("#mr_default_startart_name").attr("disabled", "disabled")
+                                                  .addClass("disabled");
                 }
             });
 
