b1aca7dbcfe2ba1b8ea01c3103a647552911804c
Simon Sprankel <discard.simon.sprankel@4fb.de>
2012-09-19 12:27:17 +0000

    CON-759 User is created although password is marked as insecure

--- contenido/includes/include.rights_create.php
+++ contenido/includes/include.rights_create.php
@@ -6,18 +6,14 @@
  * Description:
  * Display languages
  *
- * Requirements:
- * @con_php_req 5.0
- *
- *
- * @package    CONTENIDO Backend Includes
- * @version    1.0.3
- * @author     Timo A. Hummel
- * @copyright  four for business AG <www.4fb.de>
- * @license    http://www.contenido.org/license/LIZENZ.txt
- * @link       http://www.4fb.de
- * @link       http://www.contenido.org
- * @since      file available since CONTENIDO release <= 4.6
+ * @package CONTENIDO Backend Includes
+ * @version 1.0.3
+ * @author Timo A. Hummel
+ * @copyright four for business AG <www.4fb.de>
+ * @license http://www.contenido.org/license/LIZENZ.txt
+ * @link http://www.4fb.de
+ * @link http://www.contenido.org
+ * @since file available since CONTENIDO release <= 4.6
  */
 
 if (!defined('CON_FRAMEWORK')) {
@@ -45,55 +41,50 @@ if ($action == 'user_createuser') {
     } else {
         $aPerms = buildUserOrGroupPermsFromRequest(true);
 
-        $oUserCollection = new cApiUserCollection();
-        $oUser = $oUserCollection->create($username);
-        if ($oUser === false) {
+        if (cApiUser::usernameExists($username)) {
+            // username already exists
             $sNotification = $notification->returnNotification("warning", i18n("Username already exists"));
             $bError = true;
+        } else if (($passCheck = cApiUser::checkPasswordMask($password)) !== cApiUser::PASS_OK) {
+            // password is not valid
+            $sNotification = $notification->returnNotification("warning", cApiUser::getErrorString($passCheck));
+            $bError = true;
         } else if (strcmp($password, $passwordagain) == 0) {
+            // username is okay, password is valid and both passwords given are
+            // equal
+            $oUserCollection = new cApiUserCollection();
+            $oUser = $oUserCollection->create($username);
+            $oUser->setPassword($password);
+            $oUser->setRealName($realname);
+            $oUser->setMail($email);
+            $oUser->setTelNumber($telephone);
+            $oUser->setStreet($address_street);
+            $oUser->setCity($address_city);
+            $oUser->setZip($address_zip);
+            $oUser->setCountry($address_country);
+            $oUser->setUseWysi($wysi);
+            $oUser->setValidDateFrom($valid_from);
+            $oUser->setValidDateTo($valid_to);
+            $oUser->setPerms($aPerms);
 
-            // ok, both passwords given are equal, but is the password valid?
-            $iPassCheck = $oUser->setPassword($password);
-
-            if ($iPassCheck == cApiUser::PASS_OK) {
-                // yes, it is....
-                    $oUser->setRealName($realname);
-                    $oUser->setMail($email);
-                    $oUser->setTelNumber($telephone);
-                    $oUser->setStreet($address_street);
-                    $oUser->setCity($address_city);
-                    $oUser->setZip($address_zip);
-                    $oUser->setCountry($address_country);
-                    $oUser->setUseWysi($wysi);
-                    $oUser->setValidDateFrom($valid_from);
-                    $oUser->setValidDateTo($valid_to);
-                    $oUser->setPerms($aPerms);
-
-                    if ($oUser->store()) {
-                        // save user id and clean "old" values...
-                        $sNotification = $notification->returnNotification("info", i18n("User created"));
-                        $userid = $oUser->getUserId();
-
-                        $username = '';
-                        $realname = '';
-                        $email = '';
-                        $telephone = '';
-                        $address_city = '';
-                        $address_country = '';
-                        $address_street = '';
-                        $address_zip = '';
-                        $wysi = '';
-                        $valid_from = '';
-                        $valid_to = '';
-                        $aPerms = array();
-                        $password = '';
-                    } else {
-                        $sNotification = $notification->returnNotification("error", "Error saving the user to the database.");
-                        $bError = true;
-                    }
+            if ($oUser->store()) {
+                // show success message and clean "old" values
+                $sNotification = $notification->returnNotification("info", i18n("User created"));
+                $username = '';
+                $realname = '';
+                $email = '';
+                $telephone = '';
+                $address_city = '';
+                $address_country = '';
+                $address_street = '';
+                $address_zip = '';
+                $wysi = '';
+                $valid_from = '';
+                $valid_to = '';
+                $aPerms = array();
+                $password = '';
             } else {
-                // oh oh, password is NOT valid. check it...
-                $sNotification = $notification->returnNotification("warning", cApiUser::getErrorString($iPassCheck));
+                $sNotification = $notification->returnNotification("error", "Error saving the user to the database.");
                 $bError = true;
             }
         } else {
@@ -103,9 +94,8 @@ if ($action == 'user_createuser') {
     }
 }
 
-
 $tpl->reset();
-$tpl->set('s','NOTIFICATION', $sNotification);
+$tpl->set('s', 'NOTIFICATION', $sNotification);
 
 $form = '<form name="user_properties" method="post" action="' . $sess->url("main.php?") . '">
         <input type="hidden" name="area" value="' . $area . '">
@@ -168,17 +158,15 @@ $oTxtLand = new cHTMLTextbox('address_country', $address_country, 40, 255);
 $tpl->set('d', 'CATFIELD', $oTxtLand->render());
 $tpl->next();
 
-
-$tpl->set('s', 'PATH_TO_CALENDER_PIC',   cRegistry::getBackendUrl() . $cfg['path']['images'] . 'calendar.gif');
+$tpl->set('s', 'PATH_TO_CALENDER_PIC', cRegistry::getBackendUrl() . $cfg['path']['images'] . 'calendar.gif');
 
 if (($lang_short = substr(strtolower($belang), 0, 2)) != "en") {
-    $langscripts=  '<script type="text/javascript" src="scripts/jquery/plugins/timepicker-'.$lang_short.'.js"></script>
-    <script type="text/javascript" src="scripts/jquery/plugins/datepicker-'.$lang_short.'.js"></script>';
+    $langscripts = '<script type="text/javascript" src="scripts/jquery/plugins/timepicker-' . $lang_short . '.js"></script>
+    <script type="text/javascript" src="scripts/jquery/plugins/datepicker-' . $lang_short . '.js"></script>';
     $tpl->set('s', 'CAL_LANG', $langscripts);
 } else {
     $tpl->set('s', 'CAL_LANG', '');
 }
-
 
 // permissions of current logged in user
 $aAuthPerms = explode(',', $auth->auth['perm']);
@@ -197,7 +185,7 @@ $aClients = $oClientsCollection->getAvailableClients();
 $sClientCheckboxes = '';
 foreach ($aClients as $idclient => $item) {
     if (in_array("admin[" . $idclient . "]", $aAuthPerms) || in_array('sysadmin', $aAuthPerms)) {
-        $oCheckbox = new cHTMLCheckbox("madmin[" . $idclient . "]", $idclient, "madmin[" . $idclient . "]".$idclient, in_array("admin[" . $idclient . "]", $aPerms));
+        $oCheckbox = new cHTMLCheckbox("madmin[" . $idclient . "]", $idclient, "madmin[" . $idclient . "]" . $idclient, in_array("admin[" . $idclient . "]", $aPerms));
         $oCheckbox->setLabelText($item['name'] . "(" . $idclient . ")");
         $sClientCheckboxes .= $oCheckbox->toHTML();
     }
@@ -213,8 +201,8 @@ if ($sClientCheckboxes !== '') {
 $sClientCheckboxes = '';
 foreach ($aClients as $idclient => $item) {
     if (in_array("client[" . $idclient . "]", $aAuthPerms) || in_array('sysadmin', $aAuthPerms) || in_array("admin[" . $idclient . "]", $aAuthPerms)) {
-        $oCheckbox = new cHTMLCheckbox("mclient[" . $idclient . "]", $idclient, "mclient[" . $idclient . "]".$idclient, in_array("client[" . $idclient . "]", $aPerms));
-           $oCheckbox->setLabelText($item['name'] . "(" . $idclient . ")");
+        $oCheckbox = new cHTMLCheckbox("mclient[" . $idclient . "]", $idclient, "mclient[" . $idclient . "]" . $idclient, in_array("client[" . $idclient . "]", $aPerms));
+        $oCheckbox->setLabelText($item['name'] . "(" . $idclient . ")");
         $sClientCheckboxes .= $oCheckbox->toHTML();
     }
 }
@@ -228,7 +216,7 @@ $aClientsLanguages = getAllClientsAndLanguages();
 $sClientCheckboxes = '';
 foreach ($aClientsLanguages as $item) {
     if ($perm->have_perm_client("lang[" . $item['idlang'] . "]") || $perm->have_perm_client("admin[" . $item['idclient'] . "]")) {
-        $oCheckbox = new cHTMLCheckbox("mlang[" . $item['idlang'] . "]", $item['idlang'], "mlang[" . $item['idlang'] . "]".$item['idlang'], in_array("lang[" . $item['idlang'] . "]", $aPerms));
+        $oCheckbox = new cHTMLCheckbox("mlang[" . $item['idlang'] . "]", $item['idlang'], "mlang[" . $item['idlang'] . "]" . $item['idlang'], in_array("lang[" . $item['idlang'] . "]", $aPerms));
         $oCheckbox->setLabelText($item['langname'] . "(" . $item['clientname'] . ")");
         $sClientCheckboxes .= $oCheckbox->toHTML();
     }
@@ -245,13 +233,11 @@ $tpl->next();
 
 $sInputValidFrom = '<input type="text" id="valid_from" name="valid_from" value="' . $valid_from . '">';
 
-
 $tpl->set('d', 'CATNAME', i18n("Valid from"));
 $tpl->set('d', 'CATFIELD', $sInputValidFrom);
 $tpl->next();
 
-$sInputValidTo  = '<input type="text" id="valid_to" name="valid_to" value="' . $valid_to . '">';
-
+$sInputValidTo = '<input type="text" id="valid_to" name="valid_to" value="' . $valid_to . '">';
 
 $tpl->set('d', 'CATNAME', i18n("Valid to"));
 $tpl->set('d', 'CATFIELD', $sInputValidTo);
