607798c907349cac57a75ca2757dab7edf97c35c
Konstantinos Katikakis <discard.konstantinos.katikakis@4fb.de>
2012-09-20 15:59:33 +0000

    CON-382 changed for the contenido version 4.8.16

--- contenido/includes/include.mod_edit_form.php
+++ contenido/includes/include.mod_edit_form.php
@@ -1,14 +1,14 @@
 <?php
 /**
- * Project: 
+ * Project:
  * Contenido Content Management System
- * 
- * Description: 
+ *
+ * Description:
  * Edit modules
- * 
- * Requirements: 
+ *
+ * Requirements:
  * @con_php_req 5.0
- * 
+ *
  *
  * @package    Contenido Backend includes
  * @version    1.0.2
@@ -18,15 +18,15 @@
  * @link       http://www.4fb.de
  * @link       http://www.contenido.org
  * @since      file available since contenido release <= 4.6
- * 
- * {@internal 
+ *
+ * {@internal
  *   created 2003-01-21
  *   modified 2008-06-27, Frederic Schneider, add security fix
  *   modified 2011-02-07, Dominik Ziegler, removed integration of not supported java module editor
  *
  *   $Id: include.mod_edit_form.php 1284 2011-02-07 14:38:29Z dominik.ziegler $:
  * }}
- * 
+ *
  */
 
 if(!defined('CON_FRAMEWORK')) {
@@ -42,7 +42,6 @@ cInclude("includes","functions.upl.php");
 cInclude("external", "edit_area/class.edit_area.php");
 
 $noti				= "";
-$bOptionUseJava		= getEffectiveSetting("modules", "java-edit", false);
 $sOptionDebugRows	= getEffectiveSetting("modules", "show-debug-rows", "never");
 
 if (!isset($idmod)) $idmod = 0;
@@ -66,13 +65,13 @@ if (($action == "mod_new") && (!$perm->have_perm_area_action_anyitem($area, $act
 		$module = new cApiModule;
 		$module->loadByPrimaryKey($idmod);
 	}
-	
+
     if ($action == "mod_importexport_module")
     {
     	if ($mode == "export")
     	{
     		$name = uplCreateFriendlyName($module->get("name"));
-    		
+
     		if ($name != "")
     		{
     			$module->export($name.".xml");
@@ -81,10 +80,10 @@ if (($action == "mod_new") && (!$perm->have_perm_area_action_anyitem($area, $act
     	if ($mode == "import")
     	{
     		if (file_exists($_FILES["upload"]["tmp_name"]))
-    		{    		
+    		{
     			if (!$module->import($_FILES["upload"]["tmp_name"]))
     			{
-    				$noti .= sprintf(i18n("Error while importing XML file: %s"), $module->_error). "<br>";	
+    				$noti .= sprintf(i18n("Error while importing XML file: %s"), $module->_error). "<br>";
     			} else {
     				// Load the item again (clearing slashes from import)
     				$module->loadByPrimaryKey($module->get($module->primaryKey));
@@ -94,19 +93,19 @@ if (($action == "mod_new") && (!$perm->have_perm_area_action_anyitem($area, $act
     }
 
 	$idmod = $module->get("idmod");
-	
+
 	if (!$perm->have_perm_area_action_item("mod_edit","mod_edit",$idmod))
 	{
 		$link = new cHTMLLink;
 		$link->setCLink("mod_translate", 4, "");
 		$link->setCustom("idmod", $idmod);
-		
+
 		header("Location: ".$link->getHREF());
 	} else {
     	$oInUse = new InUseCollection;
     	list($bInUse, $message) = $oInUse->checkAndMark("idmod", $idmod, true, i18n("Module is in use by %s (%s)"), true, "main.php?area=$area&frame=$frame&idmod=$idmod");
     	unset ($oInUse);
-    	
+
     	if ($bInUse == true)
     	{
     		$message .= "<br>";
@@ -120,27 +119,27 @@ if (($action == "mod_new") && (!$perm->have_perm_area_action_anyitem($area, $act
     	$form->setVar("area","mod_edit");
     	$form->setVar("frame", $frame);
     	$form->setVar("idmod", $idmod);
-    	
+
     	if (!$bInUse)
     	{
     		$form->setVar("action", "mod_edit");
     	}
     	$form->setWidth("100%");
-    	        
+
        	$form->addHeader(i18n("Edit module"));
-                
+
     	$name		= new cHTMLTextbox("name", $module->get("name"),60);
     	$descr		= new cHTMLTextarea("descr", htmlspecialchars($module->get("description")), 100, 5);
-    	
+
     	// Get input and output code; if specified, prepare row fields
     	$sInputData		= htmlspecialchars($module->get("input"));
     	$sOutputData	= htmlspecialchars($module->get("output"));
-    	
+
     	if ($sOptionDebugRows !== "never")
     	{
     		$iInputNewLines		= substr_count($sInputData,  "\n") + 2; // +2: Just sanity, to have at least two more lines than the code
     		$iOutputNewLines	= substr_count($sOutputData, "\n") + 2; // +2: Just sanity, to have at least two more lines than the code
-    		
+
     		// Have at least 15 + 2 lines (15 = code textarea lines count)
     		if ($iInputNewLines < 21)
     		{
@@ -162,7 +161,7 @@ if (($action == "mod_new") && (!$perm->have_perm_area_action_anyitem($area, $act
     		}
 	    	unset($iInputNewLineChars);
     		unset($iOutputNewLineChars);
-    		    	
+
     		$sRows = "";
 	    	for ($i = 1; $i <= $iInputNewLines; $i++)
 	    	{
@@ -173,7 +172,7 @@ if (($action == "mod_new") && (!$perm->have_perm_area_action_anyitem($area, $act
     			$sRows .= sprintf("%0".$iChars."d", $i);
     		}
     		$oInputRows = new cHTMLTextarea("txtInputRows", $sRows, $iChars, 20);
-    		
+
 			$sRows = "";
 			for ($i = 1; $i <= $iOutputNewLines; $i++)
 			{
@@ -184,34 +183,34 @@ if (($action == "mod_new") && (!$perm->have_perm_area_action_anyitem($area, $act
 				$sRows .= sprintf("%0".$iChars."d", $i);
 			}
 			$oOutputRows = new cHTMLTextarea("txtOutputRows", $sRows, $iChars, 20);
-			
+
 			$oInputRows->updateAttributes(array("wrap" => "off"));
 			$oOutputRows->updateAttributes(array("wrap" => "off"));
-			
+
     		$oInputRows->updateAttributes(array("readonly" => "true"));
 			$oOutputRows->updateAttributes(array("readonly" => "true"));
-			
+
 			$oInputRows->setStyle("font-family: monospace;");
 			$oOutputRows->setStyle("font-family: monospace;");
 			$oOutputRows->setStyle("font-family: monospace;");
     	}
-    	
+
     	$input	= new cHTMLTextarea("input",  $sInputData, 100, 20, 'input');
-    	$output = new cHTMLTextarea("output", $sOutputData, 100, 20, 'output'); 
-    	
+    	$output = new cHTMLTextarea("output", $sOutputData, 100, 20, 'output');
+
     	// Style the fields
     	$input->updateAttributes(array("wrap" => "off"));
     	$output->updateAttributes(array("wrap" => "off"));
-    	
+
     	$name->setDisabled($disabled);
     	$descr->setDisabled($disabled);
     	$input->setDisabled($disabled);
     	$output->setDisabled($disabled);
-    	
+
     	$descr->setStyle("width: 100%; font-family: monospace;");
     	$input->setStyle("width: 100%; font-family: monospace;");
     	$output->setStyle("width: 100%; font-family: monospace;");
-    	
+
     	// Check, if tabs may be inserted in text areas (instead jumping to next element)
     	if (getEffectiveSetting("modules", "edit-with-tabs", "false") == "true")
     	{
@@ -220,12 +219,12 @@ if (($action == "mod_new") && (!$perm->have_perm_area_action_anyitem($area, $act
 * Insert a tab at the current text position in a textarea
 * Jan Dittmer, jdittmer@ppp0.net, 2005-05-28
 * Inspired by http://www.forum4designers.com/archive22-2004-9-127735.html
-* Tested on: 
+* Tested on:
 *   Mozilla Firefox 1.0.3 (Linux)
 *   Mozilla 1.7.8 (Linux)
 *   Epiphany 1.4.8 (Linux)
 *   Internet Explorer 6.0 (Linux)
-* Does not work in: 
+* Does not work in:
 *   Konqueror (no tab inserted, but focus stays)
 * Fix for IE "free focus" problem:
 *   Idea from mastercomputers from New Zealand
@@ -266,20 +265,20 @@ function insertTab(event, obj) {
 }
 //--></script>';
     		$page->addScript("tabScript", $sTabScript);
-    		
+
 	    	$input->setEvent("onkeydown",	"return insertTab(event,this);");
 	    	$output->setEvent("onkeydown",	"return insertTab(event,this);");
     	}
-    	
+
     	// Prepare type select box
     	$typeselect = new cHTMLSelectElement("type");
-    	
+
     	$db2 = new DB_Contenido;
     	$sql = "SELECT type FROM ".$cfg["tab"]["mod"]." ".
 			   "WHERE idclient = '".$client."' GROUP BY type"; // This query can't be designed using GenericDB...
     	$db2->query($sql);
-    	
-		$aTypes = array();	
+
+		$aTypes = array();
     	while ($db2->next_record())
     	{
     		if ($db2->f("type") != "")
@@ -287,47 +286,47 @@ function insertTab(event, obj) {
     			$aTypes[] = $db2->f("type");
     		}
     	}
-    	
+
     	// Read existing layouts
     	$oLayouts = new cApiLayoutCollection;
     	$oLayouts->setWhere("idclient", $client);
     	$oLayouts->query();
-    	
+
     	while ($oLayout = $oLayouts->next())
     	{
-    		$aTypes = array_merge(explode(";",$oLayout->getProperty("layout", "used-types")), $aTypes);	
+    		$aTypes = array_merge(explode(";",$oLayout->getProperty("layout", "used-types")), $aTypes);
     	}
     	$aTypes = array_unique($aTypes);
-    	
+
 		foreach ($aTypes as $sType)
 		{
-			$typearray[$sType] = $sType;	
+			$typearray[$sType] = $sType;
 		}
 		unset ($aTypes);
-		
+
 		if (is_array($typearray)) {
 			asort($typearray);
 			$typeselect->autoFill(array_merge(array("" => "-- ".i18n("Custom")." --"), $typearray));
 		} else {
 			$typeselect->autoFill(array("" => "-- ".i18n("Custom")." --"));
 		}
-		
+
     	$typeselect->setEvent("change", 'if (document.forms["mod_edit"].elements["type"].value == 0) { document.forms["mod_edit"].elements["customtype"].disabled=0;} else {document.forms["mod_edit"].elements["customtype"].disabled=1;}');
     	$typeselect->setDisabled($disabled);
-    	
+
 		$custom = new cHTMLTextbox("customtype", "");
 		$custom->setDisabled($disabled);
-		
+
 		if ($module->get("type") == "" || $module->get("type") == "0")
-		{  
+		{
 			$typeselect->setDefault("0");
 		} else {
 			$typeselect->setDefault($module->get("type"));
 			$custom->setDisabled("disabled");
-		}		
-    
+		}
+
 		$modulecheck = getSystemProperty("system", "modulecheck");
-        
+
         $inputok  = true;
         $outputok = true;
 		if ($modulecheck !== "false")
@@ -341,7 +340,7 @@ function insertTab(event, obj) {
 				$okMessage = i18n("Module successfully compiled");
 				$outled = '<img align="right" src="images/but_online.gif" alt="'.$okMessage.'" title="'.$okMessage.'">';
 			}
-            
+
 			$inputok = modTestModule($module->get("input"), $module->get("idmod"). "i");
 			if (!$inputok)
 			{
@@ -351,7 +350,7 @@ function insertTab(event, obj) {
 				$okMessage = i18n("Module successfully compiled");
 				$inled = '<img align="right" src="images/but_online.gif" alt="'.$okMessage.'" title="'.$okMessage.'">';
 			}
-			
+
 			// Store error information in the database (to avoid re-eval for module overview/menu)
 			if ($inputok && $outputok) {
 				$sStatus = "none";
@@ -362,22 +361,22 @@ function insertTab(event, obj) {
 			} else {
 				$sStatus = "both";
 			}
-			
+
 			// If status has been changed, store and show in overview
 			$sPrevStatus = $module->get("error");
 			if ($sPrevStatus !== $sStatus)
 			{
 				$module->set("error", $sStatus);
 				$module->store();
-				
+
 				$page->setReload();
 			}
 		}
-            
+
 		$form->add(i18n("Name"), $name->render());
-		$form->add(i18n("Type"), $typeselect->render().$custom->render());	
+		$form->add(i18n("Type"), $typeselect->render().$custom->render());
 		$form->add(i18n("Description"), $descr->render());
-        		
+
 		if ($sOptionDebugRows == "always" || ($sOptionDebugRows == "onerror" && (!$inputok || !$outputok)))
 		{
 			$sSyncScript = '<script type="text/javascript"><!--
@@ -396,22 +395,22 @@ window.onload = scrolltheother;
 			//--></script>
 			';
 			$page->addScript("syncScript", $sSyncScript);
-			
+
 			$form->add('<table width="100%" border="0" cellspacing="0" cellpadding="0"><tr><td style="vertical-align: top;">'.i18n("Input").'</td><td style="vertical-align: top;">'.$inled.'</td><td style="padding-left: 5px; vertical-align: top;">'.$oInputRows->render().'</td></tr></table>', $input->render());
 			$form->add('<table width="100%" border="0" cellspacing="0" cellpadding="0"><tr><td style="vertical-align: top;">'.i18n("Output").'</td><td style="vertical-align: top;">'.$outled.'</td><td style="padding-left: 5px; vertical-align: top;">'.$oOutputRows->render().'</td></tr></table>', $output->render());
 		} else {
 			$form->add('<table width="100%" border="0" cellspacing="0" cellpadding="0"><tr><td style="vertical-align: top;">'.i18n("Input").'</td><td style="vertical-align: top;">'.$inled.'</td></tr></table>', $input->render());
 			$form->add('<table width="100%" border="0" cellspacing="0" cellpadding="0"><tr><td style="vertical-align: top;">'.i18n("Output").'</td><td style="vertical-align: top;">'.$outled.'</td></tr></table>', $output->render());
 		}
-		       	
+
 		$noti = "";
-        	
+
 		if ($module->isOldModule())
 		{
 			$noti .= $notification->returnNotification("warning", i18n("This module uses variables and/or functions which are probably not available in this Contenido version. Please make sure that you use up-to-date modules."));
 			$noti .= "<br>";
 		}
-        	
+
 		if ($idmod != 0)
 		{
 			$import = new cHTMLRadiobutton("mode", "import");
@@ -419,12 +418,12 @@ window.onload = scrolltheother;
 
 			$import->setLabelText(i18n("Import from file"));
 			$export->setLabelText(i18n("Export to file"));
-                
+
 			$import->setEvent("click", "document.getElementById('vupload').style.display = '';");
 			$export->setEvent("click", "document.getElementById('vupload').style.display = 'none';");
 
 			$upload = new cHTMLUpload("upload");
-			                
+
 			if ($module->get("input") != "" && $module->get("output") != "")
 			{
 				$export->setChecked("checked");
@@ -437,7 +436,7 @@ window.onload = scrolltheother;
 			$form2->addHeader("Import/Export");
 			$form2->add(i18n("Mode"), array($export, "<br>", $import));
 			if ($module->get("input") != "" && $module->get("output") != "")
-			{                
+			{
 				$form2->add(i18n("File"), $upload, "vupload", "display: none;");
 			} else {
 				$form2->add(i18n("File"), $upload, "vupload");
@@ -446,20 +445,20 @@ window.onload = scrolltheother;
 			$form2->setVar("frame", $frame);
 			$form2->setVar("idmod", $idmod);
 			$form2->custom["submit"]["accesskey"] = '';
-		    
+
             $sScript = '<script type="text/javascript">
                             if (document.getElementById(\'scroll\')) {
                                 document.getElementById(\'scroll\').onmousedown = triggerClickOn;
                                 document.getElementById(\'scroll\').onmouseup = triggerClickOff;
                                 document.getElementById(\'scroll\').style.paddingTop=\'4px\';
 								document.getElementById(\'scroll\').style.paddingBottom=\'5px\';
-								
+
                             }
                         </script>';
-            
+
 			$page->setContent($noti.$message.$form->render().$applet."<br>".$form2->render().$sScript);
 		}
-    		
+
 		$page->setSubnav("idmod=$idmod", "mod");
 		if ($action)
 		{
@@ -485,7 +484,7 @@ window.onload = scrolltheother;
             $oEditAreaInput = new EditArea('input', 'php', substr(strtolower($belang), 0, 2), true, $cfg, !$bInUse);
             $oEditAreaOutput = new EditArea('output', 'php', substr(strtolower($belang), 0, 2), false, $cfg, !$bInUse);
             $page->addScript('editarea', $oEditAreaInput->renderScript().$oEditAreaOutput->renderScript());
-			            
+
             $page->render();
 		}
 	}
