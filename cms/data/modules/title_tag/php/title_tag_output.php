0a84e6d3c6550a4eb0694f76c6f05f1c3f8e15f1
Dominik Ziegler <dominik.ziegler@4fb.de>
2012-09-23 20:12:15 +0000

    moved navigation breadcrum and title tag module to logic of cCategoryHelper

--- cms/data/modules/title_tag/php/title_tag_output.php
+++ cms/data/modules/title_tag/php/title_tag_output.php
@@ -1,41 +1,21 @@
 <?php
-/**
- * Description: Print HTML title tag content
- *
- * @version    1.0.0
- * @author     Rudi Bieller
- * @copyright  four for business AG <www.4fb.de>
- *
- * {@internal
- *   created 2008-04-07
- *   $Id$
- * }}
- */
+$oClient = new cApiClient($client);
 
+$aBread = array();
+$aBread[] = cRegistry::getClient()->getField('name');
 
-$oClient = new cApiClient( $client );
-$sName = $oClient->getField('name');
-
-try {
-    $oBread = new Contenido_FrontendNavigation_Breadcrumb($db, $cfg, $client, $lang, $cfgClient);
-    $oBreadCats = $oBread->get($idcat, 1);
-    $sBread = $sName . ' - ';
-    $aBread = array();
-    foreach ($oBreadCats as $oConCat) {
-        $aBread[] = $oConCat->getCategoryLanguage()->getName();
-    }
-    $sBread .= implode(' - ', $aBread);
-    $oArticle = new cApiArticleLanguage();
-    $oArticle->loadByArticleAndLanguageId($idart, $lang);
-    $sHeadline = strip_tags($oArticle->getContent('CMS_HTMLHEAD', 1));
-    if ($sHeadline != '') {
-        $sBread .= ' - ' . $sHeadline;
-    }
-    echo $sBread;
-} catch (InvalidArgumentException $eI) {
-    echo $sName;
-} catch (Exception $e) {
-    echo $sName;
+$helper = cCategoryHelper::getInstance();
+foreach ($helper->getCategoryPath($idcat, 1) as $categoryLang) {
+	$aBread[] = $categoryLang->get('name');
 }
 
+$oArticle = new cApiArticleLanguage();
+$oArticle->loadByArticleAndLanguageId($idart, $lang);
+$sHeadline = strip_tags($oArticle->getContent('CMS_HTMLHEAD', 1));
+
+if ($sHeadline != '') {
+	$aBread[] = $sHeadline;
+}
+
+echo implode(' - ', $aBread);
 ?>
