da26ddc06ee9bbdb9c5389e1d9131fa826aa7980
Dominik Ziegler <dominik.ziegler@4fb.de>
2012-09-23 20:01:25 +0000

    changed Contenido_Category with cApiCategoryLanguage

--- cms/data/modules/header/php/header_output.php
+++ cms/data/modules/header/php/header_output.php
@@ -1,35 +1,24 @@
 <?php
-/**
- * Description: Display Header with CONTENIDO Logo, an image and the current
- * (navigation-) location.
- * If no image is selected, the default one will be used.
- *
- * @package Core
- * @subpackage Frontend
- * @version SVN Revision $Rev:$
- *
- * @author Rudi Bieller
- * @copyright four for business AG <www.4fb.de>
- * @license http://www.contenido.org/license/LIZENZ.txt
- * @link http://www.4fb.de
- * @link http://www.contenido.org
- */
-
-if (!isset($tpl) || !is_object($tpl)) {
-    $tpl = new cTemplate();
-}
+$tpl = new cTemplate();
 
 // get start idcat
 $iIdcatStart = (int) getEffectiveSetting('navigation', 'idcat-home', '1');
 
 try {
     // get headline
-    $oConCat = new Contenido_Category($db, $cfg);
-    $oConCat->load($idcat, true, $lang);
+	if ($iIdcatStart != intval($idcat)) {
+		$category = new cApiCategoryLanguage();
+		$category->loadByCategoryIdAndLanguageId($idcat, $lang);
+		
+		$sHeadline = $category->get('name');
+	} else {
+		$sHeadline = mi18n("Willkommen!");
+	}
+
     $sImgEdit = "CMS_IMGEDITOR[1]";
     $sImgSrc = "CMS_IMG[1]";
-    $sHeadline = $iIdcatStart != intval($idcat)? $oConCat->getCategoryLanguage()->getName() : mi18n("Willkommen!");
-    $sCssStyle = '';
+    
+	$sCssStyle = '';
     if ($contenido && $edit) {
         echo '<div id="modHeaderImgEdit">' . $sImgEdit . '</div>';
     }
@@ -37,7 +26,6 @@ try {
         $sCssStyle = ' style="background-image:url(' . $sImgSrc . ');"';
     }
 
-    $tpl->reset();
     $tpl->set('s', 'css-style', $sCssStyle);
     $tpl->set('s', 'url', 'front_content.php');
     $tpl->set('s', 'title', mi18n("Zur CONTENIDO Homepage"));
