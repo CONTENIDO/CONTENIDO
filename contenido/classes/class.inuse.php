1bae7f510e3b90b6212732596f152ba4affba6a1
Konstantinos Katikakis <discard.konstantinos.katikakis@4fb.de>
2012-09-26 08:21:28 +0000

    CON-506 changes for the 4.8.16 version

--- contenido/classes/class.inuse.php
+++ contenido/classes/class.inuse.php
@@ -1,14 +1,14 @@
 <?php
 /**
- * Project: 
+ * Project:
  * Contenido Content Management System
- * 
- * Description: 
+ *
+ * Description:
  * Contenido In-Use classes
- * 
- * Requirements: 
+ *
+ * Requirements:
  * @con_php_req 5.0
- * 
+ *
  *
  * @package    Contenido Backend classes
  * @version    1.0.8
@@ -18,14 +18,14 @@
  * @link       http://www.4fb.de
  * @link       http://www.contenido.org
  * @since      file available since contenido release <= 4.6
- * 
- * {@internal 
+ *
+ * {@internal
  *   created 2003-07-18
  *   modified 2008-06-30, Dominik Ziegler, add security fix
  *
  *   $Id: class.inuse.php 421 2008-06-30 13:39:59Z dominik.ziegler $:
  * }}
- * 
+ *
  */
 
 if(!defined('CON_FRAMEWORK')) {
@@ -42,7 +42,7 @@ cInclude("classes", "class.genericdb.php");
  * @copyright four for business 2003
  */
 class InUseCollection extends ItemCollection {
-	
+
 	/**
      * Constructor Function
      * @param none
@@ -51,7 +51,7 @@ class InUseCollection extends ItemCollection {
 	{
 		global $cfg;
 		parent::ItemCollection($cfg["tab"]["inuse"], "idinuse");
-		
+
 		$this->_setItemClass("InUseItem");
 	}
 
@@ -77,9 +77,9 @@ class InUseCollection extends ItemCollection {
 		$objectid 	= Contenido_Security::escapeDB($objectid, null);
 		$session 	= Contenido_Security::escapeDB($session, null);
 		$user		= Contenido_Security::escapeDB($user, null);
-		
+
 		$this->select("type = '".$type."' AND objectid = '".$objectid."'");
-		
+
 		if (!$this->next())
 		{
 			$newitem = parent::create();
@@ -97,23 +97,23 @@ class InUseCollection extends ItemCollection {
      * @param $type string Specifies the type to de-mark.
 	 * @param $objectid mixed Specifies the object ID
 	 * @param $session string Specifies the session for which the "in use" mark is valid
-     */	
+     */
 	function removeMark ($type, $objectid, $session)
 	{
 		$type 		= Contenido_Security::escapeDB($type, null);
 		$objectid 	= Contenido_Security::escapeDB($objectid, null);
 		$session 	= Contenido_Security::escapeDB($session, null);
-		
+
 		$this->select("type = '".$type."' AND objectid = '".$objectid."' AND session = '".$session."'");
-		
+
 		if ($obj = $this->next())
 		{
 			/* Extract the ID */
 			$id = $obj->get("idinuse");
-			
+
 			/* Let's save memory */
 			unset($obj);
-			
+
 			/* Remove entry */
 			$this->delete($id);
 		}
@@ -124,90 +124,90 @@ class InUseCollection extends ItemCollection {
  	 *
      * @param $type string Specifies the type to de-mark.
 	 * @param $session string Specifies the session for which the "in use" mark is valid
-     */	
+     */
 	function removeTypeMarks ($type, $session)
 	{
 		$type		= Contenido_Security::escapeDB($type, null);
 		$session	= Contenido_Security::escapeDB($session, null);
-		
+
 		$this->select("type = '".$type."' AND session = '".$session."'");
-		
+
 		while ($obj = $this->next())
 		{
 			/* Extract the ID */
 			$id = $obj->get("idinuse");
-			
+
 			/* Let's save memory */
 			unset($obj);
-			
+
 			/* Remove entry */
 			$this->delete($id);
 		}
 	}
-	
+
 	/**
      * Removes the mark for a specific item
  	 *
      * @param $type string Specifies the type to de-mark.
 	 * @param $itemid string Specifies the item
-     */	
+     */
 	function removeItemMarks ($type, $itemid)
 	{
 		$type 	= Contenido_Security::escapeDB($type, null);
 		$itemid = Contenido_Security::escapeDB($itemid, null);
-		
+
 		$this->select("type = '".$type."' AND objectid = '".$itemid."'");
-		
+
 		while ($obj = $this->next())
 		{
 			/* Extract the ID */
 			$id = $obj->get("idinuse");
-			
+
 			/* Let's save memory */
 			unset($obj);
-			
+
 			/* Remove entry */
 			$this->delete($id);
 		}
-	}	
+	}
 
 	/**
      * Removes all in-use marks for a specific session.
  	 *
 	 * @param $session string Specifies the session for which the "in use" marks should be removed
-     */	
+     */
 	function removeSessionMarks ($session)
 	{
 		$session = Contenido_Security::escapeDB($session, null);
 		$this->select("session = '".$session."'");
-		
+
 		while ($obj = $this->next())
 		{
 			/* Extract the ID */
 			$id = $obj->get("idinuse");
-			
+
 			/* Let's save memory */
 			unset($obj);
-			
+
 			/* Remove entry */
 			$this->delete($id);
 		}
 	}
-		
+
 	/**
      * Checks if a specific item is marked
  	 *
      * @param $type string Specifies the type to de-mark.
 	 * @param $objectid mixed Specifies the object ID
 	 * @return int Returns false if it's not in use or returns the object if it is.
-     */	
+     */
 	function checkMark ($type, $objectid)
 	{
 		$type 		= Contenido_Security::escapeDB($type, null);
 		$objectid 	= Contenido_Security::escapeDB($objectid, null);
-		
+
 		$this->select("type = '".$type."' AND objectid = '".$objectid."'");
-		
+
 		if ($obj = $this->next())
 		{
 			return ($obj);
@@ -215,7 +215,7 @@ class InUseCollection extends ItemCollection {
 			return false;
 		}
 	}
-	
+
 	/**
      * Checks and marks if not marked.
 	 *
@@ -234,17 +234,16 @@ class InUseCollection extends ItemCollection {
 	 * @param $location string Value to append to the override lock button
 	 * @return mixed If returnWarning is false, returns a boolean value wether the object is locked. If
 	 *				 returnWarning is true, returns a 2 item array (boolean inUse, string errormessage).
-     */		
+     */
 	function checkAndMark ($type, $objectid, $returnWarning = false, $warningTemplate = "", $allowOverride = false, $location = "")
 	{
 		global $sess, $auth, $notification, $area, $frame, $perm;
-		
-		if (($obj = $this->checkMark($type, $objectid)) === false)
-        {
+
+        if ((($obj = $this->checkMark($type, $objectid)) === false)|| ($auth->auth['uid'] == $obj->get('userid'))) {
         	$this->markInUse($type, $objectid, $sess->id, $auth->auth["uid"]);
         	$inUse = false;
-        	$disabled = "";	
-        	$noti = "";					
+        	$disabled = "";
+        	$noti = "";
         } else {
         	if ($returnWarning == true)
         	{
@@ -252,39 +251,39 @@ class InUseCollection extends ItemCollection {
             	$vuser->loadUserByUserID($obj->get("userid"));
             	$inUseUser = $vuser->getField("username");
             	$inUseUserRealName = $vuser->getField("realname");
-            	
+
             	$message = sprintf($warningTemplate, $inUseUser, $inUseUserRealName);
-            	
+
             	if ($allowOverride == true && ($auth->auth["uid"] == $obj->get("userid") || $perm->have_perm()))
             	{
             		$alt = i18n("Click here if you want to override the lock");
-            	
+
             		$link = $sess->url($location."&overridetype=".$type."&overrideid=".$objectid);
-            		
+
             		$warnmessage = i18n("Do you really want to override the lock?");
             		$script = "javascript:if (window.confirm('".$warnmessage."') == true) { window.location.href  = '".$link."';}";
             		$override = '<br><br><a alt="'.$alt.'" title="'.$alt.'" href="'.$script.'" class="standard">['.i18n("Override lock").']</a> <a href="javascript://" class="standard" onclick="elem = document.getElementById(\'contenido_notification\'); elem.style.display=\'none\'">['.i18n("Hide notification").']</a>';
             	} else {
             		$override = "";
             	}
-            	
+
             	if (!is_object($notification))
             	{
             		$notification = new Contenido_Notification;
             	}
-            	
-            	$noti = $notification->messageBox("warning", $message.$override, 0);			
+
+            	$noti = $notification->messageBox("warning", $message.$override, 0);
             	$inUse = true;
         	}
         }
-        
+
         if ($returnWarning == true)
         {
         	return (array($inUse, $noti));
         } else {
         	return $inUse;
         }
-	}	
+	}
 }
 
 /**
@@ -295,7 +294,7 @@ class InUseCollection extends ItemCollection {
  * @copyright four for business 2003
  */
 class InUseItem extends Item {
-	
+
 	/**
      * Constructor Function
      * @param string $table The table to use as information source
@@ -303,10 +302,10 @@ class InUseItem extends Item {
 	function InUseItem()
 	{
 		global $cfg;
-		
+
 		parent::Item($cfg["tab"]["inuse"], "idinuse");
 	}
-	
+
 }
 
 ?>
