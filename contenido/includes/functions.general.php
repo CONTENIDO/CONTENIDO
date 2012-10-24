217b5e94aa77254bb822bf888b832c901c5da250
Murat Purc <murat@purc.de>
2012-10-24 23:40:10 +0000

    Run JS code in function markSubMenuItem() in an anonymous JS function.

--- contenido/includes/functions.general.php
+++ contenido/includes/functions.general.php
@@ -268,23 +268,28 @@ function getParentAreaId($area) {
  * @param bool $return Return or echo script
  */
 function markSubMenuItem($menuitem, $return = false) {
-    $str = '<script type="text/javascript">
-            try {
-                // Check if we are in a dual-frame or a quad-frame
-                if (parent.parent.frames[0].name == "header") {
-                    if (parent.frames["right_top"].document.getElementById("c_' . $menuitem . '")) {
-                        menuItem = parent.frames["right_top"].document.getElementById("c_' . $menuitem . '").getElementsByTagName(\'a\')[0];
-                        parent.frames["right_top"].sub.clicked(menuItem);
-                    }
-                } else {
-                    // Check if submenuItem is existing and mark it
-                    if (parent.parent.frames["right"].frames["right_top"].document.getElementById("c_' . $menuitem . '")) {
-                        menuItem = parent.parent.frames["right"].frames["right_top"].document.getElementById("c_' . $menuitem . '").getElementsByTagName(\'a\')[0];
-                        parent.parent.frames["right"].frames["right_top"].sub.clicked(menuItem);
-                    }
+    $str = '
+    <script type="text/javascript">
+    (function(id){
+        var menuItem;
+
+        try {
+            // Check if we are in a dual-frame or a quad-frame
+            if (parent.parent.frames[0].name == "header") {
+                if (parent.frames["right_top"].document.getElementById(id)) {
+                    menuItem = parent.frames["right_top"].document.getElementById(id).getElementsByTagName("a")[0];
+                    parent.frames["right_top"].sub.clicked(menuItem);
                 }
-            } catch (e) {}
-            </script>';
+            } else {
+                // Check if submenuItem is existing and mark it
+                if (parent.parent.frames["right"].frames["right_top"].document.getElementById(id)) {
+                    menuItem = parent.parent.frames["right"].frames["right_top"].document.getElementById(id).getElementsByTagName("a")[0];
+                    parent.parent.frames["right"].frames["right_top"].sub.clicked(menuItem);
+                }
+            }
+        } catch (e) {}
+    })("c_' . $menuitem . '");
+    </script>';
 
     if ($return) {
         return $str;
