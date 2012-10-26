bf8f321020540d038e7e47c23186ed4baf3780e4
Frederic Schneider <frederic.schneider@4fb.de>
2012-10-26 13:50:56 +0000

    Renamed user_sequence to user_sequences

--- contenido/plugins/workflow/plugin_uninstall.sql
+++ contenido/plugins/workflow/plugin_uninstall.sql
@@ -2,5 +2,5 @@ DROP TABLE !PREFIX!wf_actions
 DROP TABLE !PREFIX!wf_allocation
 DROP TABLE !PREFIX!wf_art_allocation
 DROP TABLE !PREFIX!wf_items
-DROP TABLE !PREFIX!wf_user_sequence
+DROP TABLE !PREFIX!wf_user_sequences
 DROP TABLE !PREFIX!wf_workflow
