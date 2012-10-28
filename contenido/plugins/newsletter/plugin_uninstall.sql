1b132fd651c6fa819a73099df060c87da4aaa436
Frederic Schneider <frederic.schneider@4fb.de>
2012-10-28 19:31:51 +0000

    Added plugin.xml, plugin_install.sql and plugin_uninstall.sql for Newsletter Plugin

---
+++ contenido/plugins/newsletter/plugin_uninstall.sql
@@ -0,0 +1,6 @@
+DROP TABLE !PREFIX!_news
+DROP TABLE !PREFIX!_news_groupmembers
+DROP TABLE !PREFIX!_news_groups
+DROP TABLE !PREFIX!_news_jobs
+DROP TABLE !PREFIX!_news_log
+DROP TABLE !PREFIX!_news_rcp
