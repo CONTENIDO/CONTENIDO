6b7eabc8cbd8868ae07203e820490c8d5b8bf36e
Marcus Gnaß <marcus.gnass@4fb.de>
2012-11-11 03:52:19 +0000

    CON-818 added module to display social media links

--- cms/data/modules/navigation_social_media/php/navigation_social_media_output.php
+++ cms/data/modules/navigation_social_media/php/navigation_social_media_output.php
@@ -11,4 +11,30 @@
  * @link http://www.4fb.de
  */
 
+$configIdart = getEffectiveSetting('social_media_config', 'idart', 0);
+
+if (0< $configIdart) {
+
+    $article = new cApiArticleLanguage($configIdart, true);
+
+    $url = array(
+        'rss' => $article->getContent('CMS_TEXT', 1),
+        'facebook' => $article->getContent('CMS_TEXT', 2),
+        'googleplus' => $article->getContent('CMS_TEXT', 3),
+        'twitter' => $article->getContent('CMS_TEXT', 4),
+        'youtube' => $article->getContent('CMS_TEXT', 5),
+        'xing' => $article->getContent('CMS_TEXT', 6)
+    );
+
+    // use smarty template to output header text
+    $tpl = Contenido_SmartyWrapper::getInstance();
+    global $force;
+    if (1 == $force) {
+        $tpl->clearAllCache();
+    }
+    $tpl->assign('url', $url);
+    $tpl->display('navigation_social_media/template/get.tpl');
+
+}
+
 ?>
--- cms/data/modules/navigation_social_media/template/get.tpl
+++ cms/data/modules/navigation_social_media/template/get.tpl
@@ -1,1 +1,27 @@
-social media links will soon be available
+<!-- navigation_social_media -->
+
+{if 0 lt $url.rss|strlen}
+	<a href="{$url.rss}" target="_blank">rss</a>
+{/if}
+
+{if 0 lt $url.facebook|strlen}
+	<a href="{$url.facebook}" target="_blank">facebook</a>
+{/if}
+
+{if 0 lt $url.googleplus|strlen}
+	<a href="{$url.googleplus}" target="_blank">googleplus</a>
+{/if}
+
+{if 0 lt $url.twitter|strlen}
+	<a href="{$url.twitter}" target="_blank">twitter</a>
+{/if}
+
+{if 0 lt $url.youtube|strlen}
+	<a href="{$url.youtube}" target="_blank">youtube</a>
+{/if}
+
+{if 0 lt $url.xing|strlen}
+	<a href="{$url.xing}" target="_blank">xing</a>
+{/if}
+
+<!-- /navigation_social_media -->
