385757af7b3a74e6c586d8c7dc45c2f9fad9ef56
Marcus Gnaß <marcus.gnass@4fb.de>
2012-11-11 06:36:32 +0000

    CON-819 finished main navigation

--- cms/data/modules/navigation_main/php/navigation_main_output.php
+++ cms/data/modules/navigation_main/php/navigation_main_output.php
@@ -1,16 +1,33 @@
 <?php
-$startIdcat = getEffectiveSetting('navigation', 'idcat-home', 1);
-$selectedDepth = getEffectiveSetting('navigation', 'level-depth', 3);
 
-$frontendHelper = cFrontendHelper::getInstance();
-$navigation = $frontendHelper->renderNavigation($startIdcat, $selectedDepth, cRegistry::getCategoryId());
+// get client settings
+$rootIdcat = getEffectiveSetting('navigation_main', 'idcat', 1);
+$depth = getEffectiveSetting('navigation_main', 'depth', 3);
+
+$idcat = cRegistry::getCategoryId();
+
+// get category tree
+$auth = cRegistry::getAuth();
+$categoryHelper = cCategoryHelper::getInstance();
+$categoryHelper->setAuth($auth);
+$tree = $categoryHelper->getSubCategories($rootIdcat, $depth);
+
+// get breadcrumb of current category
+$helper = cCategoryHelper::getInstance();
+$path = array_map(function (cApiCategoryLanguage $categoryLanguage) {
+    return $categoryLanguage->get('idcat');
+}, $helper->getCategoryPath($idcat, 1));
 
 // use smarty template to output header text
 $tpl = Contenido_SmartyWrapper::getInstance();
+global $force;
 if (1 == $force) {
     $tpl->clearAllCache();
 }
-
-$tpl->assign('navigation_data', $navigation);
+$tpl->assign('ulId', 'navigation');
+$tpl->assign('tree', $tree);
+$tpl->assign('actualIdcat', $idcat);
+$tpl->assign('path', $path);
 $tpl->display('navigation_main/template/navigation.tpl');
+
 ?>
