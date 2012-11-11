9570a8867e033397d506ecf0c03f56213fbf48bd
Dominik Ziegler <dominik.ziegler@4fb.de>
2012-11-11 02:14:17 +0000

    CON-883 - added head title module

---
+++ cms/data/modules/head_title/php/head_title_output.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * This module handles the content of the title element.
+ *
+ * @package Module
+ * @subpackage Title Element
+ *
+ * @author Dominik Ziegler
+ * @copyright four for business AG <www.4fb.de>
+ * @license http://www.contenido.org/license/LIZENZ.txt
+ * @link http://www.4fb.de
+ * @link http://www.contenido.org
+ */
+
+$breadcrumb = array();
+
+// get category path
+$helper = cCategoryHelper::getInstance();
+foreach ($helper->getCategoryPath(cRegistry::getCategoryId(), 1) as $categoryLang) {
+    $breadcrumb[] = $categoryLang->get('name');
+}
+
+// load current article information
+$article = new cApiArticleLanguage();
+$article->loadByArticleAndLanguageId(cRegistry::getArticleId(), cRegistry::getLanguageId());
+$headline = strip_tags($article->getContent('CMS_HTMLHEAD', 1));
+
+// append headline of article if existing
+if ($headline != '') {
+    $breadcrumb[] = $headline;
+}
+
+echo implode(' - ', $breadcrumb);
+?>
