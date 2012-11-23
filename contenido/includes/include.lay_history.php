6b5cd0b9a927b5167792497ebba829fc4c0c09c6
Frederic Schneider <frederic.schneider@4fb.de>
2012-11-23 11:21:44 +0000

    CON-872: Added HtmlSpecialChars for source code at history

--- contenido/includes/include.lay_history.php
+++ contenido/includes/include.lay_history.php
@@ -1,34 +1,23 @@
 <?php
 /**
- * Project:
- * CONTENIDO Content Management System
- *
- * Description:
- * Layout history.
- * We use SimpleXml to read the xml nodes
- *
- * Requirements:
- * @con_php_req 5.0
+ * Project: CONTENIDO Content Management System
+ * Description: Layout history. We use SimpleXml to read the xml nodes
+ * Requirements: @con_php_req 5.0
  *
  *
- * @version    1.0.0
- * @author     Bilal Arslan
- * @copyright  four for business AG <www.4fb.de>
- * @license    http://www.contenido.org/license/LIZENZ.txt
- * @link       http://www.4fb.de
- * @link       http://www.contenido.org
- * @since      file available since CONTENIDO release >= 5.0
- *
- * {@internal
- *   created 2008-08-1
- *   $Id$:
- * }}
- *
+ * @version 1.0.0
+ * @author Bilal Arslan
+ * @copyright four for business AG <www.4fb.de>
+ * @license http://www.contenido.org/license/LIZENZ.txt
+ * @link http://www.4fb.de
+ * @link http://www.contenido.org
+ * @since file available since CONTENIDO release >= 5.0 {@internal created
+ *        2008-08-1 $Id: include.lay_history.php 3742 2012-11-23 10:34:09Z
+ *        frederic.schneider $: }}
  */
 if (!defined('CON_FRAMEWORK')) {
     die('Illegal call');
 }
-
 
 // For update current layout with Revision
 cInclude("includes", "functions.lay.php");
@@ -41,45 +30,47 @@ $oPage = new cGuiPage("lay_history");
 $bDeleteFile = false;
 
 if (!$perm->have_perm_area_action($area, 'lay_history_manage')) {
-  $oPage->displayError(i18n("Permission denied"));
-  $oPage->render();
+    $oPage->displayError(i18n("Permission denied"));
+    $oPage->render();
 } else if (!(int) $client > 0) {
-  $oPage->render();
+    $oPage->render();
 } else if (getEffectiveSetting('versioning', 'activated', 'false') == 'false') {
-  $oPage->displayWarning(i18n("Versioning is not activated"));
-  $oPage->render();
+    $oPage->displayWarning(i18n("Versioning is not activated"));
+    $oPage->render();
 } else {
-    if ($_POST["lay_send"] == true && $_POST["layname"]!="" && $_POST["laycode"] !="" && (int) $idlay > 0) { // save button
+    if ($_POST["lay_send"] == true && $_POST["layname"] != "" && $_POST["laycode"] != "" && (int) $idlay > 0) { // save
+                                                                                                             // button
         $oVersion = new cVersionLayout($idlay, $cfg, $cfgClient, $db, $client, $area, $frame);
         $sLayoutName = $_POST["layname"];
         $sLayoutCode = $_POST["laycode"];
         $sLayoutDescription = $_POST["laydesc"];
 
-    //    save and mak new revision
+        // save and mak new revision
         $oPage->addScript($oVersion->renderReloadScript('lay', $idlay, $sess));
         layEditLayout($idlay, $sLayoutName, $sLayoutDescription, $sLayoutCode);
         unset($oVersion);
     }
 
     // [action] => history_truncate delete all current modul history
-      if($_POST["action"] == "history_truncate") {
+    if ($_POST["action"] == "history_truncate") {
         $oVersion = new cVersionLayout($idlay, $cfg, $cfgClient, $db, $client, $area, $frame);
-          $bDeleteFile = $oVersion->deleteFile();
+        $bDeleteFile = $oVersion->deleteFile();
         unset($oVersion);
-      }
+    }
 
     // Init construct with CONTENIDO variables, in class.VersionLayout
     $oVersion = new cVersionLayout($idlay, $cfg, $cfgClient, $db, $client, $area, $frame);
 
     // Init Form variables of SelectBox
     $sSelectBox = "";
-    $oVersion->setVarForm("area",  $area);
+    $oVersion->setVarForm("area", $area);
     $oVersion->setVarForm("frame", $frame);
     $oVersion->setVarForm("idlay", $idlay);
-        // needed - otherwise history can not be deleted!
+    // needed - otherwise history can not be deleted!
     $oVersion->setVarForm("action", '');
 
-    // create and output the select box, for params please look class.version.php
+    // create and output the select box, for params please look
+    // class.version.php
     $sSelectBox = $oVersion->buildSelectBox("mod_history", "Layout History", i18n("Show history entry"), "idlayhistory");
 
     // Generate Form
@@ -101,16 +92,16 @@ if (!$perm->have_perm_area_action($area, 'lay_history_manage')) {
         // File Path
         $sPath = $oVersion->getFilePath() . $sRevision;
 
-        // Read XML Nodes  and get an array
+        // Read XML Nodes and get an array
         $aNodes = array();
         $aNodes = $oVersion->initXmlReader($sPath);
 
         // Create Textarea and fill it with xml nodes
         if (count($aNodes) > 1) {
-            //    if choose xml file read value an set it
+            // if choose xml file read value an set it
             $sName = $oVersion->getTextBox("layname", stripslashes_deep(conHtmlentities(conHtmlSpecialChars($aNodes["name"]))), 60);
             $description = $oVersion->getTextarea("laydesc", stripslashes_deep(conHtmlSpecialChars($aNodes["desc"])), 100, 10);
-            $sCode = $oVersion->getTextarea("laycode", $aNodes["code"], 100, 30, "IdLaycode");
+            $sCode = $oVersion->getTextarea("laycode", conHtmlSpecialChars($aNodes["code"]), 100, 30, "IdLaycode");
         }
     }
 
@@ -118,29 +109,29 @@ if (!$perm->have_perm_area_action($area, 'lay_history_manage')) {
     $oForm->add(i18n("Name"), $sName);
     $oForm->add(i18n("Description"), $description);
     $oForm->add(i18n("Code"), $sCode);
-    $oForm->setActionButton("apply", "images/but_ok.gif", i18n("Copy to current"), "c"/*, "mod_history_takeover"*/); //modified it
+    $oForm->setActionButton("apply", "images/but_ok.gif", i18n("Copy to current"), "c"/*, "mod_history_takeover"*/); // modified
+                                                                                                                     // it
     $oForm->unsetActionButton("submit");
 
     // Render and handle History Area
     $oCodeMirrorOutput = new CodeMirror('IdLaycode', 'php', substr(strtolower($belang), 0, 2), true, $cfg, !$bInUse);
     $oPage->addScript($oCodeMirrorOutput->renderScript());
 
-    if($sSelectBox !="") {
+    if ($sSelectBox != "") {
         $div = new cHTMLDiv();
-        $div->setContent($sSelectBox."<br>");
-        $oPage->setContent(array($div, $oForm));
-
+        $div->setContent($sSelectBox . "<br>");
+        $oPage->setContent(array(
+                $div,
+                $oForm
+        ));
     } else {
-        if($bDeleteFile){
+        if ($bDeleteFile) {
             $oPage->displayInfo(i18n("Version history was cleared"));
         } else {
             $oPage->displayWarning(i18n("No layout history available"));
         }
-
     }
     $oPage->render();
-
-
 }
 
 ?>
