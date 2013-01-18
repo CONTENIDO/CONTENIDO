c5fb5c62e00322971a612c1d54edbec7138d8058
Marcus Gnaß <marcus.gnass@4fb.de>
2013-01-18 15:36:16 +0000

    added module navigation_top

---
+++ cms/data/modules/navigation_top/php/navigation_top_output.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * description: top navigation
+ *
+ * @package Module
+ * @subpackage navigation_top
+ * @author marcus.gnass@4fb.de
+ * @copyright four for business AG <www.4fb.de>
+ * @license http://www.contenido.org/license/LIZENZ.txt
+ * @link http://www.4fb.de
+ * @link http://www.contenido.org
+ */
+
+// assert framework initialization
+defined('CON_FRAMEWORK') || die('Illegal call: Missing framework initialization - request aborted.');
+
+// get client settings
+$rootIdcat = getEffectiveSetting('navigation_top', 'idcat', 1);
+$depth = getEffectiveSetting('navigation_top', 'depth', 3);
+
+// get category tree
+$categoryHelper = cCategoryHelper::getInstance();
+$categoryHelper->setAuth(cRegistry::getAuth());
+$tree = $categoryHelper->getSubCategories($rootIdcat, $depth);
+
+// get path (breadcrumb) of current category
+$path = array_map(function (cApiCategoryLanguage $categoryLanguage) {
+    return $categoryLanguage->get('idcat');
+}, $categoryHelper->getCategoryPath(cRegistry::getCategoryId(), 1));
+
+// use template to display navigation
+$tpl = Contenido_SmartyWrapper::getInstance();
+global $force;
+if (1 == $force) {
+    $tpl->clearAllCache();
+}
+$tpl->assign('tree', $tree);
+$tpl->assign('path', $path);
+$tpl->display('navigation_top/template/get.tpl');
+
+?>
