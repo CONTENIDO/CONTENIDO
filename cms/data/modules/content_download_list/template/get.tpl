44c0076d073b0c15d360f833bf1407fcad1d11da
Timo Trautmann <timo.trautmann@4fb.de>
2013-01-25 11:55:01 +0000

    added alpha implementation for downloadlist, added dummy module for link list, added module code header for teaser modules

---
+++ cms/data/modules/content_download_list/template/get.tpl
@@ -0,0 +1,9 @@
+<!-- content_download_list -->
+
+{if 0 lt $label|strlen}
+    <label class="content-type-label">{$label}</label>
+{/if}
+
+{$filelist}
+
+<!-- /content_download_list -->
--- cms/data/modules/content_teaser_text/php/content_teaser_text_output.php
+++ cms/data/modules/content_teaser_text/php/content_teaser_text_output.php
@@ -1,4 +1,16 @@
 <?php
+	/**
+	 * description: standard teaser - text teaser
+	 *
+	 * @package Module
+	 * @subpackage content_teaser
+	 * @author timo.trautmann@4fb.de
+	 * @copyright four for business AG <www.4fb.de>
+	 * @license http://www.contenido.org/license/LIZENZ.txt
+	 * @link http://www.4fb.de
+	 * @link http://www.contenido.org
+	 */
+
     echo "CMS_TEASER[3]";
 
     mi18n("MORE");
