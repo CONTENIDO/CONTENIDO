c76b3f09e05fadf242022eef75780060ba6cd3e4
Alexander Scheider <alexander.scheider@4fb.de>
2013-02-01 14:41:33 +0000

    added bussiness logic to the module

--- cms/data/modules/content_link_list/php/content_link_list_output.php
+++ cms/data/modules/content_link_list/php/content_link_list_output.php
@@ -1,37 +1,80 @@
 <?php
-    /**
-     * description: standard link list
-     *
-     * @package Module
-     * @subpackage content_list
-     * @author timo.trautmann@4fb.de
-     * @copyright four for business AG <www.4fb.de>
-     * @license http://www.contenido.org/license/LIZENZ.txt
-     * @link http://www.4fb.de
-     * @link http://www.contenido.org
-     */
 
-    // When in backend edit mode add a label so the author
-    // knows what to type in the shown field.
-    // When not in backend edit mode any tags are removed
-    // for the template is responsible for displaying the
-    // given text as a header.
-    if (cRegistry::isBackendEditMode()) {
-        $label = mi18n("LABEL_HEADER_LINKLIST");
-    } else {
-        $label = NULL;
+/**
+ * description: standard link list
+ *
+ * @package Module
+ * @subpackage content_list
+ * @author timo.trautmann@4fb.de
+ * @copyright four for business AG <www.4fb.de>
+ * @license http://www.contenido.org/license/LIZENZ.txt
+ * @link http://www.4fb.de
+ * @link http://www.contenido.org
+ */
+
+// include class
+cInclude('classes', 'class.typegenerator.php');
+
+// use smarty template to output header text
+$tpl = Contenido_SmartyWrapper::getInstance();
+
+// initialize var's
+$type = "CMS_LINKEDITOR";
+$typeid = 500;
+$idartlang = cRegistry::getArticleLanguageId(true);
+$artId = cRegistry::getArticleId(true);
+$client = cRegistry::getClientId(true);
+$lang = cRegistry::getLanguageId(true);
+
+// create typegenerator object
+$ocType = new cTypeGenerator();
+
+global $force;
+if (1 == $force) {
+    $tpl->clearAllCache();
+}
+
+if ($_POST['linkCount']) {
+
+    $value = $_POST['linkCount'];
+    for ($i = 1; $i <= $value; $i++) {
+
+        $val = $val . $ocType->getGeneratedCmsTag("CMS_LINKEDITOR", $i);
     }
 
-    // use smarty template to output header text
-    $tpl = Contenido_SmartyWrapper::getInstance();
+    conSaveContentEntry($idartlang, $type, $typeid, $val);
+}
 
+// if backendmode then add additional fields
+if (cRegistry::isBackendEditMode()) {
+    $label = mi18n("LABEL_HEADER_LINKLIST");
+    $input = '<input type="text" name="text_field" id="text_field"/>';
+    $button = '<input type="button" id="create_linkfields" value="create"/>';
+} else {
+    $label = NULL;
+    $input = NULL;
+    $button = NULL;
+}
 
-    global $force;
-    if (1 == $force) {
-        $tpl->clearAllCache();
-    }
+// load article if exists otherwise create new one
+$art = new Article($artId, $client, $lang);
 
-    //TODO IMPLEMENTATION OF LOGIC
-    $tpl->assign('label', $label);
-    $tpl->display('content_link_list/template/get.tpl');
+global $force;
+if (1 == $force) {
+    $tpl->clearAllCache();
+}
+
+// assign data to the smarty template
+$tpl->assign('label', $label);
+$tpl->assign('usable_links', mi18n("usable_links"));
+
+// if article was successfully loaded assign the content
+if ($art->isLoaded()) {
+    $tpl->assign('contents', $art->getContent($type, $typeid));
+}
+$tpl->assign('inputfield', $input);
+$tpl->assign('button', $button);
+
+echo $tpl->fetch('content_link_list/template/get.tpl');
+
 ?>
--- cms/data/modules/content_link_list/template/get.tpl
+++ cms/data/modules/content_link_list/template/get.tpl
@@ -3,14 +3,19 @@
     <label class="content-type-label">{$label}</label>
 {/if}
 
-<h2>Nützliche Links</h2>
+
+<h2>{$usable_links}</h2>
 
 <ul class="linklist">
-    <!-- BEGIN:BLOCK -->
-    <li>
-        <a href="">www.beispiel.de</a>
-        <p>Bacon ipsum dolor sit amet pancetta beef ribs venison doner, pork loin brisket kielbasa pork chop fatback tri-tip turducken ground round jerky bresaola. Shoulder brisket shank doner strip steak. Capicola tenderloin chicken filet mignon t-bone </p>
-    </li>
-    <!-- END:BLOCK -->
+	<!-- BEGIN:BLOCK -->
+	<li>
+		{foreach $contents as $content}
+			{$content}
+		{/foreach}
+	</li>
+	{$inputfield}
+	{$button}
+	<!-- END:BLOCK -->
+
 </ul>
 <!-- content_link_list -->
