db4015bf7ce5334893e75b4da54fbb1c08073c3d
Marcus Gnaß <marcus.gnass@4fb.de>
2013-02-12 13:21:40 +0000

    reformatted code

--- cms/data/modules/content_teaser_slider/php/content_teaser_slider_output.php
+++ cms/data/modules/content_teaser_slider/php/content_teaser_slider_output.php
@@ -1,17 +1,19 @@
 <?php
-    /**
-     * description: standard teaser - sliding element
-     *
-     * @package Module
-     * @subpackage content_teaser
-     * @author timo.trautmann@4fb.de
-     * @copyright four for business AG <www.4fb.de>
-     * @license http://www.contenido.org/license/LIZENZ.txt
-     * @link http://www.4fb.de
-     * @link http://www.contenido.org
-     */
 
-    echo "CMS_TEASER[1]";
+/**
+ * description: standard teaser - sliding element
+ *
+ * @package Module
+ * @subpackage content_teaser
+ * @author timo.trautmann@4fb.de
+ * @copyright four for business AG <www.4fb.de>
+ * @license http://www.contenido.org/license/LIZENZ.txt
+ * @link http://www.4fb.de
+ * @link http://www.contenido.org
+ */
 
-    mi18n("MORE");
+echo "CMS_TEASER[1]";
+
+mi18n("MORE");
+
 ?>
