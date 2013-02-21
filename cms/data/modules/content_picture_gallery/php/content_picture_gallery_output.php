528c48cdd4ced79dcf014f2e4919ec8ec592d4b8
Timo Trautmann <timo.trautmann@4fb.de>
2013-02-21 11:19:59 +0000

    fixed bug in picture gallery

--- cms/data/modules/content_picture_gallery/php/content_picture_gallery_output.php
+++ cms/data/modules/content_picture_gallery/php/content_picture_gallery_output.php
@@ -2,7 +2,7 @@
     $tpl = Contenido_SmartyWrapper::getInstance();
     $filelistIndex = 1;
 
-    $art = new Article(cRegistry::getArticleLanguageId(), cRegistry::getClientId(), cRegistry::getLanguageId());
+    $art = new Article(cRegistry::getArticleId(), cRegistry::getClientId(), cRegistry::getLanguageId());
     $contentValue = $art->getContent("FILELIST", $filelistIndex);
 
     $filelist = new cContentTypeFilelist($contentValue, $filelistIndex, array());
