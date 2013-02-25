44b8c56237451fc3fc0b64aeda7375e31205378a
Marcus Gnaß <marcus.gnass@4fb.de>
2013-02-25 10:34:14 +0000

    added comments to form_contact

--- cms/data/modules/form_contact/php/form_contact_output.php
+++ cms/data/modules/form_contact/php/form_contact_output.php
@@ -1,5 +1,18 @@
 <?php
 
-echo "CMS_PIFAFORM[1]";
+/**
+ * description: contact (PIFA) form
+ *
+ * @package Module
+ * @subpackage form_contact
+ * @author marcus.gnass@4fb.de
+ * @copyright four for business AG <www.4fb.de>
+ * @license http://www.contenido.org/license/LIZENZ.txt
+ * @link http://www.4fb.de
+ * @link http://www.contenido.org
+ */
+
+// there's no need to echo it ...
+"CMS_PIFAFORM[1]";
 
 ?>
