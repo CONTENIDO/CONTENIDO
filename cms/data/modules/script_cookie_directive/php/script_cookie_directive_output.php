b714d80b01030e5d643c6acc739aa8d364ce7a7a
Marcus Gnaß <marcus.gnass@4fb.de>
2013-03-04 12:40:47 +0000

    fixed formatting

--- cms/data/modules/script_cookie_directive/php/script_cookie_directive_output.php
+++ cms/data/modules/script_cookie_directive/php/script_cookie_directive_output.php
@@ -1,42 +1,43 @@
 <?php
-	global $allowCookie;
-	
-	$tpl = Contenido_SmartyWrapper::getInstance();
-	
-	//Check global value
-	if (!isset($allowCookie)) {
-		
-		$allowCookie = 0;			
-		
-	}
-	
-	//Check value in get, if js is off
-	if (array_key_exists('acceptCookie', $_GET)) {
-			
-		$allowCookie = $_GET['acceptCookie'] === '1' ? 1 : 0;
-		setcookie('allowCookie', $allowCookie);
-			
-	} elseif (array_key_exists('allowCookie', $_COOKIE)) {
-		
-		//Check value in cookies
-		$allowCookie = $_COOKIE['allowCookie'] === '1' ? 1 : 0;
-		
-	}
-	
-	//Save value
-	$session = cRegistry::getSession();
-	$session->register('allowCookie');
-	
-	//Show notify
-	if ($allowCookie !== 1) {
-		
-		$idart = cRegistry::getArticleId();
-		$pageUrl = 'front_content.php?idart=' . $idart . '&acceptCookie=1';
-		
-		$tpl->assign('pageUrl', $pageUrl);
-		
-		$tpl->display('get.tpl');
-		
-	}
-		
+
+global $allowCookie;
+
+$tpl = Contenido_SmartyWrapper::getInstance();
+
+// Check global value
+if (!isset($allowCookie)) {
+
+    $allowCookie = 0;
+
+}
+
+// Check value in get, if js is off
+if (array_key_exists('acceptCookie', $_GET)) {
+
+    $allowCookie = $_GET['acceptCookie'] === '1'? 1 : 0;
+    setcookie('allowCookie', $allowCookie);
+
+} elseif (array_key_exists('allowCookie', $_COOKIE)) {
+
+    // Check value in cookies
+    $allowCookie = $_COOKIE['allowCookie'] === '1'? 1 : 0;
+
+}
+
+// Save value
+$session = cRegistry::getSession();
+$session->register('allowCookie');
+
+// Show notify
+if ($allowCookie !== 1) {
+
+    $idart = cRegistry::getArticleId();
+    $pageUrl = 'front_content.php?idart=' . $idart . '&acceptCookie=1';
+
+    $tpl->assign('pageUrl', $pageUrl);
+
+    $tpl->display('get.tpl');
+
+}
+
 ?>
