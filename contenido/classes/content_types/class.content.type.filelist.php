d6d2368397e4cd507eae75ad7e4d58871385b9c0
Frederic Schneider <frederic.schneider@4fb.de>
2013-03-19 13:52:05 +0000

    CON-1026: Catchable fatal error on empty file lists

--- contenido/classes/content_types/class.content.type.filelist.php
+++ contenido/classes/content_types/class.content.type.filelist.php
@@ -239,7 +239,9 @@ class cContentTypeFilelist extends cContentTypeAbstractTabbed {
             return '';
         }
 
-        $files = $this->_applyFileFilters($fileList);
+        if (is_array($fileList)) {
+            $files = $this->_applyFileFilters($fileList);
+        }
         unset($fileList);
 
         if (count($files) > 0) {
@@ -430,8 +432,12 @@ class cContentTypeFilelist extends cContentTypeAbstractTabbed {
         if ($info & (IMAGETYPE_GIF | IMAGETYPE_JPEG | IMAGETYPE_PNG)) {
             $imgSrc = capiImgScale($filePath, 148, 74);
         } else {
-            //file_put_contents($this->_cfgClient[$this->_client]['cache']['path'] . 'empty.gif',"\107\111\106\70\71\141\001\000\001\000\360\001\000\377\377\377\000\000\000\041\371\004\001\012\000\000\000\054\000\000\000\000\001\000\001\000\000\002\002\104\001\000\073");
-            //$imgSrc = $this->_cfgClient[$this->_client]['cache']['frontendpath'] . 'empty.gif';
+            // file_put_contents($this->_cfgClient[$this->_client]['cache']['path']
+            // .
+            // 'empty.gif',"\107\111\106\70\71\141\001\000\001\000\360\001\000\377\377\377\000\000\000\041\371\004\001\012\000\000\000\054\000\000\000\000\001\000\001\000\000\002\002\104\001\000\073");
+            // $imgSrc =
+            // $this->_cfgClient[$this->_client]['cache']['frontendpath'] .
+            // 'empty.gif';
             $imgSrc = $this->_cfgClient[$this->_client]['path']['htmlpath'] . 'images/misc/download_misc.png';
         }
 
@@ -929,4 +935,5 @@ class cContentTypeFilelist extends cContentTypeAbstractTabbed {
 
         return $htmlSelect->render();
     }
+
 }
