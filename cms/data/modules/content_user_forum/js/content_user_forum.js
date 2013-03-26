6a8cac2c78095769ce78e017e77d1a3ee08d8271
Claus Schunk <claus.schunk@4fb.de>
2013-03-26 10:05:19 +0000

    disable like/dislike - link after click : prevents bug to add more likes while loading page.

--- cms/data/modules/content_user_forum/js/content_user_forum.js
+++ cms/data/modules/content_user_forum/js/content_user_forum.js
@@ -2,7 +2,11 @@ $(document).ready(function() {
 
 $(".like, .dislike").click(function(e){
     var dir = $(this).children("a").attr("href");
+    // disable link after click : prevents bug to add more likes while loading page.
+    $(this).children("a").attr("href","javascript:void(0)");
     window.location=dir;
+    
+    
 });
 
 $("#calendarTable td").each(function(){
