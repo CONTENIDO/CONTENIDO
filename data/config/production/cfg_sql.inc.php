2216a24ffd002d394e65970203e34b223c5814ce
Alexander Scheider <alexander.scheider@4fb.de>
2013-04-09 14:21:19 +0000

    added new tables to the config

--- data/config/production/cfg_sql.inc.php
+++ data/config/production/cfg_sql.inc.php
@@ -87,7 +87,8 @@ $cfg['tab']['type'] = $cfg['sql']['sqlprefix'] . '_type';
 $cfg['tab']['upl_meta'] = $cfg['sql']['sqlprefix'] . '_upl_meta';
 $cfg['tab']['upl'] = $cfg['sql']['sqlprefix'] . '_upl';
 $cfg['tab']['user_prop'] = $cfg['sql']['sqlprefix'] . '_user_prop';
-
+$cfg['tab']['iso_639_2'] = $cfg['sql']['sqlprefix'] . '_iso_639_2';
+$cfg['tab']['iso_3166'] = $cfg['sql']['sqlprefix'] . '_iso_3166';
 
 $cfg['tab']['phplib_auth_user_md5'] = $cfg['tab']['user'];
 ?>
