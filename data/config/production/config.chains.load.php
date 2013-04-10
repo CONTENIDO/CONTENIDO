a891763e0af812f4cfc1179738aabd503b7975c4
Murat Purc <murat@purc.de>
2013-04-10 20:39:41 +0000

    Bug fix: Include chain to create meta-tags from correct location

--- data/config/production/config.chains.load.php
+++ data/config/production/config.chains.load.php
@@ -12,10 +12,10 @@ $_cecRegistry = cApiCecRegistry::getInstance();
 
 cInclude('includes', 'chains/include.chain.frontend.cat_backendaccess.php');
 cInclude('includes', 'chains/include.chain.frontend.cat_access.php');
-cInclude("plugins", "chains/includes/include.chain.content.createmetatags.php");
+cInclude('includes', 'chains/include.chain.content.createmetatags.php');
 cInclude('includes', 'chains/include.chain.frontend.createbasehref.php');
 
-$_cecRegistry->addChainFunction("Contenido.Frontend.CategoryAccess", "cecFrontendCategoryAccess");
-$_cecRegistry->addChainFunction("Contenido.Frontend.CategoryAccess", "cecFrontendCategoryAccess_Backend");
-$_cecRegistry->addChainFunction("Contenido.Content.CreateMetatags", "cecCreateMetatags");
-$_cecRegistry->addChainFunction("Contenido.Frontend.BaseHrefGeneration", "cecCreateBaseHref");
+$_cecRegistry->addChainFunction('Contenido.Frontend.CategoryAccess', 'cecFrontendCategoryAccess');
+$_cecRegistry->addChainFunction('Contenido.Frontend.CategoryAccess', 'cecFrontendCategoryAccess_Backend');
+$_cecRegistry->addChainFunction('Contenido.Content.CreateMetatags', 'cecCreateMetatags');
+$_cecRegistry->addChainFunction('Contenido.Frontend.BaseHrefGeneration', 'cecCreateBaseHref');
