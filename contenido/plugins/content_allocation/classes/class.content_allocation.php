48a25db44b3c81ecee4ce796af751ede0f1192cc
Claus Schunk <claus.schunk@4fb.de>
2013-05-03 10:39:12 +0000

    bug fix loadAllocationsFromArticle : correct mapping for key =>value

--- contenido/plugins/content_allocation/classes/class.content_allocation.php
+++ contenido/plugins/content_allocation/classes/class.content_allocation.php
@@ -2,17 +2,16 @@
 /**
  * This file contains the main class for the plugin content allocation.
  *
- * @package    Plugin
+ * @package Plugin
  * @subpackage ContentAllocation
- * @version    SVN Revision $Rev:$
+ * @version SVN Revision $Rev:$
  *
- * @author     Marco Jahn
- * @copyright  four for business AG <www.4fb.de>
- * @license    http://www.contenido.org/license/LIZENZ.txt
- * @link       http://www.4fb.de
- * @link       http://www.contenido.org
+ * @author Marco Jahn
+ * @copyright four for business AG <www.4fb.de>
+ * @license http://www.contenido.org/license/LIZENZ.txt
+ * @link http://www.4fb.de
+ * @link http://www.contenido.org
  */
-
 defined('CON_FRAMEWORK') || die('Illegal call: Missing framework initialization - request aborted.');
 
 plugin_include('repository', 'custom/FrontendNavigation.php');
@@ -20,7 +19,7 @@ plugin_include('repository', 'custom/FrontendNavigation.php');
 /**
  * Main class for content allocation
  *
- * @package    Plugin
+ * @package Plugin
  * @subpackage ContentAllocation
  */
 class pApiContentAllocation {
@@ -31,17 +30,20 @@ class pApiContentAllocation {
      * @var cDb
      */
     var $db = null;
+
     var $table = null;
+
     var $lang = null;
+
     var $client = null;
 
     var $treeObj = null;
 
     /**
-     *
-     * @modified 27.10.2005 new class variable $this->bDebug (if true print debug information)
+     * @modified 27.10.2005 new class variable $this->bDebug (if true print
+     * debug information)
      */
-    function pApiContentAllocation () {
+    function pApiContentAllocation() {
         global $db, $cfg, $lang, $client;
 
         $this->db = cRegistry::getDb();
@@ -56,49 +58,60 @@ class pApiContentAllocation {
         $this->treeObj = new pApiTree('f31a4384-e5c1-4ede-b1bb-f43657ec73a5');
     }
 
-    function storeAllocations ($idartlang, $allocations) {
+    function storeAllocations($idartlang, $allocations) {
         // empty before insert
         $this->deleteAllocationsByIdartlang($idartlang);
 
         if (is_array($allocations)) {
             foreach ($allocations as $value) {
-                $sql = "INSERT INTO ".$this->table['pica_alloc_con']." (idpica_alloc, idartlang) VALUES (".cSecurity::toInteger($value).", ".cSecurity::toInteger($idartlang).")";
+                $sql = "INSERT INTO " . $this->table['pica_alloc_con'] . " (idpica_alloc, idartlang) VALUES (" . cSecurity::toInteger($value) . ", " . cSecurity::toInteger($idartlang) . ")";
                 $this->db->query($sql);
             }
         }
     }
 
-    function deleteAllocations ($idpica_alloc) {
-        $sql = "DELETE FROM ".$this->table['pica_alloc_con']." WHERE idpica_alloc = " . cSecurity::toInteger($idpica_alloc);
+    function deleteAllocations($idpica_alloc) {
+        $sql = "DELETE FROM " . $this->table['pica_alloc_con'] . " WHERE idpica_alloc = " . cSecurity::toInteger($idpica_alloc);
         $this->db->query($sql);
     }
 
-    function deleteAllocationsByIdartlang ($idartlang) {
-        $sql = "DELETE FROM ".$this->table['pica_alloc_con']." WHERE idartlang = " . cSecurity::toInteger($idartlang);
+    function deleteAllocationsByIdartlang($idartlang) {
+        $sql = "DELETE FROM " . $this->table['pica_alloc_con'] . " WHERE idartlang = " . cSecurity::toInteger($idartlang);
         $this->db->query($sql);
     }
 
-    function loadAllocations ($idartlang) {
-        $sql = "SELECT idpica_alloc FROM ".$this->table['pica_alloc_con']." WHERE idartlang = " . cSecurity::toInteger($idartlang);
+    function loadAllocations($idartlang) {
+        $result = array();
+        // load all tagging keys
+        $sql = 'SELECT a.idpica_alloc FROM con_pica_alloc a , con_pica_alloc_con b WHERE idartlang=' . $idartlang . ' AND a.idpica_alloc=b.idpica_alloc AND parentid=0';
         $this->db->query($sql);
-
-        $items = array();
 
         while ($this->db->nextRecord()) {
-            $items[] = $this->db->f('idpica_alloc');
+            $keys[] = $this->db->f('idpica_alloc');
         }
-        return $items;
+
+        // load all values for keys
+        for ($i = 0; $i < count($keys); $i++) {
+            $sql = 'SELECT a.idpica_alloc FROM con_pica_alloc a , con_pica_alloc_con b WHERE idartlang=' . $idartlang . ' AND a.idpica_alloc=b.idpica_alloc AND parentid=' . $keys[$i];
+            $this->db->query($sql);
+
+            while ($this->db->nextRecord()) {
+                $result[] = $keys[$i];
+                $result[] = $this->db->f('idpica_alloc');
+            }
+        }
+
+        return $result;
     }
 
-    function loadAllocationsWithNames ($idartlang, $parent, $firstonly = false) {
-
+    function loadAllocationsWithNames($idartlang, $parent, $firstonly = false) {
         global $cfg;
 
-        $sql = "SELECT ".$cfg['tab']['pica_alloc'].".idpica_alloc FROM ".$cfg['tab']['pica_alloc']."
-                    INNER JOIN ".$cfg['tab']['pica_alloc_con']." ON
-                    ".$cfg['tab']['pica_alloc'].".idpica_alloc = ".$cfg['tab']['pica_alloc_con'].".idpica_alloc
-                    WHERE (".$cfg['tab']['pica_alloc'].".parentid = ".cSecurity::toInteger($parent).") AND (".$cfg['tab']['pica_alloc_con'].".idartlang=".cSecurity::toInteger($idartlang).")
-                    ORDER BY ".$cfg['tab']['pica_alloc'].".sortorder";
+        $sql = "SELECT " . $cfg['tab']['pica_alloc'] . ".idpica_alloc FROM " . $cfg['tab']['pica_alloc'] . "
+                    INNER JOIN " . $cfg['tab']['pica_alloc_con'] . " ON
+                    " . $cfg['tab']['pica_alloc'] . ".idpica_alloc = " . $cfg['tab']['pica_alloc_con'] . ".idpica_alloc
+                    WHERE (" . $cfg['tab']['pica_alloc'] . ".parentid = " . cSecurity::toInteger($parent) . ") AND (" . $cfg['tab']['pica_alloc_con'] . ".idartlang=" . cSecurity::toInteger($idartlang) . ")
+                    ORDER BY " . $cfg['tab']['pica_alloc'] . ".sortorder";
 
         $this->db->query($sql);
 
@@ -108,7 +121,6 @@ class pApiContentAllocation {
             if ($firstonly) {
                 break;
             }
-
         }
 
         return $tmp;
@@ -116,15 +128,17 @@ class pApiContentAllocation {
 
     /**
      * Build query to find matching content by ContentAllocation
+     *
      * @param array $restrictions
      * @return string SQL
-     * @modified 17.11.2005 by Willi Man
+     *         @modified 17.11.2005 by Willi Man
      */
-    function findMatchingContent ($restrictions = null, $max = 0)
-    {
-        if (!is_array($restrictions)) { return false; }
+    function findMatchingContent($restrictions = null, $max = 0) {
+        if (!is_array($restrictions)) {
+            return false;
+        }
 
-        global $aCategoriesToExclude; # @see config.local.php!
+        global $aCategoriesToExclude; // @see config.local.php!
         $sql = $this->_buildQuery($restrictions, $aCategoriesToExclude, $max);
 
         return $sql;
@@ -132,11 +146,11 @@ class pApiContentAllocation {
 
     /**
      * Build query to find matching content by ContentAllocation
+     *
      * @param array $restrictions
      * @return string SQL
      */
-    function _buildQuery ($restrictions, $aCategoriesToExclude, $max) {
-
+    function _buildQuery($restrictions, $aCategoriesToExclude, $max) {
         global $cfg;
 
         $size = sizeof($restrictions);
@@ -154,27 +168,27 @@ class pApiContentAllocation {
 
         for ($i = 0; $i < $size; $i++) {
             if ($i == 0) { // first
-                $tables[] = " ".$cfg['tab']['pica_alloc_con']." AS " . $sql_concat[$i];
+                $tables[] = " " . $cfg['tab']['pica_alloc_con'] . " AS " . $sql_concat[$i];
             } else {
-                $tables[] = " LEFT JOIN ".$cfg['tab']['pica_alloc_con']." AS " . $sql_concat[$i] . " USING (idartlang)";
+                $tables[] = " LEFT JOIN " . $cfg['tab']['pica_alloc_con'] . " AS " . $sql_concat[$i] . " USING (idartlang)";
             }
-            if (is_int((int)$restrictions[$i]) AND $restrictions[$i] > 0) {
-                $where[] =  $sql_concat[$i] . ".idpica_alloc = " . $restrictions[$i];
+            if (is_int((int) $restrictions[$i]) and $restrictions[$i] > 0) {
+                $where[] = $sql_concat[$i] . ".idpica_alloc = " . $restrictions[$i];
             }
         }
 
-        # fetch only articles which are online
+        // fetch only articles which are online
         $where[] = 'cal.online = 1';
 
-        # fetch only articles which are not in following categories
+        // fetch only articles which are not in following categories
         if (count($aCategoriesToExclude) > 0) {
-            $where[] = "cat.idcat NOT IN (".implode(',', $aCategoriesToExclude).")";
+            $where[] = "cat.idcat NOT IN (" . implode(',', $aCategoriesToExclude) . ")";
         }
 
         // join art_lang for idart
-        $tables[] = " LEFT JOIN ".$this->table['art_lang']." AS cal USING (idartlang)";
-        $tables[] = " LEFT JOIN ".$this->table['cat_art']." AS cart USING (idart)";
-        $tables[] = " LEFT JOIN ".$this->table['cat']." as cat USING (idcat)";
+        $tables[] = " LEFT JOIN " . $this->table['art_lang'] . " AS cal USING (idartlang)";
+        $tables[] = " LEFT JOIN " . $this->table['cat_art'] . " AS cart USING (idart)";
+        $tables[] = " LEFT JOIN " . $this->table['cat'] . " as cat USING (idcat)";
 
         $tables = implode('', $tables);
         $where = implode(' AND ', $where);
@@ -188,30 +202,37 @@ class pApiContentAllocation {
             $sql .= " LIMIT " . $max;
         }
 
-        if ($this->bDebug) {print "<!-- "; print $sql; print " -->";} # @modified 27.10.2005
+        if ($this->bDebug) {
+            print "<!-- ";
+            print $sql;
+            print " -->";
+        } // @modified
+                                                                      // 27.10.2005
 
         return $sql;
     }
 
     /**
      * Search articles by ContentAllocation and catgories
+     *
      * @param array $aContentAllocation
      * @param array $aCategories
      *
      * @return array of articles
      */
-    function findMatchingContentByContentAllocationByCategories ($aContentAllocation, $aCategories = array(), $iOffset = 0, $iNumOfRows = 0)
-    {
-        if (!is_array($aContentAllocation) || count($aContentAllocation) == 0) { return array(); }
+    function findMatchingContentByContentAllocationByCategories($aContentAllocation, $aCategories = array(), $iOffset = 0, $iNumOfRows = 0) {
+        if (!is_array($aContentAllocation) || count($aContentAllocation) == 0) {
+            return array();
+        }
 
         for ($i = 0; $i < count($aContentAllocation); $i++) {
-            if (!is_int((int)$aContentAllocation[$i]) OR !$aContentAllocation[$i] > 0) {
+            if (!is_int((int) $aContentAllocation[$i]) or !$aContentAllocation[$i] > 0) {
                 return array();
             }
         }
 
         for ($i = 0; $i < count($aCategories); $i++) {
-            if (!is_int((int)$aCategories[$i]) OR !$aCategories[$i] > 0) {
+            if (!is_int((int) $aCategories[$i]) or !$aCategories[$i] > 0) {
                 return array();
             }
         }
@@ -225,15 +246,12 @@ class pApiContentAllocation {
             $aResult[] = $oRow;
         }
         return $aResult;
-
     }
 
     /**
      * build SQL query to find articles by ContentAllocation and catgories
-     *
      */
-    function _buildQuery_MatchingContentByContentAllocationByCategories ($aContentAllocation, $aCategories, $iOffset, $iNumOfRows) {
-
+    function _buildQuery_MatchingContentByContentAllocationByCategories($aContentAllocation, $aCategories, $iOffset, $iNumOfRows) {
         global $cfg;
 
         $size = sizeof($aContentAllocation);
@@ -247,27 +265,27 @@ class pApiContentAllocation {
 
         for ($i = 0; $i < $size; $i++) {
             if ($i == 0) { // first
-                $tables[] = " ".$cfg['tab']['pica_alloc_con']." AS " . $sql_concat[$i];
+                $tables[] = " " . $cfg['tab']['pica_alloc_con'] . " AS " . $sql_concat[$i];
             } else {
-                $tables[] = " LEFT JOIN ".$cfg['tab']['pica_alloc_con']." AS " . $sql_concat[$i] . " USING (idartlang)";
+                $tables[] = " LEFT JOIN " . $cfg['tab']['pica_alloc_con'] . " AS " . $sql_concat[$i] . " USING (idartlang)";
             }
-            if (is_int((int)$aContentAllocation[$i]) AND $aContentAllocation[$i] > 0) {
-                $where[] =  $sql_concat[$i] . ".idpica_alloc = " . $aContentAllocation[$i];
+            if (is_int((int) $aContentAllocation[$i]) and $aContentAllocation[$i] > 0) {
+                $where[] = $sql_concat[$i] . ".idpica_alloc = " . $aContentAllocation[$i];
             }
         }
 
-        # fetch only articles which are online
+        // fetch only articles which are online
         $where[] = 'cal.online = 1';
 
-        # fetch only articles in following categories
+        // fetch only articles in following categories
         if (count($aCategories) > 0) {
-            $where[] = "cat.idcat IN (".implode(',', $aCategories).")";
+            $where[] = "cat.idcat IN (" . implode(',', $aCategories) . ")";
         }
 
         // join art_lang for idart
-        $tables[] = " LEFT JOIN ".$this->table['art_lang']." AS cal USING (idartlang)";
-        $tables[] = " LEFT JOIN ".$this->table['cat_art']." AS cart USING (idart)";
-        $tables[] = " LEFT JOIN ".$this->table['cat']." as cat USING (idcat)";
+        $tables[] = " LEFT JOIN " . $this->table['art_lang'] . " AS cal USING (idartlang)";
+        $tables[] = " LEFT JOIN " . $this->table['cat_art'] . " AS cart USING (idart)";
+        $tables[] = " LEFT JOIN " . $this->table['cat'] . " as cat USING (idcat)";
 
         $tables = implode('', $tables);
         $where = implode(' AND ', $where);
@@ -277,18 +295,23 @@ class pApiContentAllocation {
 
         $sql .= " ORDER BY cal.published DESC";
 
-        if (is_integer($iNumOfRows) AND $iNumOfRows > 0)
-        {
-            $sql .= " LIMIT ". $iOffset .", ".$iNumOfRows;
+        if (is_integer($iNumOfRows) and $iNumOfRows > 0) {
+            $sql .= " LIMIT " . $iOffset . ", " . $iNumOfRows;
         }
 
-        if ($this->bDebug) {print "<!-- "; print $sql; print " -->";} # @modified 27.10.2005
+        if ($this->bDebug) {
+            print "<!-- ";
+            print $sql;
+            print " -->";
+        } // @modified
+                                                                      // 27.10.2005
 
         return $sql;
     }
 
     /**
      * Search articles by catgories without start articles
+     *
      * @param array $aCategories
      * @param int $iOffset
      * @param int $iNumOfRows
@@ -296,12 +319,9 @@ class pApiContentAllocation {
      *
      * @return array of articles
      */
-    function findMatchingContentByCategories ($aCategories = array(), $iOffset = 0, $iNumOfRows = 0, $sResultType = '')
-    {
-
-        for ($i = 0; $i < count($aCategories); $i++)
-        {
-            if (!is_int((int)$aCategories[$i]) OR !$aCategories[$i] > 0) {
+    function findMatchingContentByCategories($aCategories = array(), $iOffset = 0, $iNumOfRows = 0, $sResultType = '') {
+        for ($i = 0; $i < count($aCategories); $i++) {
+            if (!is_int((int) $aCategories[$i]) or !$aCategories[$i] > 0) {
                 return array();
             }
         }
@@ -320,23 +340,19 @@ class pApiContentAllocation {
             }
         }
         return $aResult;
-
     }
 
     /**
      * build SQL query to find articles by catgories
-     *
      */
-    function _buildQuery_MatchingContentByCategories ($aCategories, $iOffset, $iNumOfRows)
-    {
-
+    function _buildQuery_MatchingContentByCategories($aCategories, $iOffset, $iNumOfRows) {
         if (count($aCategories) > 0) {
-            $sWHERE_Category_IN = " c.idcat IN (".implode(',', $aCategories).") AND ";
+            $sWHERE_Category_IN = " c.idcat IN (" . implode(',', $aCategories) . ") AND ";
         } else {
             $sWHERE_Category_IN = '';
         }
-        if (is_integer($iNumOfRows) AND $iNumOfRows > 0) {
-            $sLimit = " LIMIT ". cSecurity::toInteger($iOffset) .", " . cSecurity::toInteger($iNumOfRows);
+        if (is_integer($iNumOfRows) and $iNumOfRows > 0) {
+            $sLimit = " LIMIT " . cSecurity::toInteger($iOffset) . ", " . cSecurity::toInteger($iNumOfRows);
         } else {
             $sLimit = '';
         }
@@ -345,22 +361,26 @@ class pApiContentAllocation {
         SELECT
             a.idart, a.online, a.idartlang, c.idcat
         FROM
-            '.$this->table['art_lang'].' AS a,
-            '.$this->table['art'].' AS b,
-            '.$this->table['cat_art'].' AS c,
-            '.$this->table['cat_lang'].' AS d
+            ' . $this->table['art_lang'] . ' AS a,
+            ' . $this->table['art'] . ' AS b,
+            ' . $this->table['cat_art'] . ' AS c,
+            ' . $this->table['cat_lang'] . ' AS d
         WHERE
-            '.$sWHERE_Category_IN.'
-            b.idclient = '.cSecurity::toInteger($this->client).' AND
-            a.idlang = '.cSecurity::toInteger($this->lang).' AND
+            ' . $sWHERE_Category_IN . '
+            b.idclient = ' . cSecurity::toInteger($this->client) . ' AND
+            a.idlang = ' . cSecurity::toInteger($this->lang) . ' AND
             a.idartlang != d.startidartlang AND
             a.online = 1 AND
             c.idcat = d.idcat AND
             b.idart = c.idart AND
             a.idart = b.idart
-            '.$sLimit.' ';
+            ' . $sLimit . ' ';
 
-        if ($this->bDebug) {print "<!-- "; print $sql; print " -->";}
+        if ($this->bDebug) {
+            print "<!-- ";
+            print $sql;
+            print " -->";
+        }
 
         return $sql;
     }
