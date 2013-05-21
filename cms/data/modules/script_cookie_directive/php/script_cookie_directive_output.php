4836ec78a7ead160d58ea1dd399654a05710626f
Dominik Ziegler <dominik.ziegler@4fb.de>
2013-05-21 16:19:12 +0000

    CON-1168 - do not display cookie confirmation in backend edit mode

--- cms/data/modules/script_cookie_directive/php/script_cookie_directive_output.php
+++ cms/data/modules/script_cookie_directive/php/script_cookie_directive_output.php
@@ -14,53 +14,55 @@
  * @link http://www.contenido.org
  */
 
-$session = cRegistry::getSession();
+if (cRegistry::isBackendEditMode() === false) {
 
-if (array_key_exists('acceptCookie', $_GET)) {
-    // Check value in get, if js is off
-    $allowCookie = $_GET['acceptCookie'] === '1'? 1 : 0;
-    setcookie('allowCookie', $allowCookie);
+    $session = cRegistry::getSession();
 
-    // Save value
-    $session->register('allowCookie');
-} elseif (array_key_exists('allowCookie', $_COOKIE)) {
-    // Check value in cookies
-    $allowCookie = $_COOKIE['allowCookie'] === '1'? 1 : 0;
+    if (array_key_exists('acceptCookie', $_GET)) {
+        // Check value in get, if js is off
+        $allowCookie = $_GET['acceptCookie'] === '1'? 1 : 0;
+        setcookie('allowCookie', $allowCookie);
 
-    // Save value
-    $session->register('allowCookie');
+        // Save value
+        $session->register('allowCookie');
+    } elseif (array_key_exists('allowCookie', $_COOKIE)) {
+        // Check value in cookies
+        $allowCookie = $_COOKIE['allowCookie'] === '1'? 1 : 0;
+
+        // Save value
+        $session->register('allowCookie');
+    }
+
+    // Show notify
+    if (!isset($allowCookie)) {
+
+        $tpl = cSmartyFrontend::getInstance();
+
+        // build translations
+        $tpl->assign('trans', array(
+            'title' => mi18n("TITLE"),
+            'infoText' => mi18n("INFOTEXT"),
+            'userInput' => mi18n("USERINPUT"),
+            'accept' => mi18n("ACCEPT"),
+            'decline' => mi18n("DECLINE")
+        ));
+
+        // build accept url
+        $tpl->assign('pageUrlAccept', cUri::getInstance()->build(array(
+            'idart' => cRegistry::getArticleId(),
+            'lang' => cRegistry::getLanguageId(),
+            'acceptCookie' => 1
+        ), true));
+
+        // build deny url
+        $tpl->assign('pageUrlDeny', cUri::getInstance()->build(array(
+            'idart' => cRegistry::getArticleId(),
+            'lang' => cRegistry::getLanguageId(),
+            'acceptCookie' => 0
+        ), true));
+
+        $tpl->display('get.tpl');
+
+    }
 }
-
-// Show notify
-if (!isset($allowCookie)) {
-
-    $tpl = cSmartyFrontend::getInstance();
-
-    // build translations
-    $tpl->assign('trans', array(
-        'title' => mi18n("TITLE"),
-        'infoText' => mi18n("INFOTEXT"),
-        'userInput' => mi18n("USERINPUT"),
-        'accept' => mi18n("ACCEPT"),
-        'decline' => mi18n("DECLINE")
-    ));
-
-    // build accept url
-    $tpl->assign('pageUrlAccept', cUri::getInstance()->build(array(
-        'idart' => cRegistry::getArticleId(),
-        'lang' => cRegistry::getLanguageId(),
-        'acceptCookie' => 1
-    ), true));
-
-    // build deny url
-    $tpl->assign('pageUrlDeny', cUri::getInstance()->build(array(
-        'idart' => cRegistry::getArticleId(),
-        'lang' => cRegistry::getLanguageId(),
-        'acceptCookie' => 0
-    ), true));
-
-    $tpl->display('get.tpl');
-
-}
-
 ?>
