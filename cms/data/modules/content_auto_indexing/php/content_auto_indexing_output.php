f5191f3387687079692385e9beaa5fe7fb7b9598
Claus Schunk <claus.schunk@4fb.de>
2013-05-22 06:51:38 +0000

    initial auto index module.

---
+++ cms/data/modules/content_auto_indexing/php/content_auto_indexing_output.php
@@ -0,0 +1,23 @@
+<?php
+
+ /**
+  *  This module is used to start the automatic keyword generation progress
+  *  and SHOULD NOT be used permanently because of performance reasons.
+  * 
+  * 
+  * 
+  * @package Module
+  * @subpackage AutoIndexing
+  * @version SVN Revision $Rev:$
+  * @author claus.schunk
+  * @copyright four for business AG
+  * @link http://www.4fb.de
+  */
+ defined('CON_FRAMEWORK') || die('Illegal call: Missing framework initialization - request aborted.');
+
+ cInclude('frontend', 'classes/class.kvh.auto_indexing.php');
+
+ if (AutoIndexing::start() == 0) {
+     echo 'indexierung erfolgreich abgeschlossen !';
+ }
+?>
