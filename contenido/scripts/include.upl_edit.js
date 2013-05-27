6c26d396c78f25f5404e05852caf5a0afe1eba11
Claus Schunk <claus.schunk@4fb.de>
2013-05-27 14:23:24 +0000

    zip extraction functionality in uploads

---
+++ contenido/scripts/include.upl_edit.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+     $(".ZipExtract").hide();
+     $('#13').hide();  
+});
+
+function show(){
+    if($('#m8').is(':checked')){
+        $(".ZipExtract").show();
+        $('#13').show();
+    }
+    else{
+        $(".ZipExtract").hide();
+        $('#13').hide();
+    }
+}
