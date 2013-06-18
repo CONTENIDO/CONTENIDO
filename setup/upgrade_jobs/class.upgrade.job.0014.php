d5bb535015bbf90472573d4c940cec3f9715a9a7
Mischa Holz <mischa.holz@4fb.de>
2013-06-18 14:32:34 +0000

    [CON-1245]: Implemented an upgrade job to move the config file

---
+++ setup/upgrade_jobs/class.upgrade.job.0014.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * This file contains the upgrade job 13.
+ *
+ * @package    Setup
+ * @subpackage UpgradeJob
+ * @version    SVN Revision $Rev:$
+ *
+ * @author     Mischa Holz
+ * @copyright  four for business AG <www.4fb.de>
+ * @license    http://www.contenido.org/license/LIZENZ.txt
+ * @link       http://www.4fb.de
+ * @link       http://www.contenido.org
+ */
+
+defined('CON_FRAMEWORK') || die('Illegal call: Missing framework initialization - request aborted.');
+
+/**
+ * Upgrade job 13.
+ * Move the config files for the clients to the new {environment} folder
+ *
+ * @package Setup
+ * @subpackage UpgradeJob
+ */
+class cUpgradeJob_0013 extends cUpgradeJobAbstract {
+
+    public $maxVersion = "4.9.0";
+
+    public function _execute() {
+        global $cfg, $db, $cfgClient;
+
+		foreach($cfgClient as $aClient) {
+		    if(cFileHandler::exists($aClient["path"]["frontend"] . "/data/config/config.php")) {
+		        cFileHandler::move($aClient["path"]["frontend"] . "/data/config/config.php", $aClient["path"]["frontend"] . "/data/config/" . CON_ENVIRONMENT . "config.php");
+		    }
+		}
+    }
+
+}
+?>
