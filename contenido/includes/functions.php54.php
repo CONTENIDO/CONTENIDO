40bf59c24abb75ef7dbaf070e15b5a573f9e0b17
Claus Schunk <claus.schunk@4fb.de>
2013-06-19 09:34:26 +0000

    encoding changes for php5.4 :
    Input : $wfname= '<body>äökäöä</body>';
    
    
    //functions
    1. var_dump(conHtmlentities(conHtmlSpecialChars($wfname)));
    2. var_dump(conHtmlentities($wfname));
    3. var_dump(conHtmlSpecialChars($wfname));
    4 .var_dump($wfname);
    
    
    //PHP 5.3
    1. string(72) "&lt;body&gt;äökäöä&lt;/body&gt;" 
    2. string(56) "<body>äökäöä</body>" 
    3. string(36) "<body>äökäöä</body>" 
    4. string(24) "äökäöä" 
    
    //PHP 5.4
    1. string(118) "&lt;body&gt;Ã¤Ã¶kÃ¤Ã¶Ã¤&lt;/body&gt;" 
    2. string(102) "<body>Ã¤Ã¶kÃ¤Ã¶Ã¤</body>" 
    3. string(36) "<body>äökäöä</body>" 
    4. string(24) "äökäöä" 
    
    //PHP 5.4 after bugfix encode to UTF-8 and not to ISO if PHP 5.4
    string(72) "&lt;body&gt;äökäöä&lt;/body&gt;" 
    string(56) "<body>äökäöä</body>" 
    string(36) "<body>äökäöä</body>" 
    string(24) "äökäöä"

--- contenido/includes/functions.php54.php
+++ contenido/includes/functions.php54.php
@@ -2,23 +2,23 @@
 /**
  * This file contains fix functions for PHP 5.4 support (encoding related).
  *
- * @package          Core
- * @subpackage       Backend
- * @version          SVN Revision $Rev:$
+ * @package Core
+ * @subpackage Backend
+ * @version SVN Revision $Rev:$
  *
- * @author           Dominik Ziegler, Timo Trautmann
- * @copyright        four for business AG <www.4fb.de>
- * @license          http://www.contenido.org/license/LIZENZ.txt
- * @link             http://www.4fb.de
- * @link             http://www.contenido.org
+ * @author Dominik Ziegler, Timo Trautmann
+ * @copyright four for business AG <www.4fb.de>
+ * @license http://www.contenido.org/license/LIZENZ.txt
+ * @link http://www.4fb.de
+ * @link http://www.contenido.org
  */
-
 defined('CON_FRAMEWORK') || die('Illegal call: Missing framework initialization - request aborted.');
 
 if (function_exists('conPhp54Check') == false) {
+
     function conPhp54Check() {
         if (!defined('CON_PHP54')) {
-            define('CON_PHP54', version_compare(PHP_VERSION, '5.4.0', '<') ? 0 : 1);
+            define('CON_PHP54', version_compare(PHP_VERSION, '5.4.0', '<')? 0 : 1);
         }
 
         return CON_PHP54;
@@ -26,14 +26,15 @@ if (function_exists('conPhp54Check') == false) {
 }
 
 if (function_exists('conHtmlSpecialChars') == false) {
+
     function conHtmlSpecialChars($value, $flags = '', $encoding = '') {
         $isPhp54 = conPhp54Check();
 
         if ($isPhp54 == 1) {
-            $flags = ($flags == '') ? ENT_COMPAT | ENT_HTML401 : $flags;
-            $encoding = ($encoding == '') ? 'ISO-8859-1' : $encoding;
+            $flags = ($flags == '')? ENT_COMPAT | ENT_HTML401 : $flags;
+            $encoding = ($encoding == '')? 'UTF-8' : $encoding;
         } else {
-            $flags = ($flags == '') ? ENT_COMPAT : $flags;
+            $flags = ($flags == '')? ENT_COMPAT : $flags;
         }
 
         return htmlspecialchars($value, $flags, $encoding);
@@ -41,14 +42,15 @@ if (function_exists('conHtmlSpecialChars') == false) {
 }
 
 if (function_exists('conHtmlEntityDecode') == false) {
+
     function conHtmlEntityDecode($value, $flags = '', $encoding = '') {
         $isPhp54 = conPhp54Check();
 
         if ($isPhp54 == 1) {
-            $flags = ($flags == '') ? ENT_COMPAT | ENT_HTML401 : $flags;
-            $encoding = ($encoding == '') ? 'ISO-8859-1' : $encoding;
+            $flags = ($flags == '')? ENT_COMPAT | ENT_HTML401 : $flags;
+            $encoding = ($encoding == '')? 'UTF-8' : $encoding;
         } else {
-            $flags = ($flags == '') ? ENT_COMPAT : $flags;
+            $flags = ($flags == '')? ENT_COMPAT : $flags;
         }
 
         return html_entity_decode($value, $flags, $encoding);
@@ -56,14 +58,15 @@ if (function_exists('conHtmlEntityDecode') == false) {
 }
 
 if (function_exists('conHtmlentities') == false) {
+
     function conHtmlentities($value, $flags = '', $encoding = '') {
         $isPhp54 = conPhp54Check();
 
         if ($isPhp54 == 1) {
-            $flags = ($flags == '') ? ENT_COMPAT | ENT_HTML401 : $flags;
-            $encoding = ($encoding == '') ? 'ISO-8859-1' : $encoding;
+            $flags = ($flags == '')? ENT_COMPAT | ENT_HTML401 : $flags;
+            $encoding = ($encoding == '')? 'UTF-8' : $encoding;
         } else {
-            $flags = ($flags == '') ? ENT_COMPAT : $flags;
+            $flags = ($flags == '')? ENT_COMPAT : $flags;
         }
 
         return htmlentities($value, $flags, $encoding);
@@ -71,14 +74,15 @@ if (function_exists('conHtmlentities') == false) {
 }
 
 if (function_exists('conGetHtmlTranslationTable') == false) {
+
     function conGetHtmlTranslationTable($table = '', $flags = '') {
         $isPhp54 = conPhp54Check();
 
         if ($isPhp54 == 1) {
-            $table = ($table == '') ? HTML_SPECIALCHARS : $table;
-            $flags = ($flags == '') ? ENT_COMPAT | ENT_HTML401 : $flags;
+            $table = ($table == '')? HTML_SPECIALCHARS : $table;
+            $flags = ($flags == '')? ENT_COMPAT | ENT_HTML401 : $flags;
         } else {
-            $flags = ($flags == '') ? ENT_COMPAT : $flags;
+            $flags = ($flags == '')? ENT_COMPAT : $flags;
         }
 
         return get_html_translation_table($table, $flags);
