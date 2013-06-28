4ac003a0714dcfcdb9ae0afca3bfc90fcacc2451
Frederic Schneider <frederic.schneider@4fb.de>
2013-06-28 10:17:47 +0000

    PHP style

--- contenido/plugins/workflow/classes/class.workflow.php
+++ contenido/plugins/workflow/classes/class.workflow.php
@@ -51,7 +51,7 @@ class Workflows extends ItemCollection {
     public function create() {
         global $auth, $client, $lang;
         $newitem = parent::createNewItem();
-        $newitem->setField("created", date("Y-m-d H:i:s"));
+        $newitem->setField("created", date('Y-m-d H:i:s'));
         $newitem->setField("idauthor", $auth->auth["uid"]);
         $newitem->setField("idclient", $client);
         $newitem->setField("idlang", $lang);
