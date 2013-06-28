a2758cbd07b303c1c7da2767c66b9716ac0b49ea
Frederic Schneider <frederic.schneider@4fb.de>
2013-06-28 10:14:30 +0000

    CON-1286: Fixed wrong timestamp

--- contenido/plugins/workflow/classes/class.workflow.php
+++ contenido/plugins/workflow/classes/class.workflow.php
@@ -51,7 +51,7 @@ class Workflows extends ItemCollection {
     public function create() {
         global $auth, $client, $lang;
         $newitem = parent::createNewItem();
-        $newitem->setField("created", strftime("%Y-%G-%V %H:%M:%S", time()));
+        $newitem->setField("created", date("Y-m-d H:i:s"));
         $newitem->setField("idauthor", $auth->auth["uid"]);
         $newitem->setField("idclient", $client);
         $newitem->setField("idlang", $lang);
