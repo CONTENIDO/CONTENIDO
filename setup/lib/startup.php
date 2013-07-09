efd0856356a132bff80a66b3eaa0abd089da5602
Claus Schunk <claus.schunk@4fb.de>
2013-07-09 13:34:53 +0000

    FFBCON-205

--- setup/lib/startup.php
+++ setup/lib/startup.php
@@ -99,6 +99,11 @@ if (is_array($_REQUEST)) {
 }
 
 
+// set max_execution_time
+if(ini_get('max_execution_time') < 60){
+    ini_set(max_execution_time, 60);
+}
+
 // Some basic configuration
 global $cfg;
 
