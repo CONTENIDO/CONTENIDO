7a63e3529317d804a459a70857be353a1f81ae81
Dominik Ziegler <dominik.ziegler@4fb.de>
2013-07-31 08:45:01 +0000

    removed commented lines from previous checkin

--- contenido/includes/include.CMS_HEAD.php
+++ contenido/includes/include.CMS_HEAD.php
@@ -23,14 +23,9 @@ if (isset($area) && $area == 'con_content_list') {
             '&idcat='.$idcat.'&client='.$client.'&lang='.$lang.'&frame=4&contenido='.$contenido;
     $path2 = $path1;
 } else {
-    $tmp_area = "con_editcontent";
-    /*$path1 = $backendUrl . "external/backendedit/front_content.php?area=$tmp_area&action=con_editart&idart=$idart&idartlang=$idartlang&idcat=$idcat&changeview=edit&client=$client";
-    $path2 = cRegistry::getFrontendUrl() ."front_content.php?area=$tmp_area&idart=$idart&idcat=$idcat&lang=$lang&changeview=edit&client=$client";
-    */
-    
+    $tmp_area = "con_editcontent";   
     $path1 = $backendUrl . "external/backendedit/front_content.php?area=$tmp_area&idart=$idart&idcat=$idcat&changeview=edit&client=$client";
     $path2 = $backendUrl . "external/backendedit/front_content.php?area=$tmp_area&idart=$idart&idcat=$idcat&changeview=edit&client=$client&lang=$lang";
-    
 }
 
 if ($doedit == "1") {
