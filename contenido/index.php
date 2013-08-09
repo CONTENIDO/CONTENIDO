ef06bf57e8ae29f181976c5b6dbb3de27289ca71
Marcus Gnaß <marcus.gnass@4fb.de>
2013-08-09 09:56:13 +0000

    comment defined globals and reformatted sql statement; applied coding conventions

--- contenido/index.php
+++ contenido/index.php
@@ -3,15 +3,15 @@
 /**
  * This file is the main entrance point of the backend.
  *
- * @package          Core
- * @subpackage       Backend
- * @version          SVN Revision $Rev:$
+ * @package Core
+ * @subpackage Backend
+ * @version SVN Revision $Rev:$
  *
- * @author           Olaf Niemann, Jan Lengowski
- * @copyright        four for business AG <www.4fb.de>
- * @license          http://www.contenido.org/license/LIZENZ.txt
- * @link             http://www.4fb.de
- * @link             http://www.contenido.org
+ * @author Olaf Niemann, Jan Lengowski
+ * @copyright four for business AG <www.4fb.de>
+ * @license http://www.contenido.org/license/LIZENZ.txt
+ * @link http://www.4fb.de
+ * @link http://www.contenido.org
  */
 
 if (!defined('CON_FRAMEWORK')) {
@@ -19,11 +19,11 @@ if (!defined('CON_FRAMEWORK')) {
 }
 
 // CONTENIDO startup process
-include_once('./includes/startup.php');
+include_once ('./includes/startup.php');
 
 // ugly globals that are used in this script
-global $sess, $perm, $area;
-global $belang, $changelang, $changeclient;
+// global $sess, $perm, $area;
+// global $belang, $changelang, $changeclient;
 
 cRegistry::bootstrap(array(
     'sess' => 'cSession',
@@ -33,14 +33,14 @@ cRegistry::bootstrap(array(
 
 i18nInit($cfg['path']['contenido_locale'], $belang);
 
-require_once(cRegistry::getBackendPath() . $cfg['path']['includes'] . 'functions.includePluginConf.php');
+require_once (cRegistry::getBackendPath() . $cfg['path']['includes'] . 'functions.includePluginConf.php');
 
-require_once($cfg['path']['contenido_config'] . 'cfg_actions.inc.php');
+require_once ($cfg['path']['contenido_config'] . 'cfg_actions.inc.php');
 
 $sess->register('belang');
 
 // create global CONTENIDO class instances
-$db  = cRegistry::getDb();
+$db = cRegistry::getDb();
 $tpl = new cTemplate();
 
 // change lang
@@ -50,15 +50,16 @@ if (isset($changelang) && is_numeric($changelang)) {
 
 // change client
 if (isset($changeclient) && is_numeric($changeclient)) {
-     $client = $changeclient;
-     unset($lang);
+    $client = $changeclient;
+    unset($lang);
 }
 
 // preselect client, if definied
-if (!$sess->isRegistered('client')) { // only check at first login into backend
+// only check at first login into backend
+if (!$sess->isRegistered('client')) {
     $iTmpClient = getEffectiveSetting('backend', 'preferred_idclient', false);
 
-    if ($iTmpClient && ($perm->have_perm_client('admin['.$iTmpClient.']') || $perm->have_perm_client('client['.$iTmpClient.']'))) {
+    if ($iTmpClient && ($perm->have_perm_client('admin[' . $iTmpClient . ']') || $perm->have_perm_client('client[' . $iTmpClient . ']'))) {
         $client = $iTmpClient;
         unset($lang);
     }
@@ -82,7 +83,16 @@ if (!is_numeric($client) || $client == '') {
 if (!is_numeric($lang) || $lang == '') {
     $sess->register('lang');
     // search for the first language of this client
-    $sql = "SELECT * FROM ".$cfg["tab"]["lang"]." AS A, ".$cfg["tab"]["clients_lang"]." AS B WHERE A.idlang=B.idlang AND idclient='".cSecurity::toInteger($client)."' ORDER BY A.idlang ASC";
+    $sql = "SELECT
+                *
+            FROM
+                " . $cfg["tab"]["lang"] . " AS A
+                , " . $cfg["tab"]["clients_lang"] . " AS B
+            WHERE
+                A.idlang=B.idlang
+                AND idclient='" . cSecurity::toInteger($client) . "'
+            ORDER BY
+                A.idlang ASC";
     $db->query($sql);
     $db->nextRecord();
     $lang = $db->f('idlang');
@@ -104,7 +114,7 @@ $perm->load_permissions();
 if (isset($area)) {
     $sess_area = $area;
 } else {
-    $area = (isset($sess_area)) ? $sess_area : 'login';
+    $area = (isset($sess_area))? $sess_area : 'login';
 }
 
 $backendUrl = cRegistry::getBackendUrl();
@@ -117,8 +127,8 @@ $backend_label = " " . $backend_label . " ";
 $tpl->set('s', 'BACKEND_LABEL', $backend_label);
 
 // Template settings
-$tpl->set('s', 'HEADER', str_replace('&', '&amp;', $sess->url('header.php?changelang='.$lang.'&changeclient='.$client)));
-$tpl->set('s', 'CONTENT', str_replace('&', '&amp;', $sess->url('frameset.php?area=mycontenido&frame=1&menuless=1&changelang='.$changelang.'&lang='.$lang.'&client='.$client)));
+$tpl->set('s', 'HEADER', str_replace('&', '&amp;', $sess->url('header.php?changelang=' . $lang . '&changeclient=' . $client)));
+$tpl->set('s', 'CONTENT', str_replace('&', '&amp;', $sess->url('frameset.php?area=mycontenido&frame=1&menuless=1&changelang=' . $changelang . '&lang=' . $lang . '&client=' . $client)));
 $tpl->set('s', 'VERSION', $cfg['version']);
 $tpl->set('s', 'LOCATION', $backendUrl);
 $tpl->set('s', 'CONTENIDOPATH', $backendUrl . 'favicon.ico');
