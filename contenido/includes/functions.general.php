ad5211038716c706626e5e4c0143e1f1cc1c6b41
Murat Purc <murat@purc.de>
2013-08-14 12:08:08 +0000

    [#CON-1447]: Check if function escapeshellarg or exec is disabled, added new function isFunctionDisabled(). Extended logic in mime_content_type alternative.

--- contenido/includes/functions.general.php
+++ contenido/includes/functions.general.php
@@ -1512,34 +1512,34 @@ function isImageMagickAvailable()
 
 	if (is_bool($_imagemagickAvailable))
 	{
-		if ($_imagemagickAvailable === true)
-		{
-			return true;
-		} else
-		{
-			return false;
-		}
+		return $_imagemagickAvailable;
 	}
 
-	$output = array ();
+    if (isFunctionDisabled('exec'))
+    {
+		$_imagemagickAvailable = false;
+        return $_imagemagickAvailable;
+    }
+
+	$output = array();
 	$retval = 0;
 
 	@exec("convert", $output, $retval);
 
     if (!is_array($output) || count($output) == 0)
     {
-        return false;
+		$_imagemagickAvailable = false;
+    }
+    elseif (strpos($output[0], "ImageMagick") !== false)
+    {
+        $_imagemagickAvailable = true;
+    }
+    else
+    {
+        $_imagemagickAvailable = false;
     }
 
-	if (strpos($output[0], "ImageMagick") !== false)
-	{
-		$_imagemagickAvailable = true;
-		return true;
-	} else
-	{
-		$_imagemagickAvailable = false;
-		return false;
-	}
+    return $_imagemagickAvailable;
 }
 
 /**
@@ -1928,7 +1928,179 @@ if (!function_exists('mime_content_type'))
 {
 	function mime_content_type($file)
 	{
-		return exec('file -bi ' . escapeshellarg($file));
+        // Use PECL fileinfo if available
+        if (class_exists('finfo')) {
+            $finfo = new finfo(FILEINFO_MIME);
+            return $finfo->file($file);
+        }
+
+		if (false === isFunctionDisabled('escapeshellarg') && false === isFunctionDisabled('exec')) {
+            return exec('file -bi ' . escapeshellarg($file));
+        }
+
+        // Use fallback alternative, without Fileinfo which requires PECL fileinfo
+		$ext = strtolower(end(explode('.', $filename)));
+		$mimeTypes = array(
+            'ai' =>'application/postscript',
+			'aif' =>'audio/x-aiff',
+			'aifc' =>'audio/x-aiff',
+			'aiff' =>'audio/x-aiff',
+			'asc' =>'text/plain',
+			'atom' =>'application/atom+xml',
+			'avi' =>'video/x-msvideo',
+			'bcpio' =>'application/x-bcpio',
+			'bmp' =>'image/bmp',
+			'cdf' =>'application/x-netcdf',
+			'cgm' =>'image/cgm',
+			'cpio' =>'application/x-cpio',
+			'cpt' =>'application/mac-compactpro',
+			'crl' =>'application/x-pkcs7-crl',
+			'crt' =>'application/x-x509-ca-cert',
+			'csh' =>'application/x-csh',
+			'css' =>'text/css',
+			'dcr' =>'application/x-director',
+			'dir' =>'application/x-director',
+			'djv' =>'image/vnd.djvu',
+			'djvu' =>'image/vnd.djvu',
+			'doc' =>'application/msword',
+			'dtd' =>'application/xml-dtd',
+			'dvi' =>'application/x-dvi',
+			'dxr' =>'application/x-director',
+			'eps' =>'application/postscript',
+			'etx' =>'text/x-setext',
+			'ez' =>'application/andrew-inset',
+			'gif' =>'image/gif',
+			'gram' =>'application/srgs',
+			'grxml' =>'application/srgs+xml',
+			'gtar' =>'application/x-gtar',
+			'hdf' =>'application/x-hdf',
+			'hqx' =>'application/mac-binhex40',
+			'html' =>'text/html',
+			'html' =>'text/html',
+			'ice' =>'x-conference/x-cooltalk',
+			'ico' =>'image/x-icon',
+			'ics' =>'text/calendar',
+			'ief' =>'image/ief',
+			'ifb' =>'text/calendar',
+			'iges' =>'model/iges',
+			'igs' =>'model/iges',
+			'jpe' =>'image/jpeg',
+			'jpeg' =>'image/jpeg',
+			'jpg' =>'image/jpeg',
+			'js' =>'application/x-javascript',
+			'kar' =>'audio/midi',
+			'latex' =>'application/x-latex',
+			'm3u' =>'audio/x-mpegurl',
+			'man' =>'application/x-troff-man',
+			'mathml' =>'application/mathml+xml',
+			'me' =>'application/x-troff-me',
+			'mesh' =>'model/mesh',
+			'mid' =>'audio/midi',
+			'midi' =>'audio/midi',
+			'mif' =>'application/vnd.mif',
+			'mov' =>'video/quicktime',
+			'movie' =>'video/x-sgi-movie',
+			'mp2' =>'audio/mpeg',
+			'mp3' =>'audio/mpeg',
+			'mpe' =>'video/mpeg',
+			'mpeg' =>'video/mpeg',
+			'mpg' =>'video/mpeg',
+			'mpga' =>'audio/mpeg',
+			'ms' =>'application/x-troff-ms',
+			'msh' =>'model/mesh',
+			'mxu m4u' =>'video/vnd.mpegurl',
+			'nc' =>'application/x-netcdf',
+			'oda' =>'application/oda',
+			'ogg' =>'application/ogg',
+			'pbm' =>'image/x-portable-bitmap',
+			'pdb' =>'chemical/x-pdb',
+			'pdf' =>'application/pdf',
+			'pgm' =>'image/x-portable-graymap',
+			'pgn' =>'application/x-chess-pgn',
+			'php' =>'application/x-httpd-php',
+			'php4' =>'application/x-httpd-php',
+			'php3' =>'application/x-httpd-php',
+			'phtml' =>'application/x-httpd-php',
+			'phps' =>'application/x-httpd-php-source',
+			'png' =>'image/png',
+			'pnm' =>'image/x-portable-anymap',
+			'ppm' =>'image/x-portable-pixmap',
+			'ppt' =>'application/vnd.ms-powerpoint',
+			'ps' =>'application/postscript',
+			'qt' =>'video/quicktime',
+			'ra' =>'audio/x-pn-realaudio',
+			'ram' =>'audio/x-pn-realaudio',
+			'ras' =>'image/x-cmu-raster',
+			'rdf' =>'application/rdf+xml',
+			'rgb' =>'image/x-rgb',
+			'rm' =>'application/vnd.rn-realmedia',
+			'roff' =>'application/x-troff',
+			'rtf' =>'text/rtf',
+			'rtx' =>'text/richtext',
+			'sgm' =>'text/sgml',
+			'sgml' =>'text/sgml',
+			'sh' =>'application/x-sh',
+			'shar' =>'application/x-shar',
+			'shtml' =>'text/html',
+			'silo' =>'model/mesh',
+			'sit' =>'application/x-stuffit',
+			'skd' =>'application/x-koan',
+			'skm' =>'application/x-koan',
+			'skp' =>'application/x-koan',
+			'skt' =>'application/x-koan',
+			'smi' =>'application/smil',
+			'smil' =>'application/smil',
+			'snd' =>'audio/basic',
+			'spl' =>'application/x-futuresplash',
+			'src' =>'application/x-wais-source',
+			'sv4cpio' =>'application/x-sv4cpio',
+			'sv4crc' =>'application/x-sv4crc',
+			'svg' =>'image/svg+xml',
+			'swf' =>'application/x-shockwave-flash',
+			't' =>'application/x-troff',
+			'tar' =>'application/x-tar',
+			'tcl' =>'application/x-tcl',
+			'tex' =>'application/x-tex',
+			'texi' =>'application/x-texinfo',
+			'texinfo' =>'application/x-texinfo',
+			'tgz' =>'application/x-tar',
+			'tif' =>'image/tiff',
+			'tiff' =>'image/tiff',
+			'tr' =>'application/x-troff',
+			'tsv' =>'text/tab-separated-values',
+			'txt' =>'text/plain',
+			'ustar' =>'application/x-ustar',
+			'vcd' =>'application/x-cdlink',
+			'vrml' =>'model/vrml',
+			'vxml' =>'application/voicexml+xml',
+			'wav' =>'audio/x-wav',
+			'wbmp' =>'image/vnd.wap.wbmp',
+			'wbxml' =>'application/vnd.wap.wbxml',
+			'wml' =>'text/vnd.wap.wml',
+			'wmlc' =>'application/vnd.wap.wmlc',
+			'wmlc' =>'application/vnd.wap.wmlc',
+			'wmls' =>'text/vnd.wap.wmlscript',
+			'wmlsc' =>'application/vnd.wap.wmlscriptc',
+			'wmlsc' =>'application/vnd.wap.wmlscriptc',
+			'wrl' =>'model/vrml',
+			'xbm' =>'image/x-xbitmap',
+			'xht' =>'application/xhtml+xml',
+			'xhtml' =>'application/xhtml+xml',
+			'xls' =>'application/vnd.ms-excel',
+			'xml xsl' =>'application/xml',
+			'xpm' =>'image/x-xpixmap',
+			'xslt' =>'application/xslt+xml',
+			'xul' =>'application/vnd.mozilla.xul+xml',
+			'xwd' =>'image/x-xwindowdump',
+			'xyz' =>'chemical/x-xyz',
+			'zip' =>'application/zip',
+		);
+
+		if (isset($mimeTypes[$ext])) {
+			return $mimeTypes[$ext];
+		} else {
+			return 'application/octet-stream';
+		}
 	}
 }
 
@@ -2479,4 +2651,24 @@ function sendEncodingHeader ($db, $cfg, $lang) {
 
     }
 }
+
+/**
+ * Checks, if a function is disabled or not ('disable_functions' setting in php.ini)
+ * @param  string  $functionName  Name of the function to check
+ * @return bool
+ */
+function isFunctionDisabled($functionName) {
+    static $disabledFunctions;
+
+    if (empty($functionName)) {
+        return true;
+    }
+
+    if (!isset($disabledFunctions)) {
+        $disabledFunctions = array_map('trim', explode(',', ini_get('disable_functions')));
+    }
+
+    return (in_array($functionName, $disabledFunctions));
+}
+
 ?>
