b6fb524c83a7027f80ac902e6f6bc6e072ab1cdd
Frederic Schneider <frederic.schneider@4fb.de>
2013-09-06 12:02:31 +0000

    CON-1462: Fixed magic_quotes_gpc handling in globals_off.inc.php

--- contenido/includes/globals_off.inc.php
+++ contenido/includes/globals_off.inc.php
@@ -1,28 +1,33 @@
 <?php
 /**
- * Makes available those super global arrays that are made available in versions of PHP after v4.1.0.
- * This file is where all the "magic" begins. We ignore register_globals setting and retrieve any
- * variable from wherever and transform them to global variables. This is highly insecure, so variables
+ * Makes available those super global arrays that are made available in versions
+ * of PHP after v4.1.0.
+ * This file is where all the "magic" begins. We ignore register_globals setting
+ * and retrieve any
+ * variable from wherever and transform them to global variables. This is highly
+ * insecure, so variables
  * need to be checked carefully.
  *
- * @package          Core
- * @subpackage       Backend
- * @version          SVN Revision $Rev:$
+ * @package Core
+ * @subpackage Backend
+ * @version SVN Revision $Rev:$
  *
- * @author           Martin Horwarth
- * @copyright        four for business AG <www.4fb.de>
- * @license          http://www.contenido.org/license/LIZENZ.txt
- * @link             http://www.4fb.de
- * @link             http://www.contenido.org
+ * @author Martin Horwarth
+ * @copyright four for business AG <www.4fb.de>
+ * @license http://www.contenido.org/license/LIZENZ.txt
+ * @link http://www.4fb.de
+ * @link http://www.contenido.org
  */
 
 /**
  * Set constant value depending on get_magic_quotes_gpc status
- * Only with phpversion before 5.3.0
+ *
  * @var boolean
  */
-if (phpversion() <= "5.3.0") {
-    define('CON_STRIPSLASHES', get_magic_quotes_gpc());
+if (function_exists('get_magic_quotes_gpc')) {
+    define('CON_STRIPSLASHES', !get_magic_quotes_gpc());
+} else {
+    define('CON_STRIPSLASHES', true);
 }
 
 // Simulate get_magic_quotes_gpc on if turned off
@@ -30,23 +35,29 @@ if (CON_STRIPSLASHES) {
 
     /**
      * Adds slashes to passed variable
-     * @param mixed $value Either a string or a multi-dimensional array of values
+     *
+     * @param mixed $value Either a string or a multi-dimensional array of
+     *        values
      * @return array
-     * @deprecated [2013-03-12]  This function is for internal usage, use cString::addSlashes() for own purposes
+     * @deprecated [2013-03-12] This function is for internal usage, use
+     *             cString::addSlashes() for own purposes
      */
     function addslashes_deep($value) {
-        $value = is_array($value) ? array_map('addslashes_deep', $value) : addslashes($value);
+        $value = is_array($value)? array_map('addslashes_deep', $value) : addslashes($value);
         return $value;
     }
 
     /**
      * Removes slashes from passed variable.
-     * @param mixed $value Either a string or a multi-dimensional array of values
+     *
+     * @param mixed $value Either a string or a multi-dimensional array of
+     *        values
      * @return array
-     * @deprecated [2013-03-12]  This function is for internal usage, use cString::stripSlashes() for own purposes
+     * @deprecated [2013-03-12] This function is for internal usage, use
+     *             cString::stripSlashes() for own purposes
      */
     function stripslashes_deep($value) {
-        $value = is_array($value) ? array_map('stripslashes_deep', $value) : stripslashes($value);
+        $value = is_array($value)? array_map('stripslashes_deep', $value) : stripslashes($value);
         return $value;
     }
 
@@ -60,16 +71,23 @@ if (CON_STRIPSLASHES) {
 }
 
 // This should be the default setting, but only for PHP older than 5.3.0
-if (!CON_STRIPSLASHES && (version_compare(PHP_VERSION, '5.3.0', '<'))) {
+if (version_compare(PHP_VERSION, '5.3.0', '<')) {
     @set_magic_quotes_runtime(0);
 }
 
 // Register globals
-$types_to_register = array('GET', 'POST', 'COOKIE', 'SESSION', 'SERVER');
+$types_to_register = array(
+    'GET',
+    'POST',
+    'COOKIE',
+    'SESSION',
+    'SERVER'
+);
 foreach ($types_to_register as $global_type) {
     $arr = @ ${'_' . $global_type};
     if (is_array($arr) && count($arr) > 0) {
-        // Second loop to prevent overwriting of globals by other globals' values
+        // Second loop to prevent overwriting of globals by other globals'
+        // values
         foreach ($types_to_register as $global_type) {
             $key = '_' . $global_type;
             if (isset($arr[$key])) {
