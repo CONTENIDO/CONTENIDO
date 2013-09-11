81822c71beafd1f4ed46f69c5daf35249d71b262
Mischa Holz <mischa.holz@4fb.de>
2013-09-11 11:25:03 +0000

    [CON-898]: Implemented a permission check prior to accesing or changing the content allocation.

--- contenido/plugins/content_allocation/includes/include.contentallocation_article.php
+++ contenido/plugins/content_allocation/includes/include.contentallocation_article.php
@@ -1,14 +1,14 @@
 <?php
 /**
- * Project: 
+ * Project:
  * Contenido Content Management System
- * 
- * Description: 
+ *
+ * Description:
  * Content Allocation Articles
- * 
- * Requirements: 
+ *
+ * Requirements:
  * @con_php_req 5.0
- * 
+ *
  *
  * @package    Contenido Backend plugins
  * @version    1.0.1
@@ -18,14 +18,14 @@
  * @link       http://www.4fb.de
  * @link       http://www.contenido.org
  * @since      file available since contenido release <= 4.6
- * 
- * {@internal 
+ *
+ * {@internal
  *   created unknown
  *   modified 2008-07-02, Frederic Schneider, add security fix
  *
  *   $Id: include.contentallocation_article.php 742 2008-08-27 11:06:12Z timo.trautmann $:
  * }}
- * 
+ *
  */
 
 if(!defined('CON_FRAMEWORK')) {
@@ -37,15 +37,20 @@ Contenido_Security::checkRequests();
 
 cInclude("includes", "functions.pathresolver.php");
 
+if(!$perm->have_perm_area_action($area, $action)) {
+    $notification->displayNotification("error", i18n("Permission denied"));
+    return;
+}
+
 function str_replace_recursive ($array) {
 	if (!is_array($array)) return false;
-	
+
 	$result = array();
-	
+
 	foreach ($array as $value) {
 		$result[] = str_replace("e", "", $value);
 	}
-	
+
 	return $result;
 }
 
@@ -62,7 +67,7 @@ $oTree = new pApiContentAllocationComplexList('06bd456d-fe76-40cb-b041-b9ba90dc4
 $oAlloc = new pApiContentAllocation;
 
 if ($_POST['action'] == 'storeallocation') {
-	$oAlloc->storeAllocations($this_idartlang, $_POST['allocation']);	
+	$oAlloc->storeAllocations($this_idartlang, $_POST['allocation']);
 }
 if ($_GET['step'] == 'collapse') {
 	$oTree->setTreeStatus($_GET['idpica_alloc']);
@@ -93,29 +98,29 @@ if ($result == false) {
 		<input type="hidden" name="frame" value="'.$frame.'">
 		<input type="hidden" name="idcat" value="'.$idcat.'">';
 	$tpl->set('s', 'HIDDENFIELDS', $hiddenfields);
-	
-	
+
+
 	if (sizeof($loadedAllocations) > 0) {
 		$tpl->set('s', 'ARRAY_CHECKED_BOXES', 'var checkedBoxes = [' . implode(',', $loadedAllocations) . '];');
 	} else {
 		$tpl->set('s', 'ARRAY_CHECKED_BOXES', 'var checkedBoxes = [];');
 	}
-	
+
 	$oDiv = new cHTMLDiv;
 	$oDiv->updateAttributes(array('style' => 'text-align: right; padding: 5px; width: 730px; border: 1px #B3B3B3 solid; background-color: #FFFFFF;'));
 	$oDiv->setContent('<input type="image" src="images/but_ok.gif" />');
 	$tpl->set('s', 'DIV', '<br>' . $oDiv->render());
-	
+
 	$tpl->set('s', 'TREE', $result);
 
 	$tpl->set('s', 'REMOVE_ALL', i18n("Remove all", "content_allocation"));
 	$tpl->set('s', 'REMOVE', i18n("Remove", "content_allocation"));
-	
+
 	$result = $tpl->generate($cfg['pica']['treetemplate_complexlist'], true);
-	
+
 	$script = '<link rel="stylesheet" type="text/css" href="'.$cfg['pica']['style_complexlist'].'"/>
 	<script language="javascript" src="'.$cfg['pica']['script_complexlist'].'"></script>';
-	$oPage->addScript('style', $script);	
+	$oPage->addScript('style', $script);
 }
 
 
