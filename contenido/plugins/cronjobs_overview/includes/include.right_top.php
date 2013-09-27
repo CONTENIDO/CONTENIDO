faf9653bf8f771b81860a3bedd70379fe4cd7b41
Frederic Schneider <frederic.schneider@4fb.de>
2013-09-27 11:47:48 +0000

    CON-1505: Added right_top.php with blank menu

---
+++ contenido/plugins/cronjobs_overview/includes/include.right_top.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * This file contains the right top frame backend page for the plugin cronjob overview.
+ *
+ * @package    Plugin
+ * @subpackage CronjobOverview
+ * @version    SVN Revision $Rev:$
+ *
+ * @author     Frederic Schneider
+ * @copyright  four for business AG <www.4fb.de>
+ * @license    http://www.contenido.org/license/LIZENZ.txt
+ * @link       http://www.4fb.de
+ * @link       http://www.contenido.org
+ */
+
+// show blank menu when no form was selected
+cInclude('templates', $cfg['templates']['right_top_blank']);
+?>
