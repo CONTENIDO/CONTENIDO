67b93820052e80d4733599786b8124cd2215f532
Frederic Schneider <frederic.schneider@4fb.de>
2013-09-27 10:43:08 +0000

    Formatted

--- contenido/plugins/linkchecker/includes/include.linkchecker.php
+++ contenido/plugins/linkchecker/includes/include.linkchecker.php
@@ -2,15 +2,15 @@
 /**
  * This is the main backend page for the linkchecker plugin.
  *
- * @package    Plugin
+ * @package Plugin
  * @subpackage Linkchecker
- * @version    SVN Revision $Rev:$
+ * @version SVN Revision $Rev:$
  *
- * @author     Frederic Schneider
- * @copyright  four for business AG <www.4fb.de>
- * @license    http://www.contenido.org/license/LIZENZ.txt
- * @link       http://www.4fb.de
- * @link       http://www.contenido.org
+ * @author Frederic Schneider
+ * @copyright four for business AG <www.4fb.de>
+ * @license http://www.contenido.org/license/LIZENZ.txt
+ * @link http://www.4fb.de
+ * @link http://www.contenido.org
  */
 
 defined('CON_FRAMEWORK') || die('Illegal call: Missing framework initialization - request aborted.');
