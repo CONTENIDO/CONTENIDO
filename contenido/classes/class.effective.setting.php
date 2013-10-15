48cb8622348ef69846607145a359e6cdd9d86c1e
Frederic Schneider <frederic.schneider@4fb.de>
2013-10-15 09:42:04 +0000

    Fixed getEffectiveSetting bug with empty value variable

--- contenido/classes/class.effective.setting.php
+++ contenido/classes/class.effective.setting.php
@@ -120,7 +120,7 @@ class cEffectiveSetting {
             $value = getSystemProperty($type, $name);
         }
 
-        if ($value === false || $value === '') {
+        if ($value === false || $value === '' || $value === NULL) {
             $value = $default;
         }
 
