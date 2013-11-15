fd0683ddb66ee73a0b9f78ad4b0f57adbeb92195
Mischa Holz <mischa.holz@4fb.de>
2013-11-15 09:47:31 +0000

    [CON-1170]: Changed the order of the sub navigation tabs for Content->Article

---
+++ setup/upgrade_jobs/class.upgrade.job.0015.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * This file contains the upgrade job 15.
+ *
+ * @package Setup
+ * @subpackage UpgradeJob
+ * @version SVN Revision $Rev:$
+ *
+ * @author Mischa Holz
+ * @copyright four for business AG <www.4fb.de>
+ * @license http://www.contenido.org/license/LIZENZ.txt
+ * @link http://www.4fb.de
+ * @link http://www.contenido.org
+ */
+
+defined('CON_FRAMEWORK') || die('Illegal call: Missing framework initialization - request aborted.');
+
+/**
+ * Upgrade job 15.
+ * Change the order of some menus in the subnavigation
+ *
+ * @package Setup
+ * @subpackage UpgradeJob
+ */
+class cUpgradeJob_0015 extends cUpgradeJobAbstract {
+
+    public $maxVersion = "4.9.2";
+
+    public function _execute() {
+        global $db, $cfg;
+
+        if($_SESSION['setuptype'] == 'upgrade') {
+            $db->query('UPDATE `' . $cfg['tab']['nav_sub'] . '` SET idnavs=9999 WHERE location=\'navigation/content/article/content\';');
+			$db->query('UPDATE `' . $cfg['tab']['nav_sub'] . '` SET idnavs=20 WHERE location=\'navigation/content/article/editor\';');
+			$db->query('UPDATE `' . $cfg['tab']['nav_sub'] . '` SET idnavs=19 WHERE location=\'navigation/content/article/content\';');
+        }
+    }
+
+}
