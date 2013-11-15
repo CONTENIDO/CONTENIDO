14688026ceee53d3163e2e096196d34d1f868f57
Murat Purc <murat@purc.de>
2013-11-15 13:57:02 +0000

    Plugin User Forum: Ported to new JS structure.

--- contenido/plugins/user_forum/scripts/cmsUserforum.js
+++ contenido/plugins/user_forum/scripts/cmsUserforum.js
@@ -1,4 +1,6 @@
 /**
+ * @module  content-type
+ * @submodule  content-type-user-forum
  * @package Core
  * @subpackage Content Type
  * @version SVN Revision $Rev:$
@@ -9,37 +11,44 @@
  * @link http://www.contenido.org
  */
 
-/**
- * Creates a new cContentTypePifaForm with the given properties. You most
- * probably want to call initialise() after creating a new object of this class.
- *
- * @constructor
- * @property {String} frameId The ID of the frame in which the content type can
- *           be set up.
- * @property {String} imageId The ID of the button on which one clicks in order
- *           to edit the content type.
- * @property {String} pathBackend The path to the CONTENIDO backend.
- * @property {String} pathFrontend The path to the CONTENIDO frontend.
- * @property {Number} idArtLang The idArtLang of the article which is currently
- *           being edited.
- * @property {Number} id The ID of the content type, e.g. 3 if CMS_TEASER[3] is
- *           used.
- * @property {Array} fields Form element names which are used by this content
- *           type.
- * @property {String} prefix The prefix of the content type.
- * @property {String} session The session ID of the admin user.
- * @property {Object|String} settings The settings of this content type.
- */
-function cContentTypeUserForum(frameId, imageId, pathBackend, pathFrontend,
-        idArtLang, id, fields, prefix, session, settings) {
-    // call the constructor of the parent class with the same arguments
-    cContentTypeAbstractTabbed.apply(this, arguments);
-}
+(function(Con, $) {
 
-cContentTypeUserForum.prototype = new cContentTypeAbstractTabbed();
-cContentTypeUserForum.prototype.constructor = cContentTypeUserForum;
+    var NAME = 'content-type-pifa-form';
 
+	/**
+	 * Creates a new cContentTypePifaForm with the given properties. You most
+	 * probably want to call initialise() after creating a new object of this class.
+	 * @class cContentTypeUserForum
+	 * @extends cContentTypeAbstractTabbed
+	 * @constructor
+	 * @property {String} frameId The ID of the frame in which the content type can
+	 *           be set up.
+	 * @property {String} imageId The ID of the button on which one clicks in order
+	 *           to edit the content type.
+	 * @property {String} pathBackend The path to the CONTENIDO backend.
+	 * @property {String} pathFrontend The path to the CONTENIDO frontend.
+	 * @property {Number} idArtLang The idArtLang of the article which is currently
+	 *           being edited.
+	 * @property {Number} id The ID of the content type, e.g. 3 if CMS_TEASER[3] is
+	 *           used.
+	 * @property {Array} fields Form element names which are used by this content
+	 *           type.
+	 * @property {String} prefix The prefix of the content type.
+	 * @property {String} session The session ID of the admin user.
+	 * @property {Object|String} settings The settings of this content type.
+	 */
+	function cContentTypeUserForum(frameId, imageId, pathBackend, pathFrontend,
+	        idArtLang, id, fields, prefix, session, settings) {
+	    // call the constructor of the parent class with the same arguments
+	    cContentTypeAbstractTabbed.apply(this, arguments);
+	}
 
+	cContentTypeUserForum.prototype = new cContentTypeAbstractTabbed();
+	cContentTypeUserForum.prototype.constructor = cContentTypeUserForum;
 
+    Con.cContentTypeUserForum = cContentTypeUserForum;
 
+    // @deprecated [2013-11-15] Assign to windows scope (downwards compatibility)
+    window.cContentTypeUserForum = cContentTypeUserForum;
 
+})(Con, Con.$);
