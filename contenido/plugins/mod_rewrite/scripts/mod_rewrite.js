e74f6bdd60ae573afcc0989164c31d3fe5420add
Murat Purc <murat@purc.de>
2013-11-15 13:20:23 +0000

    Plugin Mod Rewrite: Ported to new JS structure, reworked plugin JS code, removed aToolTip jQuery plugin sources to use the version in core.

--- contenido/plugins/mod_rewrite/scripts/mod_rewrite.js
+++ contenido/plugins/mod_rewrite/scripts/mod_rewrite.js
@@ -1,47 +1,86 @@
+/* global Con: true, jQuery: true */
+
 /**
- * Project:
- * CONTENIDO Content Management System
- *
- * Description:
  * Plugin Advanced Mod Rewrite JavaScript functions.
  *
- * Requirements:
- * @con_php_req 5.0
- *
- *
- * @package    CONTENIDO Plugins
- * @version    0.1
+ * @module     plugin
+ * @submodule  mod-rewrite
+ * @version    SVN Revision $Rev: $
+ * @requires   jQuery, Con
  * @author     Murat Purc <murat@purc.de>
  * @copyright  four for business AG <www.4fb.de>
  * @license    http://www.contenido.org/license/LIZENZ.txt
  * @link       http://www.4fb.de
  * @link       http://www.contenido.org
- * @since      file available since CONTENIDO release 4.9.0
- *
- * {@internal
- *   created  2011-04-11
- *
- *   $Id$:
- * }}
- *
  */
 
+(function(Con, $) {
 
-var mrPlugin = {
-    lng: {
-        more_informations: "More informations"
-    },
+    var NAME = 'plugin-mod-rewrite';
 
-    toggle: function(id) {
-        // do some animation ;-)
-        $('#' + id).slideToggle("slow");
-    },
+    /**
+     * @class ModRewrite
+     * @constructor
+     * @extends Con.Plugin
+     * @param {String}  page
+     * @param {Object}  options  Options as follows:
+     * <pre>
+     * options.lng  (Object)  Translation strings 
+     * </pre>
+     */
+    var ModRewrite = function(page, options) {
+        /**
+         * Pagename
+         * @property _page
+         * @type {String}
+         * @private
+         */
+        this._page = page;
 
-    showReadme: function() {
-    },
+        /**
+         * Translation strings
+         * @property _lng
+         * @type {Object}
+         * @private
+         */
+        this._lng = $.extend({
+            more_informations: "More informations"
+        }, options.lng);
 
-    initializeSettingsPage: function() {
-        $(function() {
+        this._initializePage();
+    };
+
+    ModRewrite.prototype = {
+
+        /**
+         * Toggles an element
+         * @method toggle
+         * @param {String}  id
+         */
+        toggle: function(id) {
+            // do some animation ;-)
+            $('#' + id).slideToggle("slow");
+        },
+
+        /**
+         * Initializes the current page
+         * @method initializePage
+         */
+        _initializePage: function() {
+            var method = '_initializePage' + this._page.charAt(0).toUpperCase() + this._page.slice(1);
+            if ('function' === $.type(this[method])) {
+                this[method].call(this);
+            } else {
+                Con.log("Couldn't initialize page " + this.page, NAME, "error");
+            }
+        },
+
+        /**
+         * Initializes the settings page by registering some handler on page load
+         * @method _initializePageSettings
+         * @private
+         */
+        _initializePageSettings: function(options) {
             $("#mr_use_language").change(function() {
                 if (true == $(this).attr("checked")) {
                     $("#mr_use_language_name").removeAttr("disabled");
@@ -68,29 +107,50 @@ var mrPlugin = {
                 }
             });
 
-            mrPlugin._initializeTooltip();
-        });
-    },
+            this._initializeTooltips();
+        },
 
-    initializeExterpPage: function() {
-        $(function() {
-            mrPlugin._initializeTooltip();
-        });
-    },
-
-    _initializeTooltip: function() {
-        $(".mrPlugin a.i-link").each(function() {
-            $(this).attr("href", "javascript:void(0);");
-            $(this).attr("title", mrPlugin.lng.more_informations);
-            var id = $(this).attr("id").substring(0, $(this).attr("id").indexOf("-link"));
-            $(this).aToolTip({
-                clickIt:    true,
-                xOffset:    -20,
-                yOffset:    4,
-                outSpeed:   250,
-                tipContent: $("#" + id).html()
+        /**
+         * Initializes the expert page by registering some handler on page load
+         * @method _initializePageExpert
+         * @private
+         */
+        _initializePageExpert: function() {
+            $('.mrPlugin a.actionLink').click(function() {
+                var action = $(this).data('action'),
+                    params = $(this).data('params'),
+                    $form = $('#mr_expert');
+                $form.find('input[name="mr_action"]').val(action);
+                $form.attr('action', $form.attr('action') + '?' + params).submit();
             });
-        });
-    }
-};
 
+            this._initializeTooltips();
+        },
+
+        /**
+         * Initializes the test page by registering some handler on page load
+         * @method _initializePageTest
+         * @private
+         */
+        _initializePageTest: function() {
+            this._initializeTooltips();
+        },
+
+        /**
+         * Adds title to all tooltips
+         * @method _initializeTooltips
+         * @private
+         */
+        _initializeTooltips: function() {
+            var that = this;
+            $(".mrPlugin a.i-link").each(function() {
+                $(this).attr("href", "javascript:void(0);").attr("title", that._lng.more_informations);
+            });
+        }
+
+    };
+
+    // Assign to Con.Plugin namespace
+    Con.Plugin.ModRewrite = ModRewrite;
+
+})(Con, Con.$);
