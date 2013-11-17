a4c974baf60707e36a1cd74e14c7dcc7dd19a996
Dominik Ziegler <dominik.ziegler@4fb.de>
2013-11-17 13:38:53 +0000

    CON-1567

--- contenido/includes/functions.str.php
+++ contenido/includes/functions.str.php
@@ -432,7 +432,7 @@ function strNextBackwards($idcat) {
 function strNextDeeperAll($idcat, $ignoreLang = false) {
     global $lang;
 
-    $languageId = (true == $ignoreLang)? $lang : NULL;
+	$languageId = (true == $ignoreLang) ? NULL : $lang;
     $oCatColl = new cApiCategoryCollection();
     return $oCatColl->getAllChildCategoryIds($idcat, $languageId);
 }
