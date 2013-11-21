d0bb951d0d0dd3c992e134be237a5678b84c0d66
Murat Purc <murat@purc.de>
2013-11-21 22:36:59 +0000

    Content Type Filelist: Fixed some UI issues, reworked JS logic, replaced binding against delegate, made code more readable and maintainable, normalized logic to access to directory lists.

--- contenido/scripts/content_types/cmsFilelist.js
+++ contenido/scripts/content_types/cmsFilelist.js
@@ -40,6 +40,21 @@
     function cContentTypeFilelist(frameId, imageId, pathBackend, pathFrontend, idArtLang, id, fields, prefix, session, settings) {
         // call the constructor of the parent class with the same arguments
         Con.cContentTypeAbstractTabbed.apply(this, arguments);
+
+        // Define some jQuery selectors here for later usage
+        this.SELECTOR_FILELIST_MANUAL_FILES = $(this.frameId + ' #filelist_manual_files_' + this.id);
+        this.SELECTOR_FILELIST_FILENAME = this.frameId + ' #filelist_filename_' + this.id;
+        this.SELECTOR_CHK_FILELIST_MANUAL = this.frameId + ' #filelist_manual_' + this.id;
+        this.SELECTOR_MANUAL_FILELIST_SETTING = this.frameId + ' #manual_filelist_setting';
+        this.SELECTOR_METADATALIST = this.frameId + ' #metaDataList';
+        this.SELECTOR_FILELIST_INCL_METADATA = this.frameId + ' #filelist_incl_metadata_' + this.id;
+        this.SELECTOR_FILELIST_ALL_EXTENSIONS = this.frameId + ' #filelist_all_extensions';
+        this.SELECTOR_FILELIST_EXTENSIONS = this.frameId + ' #filelist_extensions_' + this.id;
+        this.SELECTOR_FILELIST_IGNORE_EXTENSIONS = this.frameId + ' #filelist_ignore_extensions_' + this.id;
+        this.SELECTOR_SAVE_SETTINGS = this.frameId + ' .save_settings';
+        this.SELECTOR_DIRLIST = this.frameId + ' #directories #directoryList_' + this.id + ' li li div';
+        this.SELECTOR_DIRLIST_ACTIVE = this.frameId + ' #directories #directoryList_' + this.id + ' div[class="active"]';
+        this.SELECTOR_DIRLIST_MANUAL = this.frameId + ' #manual #directoryList_' + this.id + '_manual li li div';
     }
 
     //inherit from cContentTypeAbstractTabbed
@@ -55,6 +70,7 @@
     cContentTypeFilelist.prototype.initialise = function() {
         // call the function of the parent so that it is initialised correctly
         Con.cContentTypeAbstractTabbed.prototype.initialise.call(this);
+
         // call custom functions that attach custom event handlers etc.
         this.addManualFileListEvent();
         this.addClickEvent();
@@ -69,10 +85,7 @@
     cContentTypeFilelist.prototype.loadExternalFiles = function() {
         // call the function of the parent so that all general files are included
         Con.cContentTypeAbstractTabbed.prototype.loadExternalFiles.call(this);
-        if ($('#cms_filelist_styles').length === 0) {
-            // TODO load via Con.Loader.get()
-            $('head').append('<link rel="stylesheet" id="cms_filelist_styles" href="' + this.pathBackend + 'styles/content_types/cms_filelist.css" type="text/css" media="all" />');
-        }
+        Con.Loader.get(this.pathBackend + 'styles/content_types/cms_filelist.css');
     };
 
     /**
@@ -93,26 +106,19 @@
      * @method addManualFileListEntry
      */
     cContentTypeFilelist.prototype.addManualFileListEntry = function() {
-        var filename = $(this.frameId + ' #filelist_filename_' + this.id).val();
-        var name = '';
-        var exists = false;
+        var $selectFileList = $(this.SELECTOR_FILELIST_FILENAME),
+            $selectManualFiles = $(this.SELECTOR_FILELIST_MANUAL_FILES),
+            filename = $selectFileList.val(),
+            name;
 
-        if (filename === '') {
+        if ('' === filename) {
             return;
         }
 
-        $(this.frameId + ' #filelist_manual_files_' + this.id + ' option').each(function() {
-            if (filename === $(this).val()) {
-                exists = true;
-            }
-        });
-
-        $(this.frameId + ' #filelist_filename_' + this.id + ' option:selected').each(function() {
-            name = $(this).html();
-        });
-
-        if (!exists) {
-            $(this.frameId + ' #filelist_manual_files_' + this.id).prepend('<option value="' + filename + '" selected="selected">' + name + '</option>');
+        if (!$selectManualFiles.find('option[value="' + filename + '"]')[0]) {
+            // It's not in the list, add it
+            name = $selectFileList.find('option:selected').html();
+            $selectManualFiles.prepend('<option value="' + filename + '" selected="selected">' + name + '</option>');
         }
     };
 
@@ -123,38 +129,41 @@
      */
     cContentTypeFilelist.prototype.addClickEvent = function() {
         var self = this;
-        self.addNaviActions();
-        self.addExtensionActions();
+
+        this.addNaviActions();
+        this.addExtensionActions();
 
         // check if manual filelist settings should be shown at the beginning
-        if ($(self.frameId + ' #filelist_manual_' + self.id).is(':checked')) {
-            $(self.frameId + ' #manual_filelist_setting').show();
+        var $chk = $(this.SELECTOR_CHK_FILELIST_MANUAL),
+            $settings = $(this.SELECTOR_MANUAL_FILELIST_SETTING);
+        if ($chk.is(':checked')) {
+            $settings.show();
         } else {
-            $(self.frameId + ' #manual_filelist_setting').hide();
+            $settings.hide();
         }
 
-        $(self.frameId + ' #filelist_manual_' + self.id).click(function() {
-            $(self.frameId + ' #manual_filelist_setting').slideToggle();
+        $chk.click(function() {
+            $settings.slideToggle();
         });
 
         // check if meta data should be shown at the beginning
-        if ($(self.frameId + ' #filelist_incl_metadata_' + self.id).is(':checked')) {
-            $(self.frameId + ' #metaDataList').show();
+        if ($(this.SELECTOR_FILELIST_INCL_METADATA).is(':checked')) {
+            $(this.SELECTOR_METADATALIST).show();
         } else {
-            $(self.frameId + ' #metaDataList').hide();
+            $(this.SELECTOR_METADATALIST).hide();
         }
 
-        $(self.frameId + ' #filelist_incl_metadata_' + self.id).click(function() {
-            $(self.frameId + ' #metaDataList').slideToggle();
+        $(this.SELECTOR_FILELIST_INCL_METADATA).click(function() {
+            $(self.SELECTOR_METADATALIST).slideToggle();
         });
 
-        $(self.frameId + ' #filelist_manual_files_' + self.id).dblclick(function() {
-            $(self.frameId + ' #filelist_manual_files_' + self.id + ' option').each(function() {
+        $(this.SELECTOR_FILELIST_MANUAL_FILES).dblclick(function() {
+            $(self.SELECTOR_FILELIST_MANUAL_FILES).find('option').each(function() {
                 if ($(this).is(':selected')) {
                     $(this).remove();
-                };
+                }
             });
-            $(self.frameId + ' #filelist_manual_files_' + self.id + ' option').prop('selected', 'selected');
+            $(self.SELECTOR_FILELIST_MANUAL_FILES).find('option').prop('selected', 'selected');
         });
     };
 
@@ -166,36 +175,36 @@
     cContentTypeFilelist.prototype.addExtensionActions = function() {
         var self = this;
         // let the user select all file extensions at once
-        $(self.frameId + ' #filelist_all_extensions').css('cursor', 'pointer');
-        $(self.frameId + ' #filelist_all_extensions').click(function() {
+        $(this.SELECTOR_FILELIST_ALL_EXTENSIONS).css('cursor', 'pointer');
+        $(this.SELECTOR_FILELIST_ALL_EXTENSIONS).click(function() {
             // only react if the extensions should not be ignored
-            if ($(self.frameId + ' #filelist_extensions_' + self.id).is(':not(:disabled)')) {
+            if ($(self.SELECTOR_FILELIST_EXTENSIONS).is(':not(:disabled)')) {
                 // check if all options are selected
                 var allSelected = true;
-                $(self.frameId + ' #filelist_extensions_' + self.id + ' option').each(function() {
+                $(self.SELECTOR_FILELIST_EXTENSIONS).find('option').each(function() {
                     if (!$(this).is(':selected')) {
                         allSelected = false;
                     }
                 });
                 if (allSelected) {
                     // all options are selected, so unselect them
-                    $(self.frameId + ' #filelist_all_extensions').css('font-weight', 'normal');
-                    $(self.frameId + ' #filelist_extensions_' + self.id + ' option').prop('selected', false);
+                    $(self.SELECTOR_FILELIST_ALL_EXTENSIONS).css('font-weight', 'normal');
+                    $(self.SELECTOR_FILELIST_EXTENSIONS).find('option').prop('selected', false);
                 } else {
                     // some options are not selected, so select them
-                    $(self.frameId + ' #filelist_all_extensions').css('font-weight', 'bold');
-                    $(self.frameId + ' #filelist_extensions_' + self.id + ' option').prop('selected', 'selected');
+                    $(self.SELECTOR_FILELIST_ALL_EXTENSIONS).css('font-weight', 'bold');
+                    $(self.SELECTOR_FILELIST_EXTENSIONS).find('option').prop('selected', 'selected');
                 }
             }
             return false;
         });
 
         // disable the file extension select if file extensions should be ignored
-        $(self.frameId + ' #filelist_ignore_extensions_' + self.id).click(function() {
+        $(this.SELECTOR_FILELIST_IGNORE_EXTENSIONS).click(function() {
             if ($(this).is(':checked')) {
-                $(self.frameId + ' #filelist_extensions_' + self.id).attr('disabled', 'disabled');
+                $(self.SELECTOR_FILELIST_EXTENSIONS).attr('disabled', 'disabled');
             } else {
-                $(self.frameId + ' #filelist_extensions_' + self.id).removeAttr('disabled');
+                $(self.SELECTOR_FILELIST_EXTENSIONS).removeAttr('disabled');
             }
         });
     };
@@ -207,10 +216,51 @@
      * @method addNaviActions
      */
     cContentTypeFilelist.prototype.addNaviActions = function() {
-        var self = this;
-        $(self.frameId + ' #manual #directoryList_' + self.id + '_manual a[class="on"]').parent('div').unbind('click');
-        $(self.frameId + ' #manual #directoryList_' + self.id + '_manual a[class="on"]').parent('div').click(function() {
-            var dirname = $(this).children('a[class="on"]').attr('title');
+        var self = this,
+            $dirListManual, $dirList, delegateSelector;
+
+        // Click handler for directories, the regular one and the manual one...
+        // Loads any available sub directories and toggles the collapsed state
+        // NB: The given callback is not used at the moment but is there for future purposes!
+        var _onDirectoryClick = function($context, callback) {
+            callback = callback || function() {};
+            var dirname = $context.find('a[class="on"]').attr('title');
+            if ($context.next('ul').length > 0) {
+                $context.next('ul').toggle(function() {
+                    if ($context.next('ul').is(':hidden')) {
+                        $context.parent().addClass('collapsed');
+                    } else {
+                        $context.parent().removeClass('collapsed');
+                    }
+                });
+                callback();
+            } else {
+                $.ajax({
+                    type: 'POST',
+                    url: self.pathBackend + 'ajaxmain.php',
+                    data: 'ajax=dirlist&dir=' + dirname + '&id=' + self.id + '&idartlang=' + self.idArtLang + '&contenido=' + self.session,
+                    success: function(msg) {
+                        if (msg.length > 0) {
+                            $context.after(msg);
+                        }
+                        $context.parent('li').removeClass('collapsed');
+                    }
+                });
+                callback();
+            }
+        };
+
+        // Manual directory list
+        $dirListManual = $(this.SELECTOR_DIRLIST_MANUAL),
+        delegateSelector = this.SELECTOR_DIRLIST_MANUAL.replace(this.frameId + ' ', '');
+        $dirListManual.removeClass('active');
+        $(this.frameId).delegate(delegateSelector, 'click', function() {
+            _onDirectoryClick($(this));
+
+            $dirListManual.removeClass('active');
+            $(this).addClass('active');
+
+            var dirname = $(this).find('a[class="on"]').attr('title');
             $.ajax({
                 type: 'POST',
                 url: self.pathBackend + 'ajaxmain.php',
@@ -223,66 +273,40 @@
             return false;
         });
 
-        $(self.frameId + ' #directories #directoryList_' + self.id + ' a[class="on"]').parent('div').unbind('click');
-        $(self.frameId + ' #directories #directoryList_' + self.id + ' a[class="on"]').parent('div').click(function() {
-            if ($(this).hasClass('active')) {
-                $(this).removeClass('active');
-            } else {
-                $(this).addClass('active');
-            }
+        // Directory list
+        $dirList = $(this.SELECTOR_DIRLIST);
+        delegateSelector = this.SELECTOR_DIRLIST.replace(this.frameId + ' ', '');
+        $(this.frameId).delegate(delegateSelector, 'click', function() {
+            _onDirectoryClick($(this));
 
-            return false;
-        });
-
-        $(self.frameId + ' .directoryList em a').unbind('click');
-        $(self.frameId + ' .directoryList em a').click(function() {
-            var divContainer = $(this).parent().parent();
-            var dirname = $(this).parent('em').parent().find('a[class="on"]').attr('title');
-
-            if (divContainer.next('ul').length > 0) {
-                divContainer.next('ul').toggle(function() {
-                    if (divContainer.next('ul').is(':hidden')) {
-                        divContainer.parent().addClass('collapsed');
-                    } else {
-                        divContainer.parent().removeClass('collapsed');
-                    }
-                });
-
-            } else {
-                $.ajax({
-                    type: 'POST',
-                    url: self.pathBackend + 'ajaxmain.php',
-                    data: 'ajax=dirlist&dir=' + dirname + '&id=' + self.id + '&idartlang=' + self.idArtLang + '&contenido=' + self.session,
-                    success: function(msg) {
-                        divContainer.after(msg);
-                        divContainer.parent('li').removeClass('collapsed');
-                        self.addNaviActions();
-                    }
-                });
-            }
-
+            $(this).toggleClass('active');
             return false;
         });
     };
 
     /**
      * Adds save event to the save button of content type edit form.
+     * Collects the selected directories in "directories" tab,
+     * and then it calls parents 
      * @method addSaveEvent
      * @override
      */
     cContentTypeFilelist.prototype.addSaveEvent = function() {
         var self = this;
-        $(self.frameId + ' .save_settings').click(function() {
-            // the chosen directory is no form field, so add it to the editform manually
-            var value = '';
-            $(self.frameId + ' #directories #directoryList_' + self.id + ' div[class="active"]').each(function() {
-                if (value === '') {
-                    value = $(this).find('a[class="on"]').attr('title');
-                } else {
-                    value += ',' + $(this).find('a[class="on"]').attr('title');
+
+        $(this.SELECTOR_SAVE_SETTINGS).click(function() {
+            // The chosen directory is no form field, so add it to the editform manually
+            var value = [],
+                item;
+            // Loop through all selected (active) directories and collect their names
+            $(self.SELECTOR_DIRLIST_ACTIVE).each(function() {
+                item = $(this).find('a[class="on"]');
+                if (item.attr('title').length) {
+                    value.push(item.attr('title'));
                 }
             });
-            self.appendFormField('filelist_array_directories', value);
+
+            self.appendFormField('filelist_array_directories', value.join(','));
             // remove the filelist_directories item from the fields so that it is not set again
             self.fields = $.grep(self.fields, function(e) {
                 return e !== 'filelist_directories';
