2f6e2bca492101579d844ae0fe9f6b5224af613d
Frederic Schneider <frederic.schneider@4fb.de>
2013-11-25 10:19:10 +0000

    CON-1642: Fixed bug with delete meta tag (types)

--- contenido/includes/type/action/include.con_meta_deletetype.action.php
+++ contenido/includes/type/action/include.con_meta_deletetype.action.php
@@ -2,26 +2,34 @@
 /**
  * Backend action file con_meta_deletetype
  *
- * @package          Core
- * @subpackage       Backend
- * @version          SVN Revision $Rev:$
+ * @package Core
+ * @subpackage Backend
+ * @version SVN Revision $Rev:$
  *
- * @author           Mischa Holz
- * @copyright        four for business AG <www.4fb.de>
- * @license          http://www.contenido.org/license/LIZENZ.txt
- * @link             http://www.4fb.de
- * @link             http://www.contenido.org
+ * @author Mischa Holz
+ * @copyright four for business AG <www.4fb.de>
+ * @license http://www.contenido.org/license/LIZENZ.txt
+ * @link http://www.4fb.de
+ * @link http://www.contenido.org
  */
 
 defined('CON_FRAMEWORK') || die('Illegal call: Missing framework initialization - request aborted.');
 
 if ($perm->have_perm_area_action("con", "con_meta_deletetype") || $perm->have_perm_area_action_item("con", "con_meta_deletetype", $idcat)) {
-    $oMetaTypeColl = new cApiMetaTypeCollection();
-    $oMetaTypeColl->delete((int)$idmetatype);
 
-    $oMetaTagColl = new cApiMetaTagCollection();
-    $metaTag = $oMetaTagColl->fetchByArtLangAndMetaType((int)$idartlang, (int)$idmetatype);
-    $oMetaTagColl->delete($metaTag->getField('idmetatag'));
+    // Get metavalue for selected metatype
+    $metaTagColl = new cApiMetaTagCollection();
+    $metaTagColl->select('idmetatype=' . cSecurity::toInteger($idmetatype));
+
+    // If CONTENIDO found only one entry, delete this metatag
+    if ($metaTagColl->count() == 1) {
+        $metaTag = $metaTagColl->next();
+        $metaTagColl->delete($metaTag->getField('idmetatag'));
+    }
+
+    // Delete metatype
+    $metaTypeColl = new cApiMetaTypeCollection();
+    $metaTypeColl->delete(cSecurity::toInteger($idmetatype));
 } else {
     $notification->displayNotification("error", i18n("Permission denied"));
 }
