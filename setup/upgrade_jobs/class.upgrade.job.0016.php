6716592572d2d851185c925b7a16e6fe41dc90a7
Frederic Schneider <frederic.schneider@4fb.de>
2013-12-02 16:25:01 +0000

    Deleted upgradejob 16 (add menus for the new search statistics), renamed upgradejob 18 to 17 and upgradejob 17 to 16

--- setup/upgrade_jobs/class.upgrade.job.0016.php
+++ setup/upgrade_jobs/class.upgrade.job.0016.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * This file contains the upgrade job 16.
  *
@@ -6,18 +7,21 @@
  * @subpackage UpgradeJob
  * @version SVN Revision $Rev:$
  *
- * @author Mischa Holz
+ * @author marcus.gnass
  * @copyright four for business AG <www.4fb.de>
  * @license http://www.contenido.org/license/LIZENZ.txt
  * @link http://www.4fb.de
  * @link http://www.contenido.org
  */
 
+// assert CONTENIDO framework
 defined('CON_FRAMEWORK') || die('Illegal call: Missing framework initialization - request aborted.');
 
 /**
  * Upgrade job 16.
- * Add menus for the new search statistics
+ *
+ * Add column 'uri' in table con_pifa_form so that buttons of type image can
+ * store an URI to their image.
  *
  * @package Setup
  * @subpackage UpgradeJob
@@ -29,23 +33,15 @@ class cUpgradeJob_0016 extends cUpgradeJobAbstract {
     public function _execute() {
         global $db, $cfg;
 
-        if ($_SESSION['setuptype'] == 'upgrade') {
-            // new area stat_search
-            $db->query("INSERT INTO " . $cfg['tab']['area'] . " VALUES('200', '0', 'stat_search', '1', '1', '0');");
-            
-            // 2 new files and use the left_top from the other statistic view
-            $db->query("INSERT INTO " . $cfg['tab']['files'] . " VALUES ('300', '200', 'include.stat_search_menu.php', 'main');");
-            $db->query("INSERT INTO " . $cfg['tab']['files'] . " VALUES ('301', '200', 'include.stat_left_top.php', 'main');");
-            $db->query("INSERT INTO " . $cfg['tab']['files'] . " VALUES ('302', '200', 'include.stat_search_overview.php', 'main');");
-            
-            // Link the files to frames
-            $db->query("INSERT INTO " . $cfg['tab']['frame_files'] . " VALUES('300', '200', '2', '300');");
-            $db->query("INSERT INTO " . $cfg['tab']['frame_files'] . " VALUES('301', '200', '1', '301');");
-            $db->query("INSERT INTO " . $cfg['tab']['frame_files'] . " VALUES('302', '200', '4', '302');");
-            
-            // new sub navigation
-            $db->query("INSERT INTO " . $cfg['tab']['nav_sub'] . " VALUES('200', '4', '200', '0', 'navigation/statistic/searches', '1');");
+        if ($_SESSION['setuptype'] == 'upgrade' && $cfg['tab']['pifa_form'] != "") {
+            $db->query('
+                ALTER TABLE
+                    `' . $cfg['tab']['pifa_form'] . '`
+                ADD
+                    `uri`
+                    VARCHAR(1023)
+                    DEFAULT NULL
+                    COMMENT \'URI for image buttons\';');
         }
     }
-
 }
--- setup/upgrade_jobs/class.upgrade.job.0017.php
+++ setup/upgrade_jobs/class.upgrade.job.0017.php
@@ -7,7 +7,7 @@
  * @subpackage UpgradeJob
  * @version SVN Revision $Rev:$
  *
- * @author marcus.gnass
+ * @author frederic.schneider
  * @copyright four for business AG <www.4fb.de>
  * @license http://www.contenido.org/license/LIZENZ.txt
  * @link http://www.4fb.de
@@ -20,8 +20,7 @@ defined('CON_FRAMEWORK') || die('Illegal call: Missing framework initialization 
 /**
  * Upgrade job 17.
  *
- * Add column 'uri' in table con_pifa_form so that buttons of type image can
- * store an URI to their image.
+ * Generates plugin relations for plugin specific action entries
  *
  * @package Setup
  * @subpackage UpgradeJob
@@ -31,17 +30,47 @@ class cUpgradeJob_0017 extends cUpgradeJobAbstract {
     public $maxVersion = "4.9.3";
 
     public function _execute() {
-        global $db, $cfg;
+        global $cfg;
 
-        if ($_SESSION['setuptype'] == 'upgrade' && $cfg['tab']['pifa_form'] != "") {
-            $db->query('
-                ALTER TABLE
-                    `' . $cfg['tab']['pifa_form'] . '`
-                ADD
-                    `uri`
-                    VARCHAR(1023)
-                    DEFAULT NULL
-                    COMMENT \'URI for image buttons\';');
+        if ($_SESSION['setuptype'] == 'upgrade') {
+
+            // Initializing cApiActionCollection
+            $actionColl = new cApiActionCollection();
+
+            // Include PIM collections
+            plugin_include('pim', 'classes/class.pim.plugin.collection.php');
+            plugin_include('pim', 'classes/class.pim.plugin.relations.collection.php');
+
+            // Initializing PimPluginRelationsCollection
+            $pluginRelColl = new PimPluginRelationsCollection();
+
+            // Initializing PimPluginCollection
+            $pluginColl = new PimPluginCollection();
+
+            // Get all installed plugins
+            $pluginColl->select();
+            while ($plugin = $pluginColl->next()) {
+
+                // Get path to plugin.xml
+                $pluginXmlPath = $cfg['path']['contenido'] . $cfg['path']['plugins'] . cSecurity::escapeString($plugin->get('folder')) . DIRECTORY_SEPARATOR . "plugin.xml";
+
+                // Load plugin.xml and get xml strings
+                $xml = simplexml_load_string(file_get_contents($pluginXmlPath));
+
+                // Count all actions at plugin.xml for this plugin
+                $actionCount = count($xml->contenido->actions->action);
+
+                for ($i = 0; $i < $actionCount; $i++) {
+
+                    // Get id of selected action
+                    $actionColl->select("name = '" . cSecurity::escapeString($xml->contenido->actions->action[$i]) . "'");
+                    $actionId = $actionColl->next();
+
+                    // Set a relation
+                    $pluginRelColl->create($actionId->get("idaction"), $plugin->get('idplugin'), 'action');
+                }
+            }
         }
     }
+
 }
