806b8d63359e3c35f59e37714c19adef55471839
Marcus Gnaß <marcus.gnass@4fb.de>
2013-12-03 12:30:44 +0000

    added documentation for class frontendlogic_category

--- contenido/plugins/frontendlogic/category/category.php
+++ contenido/plugins/frontendlogic/category/category.php
@@ -2,43 +2,54 @@
 /**
  * This file contains the category frontend logic class.
  *
- * @package    Plugin
+ * @package Plugin
  * @subpackage FrontendLogic
- * @version    SVN Revision $Rev:$
+ * @version SVN Revision $Rev:$
  *
- * @author     Andreas Lindner
- * @copyright  four for business AG <www.4fb.de>
- * @license    http://www.contenido.org/license/LIZENZ.txt
- * @link       http://www.4fb.de
- * @link       http://www.contenido.org
+ * @author Andreas Lindner
+ * @copyright four for business AG <www.4fb.de>
+ * @license http://www.contenido.org/license/LIZENZ.txt
+ * @link http://www.4fb.de
+ * @link http://www.contenido.org
  */
 
 defined('CON_FRAMEWORK') || die('Illegal call: Missing framework initialization - request aborted.');
 
 /**
  * Category frontend logic class.
- * TODO: comments
  *
- * @package    Plugin
+ * This "plugin" contains but a single class frontendlogic_category which
+ * extends the core class FrontendLogic. Neither frontendlogic_category nor
+ * FrontendLogic are used in the whole project and seem to be deprecated. Author
+ * of frontendlogic_category was Andreas Lindner. The author of FrontendLogic is
+ * not known.
+ *
+ * @package Plugin
  * @subpackage FrontendLogic
  */
-class frontendlogic_category extends FrontendLogic
-{
-    function getFriendlyName ()
-    {
+class frontendlogic_category extends FrontendLogic {
+
+    /**
+     * @see FrontendLogic::getFriendlyName()
+     */
+    function getFriendlyName() {
         return i18n("Category", "frontendlogic_category");
     }
 
-    function listActions ()
-    {
+    /**
+     * @see FrontendLogic::listActions()
+     */
+    function listActions() {
         $actions = array();
         $actions["access"] = i18n("Access category", "frontendlogic_category");
 
         return ($actions);
     }
 
-    function listItems ()
-    {
+    /**
+     * @see FrontendLogic::listItems()
+     */
+    function listItems() {
         global $lang, $db, $cfg;
 
         if (!is_object($db)) {
@@ -50,21 +61,19 @@ class frontendlogic_category extends FrontendLogic
                    b.name,
                    c.level
                  FROM
-                   ".$cfg['tab']['cat']." AS a,
-                   ".$cfg['tab']['cat_lang']." AS b,
-                   ".$cfg['tab']['cat_tree']." AS c
+                   " . $cfg['tab']['cat'] . " AS a,
+                   " . $cfg['tab']['cat_lang'] . " AS b,
+                   " . $cfg['tab']['cat_tree'] . " AS c
                  WHERE
                    a.idcat = b.idcat AND
                    a.idcat = c.idcat AND
-                   b.idlang = ".$lang." AND
+                   b.idlang = " . $lang . " AND
                    b.public = 0
                  ORDER BY c.idtree ASC";
 
         $db->query($sSQL);
         while ($db->nextRecord()) {
-            $items[$db->f("idcatlang")] =
-                '<span style="padding-left: '.($db->f("level")*10).'px;">'.htmldecode($db->f("name")).'</span>';
-
+            $items[$db->f("idcatlang")] = '<span style="padding-left: ' . ($db->f("level") * 10) . 'px;">' . htmldecode($db->f("name")) . '</span>';
         }
 
         return ($items);
