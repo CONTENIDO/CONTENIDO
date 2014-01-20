313a5ee579e7f98e829225aac8f69f706c1bc563
Claus Schunk <claus.schunk@4fb.de>
2014-01-20 09:03:02 +0000

    CON-1756: fixed action handling.

--- contenido/includes/type/action/include.str_makevisible.action.php
+++ contenido/includes/type/action/include.str_makevisible.action.php
@@ -18,7 +18,9 @@ defined('CON_FRAMEWORK') || die('Illegal call: Missing framework initialization 
 cInclude('includes', 'functions.str.php');
 
 if ($perm->have_perm_area_action("str", "str_makevisible") || $perm->have_perm_area_action_item("str", "str_makevisible", $idcat)) {
-    strMakeVisible($idcat, $lang, !$visible);
+    //CON-1756 offline/online toggle should not toggle offline/online tag for sub categories.
+    conMakeCatOnline($idcat, $lang, !$visible);
+   // strMakeVisible($idcat, $lang, !$visible);
 } else {
     $notification->displayNotification("error", i18n("Permission denied"));
 }
