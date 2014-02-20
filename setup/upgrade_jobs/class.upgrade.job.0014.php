61ae5d068e24b04a5aa393f5b0c27db97226bb93
frederic.schneider <frederic.schneider@4fb.de>
2014-02-20 09:57:12 +0100

    CON-1779: Added upgrade job to switch AMR and url_shortener plugin from "Content" to "Extras" navigation

---
+++ setup/upgrade_jobs/class.upgrade.job.0014.php
@@ -0,0 +1,94 @@
+<?php
+
+/**
+ * This file contains the upgrade job 14.
+ *
+ * @package Setup
+ * @subpackage UpgradeJob
+ * @version SVN Revision $Rev:$
+ *
+ * @author frederic.schneider
+ * @copyright four for business AG <www.4fb.de>
+ * @license http://www.contenido.org/license/LIZENZ.txt
+ * @link http://www.4fb.de
+ * @link http://www.contenido.org
+ */
+
+// assert CONTENIDO framework
+defined('CON_FRAMEWORK') || die('Illegal call: Missing framework initialization - request aborted.');
+
+/**
+ * Upgrade job 14
+ *
+ * Switched AMR and url_shortener plugins from "Content" navigation to "Extras" navigation
+ *
+ * @package Setup
+ * @subpackage UpgradeJob
+ */
+class cUpgradeJob_0014 extends cUpgradeJobAbstract {
+
+    public $maxVersion = "4.9.4";
+
+    public function _execute() {
+        global $cfg;
+ 
+        if ($_SESSION['setuptype'] == 'upgrade') {
+
+            // Initializing cApiNavSub
+            $navsub = new cApiNavSub();
+
+            // mod_rewrite/main
+            // Get informations for mod_rewrite/xml/;navigation/content/mod_rewrite/main
+            $navsub->loadBy('location', 'mod_rewrite/xml/;navigation/content/mod_rewrite/main');
+
+            // If extry exist, change location to mod_rewrite/xml/;navigation/extra/mod_rewrite/main
+            if ($navsub !== null) {
+            	$navsub->set('location', 'mod_rewrite/xml/;navigation/extra/mod_rewrite/main');
+            	$navsub->store();
+            }
+
+            // mod_rewrite/settings
+            // Get informations for mod_rewrite/xml/;navigation/content/mod_rewrite/settings
+            $navsub->loadBy('location', 'mod_rewrite/xml/;navigation/content/mod_rewrite/settings');
+            
+            // If entry exist, change location to mod_rewrite/xml/;navigation/extra/mod_rewrite/settings
+            if ($navsub !== null) {
+            	$navsub->set('location', 'mod_rewrite/xml/;navigation/extra/mod_rewrite/settings');
+            	$navsub->store();
+            }
+
+            // mod_rewrite/expert
+            // Get informations for mod_rewrite/xml/;navigation/content/mod_rewrite/expert
+            $navsub->loadBy('location', 'mod_rewrite/xml/;navigation/content/mod_rewrite/expert');
+            
+            // If entry exist, change location to mod_rewrite/xml/;navigation/extra/mod_rewrite/expert
+            if ($navsub !== null) {
+            	$navsub->set('location', 'mod_rewrite/xml/;navigation/extra/mod_rewrite/expert');
+            	$navsub->store();
+            }
+
+            // mod_rewrite/test
+            // Get informations for mod_rewrite/xml/;navigation/content/mod_rewrite/test
+            $navsub->loadBy('location', 'mod_rewrite/xml/;navigation/content/mod_rewrite/test');
+            
+            // If entry exist, change location to mod_rewrite/xml/;navigation/extra/mod_rewrite/test
+            if ($navsub !== null) {
+            	$navsub->set('location', 'mod_rewrite/xml/;navigation/extra/mod_rewrite/test');
+            	$navsub->store();
+            }
+            
+            // url_shortener/main
+            // Get informations for url_shortener/xml/;navigation/content/url_shortener/main
+            $navsub->loadBy('location', 'url_shortener/xml/;navigation/content/url_shortener/main');
+            
+            // If entry exist, change location to url_shortener/xml/;navigation/extra/url_shortener/main
+            if ($navsub !== null) {
+            	$navsub->set('location', 'url_shortener/xml/;navigation/extra/url_shortener/main');
+            	$navsub->store();
+            }
+            
+        }
+    }
+
+}
+
