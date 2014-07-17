127673527a67ea831a1e3b32c8dfc68c68ac1847
frederic.schneider <frederic.schneider@4fb.de>
2014-07-17 10:40:49 +0200

    CON-1872: Made filter for css/js/html switchable

--- contenido/includes/include.html_tpl_edit_form.php
+++ contenido/includes/include.html_tpl_edit_form.php
@@ -17,5 +17,20 @@
 
 defined('CON_FRAMEWORK') || die('Illegal call: Missing framework initialization - request aborted.');
 
+// Initializing editor
 $editor = new cGuiSourceEditor($_REQUEST['tmp_file']);
+
+// Show notice message if backend_file_extension filter is active
+if (empty($_REQUEST['tmp_file'])) {
+
+	// Get system properties for extension filter
+	$backend_file_extensions = getSystemProperty('backend', 'backend_file_extensions');
+
+	if($backend_file_extensions == "enabled") {
+		$editor->displayInfo(i18n("Currently only files with the extension html are displayed in the menu. If you create files with a different extension, they will not be shown on the left side!"));
+	}
+
+}
+
+// Render source editor
 $editor->render();
