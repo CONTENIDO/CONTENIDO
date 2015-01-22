c323a1f51610a6dc2024abc7fa2cfb23629ec070
frederic.schneider <frederic.schneider@4fb.de>
2015-01-22 14:59:28 +0100

    CON-2012: Wrong if case

--- contenido/includes/functions.i18n.php
+++ contenido/includes/functions.i18n.php
@@ -423,12 +423,13 @@ function mi18n($key) {
     $translation = $translations[$key];
 
     // Get module_translation_message setting value
-    $moduleTranslationMessage = getEffectiveSetting('debug', 'module_translation_message');
+    $moduleTranslationMessage = getEffectiveSetting('debug', 'module_translation_message', 'true');
+    $moduleTranslationMessage = 'true' === $moduleTranslationMessage ? true : false;
 
     // consider key as untranslated if translation has length 0
     // Don't trim translation, so that a string can be translated as ' '!
     // Show message only if module_translation_message mode is turn on
-    if (0 === strlen($translation) && $moduleTranslationMessage == true) {
+    if (0 === strlen($translation) && $moduleTranslationMessage) {
         $translation = 'Module translation not found: ' . $key;
     }
 
