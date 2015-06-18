ac618d9fa15e6956e7762487584e9ff15b10f029
Marcus Gnaß <marcus.gnass@4fb.de>
2015-06-18 10:08:18 +0200

    CON-2236

--- contenido/classes/auth/class.auth.handler.abstract.php
+++ contenido/classes/auth/class.auth.handler.abstract.php
@@ -15,7 +15,8 @@
 defined('CON_FRAMEWORK') || die('Illegal call: Missing framework initialization - request aborted.');
 
 /**
- * This class contains the abstract methods for the authentication in CONTENIDO.
+ * This class is the abstract authentication handler for CONTENIDO
+ * which may be extended differently for frontend and backend authentication.
  *
  * @package    Core
  * @subpackage Authentication
@@ -24,23 +25,28 @@ abstract class cAuthHandlerAbstract extends cAuth {
 
     /**
      * Handle the pre authorization.
-     * Let return this method a valid user ID to be set before the login form is
-     * handled, otherwise false.
      *
+     * When implementing this method let it return a valid user ID to be
+     * set before the login form is handled, otherwise false.
+     *
+     * @todo should be named preAuth or preAuthenticate
      * @return string|false
      */
     abstract public function preAuthorize();
 
     /**
      * Display the login form.
-     * Let this method include a file which displays the login form.
+     *
+     * When implementing this method let this method include a file
+     * which displays the login form.
      */
     abstract public function displayLoginForm();
 
     /**
      * Validate the credentials.
-     * Let this method validate the users input against source and return a
-     * valid user ID or false.
+     *
+     * When implementing this method let this method validate the users
+     * input against source and return a valid user ID or false.
      *
      * @return string|false
      */
