f4f75a8be2673b1a07e14d50a4f435ab755365c6
frederic.schneider <frederic.schneider@4fb.de>
2015-06-18 12:38:50 +0200

    Use displayOk instead of displayWarning for version history clearing

--- contenido/includes/include.js_history.php
+++ contenido/includes/include.js_history.php
@@ -177,7 +177,7 @@ if (!$perm->have_perm_area_action($area, 'js_history_manage')) {
         $oPage->render();
     } else {
         if ($bDeleteFile) {
-            $oPage->displayWarning(i18n('Version history was cleared'));
+            $oPage->displayOk(i18n('Version history was cleared'));
         } else {
             $oPage->displayWarning(i18n('No jscript history available'));
         }
