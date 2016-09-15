c396065789ae97f29972b42c2e9efeac3a0177aa
frederic.schneider <frederic.schneider@4fb.de>
2016-09-15 11:29:11 +0200

    CON-2562: Fixed wrong write permissions check

--- contenido/classes/module/class.module.template.handler.php
+++ contenido/classes/module/class.module.template.handler.php
@@ -233,7 +233,7 @@ class cModuleTemplateHandler extends cModuleHandler {
      * @return void|bool
      */
     public function checkWritePermissions() {
-        if ($this->moduleWriteable('template') == true && cFileHandler::exists(parent::getModulePath() . $this->_directories['template'])) {
+        if ($this->moduleWriteable('template') === false && cFileHandler::exists(parent::getModulePath() . $this->_directories['template']) === false) {
         	return $this->_notification->displayNotification(cGuiNotification::LEVEL_WARNING, sprintf(i18n("You have no write permissions for this module: %s"), parent::getModuleName()));
         } else {
             return true;
