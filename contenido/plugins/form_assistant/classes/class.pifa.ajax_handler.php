e522dd15702941e84d7afb80cea09725b460500b
dominik.ziegler <dominik.ziegler@4fb.de>
2017-03-22 11:33:57 +0100

    CON-2670 fixed pifa export files

--- contenido/plugins/form_assistant/classes/class.pifa.ajax_handler.php
+++ contenido/plugins/form_assistant/classes/class.pifa.ajax_handler.php
@@ -624,7 +624,7 @@ class PifaAjaxHandler {
         header('Cache-Control: private');
         header('Content-Type: text/xml');
         // header('Content-Type: application/xml');
-        header('Content-Length: ' . cString::getStringLength($xml));
+        header('Content-Length: ' . cString::getStringLength($xml, '8bit'));
         header('Content-Disposition: attachment; filename="' . $filename . '"');
         header('Content-Transfer-Encoding: binary');
 
