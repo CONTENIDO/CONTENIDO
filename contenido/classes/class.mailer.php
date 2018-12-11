57e233e30a43f01f2d927f5562ecabcf70d1539e
marcus.gnass <marcus.gnass@4fb.de>
2018-12-11 15:29:47 +0100

    CON-2772 fixed encoding problem and fixed error if mail is not configured correctly

--- contenido/classes/class.mailer.php
+++ contenido/classes/class.mailer.php
@@ -158,9 +158,11 @@ class cMailer extends Swift_Mailer {
      * @param Swift_Transport $transport [optional]
      *                                   a transport instance
      *
+     * @throws cDbException
+     * @throws cException
+     * @throws cInvalidArgumentException
      * @throws Swift_DependencyException
      * @throws Swift_RfcComplianceException
-     * @throws cInvalidArgumentException
      */
     public function __construct($transport = NULL) {
 
@@ -181,8 +183,7 @@ class cMailer extends Swift_Mailer {
             parent::__construct($transport);
             return;
         }
-        // if no transport object has been given, read system setting
-        // and create one
+        // if no transport object has been given, read system setting and create one
 
         // get name of mail host
         $mailHost = getSystemProperty('system', 'mail_host');
@@ -201,7 +202,7 @@ class cMailer extends Swift_Mailer {
             'ssl'
         );
 
-        $mail_type = cString::toLowerCase(getSystemProperty('system', 'mail_transport', 'smtp'));
+        $mail_type = cString::toLowerCase(getSystemProperty('system', 'mail_transport'));
 
         if ($mail_type == 'smtp') {
 
