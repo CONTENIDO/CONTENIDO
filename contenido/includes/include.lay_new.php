dad51ba5e282354d7f70fa0f788b833f283010ea
Murat Purc <murat@purc.de>
2019-12-03 21:50:04 +0100

    #28 Layout area: Click on "New Layout" and "Synchronize layouts" reloads "right_top" frame.

--- contenido/includes/include.lay_new.php
+++ contenido/includes/include.lay_new.php
@@ -16,18 +16,33 @@ defined('CON_FRAMEWORK') || die('Illegal call: Missing framework initialization 
 
 $tpl->reset();
 
+$client = cSecurity::toInteger(cRegistry::getClientId());
+
 if (!$perm->have_perm_area_action($area, $action)) {
     $tpl->set('s', 'ACTION', "");
     $tpl->set('s', 'ACTION2', '');
-} else if ((int) $client > 0) {
+} else if ($client > 0) {
+    // New layout link
    if ($perm->have_perm_area_action("lay_edit", "lay_new")) {
-        $tpl->set('s', 'ACTION', '<a class="addfunction" target="right_bottom" href="main.php?area=lay_edit&frame=4&action=lay_new&contenido=1">'. i18n("New Layout") . '</a>');
+       $str = sprintf(
+           '<a class="addfunction" href="javascript:Con.multiLink(\'%s\', \'%s\', \'%s\', \'%s\')">%s</a>',
+           'right_top', $sess->url("main.php?area=lay_edit&frame=3"),
+           'right_bottom', $sess->url("main.php?area=lay_edit&action=lay_new&frame=4"),
+           i18n("New Layout")
+       );
+       $tpl->set('s', 'ACTION', $str);
     } else {
         $tpl->set('s', 'ACTION', '<a class="addfunction_disabled" href="#">'. i18n("No permission to create layouts") . '</a>');
     }
-    //synch button for layout
-   if ($perm->have_perm_area_action("lay_edit", "lay_new")) {
-        $tpl->set('s', 'ACTION2', '<a class="syncronizefunction" target="right_bottom" href="main.php?area=lay_edit&frame=4&action=lay_sync&contenido=1">' . i18n("Synchronize layouts") . '</a>');
+    // Sync layouts link
+    if ($perm->have_perm_area_action("lay_edit", "lay_new")) {
+        $str = sprintf(
+            '<a class="syncronizefunction" href="javascript:Con.multiLink(\'%s\', \'%s\', \'%s\', \'%s\')">%s</a>',
+            'right_top', $sess->url("main.php?area=lay_edit&frame=3"),
+            'right_bottom', $sess->url("main.php?area=lay_edit&action=lay_sync&frame=4"),
+            i18n("Synchronize layouts")
+        );
+        $tpl->set('s', 'ACTION2', $str);
     } else {
         $tpl->set('s', 'ACTION2', '<a class="syncronizefunction_disabled" href="#">' . i18n("No permission to synchronize layouts") . '</a>');
     }
