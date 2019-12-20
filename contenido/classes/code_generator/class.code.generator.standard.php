de6a87d8ba9ffb06006f5864998800933e8a122d
Murat Purc <murat@purc.de>
2019-12-20 12:30:50 +0100

    #30 Adding CONTENIDO version to the generated meta tag should be configurable.

--- contenido/classes/code_generator/class.code.generator.standard.php
+++ contenido/classes/code_generator/class.code.generator.standard.php
@@ -45,7 +45,7 @@ class cCodeGeneratorStandard extends cCodeGeneratorAbstract {
      * @throws cInvalidArgumentException
      */
     public function _generate($contype = true, $editable = true, $version = NULL) {
-        global $cfg, $code;
+        $cfg = cRegistry::getConfig();
 
         $this->_cssData = '';
         $this->_jsData = '';
@@ -88,8 +88,8 @@ class cCodeGeneratorStandard extends cCodeGeneratorAbstract {
             $containerNumbers = tplGetContainerNumbersInLayout($idlay);
 
             // Initializing check arrays (CON-2706)
-            $loadedCSS = array();
-            $loadedJS = array();
+            $loadedCSS = [];
+            $loadedJS = [];
 
             foreach ($containerNumbers as $containerNr) {
                 // if there's no configured module in this container
@@ -104,7 +104,7 @@ class cCodeGeneratorStandard extends cCodeGeneratorAbstract {
                 $oModule = new cApiModule($containerModuleId);
                 $module = $oModule->toArray();
                 if (false === $module) {
-                    $module = array();
+                    $module = [];
                 }
 
                 $this->_resetModule();
@@ -141,21 +141,8 @@ class cCodeGeneratorStandard extends cCodeGeneratorAbstract {
                     $input = $moduleHandler->readInput();
                 }
 
-                // CON-1536 strip comments from module code
-                // regex is not enough to correctly remove comments
-                // use php_strip_whitespace instead of writing own parser
-                // downside: php_strip_whitespace requires a file as parameter
-                $tmpFile = dirname(cRegistry::getBackendPath()) . '/' . $cfg['path']['temp'] . uniqid('code_gen_') . '.php';
-                if (cFileHandler::exists(dirname($tmpFile))
-                    && cFileHandler::readable(dirname($tmpFile))
-                    && cFileHandler::writeable(dirname($tmpFile))) {
-                    if (false !== cFileHandler::write($tmpFile, $this->_moduleCode)) {
-                        $this->_moduleCode = php_strip_whitespace($tmpFile);
-
-                        // delete file
-                        cFileHandler::remove($tmpFile);
-                    }
-                }
+                // Strip all comments and whitespace from module code
+                $this->_moduleCode = $this->_stripWhitespace($this->_moduleCode);
 
                 // process CMS value tags
                 if (isset($containerConfigurations[$containerNr])) {
@@ -196,11 +183,11 @@ class cCodeGeneratorStandard extends cCodeGeneratorAbstract {
         $cssFile = '';
         if (cString::getStringLength($this->_cssData) > 0) {
             if (($myFileCss = $moduleHandler->saveContentToFile($this->_tplName, 'css', $this->_cssData)) !== false) {
-                $oHTML = new cHTML(array(
+                $oHTML = new cHTML([
                     'rel' => 'stylesheet',
                     'type' => 'text/css',
                     'href' => $myFileCss
-                ));
+                ]);
                 $oHTML->setTag('link');
                 $cssFile = $oHTML->render();
             }
@@ -219,7 +206,7 @@ class cCodeGeneratorStandard extends cCodeGeneratorAbstract {
             $this->_layoutCode = cString::iReplaceOnce('{CSS}', $cssFile, $this->_layoutCode);
         } else if (!empty($cssFile)) {
             if (cString::findFirstPos($this->_layoutCode, '</title>') !== false) {
-                $matches = array();
+                $matches = [];
                 if (preg_match_all("#(<head>.*?</title>)(.*?</head>)#si", $this->_layoutCode, $matches)) {
                     $this->_layoutCode = cString::iReplaceOnce($matches[1][0], $matches[1][0] . $cssFile, $this->_layoutCode);
                 }
@@ -253,8 +240,8 @@ class cCodeGeneratorStandard extends cCodeGeneratorAbstract {
             $debugPrefix .= "\techo(\"<!-- \\n\\n\");\n";
 
             $layout = new cApiLayout($idlay);
-            $layouName = $layout->get('name');
-            $debugPrefix .= "\techo(\"Layout: " . $layouName . " (" . $idlay . ")\\n\");\n";
+            $layoutName = $layout->get('name');
+            $debugPrefix .= "\techo(\"Layout: " . $layoutName . " (" . $idlay . ")\\n\");\n";
 
             $debugPrefix .= "\techo(\"Template: " . $this->_tplName . " (" . $idtpl . ")\\n\");\n";
 
@@ -262,10 +249,10 @@ class cCodeGeneratorStandard extends cCodeGeneratorAbstract {
             $catart = new cApiCategoryArticle();
             $cat = new cApiCategoryLanguage();
             $cat->loadByCategoryIdAndLanguageId($this->_idcat, $article->get('idlang'));
-            $catart->loadByMany(array(
+            $catart->loadByMany([
                 'idcat' => $cat->get('idcat'),
                 'idart' => $article->get('idart')
-            ));
+            ]);
             $lang = new cApiLanguage($article->get('idlang'));
             $debugPrefix .= "\techo(\"Language: " . $lang->get('idlang') . " (" . $lang->get('name') . ")\\n\");\n";
 
@@ -362,12 +349,9 @@ class cCodeGeneratorStandard extends cCodeGeneratorAbstract {
      * Also calls the CEC 'Contenido.Content.CreateMetatags' for user defined
      * meta tags creation.
      *
-     * @global array $encoding
      * @return string
      */
     protected function _processCodeMetaTags() {
-        global $encoding;
-
         // get basic meta tags (from article & system)
         $metaTags = $this->_getBasicMetaTags();
 
@@ -379,7 +363,7 @@ class cCodeGeneratorStandard extends cCodeGeneratorAbstract {
             }
         }
 
-        $sMetatags = '';
+        $sMetaTags = '';
 
         foreach ($metaTags as $value) {
 
@@ -422,11 +406,11 @@ class cCodeGeneratorStandard extends cCodeGeneratorAbstract {
                     $this->_layoutCode = str_replace($aMatch[1], $oMetaTagGen->render() . "\n", $this->_layoutCode);
                 }
             } else {
-                $sMetatags .= $oMetaTagGen->render() . "\n";
+                $sMetaTags .= $oMetaTagGen->render() . "\n";
             }
         }
 
-        return $sMetatags;
+        return $sMetaTags;
     }
 
     /**
@@ -440,20 +424,20 @@ class cCodeGeneratorStandard extends cCodeGeneratorAbstract {
      *                               whether the create code flag in cat_art should be set or not (optional)
      * @throws cDbException
      * @throws cInvalidArgumentException
-     * @global array $cfgClient
      */
     protected function _saveGeneratedCode($idcatart, $code = '', $flagCreateCode = true) {
-        global $cfgClient;
+        $cfgClient = cRegistry::getClientConfig();
+        $codePath = $cfgClient[$this->_client]['code']['path'];
 
         // Write code in the cache of the client. If the folder does not exist
         // create one.
         // do not write code cache into root directory of client
-        if (cRegistry::getFrontendPath() === $cfgClient[$this->_client]['code']['path']) {
+        if (cRegistry::getFrontendPath() === $codePath) {
             return;
         }
 
         // parent directory must be named cache
-        $directoryName = basename(dirname($cfgClient[$this->_client]['code']['path']));
+        $directoryName = basename(dirname($codePath));
         if ('cache' !== $directoryName) {
             // directory name is not cache -> abort
             return;
@@ -461,21 +445,21 @@ class cCodeGeneratorStandard extends cCodeGeneratorAbstract {
 
         // CON-2113
         // Do not overwrite an existing .htaccess file to prevent misconfiguring permissions
-        if ($this->_layout == false && $this->_save == true && isset($cfgClient[$this->_client]['code']['path'])) {
-            if (false === is_dir($cfgClient[$this->_client]['code']['path'])) {
-                mkdir($cfgClient[$this->_client]['code']['path']);
-                @chmod($cfgClient[$this->_client]['code']['path'], cDirHandler::getDefaultPermissions());
+        if ($this->_layout == false && $this->_save == true && isset($codePath)) {
+            if (false === is_dir($codePath)) {
+                mkdir($codePath);
+                @chmod($codePath, cDirHandler::getDefaultPermissions());
             }
 
-            if (true !== cFileHandler::exists($cfgClient[$this->_client]['code']['path'] . '.htaccess')) {
-                cFileHandler::write($cfgClient[$this->_client]['code']['path'] . '.htaccess', "Order Deny,Allow\nDeny from all\n");
+            if (true !== cFileHandler::exists($codePath . '.htaccess')) {
+                cFileHandler::write($codePath . '.htaccess', "Order Deny,Allow\nDeny from all\n");
             }
 
-            if (true === is_dir($cfgClient[$this->_client]['code']['path'])) {
+            if (true === is_dir($codePath)) {
                 $fileCode = ($code == '')? $this->_layoutCode : $code;
 
                 $code = "<?php\ndefined('CON_FRAMEWORK') or die('Illegal call');\n\n?>\n" . $fileCode;
-                cFileHandler::write($cfgClient[$this->_client]['code']['path'] . $this->_client . '.' . $this->_lang . '.' . $idcatart . '.php', $code, false);
+                cFileHandler::write($codePath . $this->_client . '.' . $this->_lang . '.' . $idcatart . '.php', $code, false);
 
                 // Update create code flag
                 if ($flagCreateCode == true) {
@@ -494,50 +478,46 @@ class cCodeGeneratorStandard extends cCodeGeneratorAbstract {
      *         List of assozative meta tag values
      */
     protected function _getBasicMetaTags() {
+        global $encoding;
 
         // collect all available meta tag entries with non empty values
-        $metaTags = array();
+        $metaTags = [];
         foreach (conGetAvailableMetaTagTypes() as $key => $value) {
             $metaValue = conGetMetaValue($this->_idartlang, $key);
             if (0 < cString::getStringLength($metaValue)) {
-                $metaTags[] = array(
+                $metaTags[] = [
                     $value['fieldname'] => $value['metatype'],
                     'content' => $metaValue
-                );
+                ];
             }
         }
 
         // add generator meta tag
-        // if the version is appended should be configurable
-        // due to security reasons
         $generator = 'CMS CONTENIDO';
-        $addVersion = true;
-        if ($addVersion) {
-            $cfg = cRegistry::getConfig();
+        if ((getEffectiveSetting('generator', 'add_version', 'true') === 'true')) {
             $aVersion = explode('.', CON_VERSION);
             $generator .= ' ' . $aVersion[0] . '.' . $aVersion[1];
         }
-        $metaTags[] = array(
+        $metaTags[] = [
             'name' => 'generator',
             'content' => $generator
-        );
+        ];
 
         // add charset or content type meta tag
-        global $encoding;
-        if (getEffectiveSetting('generator', 'html5', 'false') == 'true') {
-            $metaTags[] = array(
+        if (getEffectiveSetting('generator', 'html5', 'false') === 'true') {
+            $metaTags[] = [
                 'charset' => $encoding[$this->_lang]
-            );
-        } elseif (getEffectiveSetting('generator', 'xhtml', 'false') == 'true') {
-            $metaTags[] = array(
+            ];
+        } elseif (getEffectiveSetting('generator', 'xhtml', 'false') === 'true') {
+            $metaTags[] = [
                 'http-equiv' => 'Content-Type',
                 'content' => 'application/xhtml+xml; charset=' . $encoding[$this->_lang]
-            );
+            ];
         } else {
-            $metaTags[] = array(
+            $metaTags[] = [
                 'http-equiv' => 'Content-Type',
                 'content' => 'text/html; charset=' . $encoding[$this->_lang]
-            );
+            ];
         }
 
         // update (!) index setting of robots meta tag
@@ -563,16 +543,15 @@ class cCodeGeneratorStandard extends cCodeGeneratorAbstract {
      * @return array
      */
     protected function _updateMetaRobots(array $metaTags, $index, $follow) {
-
         // extract robots setting from current meta elements
         list($metaTags, $metaRobots) = $this->_extractMetaElement($metaTags, 'name', 'robots');
 
         if (is_null($metaRobots)) {
             // build new meta element if none could be found
-            $metaRobots = array(
+            $metaRobots = [
                 'name' => 'robots',
                 'content' => ''
-            );
+            ];
         } else {
             $content = array_map('trim', explode(',', $metaRobots['content']));
             // determine index from extracted element if given value is NULL
@@ -599,7 +578,7 @@ class cCodeGeneratorStandard extends cCodeGeneratorAbstract {
         }
 
         // build and set new content for robots element
-        $content = array();
+        $content = [];
         if (true === $index && true === $follow) {
             $content[] = 'all';
         } else {
@@ -634,12 +613,11 @@ class cCodeGeneratorStandard extends cCodeGeneratorAbstract {
      * @return array
      */
     protected function _extractMetaElement(array $metaTags, $type, $nameOrEquiv) {
-
         // prepare result structure
-        $result = array(
-            array(),
+        $result = [
+            [],
             NULL
-        );
+        ];
 
         // loop all given meta elements
         foreach ($metaTags as $metaTag) {
