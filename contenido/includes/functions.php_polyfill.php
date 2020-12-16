57e0c95f47dc669c216bc64430f77228340efcfa
Murat Purc <murat@purc.de>
2020-12-16 22:15:26 +0100

    #164 Fix reported errors related to PHP Warning: count()
    - Add is_countable polyfill
    - Unit tests for polyfills

--- contenido/includes/functions.php_polyfill.php
+++ contenido/includes/functions.php_polyfill.php
@@ -18,13 +18,35 @@ if (!function_exists('is_iterable')) {
 
     /**
      * Verify that the contents of a variable is an iterable value.
-     * is_iterable polyfill for PHP < 7.1
+     * is_iterable polyfill for PHP < 7.1.
+     *
      * @link https://www.php.net/manual/en/function.is-iterable.php
      * @param mixed $var The value to check
      * @return bool Returns TRUE if var is iterable, FALSE otherwise
      */
     function is_iterable($var) {
         return is_array($var) || (is_object($var) && ($var instanceof \Traversable));
+    }
+
+}
+
+if (!function_exists('is_countable')) {
+
+    /**
+     * Verify that the content of a variable is an array or an object
+     * implementing the Countable interface.
+     * is_countable polyfill for PHP < 7.3.
+     *
+     * @link https://www.php.net/manual/de/function.is-countable.php
+     * @param mixed $var The value to check
+     * @return bool Returns TRUE if var is countable, FALSE otherwise
+     */
+    function is_countable($var) {
+        return (is_array($var)
+            || $var instanceof \Countable
+            || $var instanceof \SimpleXMLElement
+            || $var instanceof \ResourceBundle
+        );
     }
 
 }
