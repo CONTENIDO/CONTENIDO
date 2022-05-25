48198d0d4c1308d9babf03245a17366036e729f5
Murat Purc <murat@purc.de>
2022-05-25 13:47:36 +0200

    #52 Style / Templates: Delete confirmation in frontend style when visual edit is open.
    Added missing jQuery UI script to the backend visual edit mode code output, also added new cRegistry::isBackendVisualEditMode() function and proper unit tests.

---
+++ test/contenido/classes/cRegistryTest.php
@@ -0,0 +1,62 @@
+<?php
+
+/**
+ * This file contains tests for Contenido cRegistry.
+ *
+ * @package          Testing
+ * @subpackage       Test_Registry
+ * @author           Murat Purc <murat@purc.de>
+ * @copyright        four for business AG <www.4fb.de>
+ * @license          http://www.contenido.org/license/LIZENZ.txt
+ * @link             http://www.4fb.de
+ * @link             http://www.contenido.org
+ */
+
+
+/**
+ * Class to test cRegistry.
+ *
+ * TODO Implement other unit tests for cRegistry.
+ *
+ * @package          Testing
+ * @subpackage       Test_Registry
+ */
+class cRegistryTest extends cTestingTestCase
+{
+
+    /**
+     * Test cRegistry::isBackendVisualEditMode()
+     */
+    public function testIsBackendVisualEditMode()
+    {
+        global $contenido, $area;
+
+        $sessId = md5('some_value');
+
+        // No 'contenido' & 'area'
+        $result = cRegistry::isBackendVisualEditMode();
+        $this->assertEquals(false, $result);
+
+        // 'contenido' but no 'area'
+        $contenido = $sessId;
+        $result = cRegistry::isBackendVisualEditMode();
+        $this->assertEquals(false, $result);
+        $contenido = null;
+
+        // 'contenido' but wrong 'area'
+        $contenido = $sessId;
+        $area = 'wrong_area';
+        $result = cRegistry::isBackendVisualEditMode();
+        $this->assertEquals(false, $result);
+        $contenido = null;
+        $area = null;
+
+        // 'contenido' and 'area'
+        $contenido = $sessId;
+        $area = 'tpl_visual';
+        $result = cRegistry::isBackendVisualEditMode();
+        $this->assertEquals(true, $result);
+        $contenido = null;
+        $area = null;
+    }
+}
