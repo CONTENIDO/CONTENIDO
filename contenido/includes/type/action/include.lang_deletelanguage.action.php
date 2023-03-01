28d8effc5f52420f3321bc4b55299c7c04ad99aa
Murat Purc <murat@purc.de>
2023-03-02 00:28:47 +0100

    #366 UI: Rework and unify styles.
    Language overview uses `cGuiMenu` to render the menu.

--- contenido/includes/type/action/include.lang_deletelanguage.action.php
+++ contenido/includes/type/action/include.lang_deletelanguage.action.php
@@ -21,7 +21,13 @@ if ($perm->have_perm_area_action("lang_edit", "lang_deletelanguage")) {
         $targetclient = $client;
     }
 
-    $errno = langDeleteLanguage($idlang, $targetclient);
+    $result = langDeleteLanguage($idlang, $targetclient);
+    if (is_string($result)) {
+        // Result contains language not deleted error message
+        $tmp_notification = $result;
+    } else {
+        $tmp_notification = $notification->returnNotification("ok", i18n("Language deleted"));
+    }
 } else {
     $notification->displayNotification("error", i18n("Permission denied"));
 }
