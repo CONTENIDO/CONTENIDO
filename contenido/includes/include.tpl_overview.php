38310d1bea12aef294afe30f4d32f8fe47577595
Murat Purc <murat@purc.de>
2023-03-02 11:38:22 +0100

    #366 UI: Rework and unify styles.
    Template overview uses `cGuiMenu` to render the menu, new function `cApiTemplateCollection->getAllTemplatesByClient()` to get all templates for a client.

--- contenido/includes/include.tpl_overview.php
+++ contenido/includes/include.tpl_overview.php
@@ -27,7 +27,7 @@ $oClient = cRegistry::getClient();
 
 // Display critical error if client does not exist
 if (!$oClient->isLoaded()) {
-    $oPage = new cGuiPage("tpl_overview");
+    $oPage = new cGuiPage('tpl_overview');
     $oPage->displayCriticalError(i18n('No Client selected'));
     $oPage->render();
     return;
@@ -39,90 +39,101 @@ $requestIdTpl = cSecurity::toInteger($_REQUEST['idtpl'] ?? '0');
 
 $hasCommonTplRights = null;
 
-$sql = "SELECT * FROM `%s` WHERE `idclient` = %d ORDER BY `name`";
-$db->query($sql, cRegistry::getDbTableName('tpl'), $client);
+$templateColl = new cApiTemplateCollection();
+$allTemplates = $templateColl->getAllTemplatesByClient($client);
+
 $tpl->reset();
 
-while ($db->nextRecord()) {
+$menu = new cGuiMenu('tpl_overview_list');
+
+$showLink = new cHTMLLink();
+$showLink->setClass('show_item')
+    ->setLink('javascript:void(0)')
+    ->setAttribute('data-action', 'show_template');
+
+$deleteLink = new cHTMLLink();
+$deleteLink = $deleteLink->setClass('con_img_button')
+    ->setLink('javascript:void(0)')
+    ->setAttribute('data-action', 'delete_template')
+    ->setContent(cHTMLImage::img($cfg['path']['images'] . 'delete.gif', i18n('Delete template')));
+
+$inUseLink = new cHTMLLink();
+$inUseLink = $inUseLink->setClass('con_img_button')
+    ->setLink('javascript:void(0)')
+    ->setAttribute('data-action', 'inused_template')
+    ->setContent(cHTMLImage::img($cfg['path']['images'] . 'exclamation.gif', i18n('Click for more information about usage')));
+
+$copyLink = new cHTMLLink();
+$copyLink = $copyLink->setClass('con_img_button')
+    ->setLink('javascript:void(0)')
+    ->setAttribute('data-action', 'duplicate_template')
+    ->setContent(cHTMLImage::img($cfg['path']['images'] . 'but_copy.gif', i18n('Duplicate template')));
+
+foreach ($allTemplates as $idtpl => $template) {
     if (is_null($hasCommonTplRights)) {
         $hasCommonTplRights = (
-            $perm->have_perm_area_action("tpl", "tpl_delete") ||
-            $perm->have_perm_area_action("tpl", "tpl_duplicate") ||
-            $perm->have_perm_area_action("tpl_edit", "tpl_edit") ||
-            $perm->have_perm_area_action("tpl_edit", "tpl_new") ||
-            $perm->have_perm_area_action("tpl_visual", "tpl_visedit")
+            $perm->have_perm_area_action('tpl', 'tpl_delete') ||
+            $perm->have_perm_area_action('tpl', 'tpl_duplicate') ||
+            $perm->have_perm_area_action('tpl_edit', 'tpl_edit') ||
+            $perm->have_perm_area_action('tpl_edit', 'tpl_new') ||
+            $perm->have_perm_area_action('tpl_visual', 'tpl_visedit')
         );
     }
 
-    if ($perm->have_perm_item($area, $db->f("idtpl")) || $hasCommonTplRights) {
-        $name = (cString::getStringLength(trim($db->f('name'))) > 0) ? $db->f('name') : i18n("-- New template --");
+    if ($perm->have_perm_item($area, $idtpl) || $hasCommonTplRights) {
+        $name = (cString::getStringLength(trim($template['name'])) > 0) ? $template['name'] : i18n('-- New template --');
         $name = conHtmlSpecialChars(stripslashes($name));
-        $descr = conHtmlSpecialChars(stripslashes($db->f('description') ?? ''));
-        $idtpl = $db->f("idtpl");
+        $description = conHtmlSpecialChars(stripslashes($template['description'] ?? ''));
 
-        // Create show_action item
-        $tmp_mstr = '<a class="show_item" href="javascript:void(0)" data-action="show_template" title="%s">%s</a>';
+        $menu->setId($idtpl, $idtpl);
+        $menu->setTooltip($idtpl, $description);
 
-        if ($db->f("defaulttemplate") == 1) {
-            $mstr = sprintf($tmp_mstr, $descr, '<b>' . $name . '</b>');
+        if ($perm->have_perm_area_action_item('tpl_edit', 'tpl_edit', $idtpl)) {
+            $menu->setLink($idtpl, $showLink);
         } else {
-            $mstr = sprintf($tmp_mstr, $descr, $name);
+            $menu->setLink($idtpl, new cHTMLSpan());
         }
 
-        $tpl->set('d', 'DATA_ID', $idtpl);
-
-        if ($perm->have_perm_area_action_item("tpl_edit", "tpl_edit", $idtpl)) {
-            $tpl->set('d', 'NAME', $mstr);
+        if ($template['defaulttemplate'] == 1) {
+            $menu->setTitle($idtpl, '<b>' . $name . '</b>');
         } else {
-            $tpl->set('d', 'NAME', $name);
+            $menu->setTitle($idtpl, $name);
         }
-
-        $tpl->set('d', 'DESCRIPTION', ($descr == '') ? '' : $descr);
 
         // Check if template is in use
         $inUse = tplIsTemplateInUse($idtpl);
 
-        $inUseString = i18n("Click for more information about usage");
-
-        if (!$inUse && ($perm->have_perm_area_action_item("tpl", "tpl_delete", $idtpl))) {
-            $delTitle = i18n("Delete template");
-            $deleteLink = '<a class="con_img_button" href="javascript:void(0)" data-action="delete_template" title="' . $delTitle . '">'
-                        . cHTMLImage::img($cfg['path']['images'] . 'delete.gif', $delTitle)
-                        . '</a>';
-            $inUseLink = cHTMLImage::img($cfg['path']['images'] . 'spacer.gif', '', ['class' => 'con_img_button_off']);
+        if (!$inUse && ($perm->have_perm_area_action_item('tpl', 'tpl_delete', $idtpl))) {
+            $deleteLinkStr = $deleteLink->render();
+            $inUseLinkStr = cHTMLImage::img($cfg['path']['images'] . 'spacer.gif', '', ['class' => 'con_img_button_off']);
         } else {
-            $delTitle = i18n("Template in use, cannot delete");
-            $deleteLink = cHTMLImage::img($cfg['path']['images'] . 'delete_inact.gif', $delTitle, ['class' => 'con_img_button_off']);
-
-            $inUseLink = '<a class="con_img_button" href="javascript:void(0)" data-action="inused_template">'
-                       . cHTMLImage::img($cfg['path']['images'] . 'exclamation.gif', $inUseString)
-                       . '</a>';
+            $delTitle = i18n('Template in use, cannot delete');
+            $deleteLinkStr = cHTMLImage::img($cfg['path']['images'] . 'delete_inact.gif', $delTitle, ['class' => 'con_img_button_off']);
+            $inUseLinkStr = $inUseLink->render();
         }
 
-        if ($perm->have_perm_area_action_item("tpl", "tpl_dup", $db->f("idtpl"))) {
-            $copyTitle = i18n("Duplicate template");
-            $copyLink = '<a class="con_img_button" href="javascript:void(0)" data-action="duplicate_template" title="' . $copyTitle . '">'
-                        . cHTMLImage::img($cfg['path']['images'] . 'but_copy.gif', $copyTitle)
-                        . '</a>';
+        if ($perm->have_perm_area_action_item('tpl', 'tpl_dup', $idtpl)) {
+            $copyLinkStr = $copyLink->render();
         } else {
-            $copyLink = cHTMLImage::img($cfg['path']['images'] . 'spacer.gif', '', ['class' => 'con_img_button_off']);
+            $copyLinkStr = cHTMLImage::img($cfg['path']['images'] . 'spacer.gif', '', ['class' => 'con_img_button_off']);
         }
 
-        $tpl->set('d', 'INUSE', '&nbsp;' . $inUseLink);
-        $tpl->set('d', 'COPY', '&nbsp;' . $copyLink . '&nbsp;');
-        $tpl->set('d', 'DELETE', $deleteLink . '&nbsp;');
+        $menu->setActions($idtpl, 'inuse', $inUseLinkStr);
+        $menu->setActions($idtpl, 'copy', $copyLinkStr);
+        $menu->setActions($idtpl, 'delete', $deleteLinkStr);
 
-        $marked = ($requestIdTpl == $idtpl) ? 'marked' : 'tpl' . $tpl->dyn_cnt;
-        $tpl->set('d', 'ID', $marked);
-
-        $tpl->next();
+        if ($requestIdTpl === $idtpl) {
+            $menu->setMarked($idtpl);
+        }
     }
 }
+
+$tpl->set('s', 'GENERIC_MENU', $menu->render(false));
 
 // Data for show of used info per ajax
 $tpl->set('s', 'AREA', $area);
 $tpl->set('s', 'AJAX_URL', cRegistry::getBackendUrl() . 'ajaxmain.php');
-$tpl->set('s', 'BOX_TITLE', i18n("The template '%s' is used for following categories and articles") . ":");
-$tpl->set('s', 'DELETE_MESSAGE', i18n("Do you really want to delete the following template:<br><br>%s<br>"));
+$tpl->set('s', 'BOX_TITLE', i18n("The template '%s' is used for following categories and articles") . ':');
+$tpl->set('s', 'DELETE_MESSAGE', i18n('Do you really want to delete the following template:<br><br>%s<br>'));
 
 $tpl->generate($cfg['path']['templates'] . $cfg['templates']['tpl_overview']);
