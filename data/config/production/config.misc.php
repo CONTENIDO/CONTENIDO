f5ad47a6c9de43398fac01e3c8f38ecced154ffe
Murat Purç <murat@purc.de>
2024-02-10 13:34:18 +0100

    #446 - Add more debug features.
    Configuration to output code only when editing in article editor (backend).

--- data/config/production/config.misc.php
+++ data/config/production/config.misc.php
@@ -57,7 +57,7 @@ $cfg['debug']['rendering'] = false;
 $cfg['debug']['codeoutput'] = false;
 
 // (bool) To output the code when editing in article editor, set this to true
-$cfg['debug']['article_editor_codeoutput'] = true;
+$cfg['debug']['article_editor_codeoutput'] = false;
 
 // (bool) Whether the chain system should be disabled.
 $cfg['debug']['disable_chains'] = false;
