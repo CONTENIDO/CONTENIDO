8521fc180288d6edd265df4809c0d7d8d69d5e16
Murat Purç <murat@purc.de>
2025-09-28 16:19:55 +0200

    task: #527 Deprecated callable in Swift Mailer library #527.
    Replaced `call_user_func_array()` calls in Swift Mailer sources to prevent 'Deprecated: Callables of the form...' warnings.

--- contenido/classes/swiftmailer/lib/classes/Swift/Attachment.php
+++ contenido/classes/swiftmailer/lib/classes/Swift/Attachment.php
@@ -26,11 +26,9 @@ class Swift_Attachment extends Swift_Mime_Attachment
      */
     public function __construct($data = null, $filename = null, $contentType = null)
     {
-        call_user_func_array(
-            array($this, 'Swift_Mime_Attachment::__construct'),
-            Swift_DependencyContainer::getInstance()
-                ->createDependenciesFor('mime.attachment')
-            );
+        parent::__construct(
+            ...Swift_DependencyContainer::getInstance()->createDependenciesFor('mime.attachment')
+        );
 
         $this->setBody($data);
         $this->setFilename($filename);
--- contenido/classes/swiftmailer/lib/classes/Swift/SmtpTransport.php
+++ contenido/classes/swiftmailer/lib/classes/Swift/SmtpTransport.php
@@ -31,11 +31,9 @@ class Swift_SmtpTransport extends Swift_Transport_EsmtpTransport
      */
     public function __construct($host = 'localhost', $port = 25, $security = null)
     {
-        call_user_func_array(
-            array($this, 'Swift_Transport_EsmtpTransport::__construct'),
-            Swift_DependencyContainer::getInstance()
-                ->createDependenciesFor('transport.smtp')
-            );
+        parent::__construct(
+            ...Swift_DependencyContainer::getInstance()->createDependenciesFor('transport.smtp')
+        );
 
         $this->setHost($host);
         $this->setPort($port);
